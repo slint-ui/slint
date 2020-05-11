f3deea066be3b951ca673b7b01d9efc014aaaecc
Olivier Goffart <ogoffart@sixtyfps.io>
2020-05-11 18:11:25 +0200

    Make the C++ output a feature

--- sixtyfps_compiler/generator.rs
+++ sixtyfps_compiler/generator.rs
@@ -4,8 +4,11 @@ The module responsible for the code generation.
 There is one sub module for every language
 */
 
+#[cfg(feature = "cpp")]
 mod cpp;
 
 pub fn generate(component: &crate::lower::LoweredComponent) {
+    #![allow(unused_variables)]
+    #[cfg(feature = "cpp")]
     println!("{}", cpp::generate(component));
 }
