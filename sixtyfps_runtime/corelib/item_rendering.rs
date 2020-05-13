6110e4952d91fe01af1b81aa3a73de3d16218c5b
Simon Hausmann <simon.hausmann@sixtyfps.io>
2020-05-13 10:55:13 +0200

    Move item rendering related code into a separate module
    .. and keep less code in the central lib.rs.

---
+++ sixtyfps_runtime/corelib/item_rendering.rs
@@ -0,0 +1,112 @@
+use super::abi::datastructures::{Item, RenderingInfo};
+use super::graphics::{Color, Frame, GraphicsBackend, RenderingCache, RenderingPrimitivesBuilder};
+use cgmath::{Matrix4, SquareMatrix, Vector3};
+use lyon::math::{Point, Rect, Size};
+
+pub(crate) fn update_item_rendering_data<Backend: GraphicsBackend>(
+    item: &mut Item,
+    rendering_cache: &mut RenderingCache<Backend>,
+    rendering_primitives_builder: &mut Backend::RenderingPrimitivesBuilder,
+) {
+    let item_rendering_info = {
+        match item.rendering_info() {
+            Some(info) => info,
+            None => return,
+        }
+    };
+
+    println!("Caching ... {:?}", item_rendering_info);
+
+    let rendering_data = item.cached_rendering_data_mut();
+
+    match item_rendering_info {
+        RenderingInfo::Rectangle(_x, _y, width, height, color) => {
+            if width <= 0. || height <= 0. {
+                return;
+            }
+            let primitive = rendering_primitives_builder.create_rect_primitive(
+                0.,
+                0.,
+                width,
+                height,
+                Color::from_argb_encoded(color),
+            );
+
+            rendering_data.cache_index = rendering_cache.allocate_entry(primitive);
+
+            rendering_data.cache_ok = true;
+        }
+        RenderingInfo::Image(_source) => {
+            rendering_data.cache_ok = false;
+            #[cfg(not(target_arch = "wasm32"))]
+            {
+                let mut image_path = std::env::current_exe().unwrap();
+                image_path.pop(); // pop of executable name
+                image_path.push(_source);
+                let image = image::open(image_path.as_path()).unwrap().into_rgba();
+                let source_size = image.dimensions();
+
+                let source_rect = Rect::new(
+                    Point::new(0.0, 0.0),
+                    Size::new(source_size.0 as f32, source_size.1 as f32),
+                );
+                let dest_rect = Rect::new(
+                    Point::new(200.0, 200.0),
+                    Size::new(source_size.0 as f32, source_size.1 as f32),
+                );
+
+                let image_primitive = rendering_primitives_builder.create_image_primitive(
+                    source_rect,
+                    dest_rect,
+                    image,
+                );
+                rendering_data.cache_index = rendering_cache.allocate_entry(image_primitive);
+                rendering_data.cache_ok = true;
+            }
+        }
+        RenderingInfo::NoContents => {
+            rendering_data.cache_ok = false;
+        }
+    }
+}
+
+pub(crate) fn render_component_items<Backend: GraphicsBackend>(
+    component: &super::abi::datastructures::ComponentUniquePtr,
+    frame: &mut Backend::Frame,
+    rendering_cache: &RenderingCache<Backend>,
+) {
+    let transform = Matrix4::identity();
+
+    component.visit_items(
+        |item, transform| {
+            let mut transform = transform.clone();
+            let item_rendering_info = {
+                match item.rendering_info() {
+                    Some(info) => info,
+                    None => return transform,
+                }
+            };
+
+            match item_rendering_info {
+                RenderingInfo::Rectangle(x, y, ..) => {
+                    transform = transform * Matrix4::from_translation(Vector3::new(x, y, 0.0));
+                }
+                _ => {}
+            }
+
+            if item.cached_rendering_data().cache_ok {
+                println!(
+                    "Rendering... {:?} from cache {}",
+                    item_rendering_info,
+                    item.cached_rendering_data().cache_index
+                );
+
+                let primitive = rendering_cache.entry_at(item.cached_rendering_data().cache_index);
+                frame.render_primitive(&primitive, &transform);
+            }
+
+            transform
+        },
+        transform,
+    );
+}
--- sixtyfps_runtime/corelib/lib.rs
+++ sixtyfps_runtime/corelib/lib.rs
@@ -1,6 +1,3 @@
-use cgmath::{Matrix4, SquareMatrix, Vector3};
-use lyon::math::{Point, Rect, Size};
-
 pub mod graphics;
 pub mod layout;
 
@@ -10,9 +7,7 @@ pub mod abi {
     pub mod primitives;
 }
 
-use abi::datastructures::RenderingInfo;
-use graphics::Frame;
-use graphics::RenderingPrimitivesBuilder;
+mod item_rendering;
 
 pub struct MainWindow<GraphicsBackend>
 where
@@ -94,67 +89,11 @@ pub fn run_component<GraphicsBackend, GraphicsFactoryFunc>(
     // Generate cached rendering data once
     component.visit_items_mut(
         |item, _| {
-            let item_rendering_info = {
-                match item.rendering_info() {
-                    Some(info) => info,
-                    None => return,
-                }
-            };
-
-            println!("Caching ... {:?}", item_rendering_info);
-
-            let rendering_data = item.cached_rendering_data_mut();
-
-            match item_rendering_info {
-                RenderingInfo::Rectangle(_x, _y, width, height, color) => {
-                    if width <= 0. || height <= 0. {
-                        return;
-                    }
-                    let primitive = rendering_primitives_builder.create_rect_primitive(
-                        0.,
-                        0.,
-                        width,
-                        height,
-                        graphics::Color::from_argb_encoded(color),
-                    );
-
-                    rendering_data.cache_index = rendering_cache.allocate_entry(primitive);
-
-                    rendering_data.cache_ok = true;
-                }
-                RenderingInfo::Image(_source) => {
-                    rendering_data.cache_ok = false;
-                    #[cfg(not(target_arch = "wasm32"))]
-                    {
-                        let mut image_path = std::env::current_exe().unwrap();
-                        image_path.pop(); // pop of executable name
-                        image_path.push(_source);
-                        let image = image::open(image_path.as_path()).unwrap().into_rgba();
-                        let source_size = image.dimensions();
-
-                        let source_rect = Rect::new(
-                            Point::new(0.0, 0.0),
-                            Size::new(source_size.0 as f32, source_size.1 as f32),
-                        );
-                        let dest_rect = Rect::new(
-                            Point::new(200.0, 200.0),
-                            Size::new(source_size.0 as f32, source_size.1 as f32),
-                        );
-
-                        let image_primitive = rendering_primitives_builder.create_image_primitive(
-                            source_rect,
-                            dest_rect,
-                            image,
-                        );
-                        rendering_data.cache_index =
-                            rendering_cache.allocate_entry(image_primitive);
-                        rendering_data.cache_ok = true;
-                    }
-                }
-                RenderingInfo::NoContents => {
-                    rendering_data.cache_ok = false;
-                }
-            }
+            item_rendering::update_item_rendering_data(
+                item,
+                rendering_cache,
+                &mut rendering_primitives_builder,
+            );
         },
         (),
     );
@@ -162,41 +101,7 @@ pub fn run_component<GraphicsBackend, GraphicsFactoryFunc>(
     renderer.finish_primitives(rendering_primitives_builder);
 
     main_window.run_event_loop(move |frame, rendering_cache| {
-        let transform = Matrix4::identity();
-
-        component.visit_items(
-            |item, transform| {
-                let mut transform = transform.clone();
-                let item_rendering_info = {
-                    match item.rendering_info() {
-                        Some(info) => info,
-                        None => return transform,
-                    }
-                };
-
-                match item_rendering_info {
-                    RenderingInfo::Rectangle(x, y, ..) => {
-                        transform = transform * Matrix4::from_translation(Vector3::new(x, y, 0.0));
-                    }
-                    _ => {}
-                }
-
-                if item.cached_rendering_data().cache_ok {
-                    println!(
-                        "Rendering... {:?} from cache {}",
-                        item_rendering_info,
-                        item.cached_rendering_data().cache_index
-                    );
-
-                    let primitive =
-                        rendering_cache.entry_at(item.cached_rendering_data().cache_index);
-                    frame.render_primitive(&primitive, &transform);
-                }
-
-                transform
-            },
-            transform,
-        );
+        item_rendering::render_component_items(&component, frame, &rendering_cache);
     });
 }
 
