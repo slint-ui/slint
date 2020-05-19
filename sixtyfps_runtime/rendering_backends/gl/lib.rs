3c2ad7b275b22863ab3397f7553961ab1be0304c
Simon Hausmann <simon.hausmann@sixtyfps.io>
2020-05-19 20:29:05 +0200

    Add support for atlas textures in the GL renderer
    ... and place images in there by default. This improves locality.
    
    The atlasing is also needed for individual glyph rendering, which is
    coming soon.

--- sixtyfps_runtime/rendering_backends/gl/lib.rs
+++ sixtyfps_runtime/rendering_backends/gl/lib.rs
@@ -4,11 +4,14 @@ use image::Pixel;
 use lyon::path::math::Rect;
 use lyon::tessellation::geometry_builder::{BuffersBuilder, VertexBuffers};
 use lyon::tessellation::{FillAttributes, FillOptions, FillTessellator};
-use pathfinder_geometry::{transform2d::Transform2F, vector::Vector2F, vector::Vector2I};
+use pathfinder_geometry::{
+    rect::RectI, transform2d::Transform2F, vector::Vector2F, vector::Vector2I,
+};
 use sixtyfps_corelib::abi::datastructures::ComponentVTable;
 use sixtyfps_corelib::graphics::{
     Color, FillStyle, Frame as GraphicsFrame, GraphicsBackend, RenderingPrimitivesBuilder,
 };
+use std::cell::RefCell;
 use std::marker;
 use std::mem;
 
@@ -174,12 +177,18 @@ impl<IndexType> GLIndexBuffer<IndexType> {
     */
 }
 
+#[derive(Copy, Clone)]
 struct GLTexture {
     texture_id: <GLContext as HasContext>::Texture,
 }
 
 impl GLTexture {
-    fn new(gl: &glow::Context, image: image::ImageBuffer<image::Rgba<u8>, Vec<u8>>) -> Self {
+    fn new_with_size_and_data(
+        gl: &glow::Context,
+        width: i32,
+        height: i32,
+        data: Option<&[u8]>,
+    ) -> Self {
         let texture_id = unsafe { gl.create_texture().unwrap() };
 
         unsafe {
@@ -201,16 +210,48 @@ impl GLTexture {
                 glow::TEXTURE_2D,
                 0,
                 glow::RGBA as i32,
-                image.width() as i32,
-                image.height() as i32,
+                width,
+                height,
                 0,
                 glow::RGBA,
                 glow::UNSIGNED_BYTE,
-                Some(&image.into_raw()),
+                data,
             )
         }
 
         Self { texture_id }
+    }
+
+    fn new(gl: &glow::Context, image: image::ImageBuffer<image::Rgba<u8>, Vec<u8>>) -> Self {
+        GLTexture::new_with_size_and_data(
+            gl,
+            image.width() as i32,
+            image.height() as i32,
+            Some(&image.into_raw()),
+        )
+    }
+
+    fn set_sub_image(
+        &mut self,
+        gl: &glow::Context,
+        x: i32,
+        y: i32,
+        image: image::ImageBuffer<image::Rgba<u8>, Vec<u8>>,
+    ) {
+        unsafe {
+            gl.bind_texture(glow::TEXTURE_2D, Some(self.texture_id));
+            gl.tex_sub_image_2d_u8_slice(
+                glow::TEXTURE_2D,
+                0,
+                x,
+                y,
+                image.width() as i32,
+                image.height() as i32,
+                glow::RGBA,
+                glow::UNSIGNED_BYTE,
+                Some(&image.into_raw()),
+            );
+        }
     }
 
     fn bind_to_location(
@@ -235,10 +276,152 @@ impl GLTexture {
     */
 }
 
+struct AtlasTextureAllocation {
+    texture: GLTexture,
+    texture_coordinates: RectI,
+    normalized_coordinates: [Vertex; 6],
+}
+
+impl AtlasTextureAllocation {
+    fn new(
+        texture: GLTexture,
+        allocator: &guillotiere::AtlasAllocator,
+        allocation: guillotiere::Allocation,
+    ) -> Self {
+        let atlas_width = allocator.size().width as f32;
+        let atlas_height = allocator.size().height as f32;
+        let min = allocation.rectangle.min;
+        let size = allocation.rectangle.max - allocation.rectangle.min;
+        let origin = Vector2I::new(min.x, min.y);
+        let size = Vector2I::new(size.x, size.y);
+        let texture_coordinates = RectI::new(origin, size);
+
+        let tex_left = (texture_coordinates.min_x() as f32) / atlas_width;
+        let tex_top = (texture_coordinates.min_y() as f32) / atlas_height;
+        let tex_right = (texture_coordinates.max_x() as f32) / atlas_width;
+        let tex_bottom = (texture_coordinates.max_y() as f32) / atlas_height;
+
+        let tex_vertex1 = Vertex { _pos: [tex_left, tex_top] };
+        let tex_vertex2 = Vertex { _pos: [tex_right, tex_top] };
+        let tex_vertex3 = Vertex { _pos: [tex_right, tex_bottom] };
+        let tex_vertex4 = Vertex { _pos: [tex_left, tex_bottom] };
+
+        AtlasTextureAllocation {
+            texture,
+            texture_coordinates,
+            normalized_coordinates: [
+                tex_vertex1,
+                tex_vertex2,
+                tex_vertex3,
+                tex_vertex1,
+                tex_vertex3,
+                tex_vertex4,
+            ],
+        }
+    }
+}
+
+struct GLAtlasTexture {
+    texture: GLTexture,
+    allocator: guillotiere::AtlasAllocator,
+}
+
+impl GLAtlasTexture {
+    fn new(gl: &glow::Context) -> Self {
+        let allocator = guillotiere::AtlasAllocator::new(guillotiere::Size::new(2048, 2048));
+        let texture = GLTexture::new_with_size_and_data(
+            gl,
+            allocator.size().width,
+            allocator.size().height,
+            None,
+        );
+        Self { texture, allocator }
+    }
+
+    fn allocate(
+        &mut self,
+        requested_width: i32,
+        requested_height: i32,
+    ) -> Option<guillotiere::Allocation> {
+        self.allocator.allocate(guillotiere::Size::new(requested_width, requested_height))
+    }
+}
+
+struct AtlasAllocation {
+    atlas_index: usize,
+    sub_texture: AtlasTextureAllocation,
+}
+
+struct TextureAtlas {
+    atlases: Vec<GLAtlasTexture>,
+}
+
+impl TextureAtlas {
+    fn new() -> Self {
+        Self { atlases: vec![] }
+    }
+
+    fn allocate_region(
+        &mut self,
+        gl: &glow::Context,
+        requested_width: i32,
+        requested_height: i32,
+    ) -> AtlasAllocation {
+        for (i, atlas) in self.atlases.iter_mut().enumerate() {
+            if let Some(allocation) = atlas.allocate(requested_width, requested_height) {
+                return AtlasAllocation {
+                    atlas_index: i,
+                    sub_texture: AtlasTextureAllocation::new(
+                        atlas.texture,
+                        &atlas.allocator,
+                        allocation,
+                    ),
+                };
+            }
+        }
+
+        let mut new_atlas = GLAtlasTexture::new(&gl);
+        let atlas_allocation = new_atlas.allocate(requested_width, requested_height).unwrap();
+        let atlas_index = self.atlases.len();
+        let alloc = AtlasAllocation {
+            atlas_index,
+            sub_texture: AtlasTextureAllocation::new(
+                new_atlas.texture,
+                &new_atlas.allocator,
+                atlas_allocation,
+            ),
+        };
+        self.atlases.push(new_atlas);
+        alloc
+    }
+
+    fn allocate_image_in_atlas(
+        &mut self,
+        gl: &glow::Context,
+        image: image::ImageBuffer<image::Rgba<u8>, Vec<u8>>,
+    ) -> AtlasAllocation {
+        let requested_width = image.width() as i32;
+        let requested_height = image.height() as i32;
+
+        let allocation = self.allocate_region(gl, requested_width, requested_height);
+
+        let texture = &mut self.atlases[allocation.atlas_index].texture;
+        texture.set_sub_image(
+            gl,
+            allocation.sub_texture.texture_coordinates.min_x(),
+            allocation.sub_texture.texture_coordinates.min_y(),
+            image,
+        );
+
+        allocation
+    }
+}
+
 pub struct GLRenderer {
     context: Rc<glow::Context>,
     path_program: Shader,
     image_program: Shader,
+    texture_atlas: Rc<RefCell<TextureAtlas>>,
     #[cfg(target_arch = "wasm32")]
     window: winit::window::Window,
     #[cfg(not(target_arch = "wasm32"))]
@@ -248,6 +431,7 @@ pub struct GLRenderer {
 pub struct GLRenderingPrimitivesBuilder {
     context: Rc<glow::Context>,
     fill_tesselator: FillTessellator,
+    texture_atlas: Rc<RefCell<TextureAtlas>>,
 
     #[cfg(not(target_arch = "wasm32"))]
     windowed_context: glutin::WindowedContext<glutin::PossiblyCurrent>,
@@ -358,6 +542,7 @@ impl GLRenderer {
             context: Rc::new(context),
             path_program,
             image_program,
+            texture_atlas: Rc::new(RefCell::new(TextureAtlas::new())),
             #[cfg(target_arch = "wasm32")]
             window,
             #[cfg(not(target_arch = "wasm32"))]
@@ -380,6 +565,7 @@ impl GraphicsBackend for GLRenderer {
         GLRenderingPrimitivesBuilder {
             context: self.context.clone(),
             fill_tesselator: FillTessellator::new(),
+            texture_atlas: self.texture_atlas.clone(),
 
             #[cfg(not(target_arch = "wasm32"))]
             windowed_context: current_windowed_context,
@@ -476,35 +662,26 @@ impl RenderingPrimitivesBuilder for GLRenderingPrimitivesBuilder {
         image: image::ImageBuffer<image::Rgba<u8>, Vec<u8>>,
     ) -> Self::RenderingPrimitive {
         let rect = dest_rect.into();
-        let src_left = 0.;
-        let src_top = 0.;
-        let src_right = 1.;
-        let src_bottom = 1.;
 
         let vertex1 = Vertex { _pos: [rect.min_x(), rect.min_y()] };
-        let tex_vertex1 = Vertex { _pos: [src_left, src_top] };
         let vertex2 = Vertex { _pos: [rect.max_x(), rect.min_y()] };
-        let tex_vertex2 = Vertex { _pos: [src_right, src_top] };
         let vertex3 = Vertex { _pos: [rect.max_x(), rect.max_y()] };
-        let tex_vertex3 = Vertex { _pos: [src_right, src_bottom] };
         let vertex4 = Vertex { _pos: [rect.min_x(), rect.max_y()] };
-        let tex_vertex4 = Vertex { _pos: [src_left, src_bottom] };
+
+        let mut atlas = self.texture_atlas.borrow_mut();
+        let atlas_allocation = atlas.allocate_image_in_atlas(&self.context, image);
 
         let vertices = GLArrayBuffer::new(
             &self.context,
             &vec![vertex1, vertex2, vertex3, vertex1, vertex3, vertex4],
         );
-        let texture_vertices = GLArrayBuffer::new(
-            &self.context,
-            &vec![tex_vertex1, tex_vertex2, tex_vertex3, tex_vertex1, tex_vertex3, tex_vertex4],
-        );
-
-        let texture = GLTexture::new(&self.context, image);
+        let texture_vertices =
+            GLArrayBuffer::new(&self.context, &atlas_allocation.sub_texture.normalized_coordinates);
 
         OpaqueRenderingPrimitive(GLRenderingPrimitive::Texture {
             vertices,
             texture_vertices,
-            texture,
+            texture: atlas_allocation.sub_texture.texture,
         })
     }
 
