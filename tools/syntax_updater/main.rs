3965d9048fe618c8d10777077131c24e92088ade
Olivier Goffart <ogoffart@sixtyfps.io>
2020-05-25 11:49:52 +0200

    syntax_updater:  also update the .rs files containing sixtyfps!

--- tools/syntax_updater/main.rs
+++ tools/syntax_updater/main.rs
@@ -20,29 +20,86 @@ struct Cli {
 }
 
 fn main() -> std::io::Result<()> {
-    use sixtyfps_compiler::*;
     let args = Cli::from_args();
 
     for path in args.paths {
         let source = std::fs::read_to_string(&path)?;
-        let (syntax_node, mut diag) = parser::parse(&source);
-        if diag.has_error() {
-            diag.current_path = path;
-            diag.print(source);
-            continue;
-        }
 
         if args.inline {
-            print_document(syntax_node, std::fs::File::create(path)?)?
+            let file = std::fs::File::create(&path)?;
+            process_file(source, path, file)?
         } else {
-            print_document(syntax_node, std::io::stdout())?
+            process_file(source, path, std::io::stdout())?
         }
     }
     Ok(())
 }
 
-fn print_document(node: SyntaxNode, mut file: impl Write) -> std::io::Result<()> {
-    visit_node(node, &mut file)
+fn process_file(
+    source: String,
+    path: std::path::PathBuf,
+    mut file: impl Write,
+) -> std::io::Result<()> {
+    if path.extension().map(|x| x == "rs") == Some(true) {
+        let mut source_slice = &source[..];
+        let sixtyfps_macro = format!("{}!", "sixtyfps"); // in a variable so it does not appear as is
+        'l: while let Some(idx) = source_slice.find(&sixtyfps_macro) {
+            // Note: this code ignore string literal and unbalanced comment, but that should be good enough
+            let idx2 = if let Some(idx2) =
+                source_slice[idx..].find(|c| c == '{' || c == '(' || c == '[')
+            {
+                idx2
+            } else {
+                break 'l;
+            };
+            let open = source_slice.as_bytes()[idx + idx2].into();
+            let close = match open {
+                '{' => '}',
+                '(' => ')',
+                '[' => ']',
+                _ => panic!(),
+            };
+            file.write_all(source_slice[..=idx + idx2].as_bytes())?;
+            source_slice = &source_slice[idx + idx2 + 1..];
+            let mut idx = 0;
+            let mut count = 1;
+            while count > 0 {
+                if let Some(idx2) = source_slice[idx..].find(|c| {
+                    dbg!((c, idx, idx2, count));
+                    if c == open {
+                        count += 1;
+                        true
+                    } else if c == close {
+                        count -= 1;
+                        true
+                    } else {
+                        false
+                    }
+                }) {
+                    idx += idx2 + 1;
+                } else {
+                    break 'l;
+                }
+            }
+            let code = &source_slice[..idx - 1];
+            source_slice = &source_slice[idx - 1..];
+
+            let (syntax_node, diag) = sixtyfps_compiler::parser::parse(&code);
+            if diag.has_error() {
+                diag.print(code.into());
+            }
+            visit_node(syntax_node, &mut file)?
+        }
+        return file.write_all(source_slice.as_bytes());
+    }
+
+    let (syntax_node, mut diag) = sixtyfps_compiler::parser::parse(&source);
+    if diag.has_error() {
+        diag.current_path = path;
+        diag.print(source);
+    }
+
+    visit_node(syntax_node, &mut file)
 }
 
 fn visit_node(node: SyntaxNode, file: &mut impl Write) -> std::io::Result<()> {
