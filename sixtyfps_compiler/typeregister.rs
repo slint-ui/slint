d4387b74b9089eff4937a05851ce084090481ebb
Simon Hausmann <simon.hausmann@sixtyfps.io>
2020-05-25 13:52:31 +0200

    Rename Type::Number to Type::Float32
    Properties of that type are implemented as f32.

--- sixtyfps_compiler/typeregister.rs
+++ sixtyfps_compiler/typeregister.rs
@@ -11,7 +11,7 @@ pub enum Type {
     Signal,
 
     // other property type:
-    Number,
+    Float32,
     String,
     Color,
     Image,
@@ -25,7 +25,7 @@ impl Type {
 
     /// valid type for properties
     pub fn is_property_type(&self) -> bool {
-        matches!(self, Self::Number | Self::String | Self::Color | Self::Image | Self::Bool)
+        matches!(self, Self::Float32 | Self::String | Self::Color | Self::Image | Self::Bool)
     }
 
     pub fn lookup_property(&self, name: &str) -> Type {
@@ -67,34 +67,34 @@ impl TypeRegister {
 
         let mut rectangle = BuiltinElement::default();
         rectangle.properties.insert("color".to_owned(), Type::Color);
-        rectangle.properties.insert("x".to_owned(), Type::Number);
-        rectangle.properties.insert("y".to_owned(), Type::Number);
-        rectangle.properties.insert("width".to_owned(), Type::Number);
-        rectangle.properties.insert("height".to_owned(), Type::Number);
+        rectangle.properties.insert("x".to_owned(), Type::Float32);
+        rectangle.properties.insert("y".to_owned(), Type::Float32);
+        rectangle.properties.insert("width".to_owned(), Type::Float32);
+        rectangle.properties.insert("height".to_owned(), Type::Float32);
         r.types.insert("Rectangle".to_owned(), Type::Builtin(Rc::new(rectangle)));
 
         let mut image = BuiltinElement::default();
         image.properties.insert("source".to_owned(), Type::Image);
-        image.properties.insert("x".to_owned(), Type::Number);
-        image.properties.insert("y".to_owned(), Type::Number);
-        image.properties.insert("width".to_owned(), Type::Number);
-        image.properties.insert("height".to_owned(), Type::Number);
+        image.properties.insert("x".to_owned(), Type::Float32);
+        image.properties.insert("y".to_owned(), Type::Float32);
+        image.properties.insert("width".to_owned(), Type::Float32);
+        image.properties.insert("height".to_owned(), Type::Float32);
         r.types.insert("Image".to_owned(), Type::Builtin(Rc::new(image)));
 
         let mut text = BuiltinElement::default();
         text.properties.insert("text".to_owned(), Type::String);
         text.properties.insert("font_family".to_owned(), Type::String);
-        text.properties.insert("font_pixel_size".to_owned(), Type::Number);
+        text.properties.insert("font_pixel_size".to_owned(), Type::Float32);
         text.properties.insert("color".to_owned(), Type::Color);
-        text.properties.insert("x".to_owned(), Type::Number);
-        text.properties.insert("y".to_owned(), Type::Number);
+        text.properties.insert("x".to_owned(), Type::Float32);
+        text.properties.insert("y".to_owned(), Type::Float32);
         r.types.insert("Text".to_owned(), Type::Builtin(Rc::new(text)));
 
         let mut touch_area = BuiltinElement::default();
-        touch_area.properties.insert("x".to_owned(), Type::Number);
-        touch_area.properties.insert("y".to_owned(), Type::Number);
-        touch_area.properties.insert("width".to_owned(), Type::Number);
-        touch_area.properties.insert("height".to_owned(), Type::Number);
+        touch_area.properties.insert("x".to_owned(), Type::Float32);
+        touch_area.properties.insert("y".to_owned(), Type::Float32);
+        touch_area.properties.insert("width".to_owned(), Type::Float32);
+        touch_area.properties.insert("height".to_owned(), Type::Float32);
         touch_area.properties.insert("pressed".to_owned(), Type::Bool);
         touch_area.properties.insert("clicked".to_owned(), Type::Signal);
         r.types.insert("TouchArea".to_owned(), Type::Builtin(Rc::new(touch_area)));
