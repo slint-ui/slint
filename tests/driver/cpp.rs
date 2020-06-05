3d1064f2e8200aeb3cb564fc287d61767c1a5301
Simon Hausmann <simon.hausmann@sixtyfps.io>
2020-06-05 09:00:49 +0200

    Separate the C++ bits into a separate module

---
+++ tests/driver/cpp.rs
@@ -0,0 +1,20 @@
+use std::error::Error;
+
+pub fn test(testcase: &super::TestCase) -> Result<(), Box<dyn Error>> {
+    use sixtyfps_compiler::*;
+
+    let (syntax_node, mut diag) = parser::parse(&testcase.source);
+    diag.current_path = testcase.path.clone();
+    let mut tr = typeregister::TypeRegister::builtin();
+    let doc = object_tree::Document::from_node(syntax_node, &mut diag, &mut tr);
+    run_passes(&doc, &mut diag, &mut tr);
+
+    let (mut diag, source) = diag.check_and_exit_on_error(testcase.source.clone());
+
+    let mut generated_cpp: Vec<u8> = Vec::new();
+
+    generator::generate(&mut generated_cpp, &doc.root_component, &mut diag)?;
+    diag.check_and_exit_on_error(source);
+
+    Ok(())
+}
--- tests/driver/main.rs
+++ tests/driver/main.rs
@@ -22,30 +22,11 @@ pub fn collect_test_cases() -> std::io::Result<Vec<TestCase>> {
     Ok(results)
 }
 
-impl TestCase {
-    fn cpp(&self) -> Result<(), Box<dyn Error>> {
-        use sixtyfps_compiler::*;
-
-        let (syntax_node, mut diag) = parser::parse(&self.source);
-        diag.current_path = self.path.clone();
-        let mut tr = typeregister::TypeRegister::builtin();
-        let doc = object_tree::Document::from_node(syntax_node, &mut diag, &mut tr);
-        run_passes(&doc, &mut diag, &mut tr);
-
-        let (mut diag, source) = diag.check_and_exit_on_error(self.source.clone());
-
-        let mut generated_cpp: Vec<u8> = Vec::new();
-
-        generator::generate(&mut generated_cpp, &doc.root_component, &mut diag)?;
-        diag.check_and_exit_on_error(source);
-
-        Ok(())
-    }
-}
+mod cpp;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    for test in collect_test_cases()? {
-        test.cpp()?;
+    for testcase in collect_test_cases()? {
+        cpp::test(&testcase)?;
     }
 
     Ok(())
