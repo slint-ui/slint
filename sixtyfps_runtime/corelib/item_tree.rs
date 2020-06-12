7c5ce4fea5a02777ff465812a829b88a1f2c0654
Olivier Goffart <ogoffart@sixtyfps.io>
2020-06-12 21:04:14 +0200

    Add missing file

---
+++ sixtyfps_runtime/corelib/item_tree.rs
@@ -0,0 +1,56 @@
+use crate::abi::datastructures::{
+    ComponentRef, ItemRef, ItemTreeNode, ItemVisitor, ItemVisitorVTable,
+};
+
+/// Visit each items recursively
+///
+/// The state parametter returned by the visitor is passed to each children.
+pub fn visit_items<State>(
+    component: ComponentRef,
+    mut visitor: impl FnMut(ItemRef<'_>, &State) -> State,
+    state: State,
+) {
+    visit_internal(component, &mut visitor, -1, &state)
+}
+
+fn visit_internal<State>(
+    component: ComponentRef,
+    visitor: &mut impl FnMut(ItemRef<'_>, &State) -> State,
+    index: isize,
+    state: &State,
+) {
+    let mut actual_visitor = |component: ComponentRef, index: isize, item: ItemRef| {
+        let s = visitor(item, state);
+        visit_internal(component, visitor, index, &s);
+    };
+    vtable::new_vref!(let mut actual_visitor : VRefMut<ItemVisitorVTable> for ItemVisitor = &mut actual_visitor);
+    component.visit_children_item(index, actual_visitor);
+}
+
+/// Visit the children within an array of ItemTreeNode
+pub fn visit_item_tree<Base>(
+    base: &Base,
+    component: ComponentRef,
+    item_tree: &[ItemTreeNode<Base>],
+    index: isize,
+    mut visitor: vtable::VRefMut<ItemVisitorVTable>,
+) {
+    let mut visit_at_index = |idx: usize| match &item_tree[idx] {
+        ItemTreeNode::Item { item, .. } => {
+            visitor.visit_item(component, idx as isize, item.apply(base));
+        }
+        _ => todo!(),
+    };
+    if index == -1 {
+        visit_at_index(0);
+    } else {
+        match &item_tree[index as usize] {
+            ItemTreeNode::Item { children_index, chilren_count, .. } => {
+                for c in *children_index..(*children_index + *chilren_count) {
+                    visit_at_index(c as usize);
+                }
+            }
+            ItemTreeNode::DynamicTree { .. } => todo!(),
+        };
+    };
+}
