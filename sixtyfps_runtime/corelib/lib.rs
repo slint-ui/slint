d3c6ae5c81585a20620d1f89f2967c91da5773c4
Simon Hausmann <simon.hausmann@sixtyfps.io>
2020-06-16 11:06:45 +0200

    Prepare for cleaner separation between window and event loop, part 3
    Make run_event_loop a standalone function that takes a mainwindow
    parameter. This is in preparation for running with multiple windows.

--- sixtyfps_runtime/corelib/lib.rs
+++ sixtyfps_runtime/corelib/lib.rs
@@ -70,101 +70,100 @@ impl<GraphicsBackend: graphics::GraphicsBackend> MainWindow<GraphicsBackend> {
 
         Self { graphics_backend, rendering_cache: graphics::RenderingCache::default() }
     }
-
-    #[allow(unused_mut)] // mut need changes for wasm
-    pub fn run_event_loop(
-        mut self,
-        mut event_loop: winit::event_loop::EventLoop<()>,
-        component: vtable::VRef<crate::abi::datastructures::ComponentVTable>,
-    ) where
-        GraphicsBackend: 'static,
-    {
-        use winit::event::Event;
-        use winit::event_loop::{ControlFlow, EventLoopWindowTarget};
-
-        let mut cursor_pos = winit::dpi::PhysicalPosition::new(0., 0.);
-        let mut run_fn = move |event: Event<()>,
-                               _: &EventLoopWindowTarget<()>,
-                               control_flow: &mut ControlFlow| {
-            *control_flow = ControlFlow::Wait;
-
-            match event {
-                winit::event::Event::WindowEvent {
-                    event: winit::event::WindowEvent::CloseRequested,
-                    ..
-                } => *control_flow = winit::event_loop::ControlFlow::Exit,
-                winit::event::Event::RedrawRequested(_) => {
-                    // FIXME: we should do that only if some property change
-                    component.compute_layout();
-
-                    {
-                        let mut rendering_primitives_builder =
-                            self.graphics_backend.new_rendering_primitives_builder();
-
-                        prepare_rendering(
-                            component,
-                            &mut rendering_primitives_builder,
-                            &mut self.rendering_cache,
-                        );
-
-                        self.graphics_backend.finish_primitives(rendering_primitives_builder);
-                    }
-
-                    let window = self.graphics_backend.window();
-
-                    let size = window.inner_size();
-                    let context = EvaluationContext { component: component };
-                    let mut frame =
-                        self.graphics_backend.new_frame(size.width, size.height, &Color::WHITE);
-                    item_rendering::render_component_items(
-                        component,
-                        &context,
-                        &mut frame,
-                        &mut self.rendering_cache,
-                    );
-                    self.graphics_backend.present_frame(frame);
-                }
-                winit::event::Event::WindowEvent {
-                    event: winit::event::WindowEvent::CursorMoved { position, .. },
-                    ..
-                } => {
-                    cursor_pos = position;
-                    // TODO: propagate mouse move?
-                }
-
-                winit::event::Event::WindowEvent {
-                    event: winit::event::WindowEvent::MouseInput { state, .. },
-                    ..
-                } => {
-                    let context = EvaluationContext { component };
-                    process_input(component, &context, cursor_pos, state);
-                    let window = self.graphics_backend.window();
-                    // FIXME: remove this, it should be based on actual changes rather than this
-                    window.request_redraw();
-                }
-
-                _ => (),
-            }
-        };
-
-        #[cfg(not(target_arch = "wasm32"))]
-        event_loop.run_return(run_fn);
-        #[cfg(target_arch = "wasm32")]
-        {
-            // Since wasm does not have a run_return function that takes a non-static closure,
-            // we use this hack to work that around
-            scoped_tls_hkt::scoped_thread_local!(static mut RUN_FN_TLS: for <'a> &'a mut dyn FnMut(
-                Event<'_, ()>,
-                &EventLoopWindowTarget<()>,
-                &mut ControlFlow,
-            ));
-            RUN_FN_TLS.set(&mut run_fn, move || {
-                event_loop.run(|e, t, cf| RUN_FN_TLS.with(|mut run_fn| run_fn(e, t, cf)))
-            });
-        }
-    }
 }
 
+#[allow(unused_mut)] // mut need changes for wasm
+pub fn run_event_loop<GraphicsBackend: graphics::GraphicsBackend>(
+    mut main_window: &mut MainWindow<GraphicsBackend>,
+    mut event_loop: winit::event_loop::EventLoop<()>,
+    component: vtable::VRef<crate::abi::datastructures::ComponentVTable>,
+) where
+    GraphicsBackend: 'static,
+{
+    use winit::event::Event;
+    use winit::event_loop::{ControlFlow, EventLoopWindowTarget};
+
+    let mut cursor_pos = winit::dpi::PhysicalPosition::new(0., 0.);
+    let mut run_fn = move |event: Event<()>,
+                           _: &EventLoopWindowTarget<()>,
+                           control_flow: &mut ControlFlow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            winit::event::Event::WindowEvent {
+                event: winit::event::WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = winit::event_loop::ControlFlow::Exit,
+            winit::event::Event::RedrawRequested(_) => {
+                // FIXME: we should do that only if some property change
+                component.compute_layout();
+
+                {
+                    let mut rendering_primitives_builder =
+                        main_window.graphics_backend.new_rendering_primitives_builder();
+
+                    prepare_rendering(
+                        component,
+                        &mut rendering_primitives_builder,
+                        &mut main_window.rendering_cache,
+                    );
+
+                    main_window.graphics_backend.finish_primitives(rendering_primitives_builder);
+                }
+
+                let window = main_window.graphics_backend.window();
+
+                let size = window.inner_size();
+                let context = EvaluationContext { component: component };
+                let mut frame =
+                    main_window.graphics_backend.new_frame(size.width, size.height, &Color::WHITE);
+                item_rendering::render_component_items(
+                    component,
+                    &context,
+                    &mut frame,
+                    &mut main_window.rendering_cache,
+                );
+                main_window.graphics_backend.present_frame(frame);
+            }
+            winit::event::Event::WindowEvent {
+                event: winit::event::WindowEvent::CursorMoved { position, .. },
+                ..
+            } => {
+                cursor_pos = position;
+                // TODO: propagate mouse move?
+            }
+
+            winit::event::Event::WindowEvent {
+                event: winit::event::WindowEvent::MouseInput { state, .. },
+                ..
+            } => {
+                let context = EvaluationContext { component };
+                process_input(component, &context, cursor_pos, state);
+                let window = main_window.graphics_backend.window();
+                // FIXME: remove this, it should be based on actual changes rather than this
+                window.request_redraw();
+            }
+
+            _ => (),
+        }
+    };
+
+    #[cfg(not(target_arch = "wasm32"))]
+    event_loop.run_return(run_fn);
+    #[cfg(target_arch = "wasm32")]
+    {
+        // Since wasm does not have a run_return function that takes a non-static closure,
+        // we use this hack to work that around
+        scoped_tls_hkt::scoped_thread_local!(static mut RUN_FN_TLS: for <'a> &'a mut dyn FnMut(
+            Event<'_, ()>,
+            &EventLoopWindowTarget<()>,
+            &mut ControlFlow,
+        ));
+        RUN_FN_TLS.set(&mut run_fn, move || {
+            event_loop.run(|e, t, cf| RUN_FN_TLS.with(|mut run_fn| run_fn(e, t, cf)))
+        });
+    }
+}
 pub fn run_component<GraphicsBackend: graphics::GraphicsBackend + 'static>(
     component: vtable::VRef<crate::abi::datastructures::ComponentVTable>,
     graphics_backend_factory: impl FnOnce(
@@ -173,9 +172,9 @@ pub fn run_component<GraphicsBackend: graphics::GraphicsBackend + 'static>(
     ) -> GraphicsBackend,
 ) {
     let event_loop = winit::event_loop::EventLoop::new();
-    let main_window = MainWindow::new(&event_loop, graphics_backend_factory);
+    let mut main_window = MainWindow::new(&event_loop, graphics_backend_factory);
 
-    main_window.run_event_loop(event_loop, component);
+    run_event_loop(&mut main_window, event_loop, component);
 }
 
 fn prepare_rendering<GraphicsBackend: graphics::GraphicsBackend>(
