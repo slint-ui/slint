2c1e6bcb2038589a1d11268c059818122aa4fdd7
Simon Hausmann <simon.hausmann@sixtyfps.io>
2020-07-24 07:46:17 +0200

    Add boilerplate for gallery rust build

---
+++ examples/gallery/build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    sixtyfps_build::compile("gallery.60").unwrap();
+}
---
+++ examples/gallery/main.rs
@@ -0,0 +1,8 @@
+#![deny(unsafe_code)]
+
+sixtyfps::include_modules!();
+
+fn main() {
+    let app = App::new();
+    app.run();
+}
