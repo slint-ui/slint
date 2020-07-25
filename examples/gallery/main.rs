a4938c4d8e6b6ff0dff304ce5800758adc713a2e
Simon Hausmann <simon.hausmann@sixtyfps.io>
2020-07-25 17:34:59 +0200

    Add wasm boilerplate

--- examples/gallery/main.rs
+++ examples/gallery/main.rs
@@ -1,8 +1,17 @@
 #![deny(unsafe_code)]
 
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
 sixtyfps::include_modules!();
 
-fn main() {
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub fn main() {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(all(debug_assertions, target_arch = "wasm32"))]
+    console_error_panic_hook::set_once();
+
     let app = App::new();
     app.run();
 }
