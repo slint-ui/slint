05d1edbaefdacd7b520dc04b81313e7f4ccb49e2
Simon Hausmann <simon.hausmann@sixtyfps.io>
2020-08-07 16:22:58 +0200

    Fix caching of fonts by family
    Avoid querying coretext/font-config for every lookup

--- sixtyfps_runtime/corelib/font/mod.rs
+++ sixtyfps_runtime/corelib/font/mod.rs
@@ -12,31 +12,33 @@ mod canvasfont;
 #[cfg(target_arch = "wasm32")]
 pub use canvasfont::*;
 
-#[derive(Default)]
 struct FontMatch {
+    handle: FontHandle,
     fonts_per_pixel_size: Vec<Rc<Font>>,
 }
 
 #[derive(Default)]
 pub struct FontCache {
-    loaded_fonts: RefCell<HashMap<FontHandle, FontMatch>>,
+    // index by family name
+    loaded_fonts: RefCell<HashMap<String, FontMatch>>,
 }
 
 impl FontCache {
     pub fn find_font(&self, family: &str, font_pixel_size: f32) -> Rc<Font> {
         let pixel_size = if font_pixel_size != 0. { font_pixel_size } else { 48.0 * 72. / 96. };
 
-        let handle = FontHandle::new_from_match(family);
-
         let mut loaded_fonts = self.loaded_fonts.borrow_mut();
-        let font_match = loaded_fonts.entry(handle.clone()).or_insert(FontMatch::default());
+        let font_match = loaded_fonts.entry(family.to_owned()).or_insert_with(|| FontMatch {
+            handle: FontHandle::new_from_match(family),
+            fonts_per_pixel_size: Vec::new(),
+        });
 
         font_match
             .fonts_per_pixel_size
             .iter()
             .find_map(|font| if font.pixel_size == pixel_size { Some(font.clone()) } else { None })
             .unwrap_or_else(|| {
-                let fnt = Rc::new(handle.load(pixel_size).unwrap());
+                let fnt = Rc::new(font_match.handle.load(pixel_size).unwrap());
                 font_match.fonts_per_pixel_size.push(fnt.clone());
                 fnt
             })
