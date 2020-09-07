e9b63552a7944f9a4cf59538b4d21e07fd673d59
Olivier Goffart <ogoffart@sixtyfps.io>
2020-09-07 14:13:34 +0200

    Skeleton for a TODO app

---
+++ examples/todo/rust/main.rs
@@ -0,0 +1,24 @@
+/* LICENSE BEGIN
+    This file is part of the SixtyFPS Project -- https://sixtyfps.io
+    Copyright (c) 2020 Olivier Goffart <olivier.goffart@sixtyfps.io>
+    Copyright (c) 2020 Simon Hausmann <simon.hausmann@sixtyfps.io>
+
+    SPDX-License-Identifier: GPL-3.0-only
+    This file is also available under commercial licensing terms.
+    Please contact info@sixtyfps.io for more information.
+LICENSE END */
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+sixtyfps::include_modules!();
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub fn main() {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(all(debug_assertions, target_arch = "wasm32"))]
+    console_error_panic_hook::set_once();
+
+    let main_window = MainWindow::new();
+    main_window.run();
+}
