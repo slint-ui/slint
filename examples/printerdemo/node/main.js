b6655d0049bdecfc9b3d741bdd16cceb540646b3
Olivier Goffart <ogoffart@sixtyfps.io>
2020-10-21 12:44:12 +0200

    Add signal handler in the node printer demo

--- examples/printerdemo/node/main.js
+++ examples/printerdemo/node/main.js
@@ -14,10 +14,20 @@ require("sixtyfps");
 // import * as demo from "../ui/printerdemo.60";
 let demo = require("../ui/printerdemo.60");
 let window = new demo.MainWindow();
+
 window.ink_levels = [
     { color: "#00ffff", level: 0.30 },
     { color: "#ff00ff", level: 0.80 },
     { color: "#ffff00", level: 0.60 },
     { color: "#000000", level: 0.90 }];
+
+window.fax_number_erase.setHandler(function () {
+    window.fax_number = window.fax_number.substring(0, window.fax_number.length - 1);
+})
+window.fax_send.setHandler(function () {
+    console.log("Send fax to " + window.fax_number);
+    window.fax_number = "";
+})
+
 window.show();
 
