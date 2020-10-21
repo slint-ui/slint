57ce54f2539c8db272b15acbebbaa0be775359df
Simon Hausmann <simon.hausmann@sixtyfps.io>
2020-10-21 18:11:14 +0200

    Implement tracking of todo items left in Rust

--- examples/todo/rust/main.rs
+++ examples/todo/rust/main.rs
@@ -8,6 +8,8 @@
     Please contact info@sixtyfps.io for more information.
 LICENSE END */
 
+use core::pin;
+use pin::Pin;
 use sixtyfps::Model;
 use std::rc::Rc;
 
@@ -16,6 +18,70 @@ use wasm_bindgen::prelude::*;
 
 sixtyfps::include_modules!();
 
+struct TodoModel {
+    backing_model: sixtyfps::VecModel<TodoItem>,
+    main_window: sixtyfps::re_exports::PinWeak<MainWindow>,
+}
+
+impl sixtyfps::Model for TodoModel {
+    type Data = TodoItem;
+
+    fn row_count(&self) -> usize {
+        self.backing_model.row_count()
+    }
+
+    fn row_data(&self, row: usize) -> Self::Data {
+        self.backing_model.row_data(row)
+    }
+
+    fn set_row_data(&self, row: usize, data: Self::Data) {
+        let previous_checked = self.backing_model.row_data(row).checked;
+        let new_checked = data.checked;
+        match (previous_checked, new_checked) {
+            (true, false) => self.adjust_todo_count(1),
+            (false, true) => self.adjust_todo_count(-1),
+            _ => {}
+        };
+
+        self.backing_model.set_row_data(row, data)
+    }
+
+    fn attach_peer(&self, peer: sixtyfps::ModelPeer) {
+        self.backing_model.attach_peer(peer)
+    }
+}
+
+impl TodoModel {
+    fn new(data: Vec<TodoItem>, main_window: &Pin<Rc<MainWindow>>) -> Self {
+        let todo_left_count =
+            data.iter().fold(0, |count, item| if item.checked { count } else { count + 1 });
+        main_window.as_ref().set_todo_left(todo_left_count);
+        Self {
+            backing_model: data.into(),
+            main_window: sixtyfps::re_exports::PinWeak::downgrade(main_window.clone()),
+        }
+    }
+
+    pub fn push(&self, value: TodoItem) {
+        if !value.checked {
+            self.adjust_todo_count(1);
+        }
+        self.backing_model.push(value)
+    }
+
+    pub fn remove(&self, index: usize) {
+        if !self.backing_model.row_data(index).checked {
+            self.adjust_todo_count(-1)
+        }
+        self.backing_model.remove(index)
+    }
+
+    fn adjust_todo_count(&self, adjustment: i32) {
+        let main_window = self.main_window.upgrade().unwrap();
+        main_window.as_ref().set_todo_left(main_window.as_ref().get_todo_left() + adjustment);
+    }
+}
+
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn main() {
     // This provides better error messages in debug mode.
@@ -23,18 +89,22 @@ pub fn main() {
     #[cfg(all(debug_assertions, target_arch = "wasm32"))]
     console_error_panic_hook::set_once();
 
-    let todo_model = Rc::new(sixtyfps::VecModel::<TodoItem>::from(vec![
-        TodoItem { checked: true, title: "Implement the .60 file".into() },
-        TodoItem { checked: true, title: "Do the Rust part".into() },
-        TodoItem { checked: false, title: "Make the C++ code".into() },
-        TodoItem { checked: false, title: "Write some JavaScript code".into() },
-        TodoItem { checked: false, title: "Test the application".into() },
-        TodoItem { checked: false, title: "Ship to customer".into() },
-        TodoItem { checked: false, title: "???".into() },
-        TodoItem { checked: false, title: "Profit".into() },
-    ]));
-
     let main_window = MainWindow::new();
+
+    let todo_model = Rc::new(TodoModel::new(
+        vec![
+            TodoItem { checked: true, title: "Implement the .60 file".into() },
+            TodoItem { checked: true, title: "Do the Rust part".into() },
+            TodoItem { checked: false, title: "Make the C++ code".into() },
+            TodoItem { checked: false, title: "Write some JavaScript code".into() },
+            TodoItem { checked: false, title: "Test the application".into() },
+            TodoItem { checked: false, title: "Ship to customer".into() },
+            TodoItem { checked: false, title: "???".into() },
+            TodoItem { checked: false, title: "Profit".into() },
+        ],
+        &main_window,
+    ));
+
     main_window.as_ref().on_todo_added({
         let todo_model = todo_model.clone();
         move |text| todo_model.push(TodoItem { checked: false, title: text })
