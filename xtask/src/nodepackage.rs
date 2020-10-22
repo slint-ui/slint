2f2e76cfd5075b60d2e0058848aaf5d10837ac36
Olivier Goffart <ogoffart@sixtyfps.io>
2020-10-22 11:54:59 +0200

    Missing license header

--- xtask/src/nodepackage.rs
+++ xtask/src/nodepackage.rs
@@ -1,3 +1,12 @@
+/* LICENSE BEGIN
+    This file is part of the SixtyFPS Project -- https://sixtyfps.io
+    Copyright (c) 2020 Olivier Goffart <olivier.goffart@sixtyfps.io>
+    Copyright (c) 2020 Simon Hausmann <simon.hausmann@sixtyfps.io>
+
+    SPDX-License-Identifier: GPL-3.0-only
+    This file is also available under commercial licensing terms.
+    Please contact info@sixtyfps.io for more information.
+LICENSE END */
 use anyhow::Context;
 use xshell::{cmd, pushd, read_file, write_file};
 
