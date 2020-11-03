47c99df77570105debaf54a729d10d85a3a42a46
Simon Hausmann <simon.hausmann@sixtyfps.io>
2020-11-03 09:51:43 +0100

    Rework the tab model in the online editor
    Store monaco editor models instead of the source code.

--- tools/online_editor/index.ts
+++ tools/online_editor/index.ts
@@ -22,21 +22,8 @@ var editor = monaco.editor.create(document.getElementById("editor"), {
 });
 var base_url = "";
 
-enum DocumentReferenceKind {
-    DocumentInline,
-    DocumentInActiveEditor
-}
-
-interface InlineDocument {
-    kind: DocumentReferenceKind.DocumentInline;
-    source: string
-}
-
-interface CurrentDocument {
-    kind: DocumentReferenceKind.DocumentInActiveEditor
-}
-
-var documents: Map<string, InlineDocument | CurrentDocument> = new Map;
+/// Index by url. Inline documents will use the empty string.
+var editor_documents: Map<string, monaco.editor.ITextModel> = new Map;
 
 let hello_world = `
 import { SpinBox, Button, CheckBox, Slider, GroupBox } from "sixtyfps_widgets.60";
@@ -53,15 +40,15 @@ export Demo := Window {
 }
 `
 
-function load_from_url(url) {
-    initTabs(fileNameFromURL(new URL(url)));
+function load_from_url(url: string) {
+    clearTabs();
     fetch(url).then(
         x => x.text().then(y => {
             base_url = url;
-            editor.getModel().setValue(y)
+            let model = createMainModel(y, url);
+            addTab(model, url);
         })
     );
-
 }
 
 let select = (<HTMLInputElement>document.getElementById("select_combo"));
@@ -69,43 +56,103 @@ function select_combo_changed() {
     if (select.value) {
         load_from_url("https://raw.githubusercontent.com/sixtyfpsui/sixtyfps/master/" + select.value);
     } else {
-        initTabs()
+        clearTabs();
         base_url = "";
-        editor.getModel().setValue(hello_world)
+        let model = createMainModel(hello_world, "");
+        addTab(model);
     }
 }
 select.onchange = select_combo_changed;
 
 let compile_button = (<HTMLButtonElement>document.getElementById("compile_button"));
 compile_button.onclick = function () {
-    update();
+    update_preview();
 };
 
 let auto_compile = (<HTMLInputElement>document.getElementById("auto_compile"));
 auto_compile.onchange = function () {
     if (auto_compile.checked) {
-        update()
+        update_preview()
     }
 };
 
-function fileNameFromURL(url: URL): string {
-    let path = url.pathname;
-    return path.substring(path.lastIndexOf('/') + 1);
+function tabTitleFromURL(url: string): string {
+    if (url === "") {
+        return "unnamed.60";
+    }
+    try {
+        let parsed_url = new URL(url);
+        let path = parsed_url.pathname;
+        return path.substring(path.lastIndexOf('/') + 1);
+    } catch (e) {
+        return url;
+    }
 }
 
-function initTabs(main_source_filename: string = "unnamed.60") {
+function createMainModel(source: string, url: string): monaco.editor.ITextModel {
+    let model = monaco.editor.createModel(source);
+    model.onDidChangeContent(function () {
+        let permalink = (<HTMLAnchorElement>document.getElementById("permalink"));
+        let params = new URLSearchParams();
+        params.set("snippet", editor.getModel().getValue());
+        let this_url = new URL(window.location.toString());
+        this_url.search = params.toString();
+        permalink.href = this_url.toString();
+        if (auto_compile.checked) {
+            if (keystorke_timeout_handle) {
+                clearTimeout(keystorke_timeout_handle);
+            }
+            keystorke_timeout_handle = setTimeout(update_preview, 500);
+
+        }
+    });
+    editor_documents.set(url, model);
+    update_preview();
+    return model;
+}
+
+function clearTabs() {
     let tab_bar = document.getElementById("tabs") as HTMLUListElement;
-    tab_bar.innerHTML = `<li id="main_source_file" class="nav-item"><span class="nav-link active">${main_source_filename}</span></li>`;
-    documents.clear();
-    documents.set(main_source_filename, { kind: DocumentReferenceKind.DocumentInActiveEditor });
+    tab_bar.innerHTML = "";
+    editor_documents.clear();
 }
 
-function update() {
-    let source = editor.getModel().getValue();
+function addTab(model: monaco.editor.ITextModel, url: string = "") {
+    let tab_bar = document.getElementById("tabs") as HTMLUListElement;
+    let tab = document.createElement("li");
+    tab.setAttribute("class", "nav-item");
+    tab.dataset["url"] = url;
+    tab.innerHTML = `<span class="nav-link">${tabTitleFromURL(url)}</span>`;
+    tab_bar.appendChild(tab);
+    if (tab_bar.childElementCount == 1) {
+        setCurrentTab(url);
+    }
+}
+
+function setCurrentTab(url: string) {
+    let current_tab = document.querySelector(`#tabs li[class~="nav-item"] span[class~="nav-link"][class~="active"]`);
+    if (current_tab != undefined) {
+        current_tab.className = "nav-link";
+    }
+    let new_current = document.querySelector(`#tabs li[class~="nav-item"][data-url="${url}"] span[class~="nav-link"]`);
+    if (new_current != undefined) {
+        new_current.className = "nav-link active";
+    }
+    let model = editor_documents.get(url);
+    if (model != undefined) {
+        editor.setModel(model);
+    }
+}
+
+function update_preview() {
+    let main_model = editor_documents.get(base_url);
+    if (main_model === undefined) {
+        return;
+    }
+    let source = main_model.getValue();
     let div = document.getElementById("preview");
     setTimeout(function () { render_or_error(source, base_url, div); }, 1);
 }
-
 
 async function render_or_error(source, base_url, div) {
     let canvas_id = 'canvas_' + Math.random().toString(36).substr(2, 9);
@@ -120,26 +167,17 @@ async function render_or_error(source, base_url, div) {
             let u = new URL(file_name, base_url);
             return u.toString();
         }, async (url: string) => {
-            let doc = documents.get(url);
+            console.log("ERR", url);
+            let doc = editor_documents.get(url);
             if (doc === undefined) {
                 const response = await fetch(url);
                 let doc = await response.text();
-
-                let tab = document.createElement("li");
-                tab.setAttribute("class", "nav-item");
-                tab.dataset["url"] = url;
-                tab.innerHTML = `<span class="nav-link active">${fileNameFromURL(new URL(url))}</span>`;
-                let tab_bar = document.getElementById("tabs") as HTMLUListElement;
-                tab_bar.appendChild(tab);
-
-                documents.set(url, { kind: DocumentReferenceKind.DocumentInline, source: doc });
-
+                let model = monaco.editor.createModel(doc);
+                editor_documents.set(url, model);
+                addTab(model, url);
                 return doc;
-            } else if (doc.kind == DocumentReferenceKind.DocumentInline) {
-                return Promise.resolve(doc.source)
-            } else if (doc.kind == DocumentReferenceKind.DocumentInActiveEditor) {
-                return Promise.resolve(editor.getModel().getValue())
             }
+            return doc.getValue();
         });
     } catch (e) {
         let text = document.createTextNode(e.message);
@@ -170,35 +208,22 @@ async function render_or_error(source, base_url, div) {
 let keystorke_timeout_handle;
 
 async function run() {
+    sixtyfps = await import("../../api/sixtyfps-wasm-interpreter/pkg/index.js");
     const params = new URLSearchParams(window.location.search);
     const code = params.get("snippet");
     const load_url = params.get("load_url");
     if (code) {
-        editor.getModel().setValue(code);
-        initTabs();
+        clearTabs();
+        let model = createMainModel(code, "");
+        addTab(model);
     } else if (load_url) {
         load_from_url(load_url);
     } else {
-        initTabs()
-        editor.getModel().setValue(hello_world);
+        clearTabs();
+        base_url = "";
+        let model = createMainModel(hello_world, "");
+        addTab(model);
     }
-    sixtyfps = await import("../../api/sixtyfps-wasm-interpreter/pkg/index.js");
-    update();
-    editor.getModel().onDidChangeContent(function () {
-        let permalink = (<HTMLAnchorElement>document.getElementById("permalink"));
-        let params = new URLSearchParams();
-        params.set("snippet", editor.getModel().getValue());
-        let this_url = new URL(window.location.toString());
-        this_url.search = params.toString();
-        permalink.href = this_url.toString();
-        if (auto_compile.checked) {
-            if (keystorke_timeout_handle) {
-                clearTimeout(keystorke_timeout_handle);
-            }
-            keystorke_timeout_handle = setTimeout(update, 500);
-
-        }
-    });
 }
 
 run();
