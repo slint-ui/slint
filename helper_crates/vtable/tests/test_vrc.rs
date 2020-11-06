7fbcb7860640a4b258650dc15bdb9d1541fe5459
Olivier Goffart <ogoffart@sixtyfps.io>
2020-11-06 11:31:53 +0100

    vtable: change VRc and VWeak to add the concrete type as a parameter
    ... or `Dyn` if unkown.
    
    So we can implement Deref for the concreate type.
    
    Sicne Deref is implemented for VRc, all the method of VRc are now
    associated function.

--- helper_crates/vtable/tests/test_vrc.rs
+++ helper_crates/vtable/tests/test_vrc.rs
@@ -38,29 +38,74 @@ FooVTable_static!(static SOME_STRUCT_TYPE for SomeStruct);
 fn rc_test() {
     let string = Rc::new("hello".to_string());
     let rc = VRc::new(SomeStruct { e: 42, x: "44".into(), foo: string.clone() });
-    let string_copy = rc.borrow().rc_string();
+    let string_copy = VRc::borrow(&rc).rc_string();
     assert!(Rc::ptr_eq(&string, &string_copy));
-    assert_eq!(rc.strong_count(), 1);
+    assert_eq!(VRc::strong_count(&rc), 1);
+    assert_eq!(rc.e, 42);
     drop(string_copy);
-    let w = rc.downgrade();
-    assert_eq!(rc.strong_count(), 1);
+    let w = VRc::downgrade(&rc);
+    assert_eq!(VRc::strong_count(&rc), 1);
     {
         let rc2 = w.upgrade().unwrap();
-        let string_copy = rc2.borrow().rc_string();
+        let string_copy = VRc::borrow(&rc2).rc_string();
         assert!(Rc::ptr_eq(&string, &string_copy));
-        assert_eq!(rc.strong_count(), 2);
+        assert_eq!(VRc::strong_count(&rc), 2);
         assert!(VRc::ptr_eq(&rc, &rc2));
         // one in `string`, one in `string_copy`, one in the shared region.
         assert_eq!(Rc::strong_count(&string), 3);
+        assert_eq!(rc2.e, 42);
     }
-    assert_eq!(rc.strong_count(), 1); // just the one in `string`
+    assert_eq!(VRc::strong_count(&rc), 1);
     drop(rc);
     assert_eq!(Rc::strong_count(&string), 1);
     assert!(w.upgrade().is_none());
 
     let rc = VRc::new(SomeStruct { e: 55, x: "_".into(), foo: string.clone() });
+    assert!(VRc::ptr_eq(&rc, &rc.clone()));
     assert_eq!(Rc::strong_count(&string), 2);
-    assert_eq!(rc.strong_count(), 1);
+    assert_eq!(VRc::strong_count(&rc), 1);
+    assert_eq!(rc.e, 55);
+    drop(rc);
+    assert_eq!(Rc::strong_count(&string), 1);
+}
+
+fn rc_dyn_test() {
+    let string = Rc::new("hello".to_string());
+    let origin = VRc::new(SomeStruct { e: 42, x: "44".into(), foo: string.clone() });
+    let origin_weak = VRc::downgrade(&origin);
+    let rc: VRc<FooVTable> = VRc::into_dyn(origin);
+    let string_copy = VRc::borrow(&rc).rc_string();
+    assert!(Rc::ptr_eq(&string, &string_copy));
+    assert_eq!(VRc::strong_count(&rc), 1);
+    drop(string_copy);
+    let w = VRc::downgrade(&rc);
+    assert_eq!(VRc::strong_count(&rc), 1);
+    {
+        let rc2 = w.upgrade().unwrap();
+        let string_copy = VRc::borrow(&rc2).rc_string();
+        assert!(Rc::ptr_eq(&string, &string_copy));
+        assert_eq!(VRc::strong_count(&rc), 2);
+        assert!(VRc::ptr_eq(&rc, &rc2));
+        // one in `string`, one in `string_copy`, one in the shared region.
+        assert_eq!(Rc::strong_count(&string), 3);
+    }
+    assert_eq!(VRc::strong_count(&rc), 1);
+    {
+        let rc_origin = origin_weak.upgrade().unwrap();
+        assert_eq!(rc_origin.e, 42);
+        assert!(VRc::ptr_eq(&VRc::into_dyn(rc_origin.clone()), &rc));
+    }
+    drop(rc);
+    assert_eq!(Rc::strong_count(&string), 1);
+    assert!(w.upgrade().is_none());
+    assert!(origin_weak.upgrade().is_none());
+    assert!(origin_weak.into_dyn().upgrade().is_none());
+
+    let rc: VRc<FooVTable> =
+        VRc::into_dyn(VRc::new(SomeStruct { e: 55, x: "_".into(), foo: string.clone() }));
+    assert!(VRc::ptr_eq(&rc, &rc.clone()));
+    assert_eq!(Rc::strong_count(&string), 2);
+    assert_eq!(VRc::strong_count(&rc), 1);
     drop(rc);
     assert_eq!(Rc::strong_count(&string), 1);
 }
