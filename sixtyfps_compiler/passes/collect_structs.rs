7a746c15da056474018755ae615cadd8a3a41a21
Olivier Goffart <ogoffart@sixtyfps.io>
2020-11-17 12:33:19 +0100

    Also make sure to generate struct when the property is an array of struct

--- sixtyfps_compiler/passes/collect_structs.rs
+++ sixtyfps_compiler/passes/collect_structs.rs
@@ -21,9 +21,9 @@ pub fn collect_structs(root_component: &Rc<Component>, _diag: &mut BuildDiagnost
     let mut hash = BTreeMap::new();
 
     let mut maybe_collect_object = |ty: &Type| {
-        if let Type::Object { name: Some(name), .. } = ty {
-            hash.entry(name.clone()).or_insert_with(|| ty.clone());
-        };
+        visit_named_object(ty, &mut |name, sub_ty| {
+            hash.entry(name.clone()).or_insert_with(|| sub_ty.clone());
+        });
     };
 
     recurse_elem_including_sub_components_no_borrow(
@@ -46,7 +46,7 @@ pub fn collect_structs(root_component: &Rc<Component>, _diag: &mut BuildDiagnost
     let mut used_struct = root_component.used_structs.borrow_mut();
     *used_struct = Vec::with_capacity(hash.len());
     while let Some(next) = hash.iter().next() {
-        // Here, using BTreeMap::poo_first would be great when it is stable
+        // Here, using BTreeMap::pop_first would be great when it is stable
         let key = next.0.clone();
         sort_struct(&mut hash, &mut used_struct, &key);
     }
@@ -56,23 +56,29 @@ pub fn collect_structs(root_component: &Rc<Component>, _diag: &mut BuildDiagnost
 /// it are placed before in the vector
 fn sort_struct(hash: &mut BTreeMap<String, Type>, vec: &mut Vec<Type>, key: &str) {
     let ty = if let Some(ty) = hash.remove(key) { ty } else { return };
-    if let Type::Object { fields, .. } = &ty {
+    if let Type::Object { fields, name: Some(name) } = &ty {
+        if name.contains("::") {
+            // This is a builtin type.
+            // FIXME! there should be a better way to handle builtin struct
+            return;
+        }
+
         for (_, sub_ty) in fields {
-            visit_named_object(&sub_ty, hash, vec);
+            visit_named_object(&sub_ty, &mut |name, _| sort_struct(hash, vec, name));
         }
     }
     vec.push(ty)
 }
 
-fn visit_named_object(ty: &Type, hash: &mut BTreeMap<String, Type>, vec: &mut Vec<Type>) {
+fn visit_named_object(ty: &Type, visitor: &mut impl FnMut(&String, &Type)) {
     match ty {
-        Type::Object { name: Some(name), .. } => sort_struct(hash, vec, &name),
+        Type::Object { name: Some(name), .. } => visitor(&name, ty),
         Type::Object { fields, name: None } => {
             for (_, sub_ty) in fields {
-                visit_named_object(&sub_ty, hash, vec);
+                visit_named_object(&sub_ty, visitor);
             }
         }
-        Type::Array(x) => visit_named_object(&x, hash, vec),
+        Type::Array(x) => visit_named_object(&x, visitor),
         _ => {}
     }
 }
