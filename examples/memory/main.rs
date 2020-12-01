574d17e69884060191b8cd933303cfac345c0644
Simon Hausmann <simon.hausmann@sixtyfps.io>
2020-12-01 10:56:36 +0100

    Initial version of a small memory game
    This is intended to form the basis for a tutorial.

---
+++ examples/memory/main.rs
@@ -0,0 +1,70 @@
+/* LICENSE BEGIN
+    This file is part of the SixtyFPS Project -- https://sixtyfps.io
+    Copyright (c) 2020 Olivier Goffart <olivier.goffart@sixtyfps.io>
+    Copyright (c) 2020 Simon Hausmann <simon.hausmann@sixtyfps.io>
+
+    SPDX-License-Identifier: GPL-3.0-only
+    This file is also available under commercial licensing terms.
+    Please contact info@sixtyfps.io for more information.
+LICENSE END */
+
+use sixtyfps::{Model, ModelHandle, Timer, VecModel};
+use std::rc::Rc;
+use std::time::Duration;
+
+sixtyfps::sixtyfps! {
+    import { MainWindow } from "memory.60";
+}
+
+fn shuffle(tiles: &mut Vec<TileData>) {
+    use rand::seq::SliceRandom;
+    let mut rng = rand::thread_rng();
+    tiles.shuffle(&mut rng);
+}
+
+pub fn main() {
+    let main_window = MainWindow::new();
+
+    let mut tiles: Vec<TileData> = main_window.get_tile_options().iter().collect();
+    tiles.extend(tiles.clone());
+
+    shuffle(&mut tiles);
+
+    let tiles_model = Rc::new(VecModel::from(tiles));
+
+    main_window.set_memory_tiles(ModelHandle::new(tiles_model.clone()));
+
+    let main_window_weak = main_window.as_weak();
+
+    main_window.on_check_if_pair_solved(move || {
+        let mut flipped_tiles = Vec::new();
+
+        for (index, tile) in tiles_model.iter().enumerate() {
+            if tile.image_visible && !tile.solved {
+                let index_tile_pair = (index, tile.clone());
+                flipped_tiles.push(index_tile_pair);
+            }
+        }
+
+        if flipped_tiles.len() == 2 {
+            main_window_weak.unwrap().set_disable_tiles(true);
+
+            let is_pair_solved = flipped_tiles[0].1 == flipped_tiles[1].1;
+
+            let tiles_model = tiles_model.clone();
+            let main_window_weak = main_window_weak.clone();
+
+            Timer::single_shot(Duration::from_secs(1), move || {
+                main_window_weak.unwrap().set_disable_tiles(false);
+
+                for (index, mut tile) in flipped_tiles.into_iter() {
+                    tile.solved = is_pair_solved;
+                    tile.image_visible = is_pair_solved;
+                    tiles_model.set_row_data(index, tile);
+                }
+            })
+        }
+    });
+
+    main_window.run();
+}
