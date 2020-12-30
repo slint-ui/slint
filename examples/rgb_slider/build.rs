6c8d4864b6262ab8475c7fc44e81916378a0fadf
Seo Sanghyeon <sanxiyn@gmail.com>
2020-12-31 01:49:20 +0900

    Add RGB slider example

---
+++ examples/rgb_slider/build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    sixtyfps_build::compile("rgb_slider.60").unwrap();
+}
---
+++ examples/rgb_slider/main.rs
@@ -0,0 +1,6 @@
+sixtyfps::include_modules!();
+
+fn main() {
+    let window = RGBSlider::new();
+    window.run();
+}
