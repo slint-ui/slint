23486d94f5482bc486a9548c122822e1f51f3aba
Olivier Goffart <ogoffart@sixtyfps.io>
2021-01-14 12:23:59 +0100

    Fix MSVC warning

--- examples/printerdemo/cpp/main.cpp
+++ examples/printerdemo/cpp/main.cpp
@@ -29,7 +29,7 @@ int main()
     printer_demo->on_fax_number_erase([printer_demo = sixtyfps::ComponentWeakHandle(printer_demo)] {
         std::string fax_number{(*printer_demo.lock())->get_fax_number()};
         fax_number.pop_back();
-        (*printer_demo.lock())->set_fax_number({fax_number});
+        (*printer_demo.lock())->set_fax_number(fax_number.data());
     });
 
     printer_demo->on_fax_send([printer_demo = sixtyfps::ComponentWeakHandle(printer_demo)] {
