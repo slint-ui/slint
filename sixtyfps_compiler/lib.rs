edcd4b1d0b50aab47f9491f6463abf821f5a01b1
Olivier Goffart <ogoffart@sixtyfps.io>
2021-01-27 09:21:26 +0100

    Move the generate_item_indices function in a a passes modules

--- sixtyfps_compiler/lib.rs
+++ sixtyfps_compiler/lib.rs
@@ -49,6 +49,7 @@ mod passes {
     pub mod default_geometry;
     pub mod embed_resources;
     pub mod focus_item;
+    pub mod generate_item_indices;
     pub mod inlining;
     pub mod lower_layout;
     pub mod lower_popups;
@@ -187,28 +188,7 @@ pub async fn run_passes(
     passes::resolve_native_classes::resolve_native_classes(&doc.root_component);
     passes::collect_globals::collect_globals(&doc.root_component, diag);
     passes::collect_structs::collect_structs(&doc.root_component, diag);
-
-    fn generate_item_indices(component: &Rc<object_tree::Component>) {
-        let mut current_item_index: usize = 0;
-        generator::build_array_helper(&component, move |item_rc, _, _, is_flickable_rect| {
-            let item = item_rc.borrow();
-            if is_flickable_rect {
-                current_item_index += 1;
-            } else if item.base_type == crate::langtype::Type::Void {
-            } else {
-                if let langtype::Type::Component(c) = &item.base_type {
-                    generate_item_indices(c);
-                }
-                item.item_index.set(current_item_index).unwrap();
-                current_item_index += 1;
-            }
-        });
-        for p in component.popup_windows.borrow().iter() {
-            generate_item_indices(&p.component)
-        }
-    }
-
-    generate_item_indices(&doc.root_component);
+    passes::generate_item_indices::generate_item_indices(&doc.root_component);
 }
 
 mod library {
---
+++ sixtyfps_compiler/passes/generate_item_indices.rs
@@ -0,0 +1,29 @@
+/* LICENSE BEGIN
+    This file is part of the SixtyFPS Project -- https://sixtyfps.io
+    Copyright (c) 2020 Olivier Goffart <olivier.goffart@sixtyfps.io>
+    Copyright (c) 2020 Simon Hausmann <simon.hausmann@sixtyfps.io>
+
+    SPDX-License-Identifier: GPL-3.0-only
+    This file is also available under commercial licensing terms.
+    Please contact info@sixtyfps.io for more information.
+LICENSE END */
+//! Assign the Element::item_index on each elements
+pub fn generate_item_indices(component: &std::rc::Rc<crate::object_tree::Component>) {
+    let mut current_item_index: usize = 0;
+    crate::generator::build_array_helper(&component, move |item_rc, _, _, is_flickable_rect| {
+        let item = item_rc.borrow();
+        if is_flickable_rect {
+            current_item_index += 1;
+        } else if item.base_type == crate::langtype::Type::Void {
+        } else {
+            if let crate::langtype::Type::Component(c) = &item.base_type {
+                generate_item_indices(c);
+            }
+            item.item_index.set(current_item_index).unwrap();
+            current_item_index += 1;
+        }
+    });
+    for p in component.popup_windows.borrow().iter() {
+        generate_item_indices(&p.component)
+    }
+}
