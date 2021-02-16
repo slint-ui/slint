15e0112d2d5e990f9491ab72bb048f685f29c9a0
Olivier Goffart <ogoffart@sixtyfps.io>
2021-02-16 10:10:30 +0100

    Qt: Fix image-fit of colorized image
    Since colorize property force the native item to be a ClippedImage, it had
    a source rectangle, despite with and height defaulting to 0 and therefore
    having an empty source rect would cause the scaling to not be right

--- sixtyfps_runtime/rendering_backends/qt/qt_window.rs
+++ sixtyfps_runtime/rendering_backends/qt/qt_window.rs
@@ -668,12 +668,13 @@ impl QtItemRenderer<'_> {
             _ => return,
         };
         let image_size = pixmap.size();
-        let mut source_rect = source_rect.unwrap_or_else(|| qttypes::QRectF {
-            x: 0.,
-            y: 0.,
-            width: image_size.width as _,
-            height: image_size.height as _,
-        });
+        let mut source_rect =
+            source_rect.filter(|r| r.is_valid()).unwrap_or_else(|| qttypes::QRectF {
+                x: 0.,
+                y: 0.,
+                width: image_size.width as _,
+                height: image_size.height as _,
+            });
         let mut dest_rect = dest_rect;
         adjust_to_image_fit(image_fit, &mut source_rect, &mut dest_rect);
         let painter: &mut QPainter = &mut *self.painter;
