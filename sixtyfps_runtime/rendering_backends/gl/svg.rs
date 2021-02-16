05736c78031a020a4236133a1d33a43a4ccb5d14
Olivier Goffart <ogoffart@sixtyfps.io>
2021-02-16 10:42:13 +0100

    GL: Fix rendering of scaled SVG for which height > width
    We would not allocate a buffer big enough for it.

--- sixtyfps_runtime/rendering_backends/gl/svg.rs
+++ sixtyfps_runtime/rendering_backends/gl/svg.rs
@@ -22,15 +22,18 @@ pub fn render(
     tree: &usvg::Tree,
     size: euclid::default::Size2D<u32>,
 ) -> Result<image::DynamicImage, usvg::Error> {
+    // resvg doesn't support scaling to width/height, just fit to width.
+    // FIXME: the fit should actually depends on the image-fit property
+    let fit = usvg::FitTo::Width(size.width);
+    let size = fit.fit_to(tree.svg_node().size.to_screen_size()).ok_or(usvg::Error::InvalidSize)?;
     let mut buffer =
-        vec![0u8; size.width as usize * size.height as usize * tiny_skia::BYTES_PER_PIXEL];
+        vec![0u8; size.width() as usize * size.height() as usize * tiny_skia::BYTES_PER_PIXEL];
     let skya_buffer =
-        tiny_skia::PixmapMut::from_bytes(buffer.as_mut_slice(), size.width, size.height)
+        tiny_skia::PixmapMut::from_bytes(buffer.as_mut_slice(), size.width(), size.height())
             .ok_or(usvg::Error::InvalidSize)?;
-    // FIXME: resvg doesn't support scaling to width/height, just fit to width.
-    resvg::render(&tree, usvg::FitTo::Width(size.width), skya_buffer);
+    resvg::render(&tree, fit, skya_buffer).ok_or(usvg::Error::InvalidSize)?;
     Ok(image::DynamicImage::ImageRgba8(
-        image::RgbaImage::from_raw(size.width, size.height, buffer)
+        image::RgbaImage::from_raw(size.width(), size.height(), buffer)
             .ok_or(usvg::Error::InvalidSize)?,
     ))
 }
