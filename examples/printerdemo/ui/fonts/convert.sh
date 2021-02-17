6461d44f6d971bbc05de8e038e622e3abb70fc80
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-02-17 14:16:20 +0100

    Select the Noto Sans font for the new printer demo
    Use a new `DemoText` element throughout the code base that selects the
    Noto Sans family. An ascii-only variant of the font is included in the
    repo along with the shell script to do the conversion.
    
    At the moment this only works with the interpreter, the other builds
    still need adapting and will fall back to the system default font as
    before.

---
+++ examples/printerdemo/ui/fonts/convert.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script converts the NotoSans font from https://www.google.com/get/noto/#sans-lgc
+# to a subset for this demo
+
+# You need to install `pyftsubset` from the `fonttools`. That's available via `brew install fonttools`,
+# or `sudo apt-get install fonttools`.
+
+cp NotoSans-unhinted/LICENSE_OFL.txt .
+
+for weight in Light Regular Bold; do
+    pyftsubset NotoSans-unhinted/NotoSans-$weight.ttf --unicodes="U+0020-007F" --output-file=NotoSans-$weight.ttf
+done
+
