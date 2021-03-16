ec1775f9ae0abcadb58235984627140a2dbc01d3
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-03-16 16:30:54 +0100

    Add skeleton for C++ API tests

---
+++ api/sixtyfps-cpp/tests/tests.cpp
@@ -0,0 +1,22 @@
+/* LICENSE BEGIN
+    This file is part of the SixtyFPS Project -- https://sixtyfps.io
+    Copyright (c) 2020 Olivier Goffart <olivier.goffart@sixtyfps.io>
+    Copyright (c) 2020 Simon Hausmann <simon.hausmann@sixtyfps.io>
+
+    SPDX-License-Identifier: GPL-3.0-only
+    This file is also available under commercial licensing terms.
+    Please contact info@sixtyfps.io for more information.
+LICENSE END */
+
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+SCENARIO("SixtyFPS can be shipped")
+{
+    GIVEN("a boolean")
+    {
+        bool okay = true;
+
+        REQUIRE(okay);
+    }
+}
