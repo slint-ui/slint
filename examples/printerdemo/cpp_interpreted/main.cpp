4fd4ac07549e2b2a73669561fe09bfeee9109052
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-03-25 18:33:38 +0100

    Add the C++ interpreter version of the printer queue "logic"
    Ported straight from the Rust version

--- examples/printerdemo/cpp_interpreted/main.cpp
+++ examples/printerdemo/cpp_interpreted/main.cpp
@@ -68,6 +68,54 @@ int main()
         std::cerr << "Could not set property ink_levels" << std::endl;
         return EXIT_FAILURE;
     }
+
+    auto printer_queue = std::make_shared<sixtyfps::VectorModel<Value>>();
+
+    sixtyfps::SharedVector<Value> default_queue =
+            *instance->get_property("printer_queue")->to_array();
+    for (const auto &default_item : default_queue)
+        printer_queue->push_back(default_item);
+
+    instance->set_property("printer_queue", Value(printer_queue));
+
+    instance->set_callback("start_job", [=](auto args) {
+        std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        char time_buf[100] = { 0 };
+        std::strftime(time_buf, sizeof(time_buf), "%H:%M:%S %d/%m/%Y", std::localtime(&now));
+
+        sixtyfps::interpreter::Struct item {
+            { "status", Value(sixtyfps::SharedString("WAITING...")) },
+            { "progress", Value(0.) },
+            { "title", args[0] },
+            { "owner", sixtyfps::SharedString("joe@example.com") },
+            { "pages", Value(1.) },
+            { "size", sixtyfps::SharedString("100kB") },
+            { "submission_date", sixtyfps::SharedString(time_buf) }
+        };
+        printer_queue->push_back(item);
+        return Value();
+    });
+
+    instance->set_callback("cancel_job", [=](auto args) {
+        auto index = *args[0].to_number();
+        printer_queue->erase(int(index));
+        return Value();
+    });
+
+    sixtyfps::Timer printer_queue_progress_timer(std::chrono::seconds(1), [=]() {
+        if (printer_queue->row_count() > 0) {
+            auto top_item = *printer_queue->row_data(0).to_struct();
+            auto progress = *top_item.get_field("progress")->to_number() + 1.;
+            top_item.set_field("progress", progress);
+            top_item.set_field("status", sixtyfps::SharedString("PRINTING"));
+            if (progress > 100) {
+                printer_queue->erase(0);
+            } else {
+                printer_queue->set_row_data(0, top_item);
+            }
+        }
+    });
+
     instance->set_callback("quit", [](auto) {
         std::exit(0);
         return Value();
