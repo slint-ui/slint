e754903325c5956b112827a2b15b03269b267a13
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-04-01 11:38:57 +0200

    Try to bundle the lsp server in the vscode extension package
    The rust binary creation is not automated yet though.

--- vscode_extension/README.md
+++ vscode_extension/README.md
@@ -2,6 +2,20 @@
 
 Extension for VSCode which include syntax coloration and a way to start the LSP server
 
+## How to build the extension package
+
+Until the building of the LSP binaries is automated via npm, the following commands need to be run before packaging with `vsce package`:
+
+```shell
+cd ..
+cross build --target x86_64-unknown-linux-gnu --release -p sixtyfps-lsp
+cp target/x86_64-unknown-linux-gnu/release/sixtyfps-lsp vscode_extension/bin/sixtyfps-lsp-x86_64-unknown-linux-gnu
+cross build --target x86_64-pc-windows-gnu --release -p sixtyfps-lsp
+cp target/x86_64-pc-windows-gnu/release/sixtyfps-lsp.exe vscode_extension/bin/sixtyfps-lsp-x86_64-pc-windows-gnu.exe
+cargo build --release -p sixtyfps-lsp
+cp target/release/sixtyfps-lsp vscode_extension/bin/sixtyfps-lsp-x86_64-apple-darwin
+```
+
 ## How to run the LSP
 
 At the moment you need to load this directory in VS code and then start debugging (Run -> Start Debugging). That will "debug" the vs code extension and create a new VS code window. The LSP server binary will be started if previously built. You can see the output in the output pane "SixtyFPS LSP" (that's the drop-down that usually shows "Tasks").
--- vscode_extension/src/extension.ts
+++ vscode_extension/src/extension.ts
@@ -1,15 +1,16 @@
 /* LICENSE BEGIN
-    This file is part of the SixtyFPS Project -- https://sixtyfps.io
-    Copyright (c) 2020 Olivier Goffart <olivier.goffart@sixtyfps.io>
-    Copyright (c) 2020 Simon Hausmann <simon.hausmann@sixtyfps.io>
+	This file is part of the SixtyFPS Project -- https://sixtyfps.io
+	Copyright (c) 2020 Olivier Goffart <olivier.goffart@sixtyfps.io>
+	Copyright (c) 2020 Simon Hausmann <simon.hausmann@sixtyfps.io>
 
-    SPDX-License-Identifier: GPL-3.0-only
-    This file is also available under commercial licensing terms.
-    Please contact info@sixtyfps.io for more information.
+	SPDX-License-Identifier: GPL-3.0-only
+	This file is also available under commercial licensing terms.
+	Please contact info@sixtyfps.io for more information.
 LICENSE END */
 
 import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import { existsSync } from 'fs';
+import { workspace, extensions, ExtensionContext } from 'vscode';
 
 import {
 	LanguageClient,
@@ -21,10 +22,34 @@ import {
 
 let client: LanguageClient;
 
-export function activate(context: ExtensionContext) {
+function lspProgramNameSuffix(): string | null {
+	if (process.platform == "darwin")
+		return "x86_64-apple-darwin";
+	if (process.platform === "linux") {
+		if (process.arch === "x64")
+			return "x86_64-unknown-linux-gnu"
+	}
+	if (process.platform == "win32")
+		return "x86_64-pc-windows-gnu.exe";
+	return null;
+}
 
-	let serverModule = context.asAbsolutePath(path.join('..', 'target', 'debug', 'sixtyfps-lsp'));
-	console.log(serverModule);
+export function activate(context: ExtensionContext) {
+	let lsp_suffix = lspProgramNameSuffix();
+	if (lsp_suffix == null)
+		return;
+
+	let serverModule = path.join(context.extensionPath, "bin", "sixtyfps-lsp-" + lsp_suffix);
+
+	if (!existsSync(serverModule))
+		serverModule = context.asAbsolutePath(path.join('..', 'target', 'debug', 'sixtyfps-lsp'));
+
+	if (!existsSync(serverModule)) {
+		console.warn("Could not locate sixtyfps-server server binary, neither in bundled bin/ directory nor relative in ../target");
+		return;
+	}
+
+	console.log(`Starting LSP server from {serverModule)`);
 
 	/*let test_output = vscode.window.createOutputChannel("Test Output");
 	test_output.appendLine("Hello from extension");*/
