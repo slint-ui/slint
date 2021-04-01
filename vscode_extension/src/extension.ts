bf9916d5ebb39f24b50a3fcc783c7479e74ae3c0
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-04-01 12:40:12 +0200

    Fix VS code extension license header
    Not sure why the formatter ended up using tabs :(

--- vscode_extension/src/extension.ts
+++ vscode_extension/src/extension.ts
@@ -1,11 +1,11 @@
 /* LICENSE BEGIN
-	This file is part of the SixtyFPS Project -- https://sixtyfps.io
-	Copyright (c) 2020 Olivier Goffart <olivier.goffart@sixtyfps.io>
-	Copyright (c) 2020 Simon Hausmann <simon.hausmann@sixtyfps.io>
+    This file is part of the SixtyFPS Project -- https://sixtyfps.io
+    Copyright (c) 2020 Olivier Goffart <olivier.goffart@sixtyfps.io>
+    Copyright (c) 2020 Simon Hausmann <simon.hausmann@sixtyfps.io>
 
-	SPDX-License-Identifier: GPL-3.0-only
-	This file is also available under commercial licensing terms.
-	Please contact info@sixtyfps.io for more information.
+    SPDX-License-Identifier: GPL-3.0-only
+    This file is also available under commercial licensing terms.
+    Please contact info@sixtyfps.io for more information.
 LICENSE END */
 
 import * as path from 'path';
