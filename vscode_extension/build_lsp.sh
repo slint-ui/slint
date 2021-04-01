27ee7b0758fcb564a1a6538342a698065256bd2f
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-04-01 12:57:01 +0200

    Try to avoid doing cross builds on Linux

--- vscode_extension/build_lsp.sh
+++ vscode_extension/build_lsp.sh
@@ -9,12 +9,26 @@
 # Please contact info@sixtyfps.io for more information.
 # LICENSE END
 
-cd `dirname $0`/..
-for lsp_suffix in x86_64-unknown-linux-gnu x86_64-pc-windows-gnu.exe; do
+mkdir -p bin
+cd ..
+
+suffices_to_build="x86_64-pc-windows-gnu.exe"
+
+case $OSTYPE in
+    darwin*)
+        suffices_to_build="$suffices_to_build x86_64-unknown-linux-gnu"
+        native_suffix="x86_64-apple-darwin"
+        ;;
+    linux*)
+        native_suffix="x86_64-unknown-linux-gnu"
+        ;;
+esac
+
+for lsp_suffix in $suffices_to_build; do
     triplet=`basename $lsp_suffix .exe`
     exe_suffix=`echo $lsp_suffix | sed -n -e "s,.*\(\.exe\),\1,p"`
     cross build --target $triplet --release -p sixtyfps-lsp
-    cp target/$triplet/release/sixtyfps-lsp$exe_suffix vscode_extension/bin/sixtyfps-lsp-$suffix
+    cp target/$triplet/release/sixtyfps-lsp$exe_suffix vscode_extension/bin/sixtyfps-lsp-$lsp_suffix
 done
 cargo build --release -p sixtyfps-lsp
-cp target/release/sixtyfps-lsp vscode_extension/bin/sixtyfps-lsp-x86_64-apple-darwin
+cp target/release/sixtyfps-lsp vscode_extension/bin/sixtyfps-lsp-$native_suffix
