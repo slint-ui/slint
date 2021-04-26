slint-ui/slint

commit d8ebd42712860de55e4d7245f0cabfb3e7c844ae

GL backend: fix vertical alignment of text
Fix issue in
https://github.com/sixtyfpsui/sixtyfps/issues/207#issuecomment-825780094

For the purpose of alignment, we need to use the font height, not the actual
text height, because otherwise it does not look visualy centered if the
text does not have letters that goes under the baseline

sixtyfps_runtime/rendering_backends/gl/lib.rs
@@ -695,8 +695,6 @@ impl ItemRenderer for GLItemRenderer {
 
         let wrap = text.wrap() == TextWrap::word_wrap;
         let letter_spacing = text.letter_spacing() * self.scale_factor;
-        let text_size =
-            font.text_size(letter_spacing, string, if wrap { Some(max_width) } else { None });
 
         let paint = match self
             .brush_to_paint(text.color(), &mut rect_to_path(item_rect(text, self.scale_factor)))
@@ -711,8 +709,8 @@ impl ItemRenderer for GLItemRenderer {
 
         let mut y = match vertical_alignment {
             TextVerticalAlignment::top => 0.,
-            TextVerticalAlignment::center => max_height / 2. - text_size.height / 2.,
-            TextVerticalAlignment::bottom => max_height - text_size.height,
+            TextVerticalAlignment::center => max_height / 2. - font_metrics.height() / 2.,
+            TextVerticalAlignment::bottom => max_height - font_metrics.height(),
         };
 
         let mut draw_line = |canvas: &mut femtovg::Canvas<_>, to_draw: &str| {

examples/printerdemo/ui/printer_queue.60
@@ -145,7 +145,7 @@ NarrowPrintQueueElement := Rectangle {
                     width: 10%;
                 }
                 PushButton {
-                    text: "More";
+                    text: "More   ";
                 }
                 Rectangle {
                     horizontal-stretch: 0;

examples/printerdemo/ui/printerdemo.60
@@ -103,9 +103,8 @@ MainWindow := Window {
 
         callback icon_y(int) -> length;
         icon_y(index) => {
-            100px // top padding
-            + index * 72px;
-            ;
+            return 100px // top padding
+                + index * 72px;
         }
 
         Image {

commit a61935568a91c020230c373216ff326828fb408a

Fix text alignment with multiple lines
Broken by d8ebd42712860de55e4d7245f0cabfb3e7c844ae
for issue #207

sixtyfps_runtime/rendering_backends/gl/lib.rs
@@ -695,6 +695,8 @@ impl ItemRenderer for GLItemRenderer {
 
         let wrap = text.wrap() == TextWrap::word_wrap;
         let letter_spacing = text.letter_spacing() * self.scale_factor;
+        let text_size =
+            font.text_size(letter_spacing, string, if wrap { Some(max_width) } else { None });
 
         let paint = match self
             .brush_to_paint(text.color(), &mut rect_to_path(item_rect(text, self.scale_factor)))
@@ -709,8 +711,8 @@ impl ItemRenderer for GLItemRenderer {
 
         let mut y = match vertical_alignment {
             TextVerticalAlignment::top => 0.,
-            TextVerticalAlignment::center => max_height / 2. - font_metrics.height() / 2.,
-            TextVerticalAlignment::bottom => max_height - font_metrics.height(),
+            TextVerticalAlignment::center => max_height / 2. - text_size.height / 2.,
+            TextVerticalAlignment::bottom => max_height - text_size.height,
         };
 
         let mut draw_line = |canvas: &mut femtovg::Canvas<_>, to_draw: &str| {
@@ -1583,9 +1585,8 @@ impl GLFont {
         let paint = self.init_paint(letter_spacing, femtovg::Paint::default());
         let mut canvas = self.canvas.borrow_mut();
         let font_metrics = canvas.measure_font(paint).unwrap();
-        let mut y = 0.;
+        let mut lines = 0;
         let mut width = 0.;
-        let mut height = 0.;
         let mut start = 0;
         if let Some(max_width) = max_width {
             while start < text.len() {
@@ -1596,19 +1597,17 @@ impl GLFont {
                 let index = start + index;
                 let mesure = canvas.measure_text(0., 0., &text[start..index], paint).unwrap();
                 start = index;
-                height = y + mesure.height();
-                y += font_metrics.height();
+                lines += 1;
                 width = mesure.width().max(width);
             }
         } else {
             for line in text.lines() {
                 let mesure = canvas.measure_text(0., 0., line, paint).unwrap();
-                height = y + mesure.height();
-                y += font_metrics.height();
+                lines += 1;
                 width = mesure.width().max(width);
             }
         }
-        euclid::size2(width, height)
+        euclid::size2(width, lines as f32 * font_metrics.height())
     }
 }
 