607ad5217f25882ac9703f9722d2cc1e422e05f9
Olivier Goffart <ogoffart@sixtyfps.io>
2021-04-27 10:42:40 +0200

    Printerdemo C++: add the logic for the queue

--- examples/printerdemo/cpp/main.cpp
+++ examples/printerdemo/cpp/main.cpp
@@ -26,5 +26,45 @@ int main()
     printer_demo->set_ink_levels(std::make_shared<InkLevelModel>());
     printer_demo->on_quit([] { std::exit(0); });
 
+
+    auto printer_queue = std::make_shared<sixtyfps::VectorModel<PrinterQueueItem>>();
+    auto default_queue = printer_demo->get_printer_queue();
+    for (int i = 0; i < default_queue->row_count(); ++i) {
+        printer_queue->push_back(default_queue->row_data(i));
+    }
+    printer_demo->set_printer_queue(printer_queue);
+
+    printer_demo->on_start_job([=](sixtyfps::SharedString name) {
+        std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        char time_buf[100] = { 0 };
+        std::strftime(time_buf, sizeof(time_buf), "%H:%M:%S %d/%m/%Y", std::localtime(&now));
+        PrinterQueueItem item;
+        item.status = "WAITING...";
+        item.progress = 0;
+        item.title = std::move(name);
+        item.owner = "joe@example.com";
+        item.pages = 1;
+        item.size = "100kB";
+        item.submission_date = time_buf;
+        printer_queue->push_back(item);
+    });
+
+    printer_demo->on_cancel_job([=](int index) {
+        printer_queue->erase(int(index));
+    });
+
+    sixtyfps::Timer printer_queue_progress_timer(std::chrono::seconds(1), [=]() {
+        if (printer_queue->row_count() > 0) {
+            auto top_item = printer_queue->row_data(0);
+            top_item.progress += 1;
+            if (top_item.progress > 100) {
+                printer_queue->erase(0);
+            } else {
+                top_item.status = "PRINTING";
+                printer_queue->set_row_data(0, top_item);
+            }
+        }
+    });
+
     printer_demo->run();
 }
