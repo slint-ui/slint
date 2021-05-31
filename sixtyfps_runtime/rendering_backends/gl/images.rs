slint-ui/slint

commit 8d2554ff3be581d8b44f9d9dff5483c82d759e3d

Remove internal event loop proxy clone in the GL renderer
We don't need it since we can send events through the TLS

sixtyfps_runtime/rendering_backends/gl/images.rs
@@ -201,20 +201,15 @@ impl CachedImage {
                 let image_id = image_id.clone();
                 let window_weak = Rc::downgrade(&renderer.window);
                 let cached_image_weak = Rc::downgrade(&cached_image);
-                let event_loop_proxy_weak = Rc::downgrade(&renderer.event_loop_proxy);
                 move || {
-                    let (canvas, window, event_loop_proxy, cached_image) = match (
+                    let (canvas, window, cached_image) = match (
                         canvas_weak.upgrade(),
                         window_weak.upgrade(),
-                        event_loop_proxy_weak.upgrade(),
                         cached_image_weak.upgrade(),
                     ) {
-                        (
-                            Some(canvas),
-                            Some(window),
-                            Some(event_loop_proxy),
-                            Some(cached_image),
-                        ) => (canvas, window, event_loop_proxy, cached_image),
+                        (Some(canvas), Some(window), Some(cached_image)) => {
+                            (canvas, window, cached_image)
+                        }
                         _ => return,
                     };
                     canvas
@@ -236,7 +231,12 @@ impl CachedImage {
                     // be dispatched as the next event. We are however not in an event loop
                     // call, so we also need to wake up the event loop.
                     window.request_redraw();
-                    event_loop_proxy.send_event(crate::eventloop::CustomEvent::WakeUpAndPoll).ok();
+                    crate::eventloop::with_window_target(|event_loop| {
+                        event_loop
+                            .event_loop_proxy()
+                            .send_event(crate::eventloop::CustomEvent::WakeUpAndPoll)
+                            .ok();
+                    });
                 }
             })
             .into(),

sixtyfps_runtime/rendering_backends/gl/lib.rs
@@ -215,8 +215,7 @@ struct GLRendererData {
     window: Rc<winit::window::Window>,
     #[cfg(not(target_arch = "wasm32"))]
     windowed_context: RefCell<Option<WindowedContextWrapper>>,
-    #[cfg(target_arch = "wasm32")]
-    event_loop_proxy: Rc<winit::event_loop::EventLoopProxy<eventloop::CustomEvent>>,
+
     item_graphics_cache: RefCell<RenderingCache<Option<ItemGraphicsCacheEntry>>>,
 
     // Cache used to avoid repeatedly decoding images from disk. Entries with a count
@@ -310,9 +309,6 @@ impl GLRenderer {
         };
 
         #[cfg(target_arch = "wasm32")]
-        let event_loop_proxy = Rc::new(event_loop.event_loop_proxy().clone());
-
-        #[cfg(target_arch = "wasm32")]
         let (window, renderer) = {
             use wasm_bindgen::JsCast;
 
@@ -344,7 +340,6 @@ impl GLRenderer {
             // on the web will always have 1024x768 as size otherwise.
 
             let resize_canvas = {
-                let event_loop_proxy = event_loop_proxy.clone();
                 let window = window.clone();
                 let canvas = canvas.clone();
                 move |_: web_sys::Event| {
@@ -355,7 +350,12 @@ impl GLRenderer {
 
                     window.set_inner_size(existing_canvas_size);
                     window.request_redraw();
-                    event_loop_proxy.send_event(eventloop::CustomEvent::WakeUpAndPoll).ok();
+                    crate::eventloop::with_window_target(|event_loop| {
+                        event_loop
+                            .event_loop_proxy()
+                            .send_event(crate::eventloop::CustomEvent::WakeUpAndPoll)
+                            .ok();
+                    })
                 }
             };
 
@@ -402,8 +402,6 @@ impl GLRenderer {
             }))),
             #[cfg(target_arch = "wasm32")]
             window,
-            #[cfg(target_arch = "wasm32")]
-            event_loop_proxy,
 
             item_graphics_cache: Default::default(),
             image_cache: Default::default(),

commit 357dd9826dc5d95f65662ed9c67f10a97a1cef8e

Fix HTML images not being rendered after being loaded
Instead a "manual" refresh is needed (like mouse mover). This regressed
in commit 8d2554ff3be581d8b44f9d9dff5483c82d759e3d, which removed the
event loop proxy clone. For some reason that's not sending the event
from the DOM callback, but winit requires a clone of the proxy.

sixtyfps_runtime/rendering_backends/gl/images.rs
@@ -189,6 +189,10 @@ impl CachedImage {
             Some(Box::pin(/*upload pending*/ Property::new(true))),
         ));
 
+        let event_loop_proxy = crate::eventloop::with_window_target(|event_loop| {
+            event_loop.event_loop_proxy().clone()
+        });
+
         let html_image = web_sys::HtmlImageElement::new().unwrap();
         html_image.set_cross_origin(Some("anonymous"));
         html_image.set_onload(Some(
@@ -229,12 +233,7 @@ impl CachedImage {
                     // be dispatched as the next event. We are however not in an event loop
                     // call, so we also need to wake up the event loop.
                     window.request_redraw();
-                    crate::eventloop::with_window_target(|event_loop| {
-                        event_loop
-                            .event_loop_proxy()
-                            .send_event(crate::eventloop::CustomEvent::WakeUpAndPoll)
-                            .ok();
-                    });
+                    event_loop_proxy.send_event(crate::eventloop::CustomEvent::WakeUpAndPoll).ok();
                 }
             })
             .into(),