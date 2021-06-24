376ed435443296b2e16af7f9f57c62c1bb7dbfa6
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-06-24 12:23:58 +0200

    Remove the "View Page Source" links from the sphinx docs
    The rst source is not really all that interesting :-)

--- api/sixtyfps-cpp/docs/conf.py
+++ api/sixtyfps-cpp/docs/conf.py
@@ -72,5 +72,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_show_sourcelink = False
+
 def setup(app):
     app.add_css_file('theme_tweak.css')
