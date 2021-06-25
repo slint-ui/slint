4611cfb24f72fedf9ac300a09def1ace3518d846
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-06-25 23:04:14 +0200

    Fix doxygen warn as error
    The version in the CI only supports YES

--- api/sixtyfps-cpp/docs/conf.py
+++ api/sixtyfps-cpp/docs/conf.py
@@ -49,7 +49,7 @@ EXCLUDE_SYMBOLS = sixtyfps::cbindgen_private* sixtyfps::private_api*
 EXCLUDE = ../../api/sixtyfps-cpp/include/vtable.h ../../api/sixtyfps-cpp/include/sixtyfps_testing.h
 ENABLE_PREPROCESSING = YES
 PREDEFINED += DOXYGEN
-WARN_AS_ERROR = FAIL_ON_WARNINGS'''
+WARN_AS_ERROR = YES'''
 }
 
 # Add any paths that contain templates here, relative to this directory.
