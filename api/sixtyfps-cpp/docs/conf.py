b61e2989849a2d35b74faea5696ae2834a870f38
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-07-01 18:19:29 +0200

    C++ docs: Allow expanding the tree in the navigation bar from anywhere

--- api/sixtyfps-cpp/docs/conf.py
+++ api/sixtyfps-cpp/docs/conf.py
@@ -68,6 +68,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+    "collapse_navigation" : False
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
