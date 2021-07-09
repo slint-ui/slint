9e57262487a19442d8d6187da88ce9bbe08cfd52
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-07-09 09:59:20 +0200

    C++ docs: Fix showing of the version in the theme
    Not sure where the "release" variable came from :)

--- api/sixtyfps-cpp/docs/conf.py
+++ api/sixtyfps-cpp/docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, info@sixtyfps.io'
 author = 'info@sixtyfps.io'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+version = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
