slint-ui/slint

commit ba32777cab9e58659601b58c89d66b525969b347

Refactoring: move the animation in the PropertyBinding struct
Since they always belong together.

This will help for issue #193

sixtyfps_compiler/expression_tree.rs
@@ -1078,13 +1078,21 @@ pub struct BindingExpression {
     /// 0 means the expression was added by some passes and it is not explicit in the source code
     pub priority: i32,
 
+    pub animation: Option<PropertyAnimation>,
+
     /// The analysis information. None before it is computed
     pub analysis: RefCell<Option<BindingAnalysis>>,
 }
 
 impl std::convert::From<Expression> for BindingExpression {
     fn from(expression: Expression) -> Self {
-        Self { expression, span: None, priority: 0, analysis: Default::default() }
+        Self {
+            expression,
+            span: None,
+            priority: 0,
+            animation: Default::default(),
+            analysis: Default::default(),
+        }
     }
 }
 
@@ -1094,11 +1102,18 @@ impl BindingExpression {
             expression: Expression::Uncompiled(node.clone()),
             span: Some(node.to_source_location()),
             priority: 1,
+            animation: Default::default(),
             analysis: Default::default(),
         }
     }
     pub fn new_with_span(expression: Expression, span: SourceLocation) -> Self {
-        Self { expression, span: Some(span), priority: 0, analysis: Default::default() }
+        Self {
+            expression,
+            span: Some(span),
+            priority: 0,
+            animation: Default::default(),
+            analysis: Default::default(),
+        }
     }
 }
 

sixtyfps_compiler/object_tree.rs
@@ -341,8 +341,6 @@ pub struct Element {
     /// Main owner for a reference to a property.
     pub named_references: crate::namedreference::NamedReferenceContainer,
 
-    pub property_animations: HashMap<String, PropertyAnimation>,
-
     /// Tis element is part of a `for <xxx> in <model>:
     pub repeated: Option<RepeatedElementInfo>,
 
@@ -414,10 +412,10 @@ pub fn pretty_print(
         expression_tree::pretty_print(f, &expr.expression)?;
         writeln!(f, ";")?;
         //writeln!(f, "; /*{}*/", expr.priority)?;
-    }
-    for (name, anim) in &e.property_animations {
-        indent!();
-        writeln!(f, "animate {} {:?}", name, anim)?;
+        if let Some(anim) = &expr.animation {
+            indent!();
+            writeln!(f, "animate {} {:?}", name, anim)?;
+        }
     }
     if !e.states.is_empty() {
         indent!();
@@ -716,11 +714,15 @@ impl Element {
                                 );
                             }
 
-                            if r.property_animations
-                                .insert(
-                                    resolved_name.to_string(),
-                                    PropertyAnimation::Static(anim_element),
-                                )
+                            let expr_binding =
+                                r.bindings.entry(resolved_name.to_string()).or_insert_with(|| {
+                                    let mut r = BindingExpression::from(Expression::Invalid);
+                                    r.priority = 1;
+                                    r
+                                });
+                            if expr_binding
+                                .animation
+                                .replace(PropertyAnimation::Static(anim_element))
                                 .is_some()
                             {
                                 diag.push_error("Duplicated animation".into(), &prop_name_token)
@@ -1292,6 +1294,17 @@ pub fn visit_element_expressions(
         let mut bindings = std::mem::take(&mut elem.borrow_mut().bindings);
         for (name, expr) in &mut bindings {
             vis(expr, Some(name.as_str()), &|| elem.borrow().lookup_property(name).property_type);
+
+            match &mut expr.animation {
+                Some(PropertyAnimation::Static(e)) => visit_element_expressions_simple(e, vis),
+                Some(PropertyAnimation::Transition { animations, state_ref }) => {
+                    vis(state_ref, None, &|| Type::Int32);
+                    for a in animations {
+                        visit_element_expressions_simple(&a.animation, vis)
+                    }
+                }
+                None => (),
+            }
         }
         elem.borrow_mut().bindings = bindings;
     }
@@ -1323,20 +1336,6 @@ pub fn visit_element_expressions(
         }
     }
     elem.borrow_mut().transitions = transitions;
-
-    let mut property_animations = std::mem::take(&mut elem.borrow_mut().property_animations);
-    for anim_elem in property_animations.values_mut() {
-        match anim_elem {
-            PropertyAnimation::Static(e) => visit_element_expressions_simple(e, &mut vis),
-            PropertyAnimation::Transition { animations, state_ref } => {
-                vis(state_ref, None, &|| Type::Int32);
-                for a in animations {
-                    visit_element_expressions_simple(&a.animation, &mut vis)
-                }
-            }
-        }
-    }
-    elem.borrow_mut().property_animations = property_animations;
 }
 
 /// Visit all the named reference in an element

sixtyfps_compiler/passes/lower_states.rs
@@ -130,6 +130,7 @@ fn lower_transitions_in_element(
                     "The property is not changed as part of this transition".into(),
                     &span,
                 );
+                continue;
             }
 
             let t = TransitionPropertyAnimation {
@@ -142,23 +143,19 @@ fn lower_transitions_in_element(
     }
     for (ne, (span, animations)) in props {
         let e = ne.element();
-        match e.borrow_mut().property_animations.entry(ne.name().to_owned()) {
-            std::collections::hash_map::Entry::Occupied(e) => {
-                diag.push_error(
-                    format!(
+        // We check earlier that the property is in the set of changed properties, so a binding bust have been assigned
+        let old_anim = e.borrow_mut().bindings.get_mut(ne.name()).unwrap().animation.replace(
+            PropertyAnimation::Transition { state_ref: state_property.clone(), animations },
+        );
+        if old_anim.is_some() {
+            diag.push_error(
+                format!(
                     "The property '{}' cannot have transition because it already has an animation",
-                    e.key()
+                    ne.name()
                 ),
-                    &span,
-                );
-            }
-            std::collections::hash_map::Entry::Vacant(e) => {
-                e.insert(PropertyAnimation::Transition {
-                    state_ref: state_property.clone(),
-                    animations,
-                });
-            }
-        };
+                &span,
+            );
+        }
     }
 }
 

sixtyfps_compiler/generator.rs
@@ -175,6 +175,9 @@ pub fn handle_property_bindings_init(
                 _ => {}
             })
         }
+        if matches!(binding_expression.expression, Expression::Invalid) {
+            return;
+        }
         handle_property(elem, prop_name, binding_expression);
     }
 

sixtyfps_compiler/generator/cpp.rs
@@ -225,7 +225,8 @@ use crate::expression_tree::{
 use crate::langtype::Type;
 use crate::layout::{Layout, LayoutGeometry, LayoutRect, Orientation};
 use crate::object_tree::{
-    Component, Document, Element, ElementRc, PropertyDeclaration, RepeatedElementInfo,
+    Component, Document, Element, ElementRc, PropertyAnimation, PropertyDeclaration,
+    RepeatedElementInfo,
 };
 use cpp_ast::*;
 use itertools::Itertools;
@@ -330,7 +331,7 @@ fn property_set_value_code(
     property_name: &str,
     value_expr: &str,
 ) -> String {
-    match element.property_animations.get(property_name) {
+    match element.bindings.get(property_name).and_then(|b| b.animation.as_ref()) {
         Some(crate::object_tree::PropertyAnimation::Static(animation)) => {
             let animation_code = property_animation_code(component, animation);
             format!(
@@ -348,6 +349,7 @@ fn handle_property_binding(
     prop_name: &str,
     binding_expression: &Expression,
     is_constant: bool,
+    animation: Option<&PropertyAnimation>,
     init: &mut Vec<String>,
 ) {
     let item = elem.borrow();
@@ -387,7 +389,14 @@ fn handle_property_binding(
             p2 = access_named_reference(nr, &component, "this")
         ));
         if let Some(next) = next {
-            handle_property_binding(elem, prop_name, next, is_constant || next.is_constant(), init)
+            handle_property_binding(
+                elem,
+                prop_name,
+                next,
+                is_constant || next.is_constant(),
+                animation,
+                init,
+            )
         }
     } else {
         let component = &item.enclosing_component.upgrade().unwrap();
@@ -410,7 +419,7 @@ fn handle_property_binding(
             if is_state_info {
                 format!("sixtyfps::private_api::set_state_binding({}, {});", cpp_prop, binding_code)
             } else {
-                match item.property_animations.get(prop_name) {
+                match animation {
                     Some(crate::object_tree::PropertyAnimation::Static(anim)) => {
                         let anim = property_animation_code(&component, anim);
                         format!("{}.set_animated_binding({}, {});", cpp_prop, binding_code, anim)
@@ -1065,6 +1074,7 @@ fn generate_component(
             prop,
             binding,
             binding.analysis.borrow().as_ref().map_or(false, |a| a.is_const),
+            binding.animation.as_ref(),
             &mut init,
         )
     });

sixtyfps_compiler/generator/rust.rs
@@ -22,7 +22,7 @@ use crate::expression_tree::{
 };
 use crate::langtype::Type;
 use crate::layout::{Layout, LayoutGeometry, LayoutRect, Orientation};
-use crate::object_tree::{Component, Document, ElementRc};
+use crate::object_tree::{Component, Document, ElementRc, PropertyAnimation};
 use proc_macro2::{Ident, TokenStream};
 use quote::{format_ident, quote};
 use std::{collections::BTreeMap, rc::Rc};
@@ -161,6 +161,7 @@ fn handle_property_binding(
     prop_name: &str,
     binding_expression: &Expression,
     is_constant: bool,
+    animation: Option<&PropertyAnimation>,
     init: &mut Vec<TokenStream>,
 ) {
     let rust_property = access_member(item_rc, prop_name, component, quote!(_self), false);
@@ -207,6 +208,7 @@ fn handle_property_binding(
                 prop_name,
                 next,
                 is_constant || next.is_constant(),
+                animation,
                 init,
             )
         }
@@ -228,7 +230,7 @@ fn handle_property_binding(
             if is_state_info {
                 quote! { sixtyfps::re_exports::set_state_binding(#rust_property, #binding_tokens); }
             } else {
-                match item_rc.borrow().property_animations.get(prop_name) {
+                match animation {
                     Some(crate::object_tree::PropertyAnimation::Static(anim)) => {
                         let anim = property_animation_tokens(component, anim);
                         quote! { #rust_property.set_animated_binding(#binding_tokens, #anim); }
@@ -604,6 +606,7 @@ fn generate_component(
             prop,
             binding,
             binding.analysis.borrow().as_ref().map_or(false, |a| a.is_const),
+            binding.animation.as_ref(),
             &mut init,
         )
     });
@@ -959,7 +962,7 @@ fn property_set_value_tokens(
     property_name: &str,
     value_tokens: TokenStream,
 ) -> TokenStream {
-    match element.borrow().property_animations.get(property_name) {
+    match element.borrow().bindings.get(property_name).and_then(|b| b.animation.as_ref()) {
         Some(crate::object_tree::PropertyAnimation::Static(animation)) => {
             let animation_tokens = property_animation_tokens(component, animation);
             quote!(set_animated_value(#value_tokens, #animation_tokens))

sixtyfps_compiler/passes/ensure_window.rs
@@ -40,7 +40,6 @@ pub fn ensure_window(component: &Rc<Component>, type_register: &TypeRegister) {
         enclosing_component: win_elem_mut.enclosing_component.clone(),
         property_declarations: Default::default(),
         named_references: Default::default(),
-        property_animations: Default::default(),
         repeated: Default::default(),
         states: Default::default(),
         transitions: Default::default(),
@@ -68,7 +67,6 @@ pub fn ensure_window(component: &Rc<Component>, type_register: &TypeRegister) {
     let mut base_props: HashSet<String> =
         new_root.borrow().base_type.property_list().into_iter().map(|x| x.0).collect();
     base_props.extend(win_elem.borrow().bindings.keys().cloned());
-    base_props.extend(win_elem.borrow().property_animations.keys().cloned());
     for prop in base_props {
         if prop == "width" || prop == "height" {
             continue;
@@ -82,9 +80,6 @@ pub fn ensure_window(component: &Rc<Component>, type_register: &TypeRegister) {
 
         if let Some(b) = win_elem.borrow_mut().bindings.remove(&prop) {
             new_root.borrow_mut().bindings.insert(prop.clone(), b);
-        }
-        if let Some(a) = win_elem.borrow_mut().property_animations.remove(&prop) {
-            new_root.borrow_mut().property_animations.insert(prop.clone(), a);
         }
         if let Some(a) = win_elem.borrow().property_analysis.borrow_mut().remove(&prop) {
             new_root.borrow().property_analysis.borrow_mut().insert(prop.clone(), a);

sixtyfps_compiler/passes/flickable.rs
@@ -74,10 +74,6 @@ fn create_viewport_element(flickable_elem: &ElementRc, native_rect: &Rc<NativeCl
                     entry.insert(Expression::TwoWayBinding(nr, None).into());
                 }
             }
-            // Workaround for https://github.com/sixtyfpsui/sixtyfps/issues/193
-            if let Some(a) = flickable.property_animations.remove(prop) {
-                viewport.borrow_mut().property_animations.insert(vp_prop.into(), a);
-            }
         }
     }
     flickable.children.push(viewport);

commit f544e67fa1fb6939669e38982e2380d4eafbf453

Fix panic or invalid code caused by invalid expression in the tree
When there is an animaiton but no expression, the binding is
left with an invalid expression.
State was keeping that invalid expression as part of sub expression
this is a regression since commit ba32777cab9e58659601b58c89d66b525969b347

sixtyfps_compiler/expression_tree.rs
@@ -1001,11 +1001,10 @@ impl Expression {
             | Type::ElementReference
             | Type::LayoutCache => Expression::Invalid,
             Type::Float32 => Expression::NumberLiteral(0., Unit::None),
-            Type::Int32 => Expression::NumberLiteral(0., Unit::None),
             Type::String => Expression::StringLiteral(String::new()),
-            Type::Color => Expression::Cast {
+            Type::Int32 | Type::Color | Type::UnitProduct(_) => Expression::Cast {
                 from: Box::new(Expression::NumberLiteral(0., Unit::None)),
-                to: Type::Color,
+                to: ty.clone(),
             },
             Type::Duration => Expression::NumberLiteral(0., Unit::Ms),
             Type::Angle => Expression::NumberLiteral(0., Unit::Deg),
@@ -1035,10 +1034,6 @@ impl Expression {
             Type::Enumeration(enumeration) => {
                 Expression::EnumerationValue(enumeration.clone().default_value())
             }
-            Type::UnitProduct(_) => Expression::Cast {
-                from: Box::new(Expression::NumberLiteral(0., Unit::None)),
-                to: ty.clone(),
-            },
         }
     }
 

sixtyfps_compiler/object_tree.rs
@@ -448,7 +448,7 @@ pub fn pretty_print(
         write!(f, "{}: ", name)?;
         expression_tree::pretty_print(f, &expr.expression)?;
         if expr.analysis.borrow().as_ref().map_or(false, |a| a.is_const) {
-            writeln!(f, "/*const*/")?;
+            write!(f, "/*const*/")?;
         }
         writeln!(f, ";")?;
         //writeln!(f, "; /*{}*/", expr.priority)?;

sixtyfps_compiler/passes/lower_states.rs
@@ -175,7 +175,9 @@ fn expression_for_property(element: &ElementRc, name: &str) -> Expression {
     let mut element_it = Some(element.clone());
     while let Some(element) = element_it {
         if let Some(e) = element.borrow().bindings.get(name) {
-            return e.expression.clone();
+            if !matches!(e.expression, Expression::Invalid) {
+                return e.expression.clone();
+            }
         }
         element_it = if let Type::Component(base) = &element.borrow().base_type {
             Some(base.root_element.clone())

tests/cases/properties/states_with_animation.60
@@ -1,0 +1,117 @@
+/* LICENSE BEGIN
+    This file is part of the SixtyFPS Project -- https://sixtyfps.io
+    Copyright (c) 2021 Olivier Goffart <olivier.goffart@sixtyfps.io>
+    Copyright (c) 2021 Simon Hausmann <simon.hausmann@sixtyfps.io>
+
+    SPDX-License-Identifier: GPL-3.0-only
+    This file is also available under commercial licensing terms.
+    Please contact info@sixtyfps.io for more information.
+LICENSE END */
+ TestCase := Rectangle {
+    property<int> active_index: 0;
+    property<int> some_prop: 5;
+    text1 := Text {
+        property<int> foo; // this one is not initialized
+        animate foo { duration: 100ms; }
+    }
+
+    states [
+        xxx when active_index == 1 : {
+            text1.foo: 3000;
+            some_prop: 2000;
+        }
+    ]
+
+    animate some_prop { duration: 100ms; }
+
+
+    property<int> text1_foo: text1.foo;
+
+}
+
+
+/*
+
+```rust
+let instance = TestCase::new();
+assert_eq!(instance.get_text1_foo(), 0);
+assert_eq!(instance.get_some_prop(), 5);
+instance.set_active_index(1);
+assert_eq!(instance.get_text1_foo(), 0);
+assert_eq!(instance.get_some_prop(), 5);
+sixtyfps::testing::mock_elapsed_time(75); // 75% the animation
+assert!(instance.get_some_prop() > 1500);
+assert!(instance.get_some_prop() < 1999);
+assert!(instance.get_text1_foo() > 2000);
+assert!(instance.get_text1_foo() < 2999);
+
+sixtyfps::testing::mock_elapsed_time(30); // more than 100% the animation
+assert_eq!(instance.get_text1_foo(), 3000);
+assert_eq!(instance.get_some_prop(), 2000);
+
+instance.set_active_index(2);
+assert_eq!(instance.get_text1_foo(), 3000);
+assert_eq!(instance.get_some_prop(), 2000);
+
+sixtyfps::testing::mock_elapsed_time(100);
+assert_eq!(instance.get_text1_foo(), 0);
+assert_eq!(instance.get_some_prop(), 5);
+
+```
+
+
+```cpp
+auto handle = TestCase::create();
+const TestCase &instance = *handle;
+assert_eq(instance.get_text1_foo(), 0);
+assert_eq(instance.get_some_prop(), 5);
+instance.set_active_index(1);
+assert_eq(instance.get_text1_foo(), 0);
+assert_eq(instance.get_some_prop(), 5);
+sixtyfps::testing::mock_elapsed_time(75); // 75% the animation
+assert(instance.get_some_prop() > 1500);
+assert(instance.get_some_prop() < 1999);
+assert(instance.get_text1_foo() > 2000);
+assert(instance.get_text1_foo() < 2999);
+
+sixtyfps::testing::mock_elapsed_time(30); // more than 100% the animation
+assert_eq(instance.get_text1_foo(), 3000);
+assert_eq(instance.get_some_prop(), 2000);
+
+instance.set_active_index(2);
+assert_eq(instance.get_text1_foo(), 3000);
+assert_eq(instance.get_some_prop(), 2000);
+
+sixtyfps::testing::mock_elapsed_time(100);
+assert_eq(instance.get_text1_foo(), 0);
+assert_eq(instance.get_some_prop(), 5);
+```
+
+
+```js
+var instance = new sixtyfps.TestCase({});
+assert.equal(instance.text1_foo, 0);
+assert.equal(instance.some_prop, 5);
+instance.active_index = 1;
+assert.equal(instance.text1_foo, 0);
+assert.equal(instance.some_prop, 5);
+sixtyfpslib.private_api.mock_elapsed_time(75); // 75% the animation
+assert(instance.some_prop > 1500);
+assert(instance.some_prop < 1999);
+assert(instance.text1_foo > 2000);
+assert(instance.text1_foo < 2999);
+
+sixtyfpslib.private_api.mock_elapsed_time(30); // more than 100% the animation
+assert.equal(instance.text1_foo, 3000);
+assert.equal(instance.some_prop, 2000);
+
+instance.active_index = 2;
+assert.equal(instance.text1_foo, 3000);
+assert.equal(instance.some_prop, 2000);
+
+sixtyfpslib.private_api.mock_elapsed_time(100);
+assert.equal(instance.text1_foo, 0);
+assert.equal(instance.some_prop, 5);
+```
+
+*/