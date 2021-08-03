slint-ui/slint

commit 064c39d625395b9ac74961e4ded7606cdbfe16ba

Fix disappearing elements with implicit width/height
When an element gets its width and height from the parent through an implicit 100% binding,
those bindings were missing when an animation was pre-defined.

The provided new-type wrapper offers a function to deal with replacing just
binding expression, instead of the
expression *and* the animation.

Fixes #376

sixtyfps_compiler/object_tree.rs
@@ -23,7 +23,9 @@ use crate::parser::{identifier_text, syntax_nodes, SyntaxKind, SyntaxNode};
 use crate::typeloader::ImportedTypes;
 use crate::typeregister::TypeRegister;
 use std::cell::RefCell;
+use std::collections::btree_map::Entry;
 use std::collections::{BTreeMap, HashMap};
+use std::iter::FromIterator;
 use std::rc::{Rc, Weak};
 
 macro_rules! unwrap_or_continue {
@@ -354,6 +356,59 @@ impl Clone for PropertyAnimation {
     }
 }
 
+// Wrapper around BindingsMap to offer an animation-preserving "insert" variant.
+#[derive(Debug, Clone, Default, derive_more::Deref, derive_more::DerefMut)]
+pub struct BindingsMap(BTreeMap<String, BindingExpression>);
+
+impl BindingsMap {
+    pub fn set_binding_if_not_set(&mut self, property_name: String, expression: BindingExpression) {
+        match self.0.entry(property_name) {
+            Entry::Vacant(vacant_entry) => {
+                vacant_entry.insert(expression);
+            }
+            Entry::Occupied(mut existing_entry) => {
+                existing_entry.get_mut().merge_with(&expression);
+            }
+        };
+    }
+}
+
+impl IntoIterator for BindingsMap {
+    type Item = (String, BindingExpression);
+
+    type IntoIter = std::collections::btree_map::IntoIter<String, BindingExpression>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+impl<'a> IntoIterator for &'a BindingsMap {
+    type Item = (&'a String, &'a BindingExpression);
+
+    type IntoIter = std::collections::btree_map::Iter<'a, String, BindingExpression>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        (&self.0).into_iter()
+    }
+}
+
+impl<'a> IntoIterator for &'a mut BindingsMap {
+    type Item = (&'a String, &'a mut BindingExpression);
+
+    type IntoIter = std::collections::btree_map::IterMut<'a, String, BindingExpression>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.iter_mut()
+    }
+}
+
+impl FromIterator<(String, BindingExpression)> for BindingsMap {
+    fn from_iter<T: IntoIterator<Item = (String, BindingExpression)>>(iter: T) -> Self {
+        Self(BTreeMap::from_iter(iter))
+    }
+}
+
 /// An Element is an instantiation of a Component
 #[derive(Default)]
 pub struct Element {
@@ -366,7 +421,7 @@ pub struct Element {
     //pub base: QualifiedTypeName,
     pub base_type: crate::langtype::Type,
     /// Currently contains also the callbacks. FIXME: should that be changed?
-    pub bindings: BTreeMap<String, BindingExpression>,
+    pub bindings: BindingsMap,
     pub property_analysis: RefCell<HashMap<String, PropertyAnalysis>>,
 
     pub children: Vec<ElementRc>,

sixtyfps_compiler/passes/apply_default_properties_from_style.rs
@@ -37,22 +37,24 @@ pub async fn apply_default_properties_from_style(
             let mut elem = elem.borrow_mut();
             match elem.base_type.to_string().as_str() {
                 "TextInput" => {
-                    elem.bindings.entry("text_cursor_width".into()).or_insert_with(|| {
+                    elem.bindings.set_binding_if_not_set(
+                        "text_cursor_width".into(),
                         Expression::PropertyReference(NamedReference::new(
                             &style_metrics.root_element,
                             "text_cursor_width",
                         ))
-                        .into()
-                    });
+                        .into(),
+                    );
                 }
                 "Window" => {
-                    elem.bindings.entry("background".into()).or_insert_with(|| {
+                    elem.bindings.set_binding_if_not_set(
+                        "background".into(),
                         Expression::PropertyReference(NamedReference::new(
                             &style_metrics.root_element,
                             "window_background",
                         ))
-                        .into()
-                    });
+                        .into(),
+                    );
                 }
 
                 _ => {}

sixtyfps_compiler/passes/default_geometry.rs
@@ -92,19 +92,17 @@ pub fn default_geometry(root_component: &Rc<Component>, diag: &mut BuildDiagnost
                                     property_type: image_fit_prop_type,
                                 } = elem.borrow().lookup_property("image_fit");
 
-                                elem.borrow_mut()
-                                    .bindings
-                                    .entry(image_fit_prop_name.into())
-                                    .or_insert_with(|| {
-                                        Expression::EnumerationValue(
-                                            image_fit_prop_type
-                                                .as_enum()
-                                                .clone()
-                                                .try_value_from_string("contain")
-                                                .unwrap(),
-                                        )
-                                        .into()
-                                    });
+                                elem.borrow_mut().bindings.set_binding_if_not_set(
+                                    image_fit_prop_name.into(),
+                                    Expression::EnumerationValue(
+                                        image_fit_prop_type
+                                            .as_enum()
+                                            .clone()
+                                            .try_value_from_string("contain")
+                                            .unwrap(),
+                                    )
+                                    .into(),
+                                );
                             }
                         }
                     }
@@ -205,20 +203,22 @@ fn make_default_100(elem: &ElementRc, parent_element: &ElementRc, property: &str
     if property_type != Type::LogicalLength {
         return;
     }
-    elem.borrow_mut().bindings.entry(resolved_name.to_string()).or_insert_with(|| {
+    elem.borrow_mut().bindings.set_binding_if_not_set(
+        resolved_name.to_string(),
         Expression::PropertyReference(NamedReference::new(parent_element, resolved_name.as_ref()))
-            .into()
-    });
+            .into(),
+    );
 }
 
 fn make_default_implicit(elem: &ElementRc, property: &str, orientation: Orientation) {
-    elem.borrow_mut().bindings.entry(property.into()).or_insert_with(|| {
+    elem.borrow_mut().bindings.set_binding_if_not_set(
+        property.into(),
         Expression::StructFieldAccess {
             base: implicit_layout_info_call(elem, orientation).into(),
             name: "preferred".into(),
         }
-        .into()
-    });
+        .into(),
+    );
 }
 
 // For an element with `width`, `height`, `preferred-width` and `preferred-height`, make an aspect

sixtyfps_compiler/expression_tree.rs
@@ -13,7 +13,7 @@ use crate::layout::Orientation;
 use crate::object_tree::*;
 use crate::parser::{NodeOrToken, SyntaxNode};
 use core::cell::RefCell;
-use std::collections::{BTreeMap, HashMap};
+use std::collections::HashMap;
 use std::rc::{Rc, Weak};
 
 // FIXME remove the pub
@@ -1177,7 +1177,7 @@ pub enum Path {
 #[derive(Debug, Clone)]
 pub struct PathElement {
     pub element_type: Rc<BuiltinElement>,
-    pub bindings: BTreeMap<String, BindingExpression>,
+    pub bindings: BindingsMap,
 }
 
 #[derive(Clone, Debug)]

sixtyfps_compiler/passes/move_declarations.rs
@@ -54,7 +54,7 @@ pub fn move_declarations(component: &Rc<Component>, diag: &mut BuildDiagnostics)
 
         // take the bindings so we do nt keep the borrow_mut of the element
         let bindings = core::mem::take(&mut elem.borrow_mut().bindings);
-        let mut new_bindings = BTreeMap::new();
+        let mut new_bindings = BindingsMap::default();
         for (k, e) in bindings {
             let will_be_moved = elem.borrow().property_declarations.contains_key(&k);
             if will_be_moved {

tests/cases/bindings/animated_default_geometry.60
@@ -1,0 +1,43 @@
+/* LICENSE BEGIN
+    This file is part of the SixtyFPS Project -- https://sixtyfps.io
+    Copyright (c) 2021 Olivier Goffart <olivier.goffart@sixtyfps.io>
+    Copyright (c) 2021 Simon Hausmann <simon.hausmann@sixtyfps.io>
+
+    SPDX-License-Identifier: GPL-3.0-only
+    This file is also available under commercial licensing terms.
+    Please contact info@sixtyfps.io for more information.
+LICENSE END */
+
+
+TestCase := Rectangle {
+
+    width: 100px;
+    height: 200px;
+
+    r := Rectangle {
+        // default geometry for width & height
+        animate height { duration: 1s; }
+        animate width { duration: 1s; }
+    }
+
+    property<bool> test: r.width == 100px && r.height == 200px;
+}
+
+/*
+```rust
+let instance = TestCase::new();
+assert!(instance.get_test());
+```
+
+```cpp
+auto handle = TestCase::create();
+const TestCase &instance = *handle;
+assert(instance.get_test());
+```
+
+```js
+let instance = new sixtyfps.TestCase({});
+assert(instance.test, 1);
+```
+
+*/

commit 210169bd263b56c764fcd08797399cbaee95d5e7

Fix regression of two-way binding on default geom'ed properties causing binding loop
Commit 064c39d625395b9ac74961e4ded7606cdbfe16ba introduced the regression that
if a two-way binding was set on a property that we'd also set a default geometry
on, we'd end up applying that on the two-way binding, causing a binding loop.

set_binding_if_not_set needs to only set the binding if... there's really none yet.

Fixes #385

sixtyfps_compiler/object_tree.rs
@@ -361,14 +361,21 @@ impl Clone for PropertyAnimation {
 pub struct BindingsMap(BTreeMap<String, BindingExpression>);
 
 impl BindingsMap {
-    pub fn set_binding_if_not_set(&mut self, property_name: String, expression: BindingExpression) {
+    pub fn set_binding_if_not_set(
+        &mut self,
+        property_name: String,
+        expression_fn: impl FnOnce() -> BindingExpression,
+    ) {
         match self.0.entry(property_name) {
             Entry::Vacant(vacant_entry) => {
-                vacant_entry.insert(expression);
+                vacant_entry.insert(expression_fn());
             }
-            Entry::Occupied(mut existing_entry) => {
-                existing_entry.get_mut().merge_with(&expression);
+            Entry::Occupied(mut existing_entry)
+                if matches!(existing_entry.get().expression, Expression::Invalid) =>
+            {
+                existing_entry.get_mut().merge_with(&expression_fn());
             }
+            _ => {}
         };
     }
 }

sixtyfps_compiler/passes/apply_default_properties_from_style.rs
@@ -37,24 +37,22 @@ pub async fn apply_default_properties_from_style(
             let mut elem = elem.borrow_mut();
             match elem.base_type.to_string().as_str() {
                 "TextInput" => {
-                    elem.bindings.set_binding_if_not_set(
-                        "text_cursor_width".into(),
+                    elem.bindings.set_binding_if_not_set("text_cursor_width".into(), || {
                         Expression::PropertyReference(NamedReference::new(
                             &style_metrics.root_element,
                             "text_cursor_width",
                         ))
-                        .into(),
-                    );
+                        .into()
+                    });
                 }
                 "Window" => {
-                    elem.bindings.set_binding_if_not_set(
-                        "background".into(),
+                    elem.bindings.set_binding_if_not_set("background".into(), || {
                         Expression::PropertyReference(NamedReference::new(
                             &style_metrics.root_element,
                             "window_background",
                         ))
-                        .into(),
-                    );
+                        .into()
+                    });
                 }
 
                 _ => {}

sixtyfps_compiler/passes/default_geometry.rs
@@ -94,14 +94,16 @@ pub fn default_geometry(root_component: &Rc<Component>, diag: &mut BuildDiagnost
 
                                 elem.borrow_mut().bindings.set_binding_if_not_set(
                                     image_fit_prop_name.into(),
-                                    Expression::EnumerationValue(
-                                        image_fit_prop_type
-                                            .as_enum()
-                                            .clone()
-                                            .try_value_from_string("contain")
-                                            .unwrap(),
-                                    )
-                                    .into(),
+                                    || {
+                                        Expression::EnumerationValue(
+                                            image_fit_prop_type
+                                                .as_enum()
+                                                .clone()
+                                                .try_value_from_string("contain")
+                                                .unwrap(),
+                                        )
+                                        .into()
+                                    },
                                 );
                             }
                         }
@@ -203,22 +205,20 @@ fn make_default_100(elem: &ElementRc, parent_element: &ElementRc, property: &str
     if property_type != Type::LogicalLength {
         return;
     }
-    elem.borrow_mut().bindings.set_binding_if_not_set(
-        resolved_name.to_string(),
+    elem.borrow_mut().bindings.set_binding_if_not_set(resolved_name.to_string(), || {
         Expression::PropertyReference(NamedReference::new(parent_element, resolved_name.as_ref()))
-            .into(),
-    );
+            .into()
+    });
 }
 
 fn make_default_implicit(elem: &ElementRc, property: &str, orientation: Orientation) {
-    elem.borrow_mut().bindings.set_binding_if_not_set(
-        property.into(),
+    elem.borrow_mut().bindings.set_binding_if_not_set(property.into(), || {
         Expression::StructFieldAccess {
             base: implicit_layout_info_call(elem, orientation).into(),
             name: "preferred".into(),
         }
-        .into(),
-    );
+        .into()
+    });
 }
 
 // For an element with `width`, `height`, `preferred-width` and `preferred-height`, make an aspect

tests/cases/bindings/issue_385_default_geom.60
@@ -1,0 +1,41 @@
+/* LICENSE BEGIN
+    This file is part of the SixtyFPS Project -- https://sixtyfps.io
+    Copyright (c) 2021 Olivier Goffart <olivier.goffart@sixtyfps.io>
+    Copyright (c) 2021 Simon Hausmann <simon.hausmann@sixtyfps.io>
+
+    SPDX-License-Identifier: GPL-3.0-only
+    This file is also available under commercial licensing terms.
+    Please contact info@sixtyfps.io for more information.
+LICENSE END */
+
+
+TestCase := Rectangle {
+    width: 100px;
+    height: 200px;
+
+    r := Rectangle {
+        width <=> root.width;
+        height <=> root.height;
+    }
+
+    property<bool> test: r.width == 100px && r.height == 200px;
+}
+
+/*
+```rust
+let instance = TestCase::new();
+assert!(instance.get_test());
+```
+
+```cpp
+auto handle = TestCase::create();
+const TestCase &instance = *handle;
+assert(instance.get_test());
+```
+
+```js
+let instance = new sixtyfps.TestCase({});
+assert(instance.test, 1);
+```
+
+*/