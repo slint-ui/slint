01e2a14322c7a54c69345414c8ef57f34f2a5e56
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-08-12 16:32:05 +0200

    Remove unused properties from globals
    The fluent style has a bunch of unused global color properties, which can be removed - for example.

--- sixtyfps_compiler/passes/remove_unused_properties.rs
+++ sixtyfps_compiler/passes/remove_unused_properties.rs
@@ -13,23 +13,31 @@ use crate::object_tree::Component;
 use std::collections::HashSet;
 
 pub fn remove_unused_properties(component: &Component) {
-    crate::object_tree::recurse_elem_including_sub_components_no_borrow(
-        component,
-        &(),
-        &mut |elem, _| {
-            let mut to_remove = HashSet::new();
-            for (prop, decl) in &elem.borrow().property_declarations {
-                if !decl.expose_in_public_api && !elem.borrow().named_references.is_referenced(prop)
-                {
-                    to_remove.insert(prop.to_owned());
+    fn recurse_remove_unused_properties(component: &Component) {
+        crate::object_tree::recurse_elem_including_sub_components_no_borrow(
+            component,
+            &(),
+            &mut |elem, _| {
+                let mut to_remove = HashSet::new();
+                for (prop, decl) in &elem.borrow().property_declarations {
+                    if !decl.expose_in_public_api
+                        && !elem.borrow().named_references.is_referenced(prop)
+                    {
+                        to_remove.insert(prop.to_owned());
+                    }
                 }
-            }
-            let mut elem = elem.borrow_mut();
-            for x in &to_remove {
-                elem.property_declarations.remove(x);
-                elem.property_analysis.borrow_mut().remove(x);
-                elem.bindings.remove(x);
-            }
-        },
-    )
+                let mut elem = elem.borrow_mut();
+                for x in &to_remove {
+                    elem.property_declarations.remove(x);
+                    elem.property_analysis.borrow_mut().remove(x);
+                    elem.bindings.remove(x);
+                }
+            },
+        );
+
+        for global in &component.used_types.borrow().globals {
+            recurse_remove_unused_properties(global);
+        }
+    }
+    recurse_remove_unused_properties(component)
 }
