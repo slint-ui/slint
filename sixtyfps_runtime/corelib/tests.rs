f50ba4a40fb3efa023ac66a3a7e09a696b764dda
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-08-30 18:36:07 +0200

    Fix doc build with nightly
    The lack of docs for the debug_log macro produces a warning now (and
    an error for us). Add some basic docs as remedy.

--- sixtyfps_runtime/corelib/tests.rs
+++ sixtyfps_runtime/corelib/tests.rs
@@ -102,6 +102,8 @@ cfg_if::cfg_if! {
             ($($t:tt)*) => ($crate::tests::log(&format_args!($($t)*).to_string()))
         }
     } else {
+        /// This macro allows producing debug output that will appear on stderr in regular builds
+        /// and in the console log for wasm builds.
         #[macro_export]
         macro_rules! debug_log {
             ($($t:tt)*) => (eprintln!($($t)*))
