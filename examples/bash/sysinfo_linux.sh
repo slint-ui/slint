aef4b024b46a13b639edbbec828f6bedb73bc299
Olivier Goffart <ogoffart@sixtyfps.io>
2021-10-01 19:23:31 +0200

    sysinfo_linux.sh: WIP to show the partitions

--- examples/bash/sysinfo_linux.sh
+++ examples/bash/sysinfo_linux.sh
@@ -13,12 +13,14 @@ cpu_count=`grep processor /proc/cpuinfo | wc -l`
 cpu_vendor=`awk -F ": " '/vendor_id/{ print $2; exit}' < /proc/cpuinfo`
 cpu_model=`awk -F ": " '/model name/{ print $2; exit}' < /proc/cpuinfo`
 mem_size_kb=`sed -n -e "s,MemTotal:\s\+\(.*\)\s\+.\+,\1,p"< /proc/meminfo`
+partitions=`df --block-size=1 | tail -n+2 | sed 's/\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)/{ "dev": "\1", "mnt": "\6", "total": \2, "used": \3 },/' | sed '$s/,$//'`
 
 sixtyfps-viewer `dirname $0`/sysinfo.60 --load-data - <<EOT
 {
     "cpu_count": "$cpu_count",
     "cpu_vendor": "$cpu_vendor",
     "cpu_model": "$cpu_model",
-    "mem_size_kb": $mem_size_kb
+    "mem_size_kb": $mem_size_kb,
+    "partitions": [ $partitions ]
 }
 EOT
