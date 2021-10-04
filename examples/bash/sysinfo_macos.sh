9b5e05e41b8126c0f8ba7b6bf720a0531c5a7669
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-10-04 16:15:16 +0200

    Provide a macOS version of the sysinfo bash script

---
+++ examples/bash/sysinfo_macos.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+# LICENSE BEGIN
+# This file is part of the SixtyFPS Project -- https://sixtyfps.io
+# Copyright (c) 2021 Olivier Goffart <olivier.goffart@sixtyfps.io>
+# Copyright (c) 2021 Simon Hausmann <simon.hausmann@sixtyfps.io>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+# This file is also available under commercial licensing terms.
+# Please contact info@sixtyfps.io for more information.
+# LICENSE END
+
+cpu_count=`sysctl -n hw.ncpu`
+cpu_vendor=`sysctl -n machdep.cpu.vendor`
+cpu_model=`sysctl -n machdep.cpu.brand_string`
+mem_size_kb=`sysctl -n hw.memsize`
+partitions=`df -lk | tail -n+2 | sed 's/\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)/{ "dev": "\1", "mnt": "\9", "total": \2, "used": \3 },/' | sed '$s/,$//'`
+
+sixtyfps-viewer `dirname $0`/sysinfo.60 --load-data - <<EOT
+{
+    "cpu_count": "$cpu_count",
+    "cpu_vendor": "$cpu_vendor",
+    "cpu_model": "$cpu_model",
+    "mem_size_kb": $mem_size_kb,
+    "partitions": [ $partitions ]
+}
+EOT
