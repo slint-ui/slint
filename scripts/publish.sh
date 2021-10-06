a1069e94f0c7d136e56c5218d7b98bc6af2ec1d8
Olivier Goffart <ogoffart@sixtyfps.io>
2021-10-06 15:53:53 +0200

    Fixup the publish script
    the GL bakcend don't compile alone without specifying a feature

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -12,7 +12,7 @@ cargo publish --manifest-path sixtyfps_runtime/corelib_macros/Cargo.toml
 cargo publish --manifest-path sixtyfps_compiler/Cargo.toml
 cargo publish --manifest-path sixtyfps_runtime/corelib/Cargo.toml
 cargo publish --manifest-path api/sixtyfps-rs/sixtyfps-macros/Cargo.toml
-cargo publish --manifest-path sixtyfps_runtime/rendering_backends/gl/Cargo.toml
+cargo publish --manifest-path sixtyfps_runtime/rendering_backends/gl/Cargo.toml --features x11
 cargo publish --manifest-path api/sixtyfps-rs/sixtyfps-build/Cargo.toml
 cargo publish --manifest-path sixtyfps_runtime/rendering_backends/qt/Cargo.toml
 sleep 30
