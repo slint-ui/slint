5098c915e6eeb587abeb7ad7fbfc8286acaff0fb
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-10-07 14:54:07 +0200

    Split out the memory into a separate tab
    and add some more information about memory and swap

--- examples/bash/sysinfo_linux.sh
+++ examples/bash/sysinfo_linux.sh
@@ -21,6 +21,10 @@ cpu_vendor=`awk -F ": " '/vendor_id/{ print $2; exit}' < /proc/cpuinfo | tr -d '
 cpu_model=`awk -F ": " '/model name/{ print $2; exit}' < /proc/cpuinfo | tr -d '"\\\\'`
 mem_size_kb=`sed -n -e "s,MemTotal:\s\+\(.*\)\s\+.\+,\1,p"< /proc/meminfo`
 partitions=`df -T --block-size=1 | tail -n+2 | awk 'NR > 1 { printf(", ") } {printf "{ \"dev\": \"%s\", \"mnt\": \"%s\", \"total\": %s, \"free\": %s }", $1,$7, $3, $5}'`
+buffer_mem_size_kb=`sed -n -e "s,Buffers:\s\+\(.*\)\s\+.\+,\1,p"< /proc/meminfo`
+swap_total_kb=`sed -n -e "s,SwapTotal:\s\+\(.*\)\s\+.\+,\1,p"< /proc/meminfo`
+swap_free_kb=`sed -n -e "s,SwapFree:\s\+\(.*\)\s\+.\+,\1,p"< /proc/meminfo`
+swap_used_kb=$(swap_total_kb - swap_free_kb))
 
 sixtyfps-viewer `dirname $0`/sysinfo.60 --load-data - <<EOT
 {
@@ -30,6 +34,10 @@ sixtyfps-viewer `dirname $0`/sysinfo.60 --load-data - <<EOT
     "cpu_vendor": "$cpu_vendor",
     "cpu_model": "$cpu_model",
     "mem_size_kb": $mem_size_kb,
+    "buffer_mem_size_kb": $buffer_mem_size_kb,
+    "swap_total_kb": $swap_total_kb,
+    "swap_used_kb": $swap_used_kb,
+    "swap_free_kb": $swap_free_kb,
     "partitions": [ $partitions ]
 }
 EOT
