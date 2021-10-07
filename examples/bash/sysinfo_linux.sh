19200fefacee6737449b53701b7cd6d86a1ecbff
Olivier Goffart <ogoffart@sixtyfps.io>
2021-10-07 10:45:14 +0200

    sysinfo script: escape strings

--- examples/bash/sysinfo_linux.sh
+++ examples/bash/sysinfo_linux.sh
@@ -10,15 +10,15 @@
 # LICENSE END
 
 if command lsb_release 2>/dev/null >/dev/null; then
-   os_name=`lsb_release -ds`
+   os_name=`lsb_release -ds | tr -d '"\\\\'`
 else
    os_name="(unable to determine Linux distribution)"
 fi
 
-uptime=`uptime -p | cut -d " " -f2-`
+uptime=`uptime -p | cut -d " " -f2-` | tr -d '"\\\\'
 cpu_count=`grep processor /proc/cpuinfo | wc -l`
-cpu_vendor=`awk -F ": " '/vendor_id/{ print $2; exit}' < /proc/cpuinfo`
-cpu_model=`awk -F ": " '/model name/{ print $2; exit}' < /proc/cpuinfo`
+cpu_vendor=`awk -F ": " '/vendor_id/{ print $2; exit}' < /proc/cpuinfo | tr -d '"\\\\'`
+cpu_model=`awk -F ": " '/model name/{ print $2; exit}' < /proc/cpuinfo | tr -d '"\\\\'`
 mem_size_kb=`sed -n -e "s,MemTotal:\s\+\(.*\)\s\+.\+,\1,p"< /proc/meminfo`
 partitions=`df --block-size=1 | tail -n+2 | sed 's/\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)/{ "dev": "\1", "mnt": "\6", "total": \2, "used": \3 },/' | sed '$s/,$//'`
 
