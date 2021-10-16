a04cf37b699b8845267637f8cc7a98a3d9e664e0
James Blacklock <jamesblacklock@protonmail.com>
2021-10-16 03:35:11 -0700

    remove unnecessary binding

--- sixtyfps_compiler/generator/rust.rs
+++ sixtyfps_compiler/generator/rust.rs
@@ -1303,7 +1303,7 @@ fn compile_expression(expr: &Expression, component: &Rc<Component>) -> TokenStre
             _ => panic!("Expression::ObjectAccess's base expression is not an Object type"),
         },
         Expression::ArrayIndex { array, index } => match array.ty() {
-            Type::Array(ty) => {
+            Type::Array(_) => {
                 let base_e = compile_expression(array, component);
                 let index_e = compile_expression(index, component);
                 quote!((#base_e).row_data(#index_e))
