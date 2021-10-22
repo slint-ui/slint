675e4bb8506f156268c9035f07ee37cc1acfa781
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-10-22 11:55:56 +0200

    Explain what the script does and centralize index.md

--- scripts/prepare_binary_package.sh
+++ scripts/prepare_binary_package.sh
@@ -12,6 +12,12 @@
 if [ $# != 3 ]; then
     echo "usage: $0 path/to/target/binary_package path/to/binary/Qt qt_version"
     echo
+    echo "This prepares the specified binary_package folder for distribution"
+    echo "by adding the legal copyright and license notices."
+    echo
+    echo "All files will be copied/created under the 3rdparty-licenses folder"
+    echo "along with an index.md"
+    echo
     echo "(The path to Qt could be for example ~/Qt/ where the qt installer placed"
     echo " the binaries and sources under)"
     exit 1
@@ -24,6 +30,16 @@ qt_version=$3
 mkdir -p $target_path
 cp -a `dirname $0`/../LICENSE.md $target_path
 
+cat >$target_path/index.md <<EOT
+This program is distributed under the terms outlined in [LICENSE.md]().
+
+This program also uses the Qt library, which is licensed under the
+LGPL v3: [qt/LICENSE.LGPLv3]().
+
+Qt may include additional third-party components:
+
+EOT
+
 mkdir -p $target_path/qt
 attribution_files=(
     $qt_path/Docs/Qt-$qt_version/qtcore/*-attribution-*.html
@@ -34,15 +50,10 @@ attribution_files=(
 
 cp $qt_path/$qt_version/Src/LICENSE.LGPLv3 $target_path/qt
 
-cat >$target_path/qt/index.md <<EOT
-[LICENSE.LGPLv3](LICENSE.LGPLv3)
-
-EOT
-
 for file in ${attribution_files[@]}; do
     cp $file $target_path/qt/
     title=`sed -n -e "s,<title>\(.*\)</title>,\1,p" < $file`
     link=`basename $file`
-    echo "[$title]($link)" >> $target_path/qt/index.md
+    echo "[$title]($link)" >> $target_path/index.md
     echo "" >> $target_path/qt/index.md
 done
