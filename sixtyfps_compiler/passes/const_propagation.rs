de43f0a783e88f4f28d64ab2cf4e452e050dc3f5
Olivier Goffart <ogoffart@sixtyfps.io>
2021-11-10 20:01:35 +0100

    Start doing some constant evaluation
    There was a warning in the C++ generated code in the tests:  (in test_cpp_image_geometry)
    
    ```
    /tmp/.tmpsAMWqZ.cpp:307:85: error: self-comparison always evaluates to true [-Werror,-Wtautological-compare]
        this->image_in_layout_fit_ok.set((sixtyfps::cbindgen_private::ImageFit::contain == sixtyfps::cbindgen_private::ImageFit::contain));
                                                                                        ^
    ```
    
    Deoing some simple evaluation prevent this warning at least

--- sixtyfps_compiler/passes/const_propagation.rs
+++ sixtyfps_compiler/passes/const_propagation.rs
@@ -35,6 +35,79 @@ fn simplify_expression(expr: &mut Expression) -> bool {
             }
             false
         }
+        Expression::BinaryExpression { lhs, op, rhs } => {
+            let mut can_inline = simplify_expression(lhs);
+            can_inline &= simplify_expression(rhs);
+
+            let new = match (*op, &mut **lhs, &mut **rhs) {
+                ('+', Expression::StringLiteral(a), Expression::StringLiteral(b)) => {
+                    Some(Expression::StringLiteral(format!("{}{}", a, b)))
+                }
+                ('+', Expression::NumberLiteral(a, un1), Expression::NumberLiteral(b, un2))
+                    if un1 == un2 =>
+                {
+                    Some(Expression::NumberLiteral(*a + *b, *un1))
+                }
+                ('-', Expression::NumberLiteral(a, un1), Expression::NumberLiteral(b, un2))
+                    if un1 == un2 =>
+                {
+                    Some(Expression::NumberLiteral(*a - *b, *un1))
+                }
+                // TODO: * and / are more complicated because they need to take care about units
+                ('=' | '!', Expression::NumberLiteral(a, _), Expression::NumberLiteral(b, _)) => {
+                    Some(Expression::BoolLiteral((a == b) == (*op == '=')))
+                }
+                ('=' | '!', Expression::StringLiteral(a), Expression::StringLiteral(b)) => {
+                    Some(Expression::BoolLiteral((a == b) == (*op == '=')))
+                }
+                ('=' | '!', Expression::EnumerationValue(a), Expression::EnumerationValue(b)) => {
+                    Some(Expression::BoolLiteral((a == b) == (*op == '=')))
+                }
+                // TODO: more types and more comparison operators
+                ('&', Expression::BoolLiteral(false), _) => {
+                    can_inline = true;
+                    Some(Expression::BoolLiteral(false))
+                }
+                ('&', _, Expression::BoolLiteral(false)) => {
+                    can_inline = true;
+                    Some(Expression::BoolLiteral(false))
+                }
+                ('&', Expression::BoolLiteral(true), e) => Some(std::mem::take(e)),
+                ('&', e, Expression::BoolLiteral(true)) => Some(std::mem::take(e)),
+                ('|', Expression::BoolLiteral(true), _) => {
+                    can_inline = true;
+                    Some(Expression::BoolLiteral(true))
+                }
+                ('|', _, Expression::BoolLiteral(true)) => {
+                    can_inline = true;
+                    Some(Expression::BoolLiteral(true))
+                }
+                ('|', Expression::BoolLiteral(false), e) => Some(std::mem::take(e)),
+                ('|', e, Expression::BoolLiteral(false)) => Some(std::mem::take(e)),
+                _ => None,
+            };
+            if let Some(new) = new {
+                *expr = new;
+            }
+            can_inline
+        }
+        Expression::Cast { from, to } => {
+            let can_inline = simplify_expression(from);
+            let new = if from.ty() == *to {
+                Some(std::mem::take(&mut **from))
+            } else {
+                match (&**from, to) {
+                    (Expression::NumberLiteral(x, Unit::None), Type::String) => {
+                        Some(Expression::StringLiteral((*x).to_string()))
+                    }
+                    _ => None,
+                }
+            };
+            if let Some(new) = new {
+                *expr = new;
+            }
+            can_inline
+        }
         Expression::CallbackReference { .. } => false,
         Expression::ElementReference { .. } => false,
         // FIXME
