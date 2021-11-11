7eaaf45ba48249050f3cb3a99d9813738a0f7357
Simon Hausmann <simon.hausmann@sixtyfps.io>
2021-11-11 11:08:05 +0100

    Make it possible to opt out of the default rendering backend selection
    The default backend does not select any features or backends by default
    on the Cargo.toml feature level. And with this change it also doesn't do
    that anymore based on the target platform.
    
    That means if the rust api crate is built with default-features = false,
    no backend will be selected and the user has to take care of backend
    initialization through other means.
    
    The feature defaults, generally, are in the rust API crate, the
    interpreter crate and C++ CMakeLists.txt. The latter triggers a cargo
    build of sixtyfps-cpp with --no-default-features. However the cpp_test
    job in the CI doesn't build sixtyfps-cpp with CMake but instead calls
    cargo directly. Therefore the defaults are now also coded in the
    sixtyfps-cpp Cargo.toml, to ensure that *a* rendering backend is chosen
    (since the cfg.target bit is gone).

--- sixtyfps_runtime/rendering_backends/default/lib.rs
+++ sixtyfps_runtime/rendering_backends/default/lib.rs
@@ -27,57 +27,68 @@ In order for the crate to be available at runtime, they need to be added as feat
 #![doc(html_logo_url = "https://sixtyfps.io/resources/logo.drawio.svg")]
 
 cfg_if::cfg_if! {
-    if #[cfg(any(target_os="windows", target_os="macos", target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd", target_os = "netbsd"))] {
+    if #[cfg(feature = "sixtyfps-rendering-backend-qt")] {
         use sixtyfps_rendering_backend_qt as default_backend;
-    } else {
+    } else if #[cfg(feature = "sixtyfps-rendering-backend-gl")] {
         use sixtyfps_rendering_backend_gl as default_backend;
     }
 }
 
-pub fn backend() -> &'static dyn sixtyfps_corelib::backend::Backend {
-    sixtyfps_corelib::backend::instance_or_init(|| {
-        #[cfg(any(
+cfg_if::cfg_if! {
+    if #[cfg(any(
             feature = "sixtyfps-rendering-backend-qt",
             feature = "sixtyfps-rendering-backend-gl"
-        ))]
-        let backend_config = std::env::var("SIXTYFPS_BACKEND").unwrap_or_default();
+        ))] {
+        pub fn backend() -> &'static dyn sixtyfps_corelib::backend::Backend {
+            sixtyfps_corelib::backend::instance_or_init(|| {
+                let backend_config = std::env::var("SIXTYFPS_BACKEND").unwrap_or_default();
 
-        #[cfg(feature = "sixtyfps-rendering-backend-qt")]
-        if backend_config == "Qt" {
-            return Box::new(sixtyfps_rendering_backend_qt::Backend);
+                #[cfg(feature = "sixtyfps-rendering-backend-qt")]
+                if backend_config == "Qt" {
+                    return Box::new(sixtyfps_rendering_backend_qt::Backend);
+                }
+                #[cfg(feature = "sixtyfps-rendering-backend-gl")]
+                if backend_config == "GL" {
+                    return Box::new(sixtyfps_rendering_backend_gl::Backend);
+                }
+
+                #[cfg(any(
+                    feature = "sixtyfps-rendering-backend-qt",
+                    feature = "sixtyfps-rendering-backend-gl"
+                ))]
+                if !backend_config.is_empty() {
+                    eprintln!("Could not load rendering backend {}, fallback to default", backend_config)
+                }
+
+                #[cfg(feature = "sixtyfps-rendering-backend-gl")]
+                if !default_backend::IS_AVAILABLE {
+                    // If Qt is not available always fallback to Gl
+                    return Box::new(sixtyfps_rendering_backend_gl::Backend);
+                }
+
+                Box::new(default_backend::Backend)
+            })
         }
-        #[cfg(feature = "sixtyfps-rendering-backend-gl")]
-        if backend_config == "GL" {
-            return Box::new(sixtyfps_rendering_backend_gl::Backend);
+        pub use default_backend::{
+            native_widgets, Backend, NativeGlobals, NativeWidgets, HAS_NATIVE_STYLE,
+        };
+    } else {
+        pub fn backend() -> &'static dyn sixtyfps_corelib::backend::Backend {
+            sixtyfps_corelib::backend::instance().expect("no default backend configured, the backend must be initialized manually")
         }
 
-        #[cfg(any(
-            feature = "sixtyfps-rendering-backend-qt",
-            feature = "sixtyfps-rendering-backend-gl"
-        ))]
-        if !backend_config.is_empty() {
-            eprintln!("Could not load rendering backend {}, fallback to default", backend_config)
-        }
-
-        #[cfg(feature = "sixtyfps-rendering-backend-gl")]
-        if !default_backend::IS_AVAILABLE {
-            // If Qt is not available always fallback to Gl
-            return Box::new(sixtyfps_rendering_backend_gl::Backend);
-        }
-        Box::new(default_backend::Backend)
-    })
+        pub type NativeWidgets = ();
+        pub type NativeGlobals = ();
+        pub mod native_widgets {}
+        pub const HAS_NATIVE_STYLE: bool = false;
+    }
 }
-
-pub use default_backend::{
-    native_widgets, Backend, NativeGlobals, NativeWidgets, HAS_NATIVE_STYLE,
-};
 
 #[doc(hidden)]
 #[cold]
 #[cfg(not(target_arch = "wasm32"))]
 pub fn use_modules() {
     sixtyfps_corelib::use_modules();
-    default_backend::use_modules();
     #[cfg(feature = "sixtyfps-rendering-backend-qt")]
     sixtyfps_rendering_backend_qt::use_modules();
     #[cfg(feature = "sixtyfps-rendering-backend-gl")]
