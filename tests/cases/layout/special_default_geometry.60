slint-ui/slint

commit 684a8c29a299971b9fff18f550d44e2204f9d417

Fix opacity property not affecting the shadow color
The opacity on a rectangle should be applied before the shadow, so that it has
a visible effect.

This is a partial fix for #714 but needs also #725 to be entirely visually correct.

sixtyfps_compiler/passes.rs
@@ -105,15 +105,18 @@ pub async fn run_passes(
         lower_popups::lower_popups(component, &doc.local_registry, diag);
         lower_layout::lower_layouts(component, &mut type_loader, diag).await;
         z_order::reorder_by_z_order(component, diag);
-        lower_shadows::lower_shadow_properties(component, &doc.local_registry, diag);
-        clip::handle_clip(component, &global_type_registry.borrow(), diag);
         transform_and_opacity::handle_transform_and_opacity(
             component,
             &global_type_registry.borrow(),
             diag,
         );
-        default_geometry::default_geometry(component, diag);
+        lower_shadows::lower_shadow_properties(component, &doc.local_registry, diag);
+        clip::handle_clip(component, &global_type_registry.borrow(), diag);
         visible::handle_visible(component, &global_type_registry.borrow());
+        // Apply the default geometry after any synthetic elements may have been injected as root in repeaters (opacity, box shadow, etc.).
+        // The geometry bindings are moved to the new injected roots, but the previous elements rely on the default geometry pass to size
+        // them correctly relative to the new root.
+        default_geometry::default_geometry(component, diag);
         materialize_fake_properties::materialize_fake_properties(component);
     }
     collect_globals::collect_globals(&doc, diag);

commit 0b5648c2f857d06c97370d96436bdc23d9583470

Fix default_geometry and visibility combination
Regression since 684a8c29a299971b9fff18f550d44e2204f9d417

(This fixes the issue 107 on the cw. gitlab)

sixtyfps_compiler/passes.rs
@@ -117,11 +117,12 @@ pub async fn run_passes(
         );
         lower_shadows::lower_shadow_properties(component, &doc.local_registry, diag);
         clip::handle_clip(component, &global_type_registry.borrow(), diag);
-        visible::handle_visible(component, &global_type_registry.borrow());
-        // Apply the default geometry after any synthetic elements may have been injected as root in repeaters (opacity, box shadow, etc.).
-        // The geometry bindings are moved to the new injected roots, but the previous elements rely on the default geometry pass to size
-        // them correctly relative to the new root.
+        // Apply the default geometry after any synthetic elements may have been injected
+        // as root in repeaters(opacity, box shadow, etc.).
+        // The geometry bindings are moved to the new injected roots, but the previous elements
+        // rely on the default geometry pass to size them correctly relative to the new root.
         default_geometry::default_geometry(component, diag);
+        visible::handle_visible(component, &global_type_registry.borrow());
         materialize_fake_properties::materialize_fake_properties(component);
     }
     collect_globals::collect_globals(&doc, diag);

tests/cases/layout/special_default_geometry.60
@@ -1,0 +1,69 @@
+// Copyright © SixtyFPS GmbH <info@sixtyfps.io>
+// SPDX-License-Identifier: (GPL-3.0-only OR LicenseRef-SixtyFPS-commercial)
+
+// Test that the default geometry is taken from the right parent, even when some
+//
+
+export TestCase := Window {
+    property <bool> condition: true;
+    Rectangle {
+        width: 10px;
+        height: 20px;
+
+        invisible := Rectangle {
+            visible: condition;
+            background: blue;
+        }
+
+        opaque := Rectangle {
+            opacity: 0.5;
+            background: red;
+        }
+
+        clipped := Rectangle {
+            clip: condition;
+            background: yellow;
+        }
+
+        shadowed := Rectangle {
+            drop-shadow-color: #00000054;
+            drop-shadow-blur: 8px;
+            background: pink;
+        }
+
+        all := Rectangle {
+            visible: condition;
+            clip: condition;
+            opacity: 0.2;
+            drop-shadow-color: #00000054;
+            drop-shadow-blur: 8px;
+            background: orange;
+        }
+    }
+
+    property <bool> test:
+        invisible.width == 10px && invisible.height == 20px &&
+        opaque.width == 10px && opaque.height == 20px &&
+        clipped.width == 10px && clipped.height == 20px &&
+        shadowed.width == 10px && shadowed.height == 20px &&
+        all.width == 10px && all.height == 20px;
+}
+
+/*
+```cpp
+auto handle = TestCase::create();
+const TestCase &instance = *handle;
+assert(instance.get_test());
+```
+
+
+```rust
+let instance = TestCase::new();
+assert!(instance.get_test());
+```
+
+```js
+var instance = new sixtyfps.TestCase({});
+assert(instance.test);
+```
+*/
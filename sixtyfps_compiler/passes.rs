0b5648c2f857d06c97370d96436bdc23d9583470
Olivier Goffart <ogoffart@sixtyfps.io>
2021-12-28 18:05:02 +0100

    Fix default_geometry and visibility combination
    Regression since 684a8c29a299971b9fff18f550d44e2204f9d417
    
    (This fixes the issue 107 on the cw. gitlab)

--- sixtyfps_compiler/passes.rs
+++ sixtyfps_compiler/passes.rs
@@ -117,11 +117,12 @@ pub async fn run_passes(
         );
         lower_shadows::lower_shadow_properties(component, &doc.local_registry, diag);
         clip::handle_clip(component, &global_type_registry.borrow(), diag);
-        visible::handle_visible(component, &global_type_registry.borrow());
-        // Apply the default geometry after any synthetic elements may have been injected as root in repeaters (opacity, box shadow, etc.).
-        // The geometry bindings are moved to the new injected roots, but the previous elements rely on the default geometry pass to size
-        // them correctly relative to the new root.
+        // Apply the default geometry after any synthetic elements may have been injected
+        // as root in repeaters(opacity, box shadow, etc.).
+        // The geometry bindings are moved to the new injected roots, but the previous elements
+        // rely on the default geometry pass to size them correctly relative to the new root.
         default_geometry::default_geometry(component, diag);
+        visible::handle_visible(component, &global_type_registry.borrow());
         materialize_fake_properties::materialize_fake_properties(component);
     }
     collect_globals::collect_globals(&doc, diag);
