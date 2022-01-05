0df394a389e16a4a03c762b80c479107634e60e5
Simon Hausmann <simon.hausmann@sixtyfps.io>
2022-01-05 20:42:28 +0100

    Make it faster to develop the online editor
    Switch from webpack to vite. The dev mode is practically instant.

--- tools/online_editor/index.ts
+++ tools/online_editor/index.ts
@@ -2,9 +2,36 @@
 // SPDX-License-Identifier: (GPL-3.0-only OR LicenseRef-SixtyFPS-commercial)
 
 import { sixtyfps_language } from "./highlighting";
-import type * as monaco from 'monaco-editor';
+
+import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+
+self.MonacoEnvironment = {
+    getWorker(_, label) {
+        if (label === 'json') {
+            return new jsonWorker();
+        }
+        if (label === 'css' || label === 'scss' || label === 'less') {
+            return new cssWorker();
+        }
+        if (label === 'html' || label === 'handlebars' || label === 'razor') {
+            return new htmlWorker();
+        }
+        if (label === 'typescript' || label === 'javascript') {
+            return new tsWorker();
+        }
+        return new editorWorker();
+    }
+};
+
+import sixtyfps_init, * as sixtyfps from "../../api/sixtyfps-wasm-interpreter/pkg/sixtyfps_wasm_interpreter.js";
+
 (async function () {
-    let [monaco, sixtyfps] = await Promise.all([import('monaco-editor'), import("../../api/sixtyfps-wasm-interpreter/pkg/index.js")]);
+    let [monaco, _] = await Promise.all([import('monaco-editor'), sixtyfps_init()]);
 
     monaco.languages.register({
         id: 'sixtyfps'
