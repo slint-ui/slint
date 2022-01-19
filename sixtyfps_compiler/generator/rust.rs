besok/sixtyfps

Make it possible to use a string binding for Path commands
This was requested a few days ago in the chat and might also present an alternative for #754

sixtyfps_compiler/generator/rust.rs
@@ -2517,6 +2517,10 @@ fn compile_path(path: &Path, component: &Rc<Component>) -> TokenStream {
             let events = compile_path_events(events);
             quote!(sixtyfps::re_exports::PathData::Events(#events))
         }
+        Path::Commands(commands) => {
+            let commands_expr = compile_expression(commands, component);
+            quote!(sixtyfps::re_exports::PathData::Commands(#commands_expr))
+        }
     }
 }
 

slint-ui/slint

sixtyfps_compiler/generator/rust.rs
@@ -1335,6 +1335,9 @@ fn compile_expression(expr: &Expression, ctx: &EvaluationContext) -> TokenStream
                     };
                     quote!(sixtyfps::re_exports::PathData::Events(sixtyfps::re_exports::SharedVector::<_>::from_slice(&#events), sixtyfps::re_exports::SharedVector::<_>::from_slice(&#points)))
                 }
+                (Type::String, Type::PathData) => {
+                    quote!(sixtyfps::re_exports::PathData::Commands(#f))
+                }
                 _ => f,
             }
         }