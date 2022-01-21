007d992ea9ac53e9ef2baea081a6cbd068aeda90
Olivier Goffart <ogoffart@sixtyfps.io>
2022-01-21 10:51:54 +0100

    Fix the publish script:
     - build corelib_macros between common and the compilerlib so that the
       crates.io cache has time to propagate
     - add a feature to the default backend otherwise it doesn't compile

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,8 +2,8 @@
 # Copyright © SixtyFPS GmbH <info@sixtyfps.io>
 # SPDX-License-Identifier: (GPL-3.0-only OR LicenseRef-SixtyFPS-commercial)
 
-cargo publish --manifest-path sixtyfps_runtime/corelib_macros/Cargo.toml
 cargo publish --manifest-path sixtyfps_runtime/common/Cargo.toml
+cargo publish --manifest-path sixtyfps_runtime/corelib_macros/Cargo.toml
 cargo publish --manifest-path sixtyfps_compiler/Cargo.toml
 cargo publish --manifest-path sixtyfps_runtime/corelib/Cargo.toml
 cargo publish --manifest-path api/sixtyfps-rs/sixtyfps-macros/Cargo.toml
@@ -11,7 +11,7 @@ cargo publish --manifest-path sixtyfps_runtime/rendering_backends/gl/Cargo.toml 
 cargo publish --manifest-path api/sixtyfps-rs/sixtyfps-build/Cargo.toml
 cargo publish --manifest-path sixtyfps_runtime/rendering_backends/qt/Cargo.toml
 sleep 30
-cargo publish --manifest-path sixtyfps_runtime/rendering_backends/default/Cargo.toml
+cargo publish --manifest-path sixtyfps_runtime/rendering_backends/default/Cargo.toml --features x11
 sleep 30
 cargo publish --manifest-path sixtyfps_runtime/interpreter/Cargo.toml
 cargo publish --manifest-path api/sixtyfps-rs/Cargo.toml
