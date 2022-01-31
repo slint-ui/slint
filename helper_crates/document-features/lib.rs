d22b00ca33c478e1bd9f37f4abe5c0bde82c4e5d
Olivier Goffart <ogoffart@sixtyfps.io>
2022-01-31 15:03:11 +0100

    document-features: Change licenses and add descriptions and keywords

--- helper_crates/document-features/lib.rs
+++ helper_crates/document-features/lib.rs
@@ -1,5 +1,5 @@
 // Copyright © SixtyFPS GmbH <info@sixtyfps.io>
-// SPDX-License-Identifier: (GPL-3.0-only OR LicenseRef-SixtyFPS-commercial)
+// SPDX-License-Identifier: MIT OR Apache-2.0
 
 /*!
 Document your crate's feature flags.
