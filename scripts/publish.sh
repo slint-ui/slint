e6b24bceece14b7e15443bd2ebc774cf92d09c6a
Tobias Hunger <tobias.hunger@sixtyfps.io>
2022-01-31 11:45:55 +0100

    [reorg]: Set up and populate the `internal` directory
    Move "internal" crates into the `internal` directory. This first batch
    includes most of sixtyfps_runtime but leaves the rendering backends
    alone for now.
    
    pre-commit applied some cleanups to the moved files:
     - Consistent newline at end of file policy
     - trimming trailing whitespace
     - Formatting Cargo.toml files.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,10 +2,10 @@
 # Copyright © SixtyFPS GmbH <info@sixtyfps.io>
 # SPDX-License-Identifier: (GPL-3.0-only OR LicenseRef-SixtyFPS-commercial)
 
-cargo publish --manifest-path sixtyfps_runtime/common/Cargo.toml
-cargo publish --manifest-path sixtyfps_runtime/corelib_macros/Cargo.toml
-cargo publish --manifest-path sixtyfps_compiler/Cargo.toml
-cargo publish --manifest-path sixtyfps_runtime/corelib/Cargo.toml
+cargo publish --manifest-path internal/common/Cargo.toml
+cargo publish --manifest-path internal/core-macros/Cargo.toml
+cargo publish --manifest-path internal/compiler/Cargo.toml
+cargo publish --manifest-path internal/core/Cargo.toml
 cargo publish --manifest-path api/sixtyfps-rs/sixtyfps-macros/Cargo.toml
 cargo publish --manifest-path sixtyfps_runtime/rendering_backends/gl/Cargo.toml --features x11
 cargo publish --manifest-path api/sixtyfps-rs/sixtyfps-build/Cargo.toml
@@ -13,7 +13,7 @@ cargo publish --manifest-path sixtyfps_runtime/rendering_backends/qt/Cargo.toml
 sleep 30
 cargo publish --manifest-path sixtyfps_runtime/rendering_backends/default/Cargo.toml --features x11
 sleep 30
-cargo publish --manifest-path sixtyfps_runtime/interpreter/Cargo.toml
+cargo publish --manifest-path internal/interpreter/Cargo.toml
 cargo publish --manifest-path api/sixtyfps-rs/Cargo.toml
 cargo publish --manifest-path tools/lsp/Cargo.toml
 cargo publish --manifest-path tools/viewer/Cargo.toml
