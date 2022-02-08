90d67be1a4083e62145069c365a0bb7da1a098f9
Simon Hausmann <simon.hausmann@slint-ui.com>
2022-02-08 13:20:34 +0100

    Document the feature renaming in the migration guides

--- internal/interpreter/migration.rs
+++ internal/interpreter/migration.rs
@@ -10,6 +10,16 @@
 * `WeakComponentInstance` was removed. Use `slint::Weak<slint::interpreter::ComponentInstance>` instead.
   You might need to `use slint::ComponentHandle;` in your code to bring the trait into scope.
 
+### Crate features
+
+Some crate features have been renamed:
+
+| Old Feature Name                    | New Feature Name                   | Note                                                                          |
+| ------------------------------------| ---------------------------------- | ----------------------------------------------------------------------------- |
+| `backend-gl` | `backend-gl-all`     | Enable this feature if you want to use the OpenGL ES 2.0 rendering backend with support for all windowing systems. |
+| `x11`        | `backend-gl-x11`     | Enable this feature and switch off `backend-gl-all` if you want a smaller build with just X11 support.             |
+| `wayland`    | `backend-gl-wayland` | Enable this feature and switch off `backend-gl-all` if you want a smaller build with just wayland support.         |
+
 */
 
 use crate::*;
