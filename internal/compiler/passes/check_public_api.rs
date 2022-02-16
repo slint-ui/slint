a90c53b9bd4caad5440c42205a391346b1d1cc03
Olivier Goffart <olivier.goffart@slint-ui.com>
2022-02-16 17:09:12 +0100

    Mark public properties as set
    Since they can be set from the native code
    
    The current code in NamedReference::is_constant and such always check for
    expose_in_public_api already, this is now redundent

--- internal/compiler/passes/check_public_api.rs
+++ internal/compiler/passes/check_public_api.rs
@@ -23,9 +23,13 @@ pub fn check_public_api(doc: &Document, diag: &mut BuildDiagnostics) {
 }
 
 fn check_public_api_component(root_component: &Rc<Component>, diag: &mut BuildDiagnostics) {
-    root_component.root_element.borrow_mut().property_declarations.values_mut().for_each(|d| {
+    let mut root_elem = root_component.root_element.borrow_mut();
+    let root_elem = &mut *root_elem;
+    let mut pa = root_elem.property_analysis.borrow_mut();
+    root_elem.property_declarations.iter_mut().for_each(|(n, d)| {
         if d.property_type.ok_for_public_api() {
-            d.expose_in_public_api = true
+            d.expose_in_public_api = true;
+            pa.entry(n.to_string()).or_default().is_set = true;
         } else {
             diag.push_diagnostic(
                  format!("Properties of type {} are not supported yet for public API. The property will not be exposed", d.property_type),
