3af7b6f323ec3ea9e9b38276410516ae868f9186
Simon Hausmann <simon.hausmann@slint-ui.com>
2022-02-16 16:54:44 +0100

    Fix simulator build

--- internal/backends/mcu/simulator.rs
+++ internal/backends/mcu/simulator.rs
@@ -181,10 +181,12 @@ impl PlatformWindow for SimulatorWindow {
         text: &str,
         max_width: Option<f32>,
     ) -> i_slint_core::graphics::Size {
+        let runtime_window = self.self_weak.upgrade().unwrap();
         crate::fonts::text_size(
             font_request.merge(&self.self_weak.upgrade().unwrap().default_font_properties()),
             text,
             max_width,
+            crate::renderer::ScaleFactor(runtime_window.scale_factor()),
         )
     }
 
