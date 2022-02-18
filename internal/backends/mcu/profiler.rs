373f57bb917fa41175fd85a6e4a61d9f5c079945
Simon Hausmann <simon.hausmann@slint-ui.com>
2022-02-18 17:27:36 +0100

    Prefix cfg for mcu performance debugging with slint

--- internal/backends/mcu/profiler.rs
+++ internal/backends/mcu/profiler.rs
@@ -4,9 +4,9 @@
 use crate::Devices;
 
 pub enum Timer {
-    #[cfg(debug_performance)]
+    #[cfg(slint_debug_performance)]
     Running { start_time: core::time::Duration },
-    #[cfg(debug_performance)]
+    #[cfg(slint_debug_performance)]
     Stopped { elapsed: core::time::Duration },
     #[cfg(not(debug_performance))]
     Noop,
@@ -14,31 +14,31 @@ pub enum Timer {
 
 impl Timer {
     pub fn new(_devices: &dyn Devices) -> Self {
-        #[cfg(debug_performance)]
+        #[cfg(slint_debug_performance)]
         return Self::Running { start_time: _devices.time() };
         #[cfg(not(debug_performance))]
         return Self::Noop;
     }
     pub fn new_stopped() -> Self {
-        #[cfg(debug_performance)]
+        #[cfg(slint_debug_performance)]
         return Self::Stopped { elapsed: core::time::Duration::new(0, 0) };
         #[cfg(not(debug_performance))]
         return Self::Noop;
     }
 
-    #[cfg(debug_performance)]
+    #[cfg(slint_debug_performance)]
     pub fn elapsed(&self, _devices: &dyn Devices) -> core::time::Duration {
         match self {
-            #[cfg(debug_performance)]
+            #[cfg(slint_debug_performance)]
             Self::Running { start_time } => _devices.time().saturating_sub(*start_time),
-            #[cfg(debug_performance)]
+            #[cfg(slint_debug_performance)]
             Self::Stopped { elapsed } => *elapsed,
         }
     }
 
     pub fn stop(&mut self, _devices: &dyn Devices) {
         match self {
-            #[cfg(debug_performance)]
+            #[cfg(slint_debug_performance)]
             Self::Running { .. } => {
                 *self = Timer::Stopped { elapsed: self.elapsed(_devices) };
             }
@@ -48,7 +48,7 @@ impl Timer {
 
     pub fn start(&mut self, _devices: &dyn Devices) {
         match self {
-            #[cfg(debug_performance)]
+            #[cfg(slint_debug_performance)]
             Self::Stopped { elapsed } => {
                 *self = Self::Running { start_time: _devices.time().saturating_sub(*elapsed) }
             }
@@ -57,7 +57,7 @@ impl Timer {
     }
 
     pub fn stop_profiling(self, _devices: &dyn Devices, _context: &'static str) {
-        #[cfg(debug_performance)]
+        #[cfg(slint_debug_performance)]
         i_slint_core::debug_log!("{} took: {}ms", _context, self.elapsed(_devices).as_millis())
     }
 }
