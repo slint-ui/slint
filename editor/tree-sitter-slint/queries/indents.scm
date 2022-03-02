01ad85aae80c2b863d4de6bd765ee0d7eb93c766
Jared Moulton <jaredmoulton3@gmail.com>
2022-03-02 14:02:01 -0700

    Integrate treesitter-slint code
    Remove .gitignore
    
    Remove old tests and update queries
    
    Update readme with Tree Sitter install info
    
    Add licences and move tree-sitter folder

---
+++ editor/tree-sitter-slint/queries/indents.scm
@@ -0,0 +1,15 @@
+; Copyright © SixtyFPS GmbH <info@slint-ui.com>
+; SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
+
+[
+(field_declaration_list_body) 
+(list_definition_body) 
+(struct_field_declaration_list_body)
+] @indent
+
+"}" @indent_end
+
+(comment) @auto
+
+(string) @auto
+
