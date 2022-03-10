slint-ui/slint

commit aee3ffca270596c6ff32c1444d9bfc025506be36

Add additional glyphs for embedding
Co-authored-by: Olivier Goffart <olivier.goffart@slint-ui.com>

internal/compiler/passes/embed_glyphs.rs
@@ -156,7 +156,7 @@ fn embed_font(family_name: String, font: fontdue::Font, scale_factor: f64) -> Bi
     let coverage = ('a'..='z')
         .chain('A'..='Z')
         .chain('0'..='9')
-        .chain([' ', '.', '+', '-', '!', '%', ':', '…']);
+        .chain(" !\"#$%&'()*+,-./:;<=>?@\\]^_|~".chars());
 
     let mut character_map: Vec<CharacterMapEntry> = coverage
         .enumerate()

commit 43759cd1bb932467197bc2036193bc182129c6b6

Fix elided text rendering on mcu
Commit aee3ffca270596c6ff32c1444d9bfc025506be36 accidentally removed the elision character

internal/compiler/passes/embed_glyphs.rs
@@ -156,7 +156,8 @@ fn embed_font(family_name: String, font: fontdue::Font, scale_factor: f64) -> Bi
     let coverage = ('a'..='z')
         .chain('A'..='Z')
         .chain('0'..='9')
-        .chain(" !\"#$%&'()*+,-./:;<=>?@\\]^_|~".chars());
+        .chain(" !\"#$%&'()*+,-./:;<=>?@\\]^_|~".chars())
+        .chain(std::iter::once('…'));
 
     let mut character_map: Vec<CharacterMapEntry> = coverage
         .enumerate()