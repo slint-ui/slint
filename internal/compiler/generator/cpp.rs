5d3f296af98dd3da31c0ac41e6da78f52742c1da
Tobias Hunger <tobias.hunger@slint-ui.com>
2022-03-11 10:31:37 +0100

    API: Change WindowRc::free_graphics_resources and init_items on C++
    Use the ItemArray in both cases.

--- internal/compiler/generator/cpp.rs
+++ internal/compiler/generator/cpp.rs
@@ -1022,7 +1022,7 @@ fn generate_item_tree(
     }
 
     create_code.extend([
-        format!("{}->m_window.window_handle().init_items(self, item_tree());", root_access),
+        format!("{}->m_window.window_handle().init_items(self, self->item_array());", root_access),
         format!("self->init({}, self->self_weak, 0, 1 {});", root_access, init_parent_parameters),
         format!("return slint::ComponentHandle<{0}>{{ self_rc }};", target_struct.name),
     ]);
@@ -1045,7 +1045,7 @@ fn generate_item_tree(
     let mut destructor = vec!["auto self = this;".to_owned()];
 
     destructor.push(format!(
-        "{}->m_window.window_handle().free_graphics_resources(self, item_tree());",
+        "{}->m_window.window_handle().free_graphics_resources(self, item_array());",
         root_access
     ));
 
