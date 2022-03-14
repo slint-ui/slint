858c327a8f23fa78fed9b061481d248ba75e34df
Lukas Jung <lukas.jung@slint-ui.com>
2022-03-14 09:59:10 +0100

    implement update and delete for crud example

--- examples/7guis/crud/main.rs
+++ examples/7guis/crud/main.rs
@@ -1,5 +1,6 @@
 // Copyright © SixtyFPS GmbH <info@slint-ui.com>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
+use slint::Model;
 use std::rc::Rc;
 
 slint::slint!(import { MainWindow } from "crud.slint";);
@@ -14,11 +15,39 @@ pub fn main() {
     ]));
     main_window.set_names_list(model.clone().into());
 
-    let main_window_weak = main_window.as_weak();
-    main_window.on_createClicked(move || {
-        let main_window = main_window_weak.unwrap();
-        let new_entry = main_window.get_surname() + ", " + main_window.get_name().as_str();
-        model.push(new_entry.into());
-    });
+    {
+        let main_window_weak = main_window.as_weak();
+        let model = model.clone();
+        main_window.on_createClicked(move || {
+            let main_window = main_window_weak.unwrap();
+            let new_entry = main_window.get_surname() + ", " + main_window.get_name().as_str();
+
+            model.push(new_entry.into());
+        });
+    }
+
+    {
+        let main_window_weak = main_window.as_weak();
+        let model = model.clone();
+        main_window.on_updateClicked(move || {
+            let main_window = main_window_weak.unwrap();
+            let index = main_window.get_current_item() as usize;
+            let entry = main_window.get_surname() + ", " + main_window.get_name().as_str();
+
+            model.set_row_data(index, entry.into());
+        });
+    }
+
+    {
+        let main_window_weak = main_window.as_weak();
+        let model = model.clone();
+        main_window.on_deleteClicked(move || {
+            let main_window = main_window_weak.unwrap();
+            let index = main_window.get_current_item() as usize;
+
+            model.remove(index);
+        });
+    }
+
     main_window.run();
 }
