be51ae0938908e4d16a31a493e220ed260e5597a
Olivier Goffart <olivier.goffart@slint-ui.com>
2022-03-29 18:04:07 +0200

    WIP: STM32H735G support

--- examples/printerdemo_mcu/main.rs
+++ examples/printerdemo_mcu/main.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
 
 #![no_std]
-#![cfg_attr(feature = "mcu-pico-st7789", no_main)]
+#![cfg_attr(not(feature = "mcu-simulator"), no_main)]
 
 extern crate alloc;
 
--- internal/backends/mcu/lib.rs
+++ internal/backends/mcu/lib.rs
@@ -4,7 +4,7 @@
 #![doc = include_str!("README.md")]
 #![doc(html_logo_url = "https://slint-ui.com/logo/slint-logo-square-light.svg")]
 #![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(feature = "pico-st7789", feature(alloc_error_handler))]
+#![cfg_attr(any(feature = "pico-st7789", feature = "stm32h735g"), feature(alloc_error_handler))]
 
 extern crate alloc;
 
@@ -425,7 +425,7 @@ pub fn init_with_display<Display: Devices + 'static>(display: Display) {
     });
 }
 
-#[cfg(not(any(feature = "pico-st7789", feature = "simulator")))]
+#[cfg(not(any(feature = "pico-st7789", feature = "stm32h735g", feature = "simulator")))]
 pub fn init() {
     struct EmptyDisplay;
     impl embedded_graphics::draw_target::DrawTarget for EmptyDisplay {
@@ -453,5 +453,11 @@ mod pico_st7789;
 #[cfg(feature = "pico-st7789")]
 pub use pico_st7789::*;
 
-#[cfg(not(feature = "pico-st7789"))]
+#[cfg(feature = "stm32h735g")]
+mod stm32h735g;
+
+#[cfg(feature = "stm32h735g")]
+pub use stm32h735g::*;
+
+#[cfg(not(any(feature = "pico-st7789", feature = "stm32h735g")))]
 pub use i_slint_core_macros::identity as entry;
