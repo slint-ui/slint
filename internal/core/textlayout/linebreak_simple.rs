2b93504b9321192eddda33abbed42f85a825d831
Simon Hausmann <simon.hausmann@slint-ui.com>
2022-04-14 11:30:33 +0200

    Fix MCU build

--- internal/core/textlayout/linebreak_simple.rs
+++ internal/core/textlayout/linebreak_simple.rs
@@ -1,6 +1,7 @@
 // Copyright © SixtyFPS GmbH <info@slint-ui.com>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
 
+#[derive(Copy, Clone)]
 pub enum BreakOpportunity {
     Allowed,
     Mandatory,
