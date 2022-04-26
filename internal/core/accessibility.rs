3822f1f51728470278c4baa36e35e8504ccb2c4b
Olivier Goffart <olivier.goffart@slint-ui.com>
2022-04-26 11:37:03 +0200

    accessibility: add component vtable function and implement them in the interpreter
    (untested)

---
+++ internal/core/accessibility.rs
@@ -0,0 +1,8 @@
+// Copyright © SixtyFPS GmbH <info@slint-ui.com>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
+
+#[repr(C)]
+#[derive(PartialEq, Eq, Copy, Clone, strum::Display)]
+pub enum AccessibleStringProperty {
+    Label,
+}
