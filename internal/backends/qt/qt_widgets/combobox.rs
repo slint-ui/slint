slint-ui/slint

commit 0c19751a1e0d3a8036affbb797724b77329ba9ec

Qt: do not wrap QPainter directly, but use a unique_ptr instead
Because QPainter can't be relocated.

Fixes #1230

internal/backends/qt/qt_widgets/combobox.rs
@@ -103,7 +103,7 @@ impl Item for NativeComboBox {
             this.current_value().as_str().into();
         let enabled = this.enabled();
         cpp!(unsafe [
-            painter as "QPainter*",
+            painter as "QPainterPtr*",
             widget as "QWidget*",
             text as "QString",
             enabled as "bool",
@@ -132,8 +132,8 @@ impl Item for NativeComboBox {
             //    option.state |= QStyle::State_On;
             }
             option.subControls = QStyle::SC_All;
-            qApp->style()->drawComplexControl(QStyle::CC_ComboBox, &option, painter, widget);
-            qApp->style()->drawControl(QStyle::CE_ComboBoxLabel, &option, painter, widget);
+            qApp->style()->drawComplexControl(QStyle::CC_ComboBox, &option, painter->get(), widget);
+            qApp->style()->drawControl(QStyle::CE_ComboBoxLabel, &option, painter->get(), widget);
         });
     }
 }

internal/backends/qt/qt_widgets.rs
@@ -17,6 +17,7 @@ it needs to be kept in sync with different place.
 
 #![allow(non_upper_case_globals)]
 
+use crate::qt_window::QPainterPtr;
 use const_field_offset::FieldOffsets;
 use core::pin::Pin;
 use cpp::cpp;
@@ -37,8 +38,6 @@ use i_slint_core_macros::*;
 use std::rc::Rc;
 
 type ItemRendererRef<'a> = &'a mut dyn ItemRenderer;
-
-use qttypes::QPainter;
 
 /// Helper macro to get the size from the width and height property,
 /// and return Default::default in case the size is too small
@@ -69,12 +68,12 @@ macro_rules! fn_render {
                 return (int)state;
             });
 
-            if let Some(painter) = <dyn std::any::Any>::downcast_mut::<QPainter>(backend.as_any()) {
+            if let Some(painter) = <dyn std::any::Any>::downcast_mut::<QPainterPtr>(backend.as_any()) {
                 let $size: qttypes::QSize = get_size!(self);
                 let $this = self;
                 painter.save();
-                let $widget = cpp!(unsafe [painter as "QPainter*"] -> * const () as "QWidget*" {
-                    return painter->device()->devType() == QInternal::Widget ? static_cast<QWidget *>(painter->device()) : nullptr;
+                let $widget = cpp!(unsafe [painter as "QPainterPtr*"] -> * const () as "QWidget*" {
+                    return (*painter)->device()->devType() == QInternal::Widget ? static_cast<QWidget *>((*painter)->device()) : nullptr;
                 });
                 let $painter = painter;
                 $($tt)*
@@ -92,12 +91,14 @@ macro_rules! fn_render {
                         let $size = qttypes::QSize { width: width as _, height: height as _ };
                         let mut imgarray = QImageWrapArray::new($size, $dpr);
                         let img = &mut imgarray.img;
-                        let mut painter_ = cpp!(unsafe [img as "QImage*"] -> QPainter as "QPainter" { return QPainter(img); });
+                        let mut painter = cpp!(unsafe [img as "QImage*"] -> QPainterPtr as "std::unique_ptr<QPainter>" {
+                            return std::make_unique<QPainter>(img);
+                        });
                         let $widget: * const () = core::ptr::null();
-                        let $painter = &mut painter_;
+                        let $painter = &mut painter;
                         let $this = self;
                         $($tt)*
-                        drop(painter_);
+                        drop(painter);
                         imgarray.draw(callback);
                     },
                 );
@@ -143,6 +144,8 @@ cpp! {{
     #include <QtCore/QMimeData>
     #include <QtCore/QDebug>
     #include <QtCore/QScopeGuard>
+
+    using QPainterPtr = std::unique_ptr<QPainter>;
 
     /// Make sure there is an instance of QApplication.
     /// The `from_qt_backend` argument specifies if we know that we are running

internal/backends/qt/qt_widgets/button.rs
@@ -298,7 +298,7 @@ impl Item for NativeButton {
         let has_focus = this.has_focus();
 
         cpp!(unsafe [
-            painter as "QPainter*",
+            painter as "QPainterPtr*",
             widget as "QWidget*",
             text as "QString",
             icon as "QPixmap",
@@ -328,7 +328,7 @@ impl Item for NativeButton {
             if (has_focus) {
                 option.state |= QStyle::State_HasFocus | QStyle::State_KeyboardFocusChange | QStyle::State_Item;
             }
-            qApp->style()->drawControl(QStyle::CE_PushButton, &option, painter, widget);
+            qApp->style()->drawControl(QStyle::CE_PushButton, &option, painter->get(), widget);
         });
     }
 }

internal/backends/qt/qt_widgets/checkbox.rs
@@ -109,7 +109,7 @@ impl Item for NativeCheckBox {
         let text: qttypes::QString = this.text().as_str().into();
 
         cpp!(unsafe [
-            painter as "QPainter*",
+            painter as "QPainterPtr*",
             widget as "QWidget*",
             enabled as "bool",
             text as "QString",
@@ -132,7 +132,7 @@ impl Item for NativeCheckBox {
             if (has_focus) {
                 option.state |= QStyle::State_HasFocus | QStyle::State_KeyboardFocusChange | QStyle::State_Item;
             }
-            qApp->style()->drawControl(QStyle::CE_CheckBox, &option, painter, widget);
+            qApp->style()->drawControl(QStyle::CE_CheckBox, &option, painter->get(), widget);
         });
     }
 }

internal/backends/qt/qt_widgets/groupbox.rs
@@ -162,7 +162,7 @@ impl Item for NativeGroupBox {
         let enabled = this.enabled();
 
         cpp!(unsafe [
-            painter as "QPainter*",
+            painter as "QPainterPtr*",
             widget as "QWidget*",
             text as "QString",
             enabled as "bool",
@@ -187,7 +187,7 @@ impl Item for NativeGroupBox {
             }
             option.textColor = QColor(qApp->style()->styleHint(
                 QStyle::SH_GroupBox_TextLabelColor, &option));
-            qApp->style()->drawComplexControl(QStyle::CC_GroupBox, &option, painter, widget);
+            qApp->style()->drawComplexControl(QStyle::CC_GroupBox, &option, painter->get(), widget);
         });
     }
 }

internal/backends/qt/qt_widgets/lineedit.rs
@@ -115,7 +115,7 @@ impl Item for NativeLineEdit {
         let enabled: bool = this.enabled();
 
         cpp!(unsafe [
-            painter as "QPainter*",
+            painter as "QPainterPtr*",
             widget as "QWidget*",
             size as "QSize",
             dpr as "float",
@@ -135,7 +135,7 @@ impl Item for NativeLineEdit {
             } else {
                 option.palette.setCurrentColorGroup(QPalette::Disabled);
             }
-            qApp->style()->drawPrimitive(QStyle::PE_PanelLineEdit, &option, painter, widget);
+            qApp->style()->drawPrimitive(QStyle::PE_PanelLineEdit, &option, painter->get(), widget);
         });
     }
 }

internal/backends/qt/qt_widgets/listviewitem.rs
@@ -90,7 +90,7 @@ impl Item for NativeStandardListViewItem {
         let item = this.item();
         let text: qttypes::QString = item.text.as_str().into();
         cpp!(unsafe [
-            painter as "QPainter*",
+            painter as "QPainterPtr*",
             widget as "QWidget*",
             size as "QSize",
             dpr as "float",
@@ -120,14 +120,14 @@ impl Item for NativeStandardListViewItem {
             option.features |= QStyleOptionViewItem::HasDisplay;
             option.text = text;
             // CE_ItemViewItem in QCommonStyle calls setClipRect on the painter and replace the clips. So we need to cheat.
-            auto engine = painter->paintEngine();
+            auto engine = (*painter)->paintEngine();
             auto old_clip = engine->systemClip();
-            auto new_clip = old_clip & (painter->clipRegion() * painter->transform());
+            auto new_clip = old_clip & ((*painter)->clipRegion() * (*painter)->transform());
             if (new_clip.isEmpty()) return;
             engine->setSystemClip(new_clip);
 
-            qApp->style()->drawPrimitive(QStyle::PE_PanelItemViewRow, &option, painter, widget);
-            qApp->style()->drawControl(QStyle::CE_ItemViewItem, &option, painter, widget);
+            qApp->style()->drawPrimitive(QStyle::PE_PanelItemViewRow, &option, painter->get(), widget);
+            qApp->style()->drawControl(QStyle::CE_ItemViewItem, &option, painter->get(), widget);
             engine->setSystemClip(old_clip);
         });
     }

internal/backends/qt/qt_widgets/scrollview.rs
@@ -272,7 +272,7 @@ impl Item for NativeScrollView {
         let enabled: bool = this.enabled();
         let has_focus: bool = this.has_focus();
         let frame_around_contents = cpp!(unsafe [
-            painter as "QPainter*",
+            painter as "QPainterPtr*",
             widget as "QWidget*",
             size as "QSize",
             dpr as "float",
@@ -303,13 +303,13 @@ impl Item for NativeScrollView {
             QSize corner_size = QSize(margins.right() - margins.left(), margins.bottom() - margins.top());
             if (foac) {
                 frameOption.rect = QRect(QPoint(), (size / dpr) - corner_size);
-                qApp->style()->drawControl(QStyle::CE_ShapedFrame, &frameOption, painter, widget);
+                qApp->style()->drawControl(QStyle::CE_ShapedFrame, &frameOption, painter->get(), widget);
                 frameOption.rect = QRect(frameOption.rect.bottomRight() + QPoint(1, 1), corner_size);
-                qApp->style()->drawPrimitive(QStyle::PE_PanelScrollAreaCorner, &frameOption, painter, widget);
+                qApp->style()->drawPrimitive(QStyle::PE_PanelScrollAreaCorner, &frameOption, painter->get(), widget);
             } else {
-                qApp->style()->drawControl(QStyle::CE_ShapedFrame, &frameOption, painter, widget);
+                qApp->style()->drawControl(QStyle::CE_ShapedFrame, &frameOption, painter->get(), widget);
                 frameOption.rect = QRect(frameOption.rect.bottomRight() + QPoint(1, 1) - QPoint(margins.right(), margins.bottom()), corner_size);
-                qApp->style()->drawPrimitive(QStyle::PE_PanelScrollAreaCorner, &frameOption, painter, widget);
+                qApp->style()->drawPrimitive(QStyle::PE_PanelScrollAreaCorner, &frameOption, painter->get(), widget);
             }
             return foac;
         });
@@ -323,7 +323,7 @@ impl Item for NativeScrollView {
                               pressed: bool,
                               initial_state: i32| {
             cpp!(unsafe [
-                painter as "QPainter*",
+                painter as "QPainterPtr*",
                 widget as "QWidget*",
                 value as "int",
                 page_size as "int",
@@ -336,6 +336,7 @@ impl Item for NativeScrollView {
                 has_focus as "bool",
                 initial_state as "int"
             ] {
+                QPainter *painter_ = painter->get();
                 auto r = rect.toAlignedRect();
                 // The mac style may crash on invalid rectangles (#595)
                 if (!r.isValid())
@@ -345,11 +346,11 @@ impl Item for NativeScrollView {
             #if defined(Q_OS_MAC)
                 QImage scrollbar_image(r.size(), QImage::Format_ARGB32_Premultiplied);
                 scrollbar_image.fill(Qt::transparent);
-                {QPainter p(&scrollbar_image); QPainter *painter = &p;
+                {QPainter p(&scrollbar_image); QPainter *painter_ = &p;
             #else
-                painter->save();
-                auto cleanup = qScopeGuard([&] { painter->restore(); });
-                painter->translate(r.topLeft()); // There is bugs in the styles if the scrollbar is not in (0,0)
+                painter_->save();
+                auto cleanup = qScopeGuard([&] { painter_->restore(); });
+                painter_->translate(r.topLeft()); // There is bugs in the styles if the scrollbar is not in (0,0)
             #endif
                 QStyleOptionSlider option;
                 option.state |= QStyle::State(initial_state);
@@ -366,10 +367,10 @@ impl Item for NativeScrollView {
                 }
 
                 auto style = qApp->style();
-                style->drawComplexControl(QStyle::CC_ScrollBar, &option, painter, widget);
+                style->drawComplexControl(QStyle::CC_ScrollBar, &option, painter_, widget);
             #if defined(Q_OS_MAC)
                 }
-                painter->drawImage(r.topLeft(), scrollbar_image);
+                (painter_)->drawImage(r.topLeft(), scrollbar_image);
             #endif
             });
         };

commit afed211674f266e9480abc1c63e4b456dfca63d0

Native style: Fix crash when opening a combobox
Bug introduced by https://github.com/slint-ui/slint/pull/1231

internal/backends/qt/qt_widgets/combobox.rs
@@ -197,7 +197,7 @@ impl Item for NativeComboBoxPopup {
 
     fn_render! { _this dpr size painter widget initial_state =>
         cpp!(unsafe [
-            painter as "QPainter*",
+            painter as "QPainterPtr*",
             widget as "QWidget*",
             size as "QSize",
             dpr as "float",
@@ -214,7 +214,7 @@ impl Item for NativeComboBoxPopup {
             auto style = qApp->style();
 
             if (style->styleHint(QStyle::SH_ComboBox_Popup, &option, widget)) {
-                style->drawPrimitive(QStyle::PE_PanelMenu, &option, painter, widget);
+                style->drawPrimitive(QStyle::PE_PanelMenu, &option, painter->get(), widget);
             } else {
                 option.lineWidth = 1;
             }
@@ -224,7 +224,7 @@ impl Item for NativeComboBoxPopup {
             else if ((frameStyle & QFrame::Shadow_Mask) == QFrame::Raised)
                 option.state |= QStyle::State_Raised;
             option.frameShape = QFrame::Shape(frameStyle & QFrame::Shape_Mask);
-            style->drawControl(QStyle::CE_ShapedFrame, &option, painter, widget);
+            style->drawControl(QStyle::CE_ShapedFrame, &option, painter->get(), widget);
         });
     }
 }