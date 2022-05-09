c160ec2aef6b963cb926d362dd920f0c06ced844
ogoffart <ogoffart@users.noreply.github.com>
2022-05-09 13:49:25 +0000

    Bump version number to 0.2.4

--- api/cpp/docs/conf.py
+++ api/cpp/docs/conf.py
@@ -26,7 +26,7 @@ copyright = "2022, info@slint-ui.com"
 author = "info@slint-ui.com"
 
 # The full version, including alpha/beta/rc tags
-version = "0.2.3"
+version = "0.2.4"
 
 cpp_index_common_prefix = ["slint::", "slint::interpreter::"]
 
