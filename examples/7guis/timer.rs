84e5c7e6e9ebf5e33b582b314c88f9496a5c82fb
Lukas Jung <lukas.jung@slint-ui.com>
2022-05-11 09:38:58 +0200

    improve the 7guis timer example

---
+++ examples/7guis/timer.rs
@@ -0,0 +1,18 @@
+// Copyright © SixtyFPS GmbH <info@slint-ui.com>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
+use slint::{Timer, TimerMode};
+
+slint::slint!(import { MainWindow } from "timer.slint";);
+
+pub fn main() {
+    let main_window = MainWindow::new();
+    let timer = Timer::default();
+    {
+        let main_window_weak = main_window.as_weak();
+        timer.start(TimerMode::Repeated, std::time::Duration::from_millis(10), move || {
+            let main_window = main_window_weak.unwrap();
+            main_window.invoke_tick(10);
+        });
+    }
+    main_window.run();
+}
