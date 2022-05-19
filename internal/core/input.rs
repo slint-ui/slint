e7aef68d1ffd76ad1aac7b32043c247dcc877e38
Levin Rickert <me@levrik.io>
2022-05-19 12:29:12 +0200

    accept wheel event from flickable

--- internal/core/input.rs
+++ internal/core/input.rs
@@ -500,8 +500,13 @@ pub fn process_mouse_input(
                 }
                 match item.as_ref().input_event(event2, window, &item_rc) {
                     InputEventResult::EventAccepted => {
-                        result.item_stack = mouse_grabber_stack;
-                        result.grabbed = false;
+                        if result.item_stack.len() == 0 {
+                            // In case the item stack is set already, it shouldn't
+                            // be overriden as we have to keep the deepest stack
+                            // for `send_exit_events` to work properly.
+                            result.item_stack = mouse_grabber_stack;
+                            result.grabbed = false;
+                        }
                         return VisitChildrenResult::abort(item_rc.index(), 0);
                     }
                     InputEventResult::EventIgnored => {
