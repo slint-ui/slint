slint-ui/slint

commit 6cbf2c0609a68318b4f77354dd3de788fb0320a3

Put the new cache in corelib and use it in the GL backend

internal/core/item_rendering.rs
@@ -15,6 +15,8 @@ use crate::Coord;
 use alloc::boxed::Box;
 use core::cell::{Cell, RefCell};
 use core::pin::Pin;
+#[cfg(feature = "std")]
+use std::collections::HashMap;
 use vtable::VRc;
 
 /// This structure must be present in items that are Rendered and contains information.
@@ -88,6 +90,82 @@ impl CachedRenderingData {
             cache.get_mut(index)
         } else {
             None
+        }
+    }
+}
+
+/// A per-item cache.
+///
+/// Cache rendering information for a given item.
+///
+/// Use [`ItemCache::get_or_update_cache_entry`] to get or update the items, the
+/// cache is automatically invalided when the property gets dirty.
+/// [`ItemCache::component_destroyed`] must be called to clear the cache for that
+/// component.
+#[cfg(feature = "std")]
+#[derive(Default)]
+pub struct ItemCache<T> {
+    /// The pointer is a pointer to a component
+    map: RefCell<HashMap<*const vtable::Dyn, HashMap<usize, CachedGraphicsData<T>>>>,
+}
+#[cfg(feature = "std")]
+impl<T: Clone> ItemCache<T> {
+    /// Returns the cached value associated to the `item_rc` if it is still valid.
+    /// Otherwise call the `update_fn` to compute that value, and track property access
+    /// so it is automatically invalided when property becomes dirty.
+    pub fn get_or_update_cache_entry(&self, item_rc: &ItemRc, update_fn: impl FnOnce() -> T) -> T {
+        let component = &(*item_rc.component()) as *const _;
+        let mut borrowed = self.map.borrow_mut();
+        match borrowed.entry(component).or_default().entry(item_rc.index()) {
+            std::collections::hash_map::Entry::Occupied(mut entry) => {
+                let mut tracker = entry.get_mut().dependency_tracker.take();
+                drop(borrowed);
+                let maybe_new_data = tracker
+                    .get_or_insert_with(|| Box::pin(Default::default()))
+                    .as_ref()
+                    .evaluate_if_dirty(update_fn);
+                let mut borrowed = self.map.borrow_mut();
+                let e = borrowed.get_mut(&component).unwrap().get_mut(&item_rc.index()).unwrap();
+                if let Some(new_data) = maybe_new_data {
+                    e.data = new_data.clone();
+                    new_data
+                } else {
+                    e.data.clone()
+                }
+            }
+            std::collections::hash_map::Entry::Vacant(_) => {
+                drop(borrowed);
+                let new_entry = CachedGraphicsData::new(update_fn);
+                let data = new_entry.data.clone();
+                self.map
+                    .borrow_mut()
+                    .get_mut(&component)
+                    .unwrap()
+                    .insert(item_rc.index(), new_entry);
+                data
+            }
+        }
+    }
+
+    /// free the whole cache
+    pub fn clear_all(&self) {
+        self.map.borrow_mut().clear();
+    }
+
+    /// Function that must be called when a component is destroyed.
+    ///
+    /// Usually can be called from [`crate::window::PlatformWindow::free_graphics_resources`]
+    pub fn component_destroyed(&self, component: crate::component::ComponentRef) {
+        let component_ptr: *const _ =
+            crate::component::ComponentRef::as_ptr(component).cast().as_ptr();
+        self.map.borrow_mut().remove(&component_ptr);
+    }
+
+    /// free the cache for a given item
+    pub fn release(&self, item_rc: &ItemRc) {
+        let component = &(*item_rc.component()) as *const _;
+        if let Some(sub) = self.map.borrow_mut().get_mut(&component) {
+            sub.remove(&item_rc.index());
         }
     }
 }
@@ -266,7 +344,7 @@ pub trait ItemRenderer {
     /// RGBA premultiplied pixel values
     fn draw_cached_pixmap(
         &mut self,
-        item_cache: &CachedRenderingData,
+        item_cache: &ItemRc,
         update_fn: &dyn Fn(&mut dyn FnMut(u32, u32, &[u8])),
     );
 
@@ -468,10 +546,10 @@ impl<'a, T: ItemRenderer> ItemRenderer for PartialRenderer<'a, T> {
 
     fn draw_cached_pixmap(
         &mut self,
-        item_cache: &CachedRenderingData,
+        item_rc: &ItemRc,
         update_fn: &dyn Fn(&mut dyn FnMut(u32, u32, &[u8])),
     ) {
-        self.actual_renderer.draw_cached_pixmap(item_cache, update_fn)
+        self.actual_renderer.draw_cached_pixmap(item_rc, update_fn)
     }
 
     fn draw_string(&mut self, string: &str, color: crate::Color) {

internal/backends/gl/glrenderer.rs
@@ -7,8 +7,8 @@ use std::rc::Rc;
 
 use euclid::approxeq::ApproxEq;
 use i_slint_core::graphics::rendering_metrics_collector::RenderingMetrics;
-use i_slint_core::graphics::{Image, IntRect, Point, Rect, RenderingCache, Size};
-use i_slint_core::item_rendering::{CachedRenderingData, ItemRenderer};
+use i_slint_core::graphics::{Image, IntRect, Point, Rect, Size};
+use i_slint_core::item_rendering::{ItemCache, ItemRenderer};
 use i_slint_core::items::{
     self, Clip, FillRule, ImageFit, ImageRendering, InputType, Item, ItemRc, Layer, Opacity,
     RenderingResult,
@@ -47,7 +47,7 @@ impl ItemGraphicsCacheEntry {
     }
 }
 
-pub type ItemGraphicsCache = RenderingCache<Option<ItemGraphicsCacheEntry>>;
+pub type ItemGraphicsCache = ItemCache<Option<ItemGraphicsCacheEntry>>;
 
 const KAPPA90: f32 = 0.55228;
 
@@ -195,9 +195,9 @@ impl ItemRenderer for GLItemRenderer {
         }
     }
 
-    fn draw_image(&mut self, image: Pin<&items::ImageItem>, _: &ItemRc) {
+    fn draw_image(&mut self, image: Pin<&items::ImageItem>, item_rc: &ItemRc) {
         self.draw_image_impl(
-            &image.cached_rendering_data,
+            item_rc,
             items::ImageItem::FIELD_OFFSETS.source.apply_pin(image),
             IntRect::default(),
             items::ImageItem::FIELD_OFFSETS.width.apply_pin(image),
@@ -208,14 +208,14 @@ impl ItemRenderer for GLItemRenderer {
         );
     }
 
-    fn draw_clipped_image(&mut self, clipped_image: Pin<&items::ClippedImage>, _: &ItemRc) {
+    fn draw_clipped_image(&mut self, clipped_image: Pin<&items::ClippedImage>, item_rc: &ItemRc) {
         let source_clip_rect = IntRect::new(
             [clipped_image.source_clip_x(), clipped_image.source_clip_y()].into(),
             [clipped_image.source_clip_width(), clipped_image.source_clip_height()].into(),
         );
 
         self.draw_image_impl(
-            &clipped_image.cached_rendering_data,
+            item_rc,
             items::ClippedImage::FIELD_OFFSETS.source.apply_pin(clipped_image),
             source_clip_rect,
             items::ClippedImage::FIELD_OFFSETS.width.apply_pin(clipped_image),
@@ -541,7 +541,7 @@ impl ItemRenderer for GLItemRenderer {
     ///  * Blur the image
     ///  * Fill the image with the shadow color and SourceIn as composition mode
     ///  * Draw the shadow image
-    fn draw_box_shadow(&mut self, box_shadow: Pin<&items::BoxShadow>, _item_rc: &ItemRc) {
+    fn draw_box_shadow(&mut self, box_shadow: Pin<&items::BoxShadow>, item_rc: &ItemRc) {
         if box_shadow.color().alpha() == 0
             || (box_shadow.blur() == 0.0
                 && box_shadow.offset_x() == 0.
@@ -550,9 +550,8 @@ impl ItemRenderer for GLItemRenderer {
             return;
         }
 
-        let cache_entry = box_shadow.cached_rendering_data.get_or_update(
-            &self.graphics_window.clone().graphics_cache,
-            || {
+        let cache_entry =
+            self.graphics_window.clone().graphics_cache.get_or_update_cache_entry(item_rc, || {
                 ItemGraphicsCacheEntry::Image({
                     let blur = box_shadow.blur() * self.scale_factor;
                     let width = box_shadow.width() * self.scale_factor;
@@ -632,8 +631,7 @@ impl ItemRenderer for GLItemRenderer {
                     Rc::new(shadow_image)
                 })
                 .into()
-            },
-        );
+            });
 
         let shadow_image = match &cache_entry {
             Some(cached_shadow_image) => cached_shadow_image.as_image(),
@@ -666,28 +664,26 @@ impl ItemRenderer for GLItemRenderer {
         });
     }
 
-    fn visit_opacity(&mut self, opacity_item: Pin<&Opacity>, self_rc: &ItemRc) -> RenderingResult {
+    fn visit_opacity(&mut self, opacity_item: Pin<&Opacity>, item_rc: &ItemRc) -> RenderingResult {
         let opacity = opacity_item.opacity();
-        if Opacity::need_layer(self_rc, opacity) {
-            self.render_and_blend_layer(&opacity_item.cached_rendering_data, opacity, self_rc)
+        if Opacity::need_layer(item_rc, opacity) {
+            self.render_and_blend_layer(opacity, item_rc)
         } else {
             self.apply_opacity(opacity);
-            opacity_item
-                .cached_rendering_data
-                .release(&mut self.graphics_window.graphics_cache.borrow_mut());
+            self.graphics_window.graphics_cache.release(item_rc);
             RenderingResult::ContinueRenderingChildren
         }
     }
 
     fn visit_layer(&mut self, layer_item: Pin<&Layer>, self_rc: &ItemRc) -> RenderingResult {
         if layer_item.cache_rendering_hint() {
-            self.render_and_blend_layer(&layer_item.cached_rendering_data, 1.0, self_rc)
+            self.render_and_blend_layer(1.0, self_rc)
         } else {
             RenderingResult::ContinueRenderingChildren
         }
     }
 
-    fn visit_clip(&mut self, clip_item: Pin<&Clip>, self_rc: &ItemRc) -> RenderingResult {
+    fn visit_clip(&mut self, clip_item: Pin<&Clip>, item_rc: &ItemRc) -> RenderingResult {
         if !clip_item.clip() {
             return RenderingResult::ContinueRenderingChildren;
         }
@@ -705,9 +701,7 @@ impl ItemRenderer for GLItemRenderer {
 
         if radius > 0. {
             if let Some(layer_image) =
-                self.render_layer(&clip_item.cached_rendering_data, self_rc, &|| {
-                    clip_item.as_ref().geometry().size
-                })
+                self.render_layer(item_rc, &|| clip_item.as_ref().geometry().size)
             {
                 let layer_image_paint = layer_image.as_paint();
 
@@ -723,10 +717,7 @@ impl ItemRenderer for GLItemRenderer {
 
             RenderingResult::ContinueRenderingWithoutChildren
         } else {
-            clip_item
-                .cached_rendering_data
-                .release(&mut self.graphics_window.graphics_cache.borrow_mut());
-
+            self.graphics_window.graphics_cache.release(item_rc);
             self.combine_clip(
                 euclid::rect(0., 0., geometry.width(), geometry.height()),
                 radius,
@@ -784,26 +775,27 @@ impl ItemRenderer for GLItemRenderer {
 
     fn draw_cached_pixmap(
         &mut self,
-        item_cache: &CachedRenderingData,
+        item_rc: &ItemRc,
         update_fn: &dyn Fn(&mut dyn FnMut(u32, u32, &[u8])),
     ) {
         let canvas = &self.canvas;
 
-        let cache_entry = item_cache.get_or_update(&self.graphics_window.graphics_cache, || {
-            let mut cached_image = None;
-            update_fn(&mut |width: u32, height: u32, data: &[u8]| {
-                use rgb::FromSlice;
-                let img = imgref::Img::new(data.as_rgba(), width as usize, height as usize);
-                if let Ok(image_id) =
-                    canvas.borrow_mut().create_image(img, femtovg::ImageFlags::PREMULTIPLIED)
-                {
-                    cached_image = Some(ItemGraphicsCacheEntry::Image(Rc::new(
-                        CachedImage::new_on_gpu(canvas, image_id),
-                    )))
-                };
+        let cache_entry =
+            self.graphics_window.graphics_cache.get_or_update_cache_entry(item_rc, || {
+                let mut cached_image = None;
+                update_fn(&mut |width: u32, height: u32, data: &[u8]| {
+                    use rgb::FromSlice;
+                    let img = imgref::Img::new(data.as_rgba(), width as usize, height as usize);
+                    if let Ok(image_id) =
+                        canvas.borrow_mut().create_image(img, femtovg::ImageFlags::PREMULTIPLIED)
+                    {
+                        cached_image = Some(ItemGraphicsCacheEntry::Image(Rc::new(
+                            CachedImage::new_on_gpu(canvas, image_id),
+                        )))
+                    };
+                });
+                cached_image
             });
-            cached_image
-        });
         let image_id = match cache_entry {
             Some(ItemGraphicsCacheEntry::Image(image)) => image.ensure_uploaded_to_gpu(self, None),
             Some(ItemGraphicsCacheEntry::ColorizedImage { .. }) => unreachable!(),
@@ -909,12 +901,11 @@ impl GLItemRenderer {
 
     fn render_layer(
         &mut self,
-        item_cache: &CachedRenderingData,
         item_rc: &ItemRc,
         layer_logical_size_fn: &dyn Fn() -> Size,
     ) -> Option<Rc<CachedImage>> {
         let cache_entry =
-            item_cache.get_or_update(&self.graphics_window.clone().graphics_cache, || {
+            self.graphics_window.clone().graphics_cache.get_or_update_cache_entry(item_rc, || {
                 ItemGraphicsCacheEntry::Image({
                     let size = layer_logical_size_fn() * self.scale_factor;
 
@@ -974,22 +965,17 @@ impl GLItemRenderer {
         cache_entry.map(|item_cache_entry| item_cache_entry.as_image().clone())
     }
 
-    fn render_and_blend_layer(
-        &mut self,
-        item_cache: &CachedRenderingData,
-        alpha_tint: f32,
-        self_rc: &ItemRc,
-    ) -> RenderingResult {
+    fn render_and_blend_layer(&mut self, alpha_tint: f32, item_rc: &ItemRc) -> RenderingResult {
         let current_clip = self.get_current_clip();
         if let Some((layer_image, layer_size)) = self
-            .render_layer(item_cache, &self_rc.clone(), &|| {
+            .render_layer(item_rc, &|| {
                 // We don't need to include the size of the opacity item itself, since it has no content.
                 let children_rect = i_slint_core::properties::evaluate_no_tracking(|| {
-                    let self_ref = self_rc.borrow();
+                    let self_ref = item_rc.borrow();
                     self_ref.as_ref().geometry().union(
                         &i_slint_core::item_rendering::item_children_bounding_rect(
-                            &self_rc.component(),
-                            self_rc.index() as isize,
+                            &item_rc.component(),
+                            item_rc.index() as isize,
                             &current_clip,
                         ),
                     )
@@ -1088,7 +1074,7 @@ impl GLItemRenderer {
 
     fn draw_image_impl(
         &mut self,
-        item_cache: &CachedRenderingData,
+        item_rc: &ItemRc,
         source_property: Pin<&Property<Image>>,
         source_clip_rect: IntRect,
         target_width: Pin<&Property<f32>>,
@@ -1106,7 +1092,7 @@ impl GLItemRenderer {
 
         let cached_image = loop {
             let image_cache_entry =
-                item_cache.get_or_update(&self.graphics_window.graphics_cache, || {
+                self.graphics_window.graphics_cache.get_or_update_cache_entry(item_rc, || {
                     let image = source_property.get();
                     let image_inner: &ImageInner = (&image).into();
 
@@ -1167,8 +1153,7 @@ impl GLItemRenderer {
             if colorize_property.map_or(false, |prop| !prop.get().is_transparent())
                 && !cached_image.is_colorized_image()
             {
-                let mut cache = self.graphics_window.graphics_cache.borrow_mut();
-                item_cache.release(&mut cache);
+                self.graphics_window.graphics_cache.release(item_rc);
                 continue;
             }
 

internal/backends/gl/glwindow.rs
@@ -36,7 +36,7 @@ pub struct GLWindow {
     keyboard_modifiers: std::cell::Cell<KeyboardModifiers>,
     currently_pressed_key_code: std::cell::Cell<Option<winit::event::VirtualKeyCode>>,
 
-    pub(crate) graphics_cache: RefCell<ItemGraphicsCache>,
+    pub(crate) graphics_cache: ItemGraphicsCache,
     // This cache only contains textures. The cache for decoded CPU side images is in crate::IMAGE_CACHE.
     pub(crate) texture_cache: RefCell<TextureCache>,
 
@@ -124,7 +124,7 @@ impl GLWindow {
     fn release_graphics_resources(&self) {
         // Release GL textures and other GPU bound resources.
         self.with_current_context(|context| {
-            self.graphics_cache.borrow_mut().clear();
+            self.graphics_cache.clear_all();
             self.texture_cache.borrow_mut().clear();
 
             self.invoke_rendering_notifier(RenderingState::RenderingTeardown, context);
@@ -301,23 +301,13 @@ impl PlatformWindow for GLWindow {
 
     fn free_graphics_resources<'a>(
         &self,
-        _: corelib::component::ComponentRef,
-        items: &mut dyn Iterator<Item = Pin<ItemRef<'a>>>,
+        component: corelib::component::ComponentRef,
+        _items: &mut dyn Iterator<Item = Pin<ItemRef<'a>>>,
     ) {
         match &*self.map_state.borrow() {
             GraphicsWindowBackendState::Unmapped => {}
             GraphicsWindowBackendState::Mapped(_) => {
-                let mut cache_entries_to_clear = items
-                    .flat_map(|item| {
-                        let cached_rendering_data = item.cached_rendering_data_offset();
-                        cached_rendering_data.release(&mut *self.graphics_cache.borrow_mut())
-                    })
-                    .peekable();
-                if cache_entries_to_clear.peek().is_some() {
-                    self.with_current_context(|_| {
-                        cache_entries_to_clear.for_each(drop);
-                    });
-                }
+                self.with_current_context(|_| self.graphics_cache.component_destroyed(component));
             }
         }
     }

internal/backends/mcu/lib.rs
@@ -134,6 +134,7 @@ mod the_backend {
         }
         fn free_graphics_resources<'a>(
             &self,
+            _: i_slint_core::component::ComponentRef,
             items: &mut dyn Iterator<Item = Pin<i_slint_core::items::ItemRef<'a>>>,
         ) {
             super::LINE_RENDERER.with(|renderer| {

internal/backends/mcu/simulator.rs
@@ -139,6 +139,7 @@ impl PlatformWindow for SimulatorWindow {
 
     fn free_graphics_resources<'a>(
         &self,
+        _: i_slint_core::component::ComponentRef,
         items: &mut dyn Iterator<Item = std::pin::Pin<i_slint_core::items::ItemRef<'a>>>,
     ) {
         super::LINE_RENDERER.with(|cache| {

internal/backends/qt/qt_widgets.rs
@@ -54,7 +54,7 @@ macro_rules! get_size {
 
 macro_rules! fn_render {
     ($this:ident $dpr:ident $size:ident $painter:ident $widget:ident $initial_state:ident => $($tt:tt)*) => {
-        fn render(self: Pin<&Self>, backend: &mut &mut dyn ItemRenderer, _self_rc: &ItemRc) -> RenderingResult {
+        fn render(self: Pin<&Self>, backend: &mut &mut dyn ItemRenderer, item_rc: &ItemRc) -> RenderingResult {
             let $dpr: f32 = backend.scale_factor();
 
             let window = backend.window();
@@ -81,7 +81,7 @@ macro_rules! fn_render {
             } else {
                 // Fallback: this happen when the Qt backend is not used and the gl backend is used instead
                 backend.draw_cached_pixmap(
-                    &self.cached_rendering_data,
+                    item_rc,
                     &|callback| {
                         let width = self.width() * $dpr;
                         let height = self.height() * $dpr;

internal/backends/qt/qt_window.rs
@@ -10,10 +10,10 @@ use i_slint_core::graphics::rendering_metrics_collector::{
     RenderingMetrics, RenderingMetricsCollector,
 };
 use i_slint_core::graphics::{
-    Brush, CachedGraphicsData, Color, FontRequest, Image, Point, Rect, SharedImageBuffer, Size,
+    Brush, Color, FontRequest, Image, Point, Rect, SharedImageBuffer, Size,
 };
 use i_slint_core::input::{KeyEvent, KeyEventType, MouseEvent};
-use i_slint_core::item_rendering::ItemRenderer;
+use i_slint_core::item_rendering::{ItemCache, ItemRenderer};
 use i_slint_core::items::{
     self, FillRule, ImageRendering, InputType, ItemRc, ItemRef, Layer, MouseCursor, Opacity,
     PointerEventButton, RenderingResult, TextOverflow, TextWrap,
@@ -24,7 +24,6 @@ use i_slint_core::{ImageInner, PathData, Property, SharedString};
 use items::{ImageFit, TextHorizontalAlignment, TextVerticalAlignment};
 
 use std::cell::RefCell;
-use std::collections::HashMap;
 use std::pin::Pin;
 use std::ptr::NonNull;
 use std::rc::{Rc, Weak};
@@ -442,54 +441,6 @@ fn adjust_rect_and_border_for_inner_drawing(rect: &mut qttypes::QRectF, border_w
     rect.y += *border_width as f64 / 2.;
     rect.width -= *border_width as f64;
     rect.height -= *border_width as f64;
-}
-
-#[derive(Default)]
-struct ItemCache<T> {
-    /// The pointer is a pointer to a component
-    map: RefCell<HashMap<*const vtable::Dyn, HashMap<usize, CachedGraphicsData<T>>>>,
-}
-impl<T: Clone> ItemCache<T> {
-    pub fn get_or_update_cache_entry(&self, item_rc: &ItemRc, update_fn: impl FnOnce() -> T) -> T {
-        let component = &(*item_rc.component()) as *const _;
-        let mut borrowed = self.map.borrow_mut();
-        match borrowed.entry(component).or_default().entry(item_rc.index()) {
-            std::collections::hash_map::Entry::Occupied(mut entry) => {
-                let mut tracker = entry.get_mut().dependency_tracker.take();
-                drop(borrowed);
-                let maybe_new_data = tracker
-                    .get_or_insert_with(|| Box::pin(Default::default()))
-                    .as_ref()
-                    .evaluate_if_dirty(update_fn);
-                let mut borrowed = self.map.borrow_mut();
-                let e = borrowed.get_mut(&component).unwrap().get_mut(&item_rc.index()).unwrap();
-                if let Some(new_data) = maybe_new_data {
-                    e.data = new_data.clone();
-                    new_data
-                } else {
-                    e.data.clone()
-                }
-            }
-            std::collections::hash_map::Entry::Vacant(entry) => {
-                entry.insert(CachedGraphicsData::new(update_fn)).data.clone()
-            }
-        }
-    }
-
-    pub fn clear_all(&self) {
-        self.map.borrow_mut().clear();
-    }
-    pub fn component_destroyed(&self, component: ComponentRef) {
-        let component_ptr: *const _ = ComponentRef::as_ptr(component).cast().as_ptr();
-        self.map.borrow_mut().remove(&component_ptr);
-    }
-
-    pub fn release(&self, item_rc: &ItemRc) {
-        let component = &(*item_rc.component()) as *const _;
-        if let Some(sub) = self.map.borrow_mut().get_mut(&component) {
-            sub.remove(&item_rc.index());
-        }
-    }
 }
 
 struct QtItemRenderer<'a> {
@@ -944,7 +895,7 @@ impl ItemRenderer for QtItemRenderer<'_> {
 
     fn draw_cached_pixmap(
         &mut self,
-        _item_cache: &i_slint_core::item_rendering::CachedRenderingData,
+        _item_rc: &ItemRc,
         update_fn: &dyn Fn(&mut dyn FnMut(u32, u32, &[u8])),
     ) {
         update_fn(&mut |width: u32, height: u32, data: &[u8]| {

internal/core/Cargo.toml
@@ -40,7 +40,7 @@ i-slint-common = { version = "=0.2.5", path = "../common" }
 i-slint-core-macros = { version = "=0.2.5", path = "../core-macros" }
 
 const-field-offset = { version = "0.1", path = "../../helper_crates/const-field-offset" }
-vtable = { version="0.1.7", path = "../../helper_crates/vtable" }
+vtable = { version="0.1.8", path = "../../helper_crates/vtable" }
 
 atomic-polyfill = { version = "0.1.5" }
 auto_enums = "0.7"

commit a5fbfe220d417fa3b200b3b760f861873d654c51

Fix per-item pixmap/texture caching in the Qt and GL renderer
The borrow logic in ItemCache<T>::get_or_update_cache_entry requires us
to take the per-item cache dirty tracker out of the cache entry, so that
we can drop the ref and allow the update_fn to access the cache for
other items during evaluation.

However in order for the cache to work by not re-evaluating the
update_fn next time if nothing rendering related change, we must put the
tracker back after the check/evaluation, otherwise take() will always
return none.

This is a regression from commit
6cbf2c0609a68318b4f77354dd3de788fb0320a3 that changed the call from
get_or_update() - which puts the tracker back - to
get_or_update_cache_entry(), which doesn't. Since the former is now
unused, this commit also removes it.

internal/core/item_rendering.rs
@@ -33,40 +33,6 @@ pub struct CachedRenderingData {
 }
 
 impl CachedRenderingData {
-    /// This function allows retrieving the backend specific per-item data cache, updating
-    /// it if depending properties have changed. The supplied update_fn will be called when
-    /// properties have changed or the cache is initialized the first time.
-    pub fn get_or_update<T: Clone>(
-        &self,
-        cache: &RefCell<RenderingCache<T>>,
-        update_fn: impl FnOnce() -> T,
-    ) -> T {
-        let mut cache_borrow = cache.borrow_mut();
-        if let Some(entry) = self.get_entry(&mut cache_borrow) {
-            let index = self.cache_index.get();
-            let tracker = entry.dependency_tracker.take();
-            drop(cache_borrow);
-
-            let maybe_new_data =
-                tracker.as_ref().and_then(|tracker| tracker.as_ref().evaluate_if_dirty(update_fn));
-
-            let mut cache = cache.borrow_mut();
-            let cache_entry = cache.get_mut(index).unwrap();
-            cache_entry.dependency_tracker = tracker;
-
-            if let Some(new_data) = maybe_new_data {
-                cache_entry.data = new_data
-            }
-
-            return cache_entry.data.clone();
-        }
-        drop(cache_borrow);
-        let cache_entry = crate::graphics::CachedGraphicsData::new(update_fn);
-        self.cache_index.set(cache.borrow_mut().insert(cache_entry));
-        self.cache_generation.set(cache.borrow().generation());
-        cache.borrow().get(self.cache_index.get()).unwrap().data.clone()
-    }
-
     /// This function can be used to remove an entry from the rendering cache for a given item, if it
     /// exists, i.e. if any data was ever cached. This is typically called by the graphics backend's
     /// implementation of the release_item_graphics_cache function.
@@ -126,6 +92,7 @@ impl<T: Clone> ItemCache<T> {
                     .evaluate_if_dirty(update_fn);
                 let mut borrowed = self.map.borrow_mut();
                 let e = borrowed.get_mut(&component).unwrap().get_mut(&item_rc.index()).unwrap();
+                e.dependency_tracker = tracker;
                 if let Some(new_data) = maybe_new_data {
                     e.data = new_data.clone();
                     new_data