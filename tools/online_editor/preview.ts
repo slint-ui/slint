e9b8a245ce08f6c39c19838284ecda45a7424a49
Tobias Hunger <tobias.hunger@slint-ui.com>
2022-08-15 10:09:57 +0200

    online_editor: Reformat files and make cspell happy (no code change!)

--- tools/online_editor/preview.ts
+++ tools/online_editor/preview.ts
@@ -4,14 +4,14 @@
 import slint_init, * as slint from "../../../wasm-interpreter/slint_wasm_interpreter.js";
 
 (async function () {
-    await slint_init();
+  await slint_init();
 
-    var base_url = "";
+  var base_url = "";
 
-    /// Index by url. Inline documents will use the empty string.
-    var loaded_documents: Map<string, string> = new Map;
+  /// Index by url. Inline documents will use the empty string.
+  var loaded_documents: Map<string, string> = new Map();
 
-    let main_source = `
+  let main_source = `
 import { SpinBox, Button, CheckBox, Slider, GroupBox } from "std-widgets.slint";
 export Demo := Window {
     width:  300px;   // Width in logical pixels. All 'px' units are automatically scaled with screen resolution.
@@ -25,58 +25,73 @@ export Demo := Window {
         source: @image-url("https://slint-ui.com/logo/slint-logo-full-light.svg");
     }
 }
-`
+`;
 
-    function update_preview() {
-        let div = document.getElementById("preview") as HTMLDivElement;
-        setTimeout(function () { render_or_error(main_source, base_url, div); }, 1);
-    }
+  function update_preview() {
+    let div = document.getElementById("preview") as HTMLDivElement;
+    setTimeout(function () {
+      render_or_error(main_source, base_url, div);
+    }, 1);
+  }
 
-    async function render_or_error(source: string, base_url: string, div: HTMLDivElement) {
-        let canvas_id = 'canvas_' + Math.random().toString(36).substr(2, 9);
-        let canvas = document.createElement("canvas");
-        canvas.width = 800;
-        canvas.height = 600;
-        canvas.id = canvas_id;
-        div.innerHTML = "";
-        div.appendChild(canvas);
+  async function render_or_error(
+    source: string,
+    base_url: string,
+    div: HTMLDivElement
+  ) {
+    let canvas_id = "canvas_" + Math.random().toString(36).substr(2, 9);
+    let canvas = document.createElement("canvas");
+    canvas.width = 800;
+    canvas.height = 600;
+    canvas.id = canvas_id;
+    div.innerHTML = "";
+    div.appendChild(canvas);
 
-        let { component, error_string } = await slint.compile_from_string_with_style(source, base_url, style, async (url: string): Promise<string> => {
-            let file_source = loaded_documents.get(url);
-            if (file_source === undefined) {
-                const response = await fetch(url);
-                let doc = await response.text();
-                loaded_documents.set(url, doc);
-                return doc;
-            }
-            return file_source;
-        });
-
-        if (error_string != "") {
-            let text = document.createTextNode(error_string);
-            let p = document.createElement('pre');
-            p.appendChild(text);
-            div.innerHTML = "<pre style='color: red; background-color:#fee; margin:0'>" + p.innerHTML + "</pre>";
-        } else {
-            document.getElementById("spinner").remove()
+    let { component, error_string } =
+      await slint.compile_from_string_with_style(
+        source,
+        base_url,
+        style,
+        async (url: string): Promise<string> => {
+          let file_source = loaded_documents.get(url);
+          if (file_source === undefined) {
+            const response = await fetch(url);
+            let doc = await response.text();
+            loaded_documents.set(url, doc);
+            return doc;
+          }
+          return file_source;
         }
+      );
 
-        if (component !== undefined) {
-            component.run(canvas_id)
-        }
+    if (error_string != "") {
+      let text = document.createTextNode(error_string);
+      let p = document.createElement("pre");
+      p.appendChild(text);
+      div.innerHTML =
+        "<pre style='color: red; background-color:#fee; margin:0'>" +
+        p.innerHTML +
+        "</pre>";
+    } else {
+      document.getElementById("spinner").remove();
     }
 
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get("snippet");
-    const load_url = params.get("load_url");
-    const style = params.get("style") || "";
-
-    if (code) {
-        main_source = code;
-    } else if (load_url) {
-        base_url = load_url;
-        const response = await fetch(load_url);
-        main_source = await response.text();
+    if (component !== undefined) {
+      component.run(canvas_id);
     }
-    update_preview();
+  }
+
+  const params = new URLSearchParams(window.location.search);
+  const code = params.get("snippet");
+  const load_url = params.get("load_url");
+  const style = params.get("style") || "";
+
+  if (code) {
+    main_source = code;
+  } else if (load_url) {
+    base_url = load_url;
+    const response = await fetch(load_url);
+    main_source = await response.text();
+  }
+  update_preview();
 })();
--- tools/online_editor/vite.config.js
+++ tools/online_editor/vite.config.js
@@ -1,35 +1,36 @@
 // Copyright © SixtyFPS GmbH <info@slint-ui.com>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
 
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 export default defineConfig(({ command, mode }) => {
   let base_config = {
     server: {
       fs: {
         // Allow serving files from the project root
-        allow: ['../../']
-      }
+        allow: ["../../"],
+      },
     },
-    base: '',
+    base: "",
   };
 
   if (command === "serve") {
     // For development builds, serve the wasm interpreter straight out of the local file system.
     base_config.resolve = {
       alias: {
-        '../../../wasm-interpreter/slint_wasm_interpreter.js': "../../api/wasm-interpreter/pkg/slint_wasm_interpreter.js"
-      }
-    }
+        "../../../wasm-interpreter/slint_wasm_interpreter.js":
+          "../../api/wasm-interpreter/pkg/slint_wasm_interpreter.js",
+      },
+    };
   } else {
     // For distribution builds,
     // assume deployment on the main website where the loading file (index.js) is in the assets/ sub-directory and the
     // relative path to the interpreter is as below.
-    base_config.build = {}
+    base_config.build = {};
     base_config.build.rollupOptions = {
-      external: ['../../../wasm-interpreter/slint_wasm_interpreter.js'],
+      external: ["../../../wasm-interpreter/slint_wasm_interpreter.js"],
       input: ["index.html", "preview.html"],
-    }
+    };
   }
 
   return base_config;
-})
+});
