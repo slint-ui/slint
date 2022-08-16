slint-ui/slint

commit 49ea24c44a6ff2b00f007c5165d479bc3a28ddca

janitor: move code for backspace and delete key shortcut recognition
... into the section that checks move_mod, since the code now depends on it.

internal/core/input.rs
@@ -230,6 +230,20 @@ impl KeyEvent {
                 key_codes::DownArrow => {
                     return Some(TextShortcut::Move(TextCursorDirection::EndOfParagraph))
                 }
+                key_codes::Backspace => {
+                    return Some(if move_mod {
+                        TextShortcut::DeleteWordBackward
+                    } else {
+                        TextShortcut::DeleteBackward
+                    });
+                }
+                key_codes::Delete => {
+                    return Some(if move_mod {
+                        TextShortcut::DeleteWordForward
+                    } else {
+                        TextShortcut::DeleteForward
+                    });
+                }
                 _ => (),
             };
         }
@@ -274,20 +288,6 @@ impl KeyEvent {
             Ok(direction) => return Some(TextShortcut::Move(direction)),
             _ => (),
         };
-
-        match keycode {
-            key_codes::Backspace => Some(if move_mod {
-                TextShortcut::DeleteWordBackward
-            } else {
-                TextShortcut::DeleteBackward
-            }),
-            key_codes::Delete => Some(if move_mod {
-                TextShortcut::DeleteWordForward
-            } else {
-                TextShortcut::DeleteForward
-            }),
-            _ => None,
-        }
     }
 }
 

commit 8363ca6b1d829172412caf6cde06ada7e6788972

Fix CI build
This got broken when moving code in commit
49ea24c44a6ff2b00f007c5165d479bc3a28ddca

internal/core/input.rs
@@ -288,6 +288,8 @@ impl KeyEvent {
             Ok(direction) => return Some(TextShortcut::Move(direction)),
             _ => (),
         };
+
+        None
     }
 }
 
@@ -530,7 +532,7 @@ pub fn process_mouse_input(
                     InputEventResult::EventAccepted => {
                         if result.item_stack.is_empty() {
                             // In case the item stack is set already, it shouldn't
-                            // be overriden as we have to keep the deepest stack
+                            // be overridden as we have to keep the deepest stack
                             // for `send_exit_events` to work properly.
                             result.item_stack = mouse_grabber_stack;
                             result.grabbed = false;