6253d3265c5a1eadfa3552001811c22ff969c828
Tobias Hunger <tobias.hunger@slint-ui.com>
2022-08-25 17:01:04 +0200

    online_editor: Implement custom "slint/load_file"
    The LSP running in the web worker needs to access (virtual) files in the
    editor. So it can send a request to the editor for files.
    
    Handle such requests. Also make sure to provide the editor with the
    correct URLs of open files so that the base URL to request additional
    data with is correct.
    
    Also do not error out when LSP and interpreter request the same file
    concurrently.

---
+++ tools/online_editor/src/proxy.ts
@@ -0,0 +1,37 @@
+// Copyright © SixtyFPS GmbH <info@slint-ui.com>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
+
+import {
+  MessageReader,
+  DataCallback,
+  Message,
+  Disposable,
+  Event,
+  PartialMessageInfo,
+} from "monaco-languageclient";
+
+export class FilterProxyReader implements MessageReader {
+  constructor(reader: MessageReader, callback: (_: Message) => boolean) {
+    this.#reader = reader;
+    this.#callback = callback;
+    this.onError = this.#reader.onError;
+    this.onClose = this.#reader.onClose;
+    this.onPartialMessage = this.#reader.onPartialMessage;
+  }
+
+  readonly #reader: MessageReader;
+  readonly #callback: (_: Message) => boolean;
+  onError: Event<Error>;
+  onClose: Event<void>;
+  onPartialMessage: Event<PartialMessageInfo>;
+
+  dispose() {
+    this.#reader.dispose();
+  }
+
+  listen(callback: DataCallback): Disposable {
+    return this.#reader.listen((data: Message) => {
+      this.#callback(data) || callback(data);
+    });
+  }
+}
