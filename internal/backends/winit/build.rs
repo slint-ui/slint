b16d2e6de673ae74338c5b43a705000d5614b5c9
Simon Hausmann <simon.hausmann@slint-ui.com>
2022-09-05 12:29:50 +0200

    Make sure that skia_backend_opengl is always set on Linux

--- internal/backends/winit/build.rs
+++ internal/backends/winit/build.rs
@@ -7,7 +7,7 @@ fn main() {
     // Setup cfg aliases
     cfg_aliases! {
        enable_skia_renderer: { any(feature = "renderer-skia", feature = "renderer-skia-opengl")},
-       skia_backend_opengl: { feature = "renderer-skia-opengl" },
+       skia_backend_opengl: { any(feature = "renderer-skia-opengl", all(feature = "renderer-skia", not(any(target_os = "macos", target_family = "windows", target_arch = "wasm32")))) },
        skia_backend_metal: { all(target_os = "macos", not(feature = "renderer-skia-opengl")) },
        skia_backend_d3d: { all(target_family = "windows", not(feature = "renderer-skia-opengl")) },
     }
