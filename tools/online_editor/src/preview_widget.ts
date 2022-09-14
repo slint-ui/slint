464e11fa0017ba6681c7c7a61d4da8e23c4b45ed
Tobias Hunger <tobias.hunger@slint-ui.com>
2022-09-14 11:22:03 +0200

    online_editor: Make preview bigger by default
    I removed the default size when trying to find a way to work around a
    bug in the interpreter that made it stick to the size of the canvas.
    
    This bug is fixed, so suggest a decent default size again.

--- tools/online_editor/src/preview_widget.ts
+++ tools/online_editor/src/preview_widget.ts
@@ -23,6 +23,8 @@ export class PreviewWidget extends Widget {
     content.className = "preview-container";
 
     const canvas = document.createElement("canvas");
+    canvas.width = 800;
+    canvas.height = 600;
     content.appendChild(canvas);
 
     const error_area = document.createElement("div");
