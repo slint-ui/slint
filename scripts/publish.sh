b82710870a2446ce1a74ae66e8e8acd18931081b
Olivier Goffart <olivier.goffart@slint-ui.com>
2022-09-14 16:02:55 +0200

    Fix publishing script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,11 +8,11 @@ sleep 15
 cargo publish --manifest-path internal/compiler/Cargo.toml
 cargo publish --manifest-path internal/core/Cargo.toml
 cargo publish --manifest-path api/rs/macros/Cargo.toml
-cargo publish --manifest-path internal/backends/gl/Cargo.toml --features x11
+cargo publish --manifest-path internal/backends/winit/Cargo.toml --features x11,renderer-winit-femtovg
 cargo publish --manifest-path api/rs/build/Cargo.toml
 cargo publish --manifest-path internal/backends/qt/Cargo.toml
 sleep 30
-cargo publish --manifest-path internal/backends/selector/Cargo.toml --features backend-gl-x11
+cargo publish --manifest-path internal/backends/selector/Cargo.toml --features backend-winit-x11,renderer-winit-femtovg
 sleep 30
 cargo publish --manifest-path internal/interpreter/Cargo.toml
 cargo publish --manifest-path api/rs/slint/Cargo.toml
