de6df500507c01208f79731f09b37e328efc2673
Tobias Hunger <tobias.hunger@slint-ui.com>
2022-09-16 10:59:10 +0200

    online_editor: Smoke-test the online_editor

---
+++ tools/online_editor/cypress/support/commands.ts
@@ -0,0 +1,6 @@
+// Copyright © SixtyFPS GmbH <info@slint-ui.com>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
+
+// cSpell: ignore frsource
+
+import "@frsource/cypress-plugin-visual-regression-diff/dist/support";
