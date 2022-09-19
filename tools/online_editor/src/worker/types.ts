43f106d0b84483b9498f38adfb3bfdc3d7d42a39
Tobias Hunger <tobias.hunger@slint-ui.com>
2022-09-19 18:57:58 +0200

    online_editor: Make syntax_check work again
    Turns out that was not actually doing anything locally :-/
    
    So force the compiler to run, add some types that went missing and
    disable checking of the webworkers -- there is a type conflict in there
    that needs to be fixed upstream (Issue is:
    https://github.com/DefinitelyTyped/DefinitelyTyped/issues/60868).

---
+++ tools/online_editor/src/worker/types.ts
@@ -0,0 +1,8 @@
+// Copyright © SixtyFPS GmbH <info@slint-ui.com>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
+
+/* eslint-disable */
+type Document = any; // This type is defined in the `dom` library, which is not compatible with `webworker`
+type DocumentFragment = any; // This type is defined in the `dom` library, which is not compatible with `webworker`
+type Element = any; // This type is defined in the `dom` library, which is not compatible with `webworker`
+/* eslint-enable */
