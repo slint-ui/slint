126ac5b83485fdd04e40359b962ce52dc85aecda
Florian Blasius <florian.blasius@slint-ui.com>
2022-10-18 10:11:53 +0200

    Cleanup carousel example

--- examples/carousel/node/main.js
+++ examples/carousel/node/main.js
@@ -2,9 +2,9 @@
 // Copyright © SixtyFPS GmbH <info@slint-ui.com>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
 
-// import "slint";
+const path = require("path");
 let slint = require("slint-ui");
-// import * as demo from "../ui/carousel_demo.slint";
+
 let demo = require("../ui/carousel_demo.slint");
 let app = new demo.MainWindow();
 
