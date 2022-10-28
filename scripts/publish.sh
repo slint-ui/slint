ea0de6878d82250645bafb4b1167656987bfb3f3
Olivier Goffart <olivier.goffart@slint-ui.com>
2022-10-28 11:12:03 +0200

    Release script: add a sleep
    i_slint_core compiled too fast, and the compiler was still not on
    crates.io when trying to build the macro

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,6 +7,7 @@ cargo publish --manifest-path internal/core-macros/Cargo.toml
 sleep 15
 cargo publish --manifest-path internal/compiler/Cargo.toml
 cargo publish --manifest-path internal/core/Cargo.toml
+sleep 15
 cargo publish --manifest-path api/rs/macros/Cargo.toml
 cargo publish --manifest-path internal/backends/winit/Cargo.toml --features x11,renderer-winit-femtovg
 cargo publish --manifest-path api/rs/build/Cargo.toml
