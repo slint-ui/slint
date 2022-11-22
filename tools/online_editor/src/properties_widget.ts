f8a0cf6d819bfb0c2f9630f9799465a61e081975
Tobias Hunger <tobias.hunger@slint-ui.com>
2022-11-22 21:52:04 +0100

    tools: Use setBinding in the LSP to set bindings

--- tools/online_editor/src/properties_widget.ts
+++ tools/online_editor/src/properties_widget.ts
@@ -3,11 +3,11 @@
 
 // cSpell: ignore lumino
 
-import { GotoPositionCallback, ReplaceTextFunction } from "./text";
-import { LspPosition, LspRange, LspURI } from "./lsp_integration";
+import { GotoPositionCallback } from "./text";
+import { LspPosition, LspURI } from "./lsp_integration";
 
-import { PropertyQuery, PropertiesView } from "./shared/properties";
-import { query_properties } from "./shared/properties_client";
+import { PropertiesView } from "./shared/properties";
+import { change_property, query_properties } from "./shared/properties_client";
 
 import { Message } from "@lumino/messaging";
 import { Widget } from "@lumino/widgets";
@@ -21,10 +21,6 @@ export class PropertiesWidget extends Widget {
     #onGotoPosition: GotoPositionCallback = (_u, _p) => {
         return;
     };
-    #replaceText: ReplaceTextFunction = (_u, _r, _t, _v) => {
-        return true;
-    };
-
     #propertiesView: PropertiesView;
 
     constructor(language_client_getter: () => BaseLanguageClient | null) {
@@ -37,27 +33,25 @@ export class PropertiesWidget extends Widget {
         this.title.closable = true;
         this.title.caption = `Element Properties`;
 
-        this.#language_client_getter = language_client_getter;
-        this.#propertiesView = new PropertiesView(node);
+        this.#propertiesView = new PropertiesView(
+            node,
+            (doc, element, property_name, value, dry_run) => {
+                return change_property(
+                    this.language_client,
+                    doc,
+                    element,
+                    property_name,
+                    value,
+                    dry_run,
+                );
+            },
+        );
 
-        this.#propertiesView.property_clicked = (uri, _v, p) => {
+        this.#language_client_getter = language_client_getter;
+
+        this.#propertiesView.property_clicked = (uri, _, p) => {
             if (p.defined_at != null) {
                 this.#onGotoPosition(uri, p.defined_at.expression_range);
-            }
-        };
-        this.#propertiesView.change_property = (
-            uri,
-            p,
-            current_text,
-            code_text,
-        ) => {
-            if (p.defined_at != null) {
-                this.replace_property_value(
-                    uri,
-                    p.defined_at.expression_range,
-                    current_text,
-                    (old_text) => old_text == code_text,
-                );
             }
         };
     }
@@ -75,15 +69,6 @@ export class PropertiesWidget extends Widget {
         });
     }
 
-    private replace_property_value(
-        uri: string,
-        range: LspRange,
-        new_value: string,
-        validator: (_old: string) => boolean,
-    ): boolean {
-        return this.#replaceText(uri, range, new_value, validator);
-    }
-
     protected onCloseRequest(msg: Message): void {
         super.onCloseRequest(msg);
         this.dispose();
@@ -91,13 +76,5 @@ export class PropertiesWidget extends Widget {
 
     set on_goto_position(callback: GotoPositionCallback) {
         this.#onGotoPosition = callback;
-    }
-
-    set replace_text_function(fn: ReplaceTextFunction) {
-        this.#replaceText = fn;
-    }
-
-    set_properties(properties: PropertyQuery) {
-        this.#propertiesView.set_properties(properties);
     }
 }
--- tools/online_editor/src/shared/properties_client.ts
+++ tools/online_editor/src/shared/properties_client.ts
@@ -1,20 +1,51 @@
 // Copyright © SixtyFPS GmbH <info@slint-ui.com>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
 
-import { PropertyQuery } from "./properties";
+import { PropertyQuery, SetBindingResponse } from "./properties";
 
 import {
     ExecuteCommandRequest,
     ExecuteCommandParams,
 } from "vscode-languageserver-protocol";
 import {
+    OptionalVersionedTextDocumentIdentifier,
     Position as LspPosition,
+    Range as LspRange,
     URI as LspURI,
+    WorkspaceEdit,
 } from "vscode-languageserver-types";
 
 import { BaseLanguageClient } from "vscode-languageclient";
 
+export type WorkspaceEditor = (_we: WorkspaceEdit) => boolean;
 export type SetPropertiesHelper = (_p: PropertyQuery) => void;
+
+export async function change_property(
+    client: BaseLanguageClient | null,
+    doc: OptionalVersionedTextDocumentIdentifier,
+    element_range: LspRange,
+    property_name: string,
+    current_text: string,
+    dry_run: boolean,
+): Promise<SetBindingResponse> {
+    if (client != null) {
+        console.log("Calling into LSP");
+        const result = await client.sendRequest(ExecuteCommandRequest.type, {
+            command: "setBinding",
+            arguments: [
+                doc,
+                element_range,
+                property_name,
+                current_text,
+                dry_run,
+            ],
+        } as ExecuteCommandParams);
+
+        console.log("Returning from LSP:", result.diagnostics);
+        return result;
+    }
+    return new Promise((accept, _) => accept({ diagnostics: [] }));
+}
 
 export function query_properties(
     client: BaseLanguageClient | null,
