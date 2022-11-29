578cf2c87e5634ba07511a53b36a1b5df19d20fe
Florian Blasius <florian.blasius@slint-ui.com>
2022-11-29 13:09:59 +0100

    node_editor: generic drag

--- examples/fancy_demo/build.rs
+++ examples/fancy_demo/build.rs
@@ -2,5 +2,9 @@
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
 
 fn main() {
-    slint_build::compile("main.slint").unwrap();
+    slint_build::compile_with_config(
+        "main.slint",
+        slint_build::CompilerConfiguration::new().with_style("material".into()),
+    )
+    .unwrap();
 }
--- examples/fancy_demo/main.rs
+++ examples/fancy_demo/main.rs
@@ -8,8 +8,8 @@ use wasm_bindgen::prelude::*;
 
 slint::include_modules!();
 
+use slint::{Model, VecModel};
 use std::rc::Rc;
-use slint::{VecModel, Model};
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn main() {
@@ -22,28 +22,59 @@ pub fn main() {
 
     let model = Rc::new(VecModel::default());
 
-    app.global::<NodeController>().set_model(model.clone().into());
+    let drag_item_width = app.global::<Sizes>().get_drag_item_width();
+    let drag_item_height = app.global::<Sizes>().get_drag_item_height();
 
+    for i in 0..10 {
+        model.push(DragItem {
+            x: 0.,
+            y: i as f32 * drag_item_height,
+            width: drag_item_width,
+            height: drag_item_height,
+            text: format!("Node {i}").into(),
+        });
+    }
 
-    app.global::<NodeController>().on_push({
+    app.set_drag_items(model.into());
+
+    app.on_check_drag({
         let app = app.as_weak();
-
-      
-
-        move |x, y, title| {
+        move |x, y| {
             app.upgrade_in_event_loop(move |app| {
-                if let Some(model) = app.global::<NodeController>().get_model().as_any()
-                .downcast_ref::<VecModel<NodeModel>>() {
-                    model.push(NodeModel {
-                        title,
-                        x,
-                        y
-                    });
+                for drag_item in app.get_drag_items().iter() {
+                    if x >= drag_item.x
+                        && x <= drag_item.x + drag_item.width
+                        && y >= drag_item.y
+                        && y <= drag_item.y + drag_item.height
+                    {
+                        println!("yes");
+                        app.invoke_start_drag(x, y, drag_item.text);
+                    }
                 }
-                // model.push()
-            }).expect("Cannot add node.");
+            })
+            .expect("Cannot check drag.");
         }
     });
+
+    // app.global::<NodeController>().set_model(model.clone().into());
+
+    // app.global::<NodeController>().on_push({
+    //     let app = app.as_weak();
+
+    //     move |x, y, title| {
+    //         app.upgrade_in_event_loop(move |app| {
+    //             if let Some(model) = app.global::<NodeController>().get_model().as_any()
+    //             .downcast_ref::<VecModel<NodeModel>>() {
+    //                 model.push(NodeModel {
+    //                     title,
+    //                     x,
+    //                     y
+    //                 });
+    //             }
+    //             // model.push()
+    //         }).expect("Cannot add node.");
+    //     }
+    // });
 
     app.run();
 }
