59042f8e16a05359c048c01066d1e64daa678e6c
Florian Blasius <florian.blasius@slint-ui.com>
2022-12-07 12:14:03 +0100

    ci fix

--- tests/driver/screenshot/build.rs
+++ tests/driver/screenshot/build.rs
@@ -127,39 +127,3 @@ fn generate_macro(
     output.write_all(b"}\n")?;
     Ok(true)
 }
-
-#[cfg(feature = "build-time")]
-fn generate_source(
-    source: &str,
-    output: &mut std::fs::File,
-    testcase: test_driver_lib::TestCase,
-) -> Result<(), std::io::Error> {
-    use i_slint_compiler::{diagnostics::BuildDiagnostics, *};
-
-    let include_paths = test_driver_lib::extract_include_paths(source)
-        .map(std::path::PathBuf::from)
-        .collect::<Vec<_>>();
-
-    let mut diag = BuildDiagnostics::default();
-    diag.enable_experimental = true;
-    let syntax_node = parser::parse(source.to_owned(), Some(&testcase.absolute_path), &mut diag);
-    let mut compiler_config = CompilerConfiguration::new(generator::OutputFormat::Rust);
-    compiler_config.include_paths = include_paths;
-    compiler_config.embed_resources = EmbedResourcesKind::EmbedTextures;
-    compiler_config.style = Some("fluent".to_string());
-    let (root_component, diag) =
-        spin_on::spin_on(compile_syntax_node(syntax_node, diag, compiler_config));
-
-    if diag.has_error() {
-        diag.print_warnings_and_exit_on_error();
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            format!("build error in {:?}", testcase.absolute_path),
-        ));
-    } else {
-        diag.print();
-    }
-
-    generator::generate(generator::OutputFormat::Rust, output, &root_component)?;
-    Ok(())
-}
