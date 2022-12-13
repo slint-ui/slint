73024beb98552063e319ad7b9aadbfcb82edf759
Simon Hausmann <simon.hausmann@slint-ui.com>
2022-12-13 20:44:13 +0100

    Begin wrapping the component compiler

--- api/python/lib.rs
+++ api/python/lib.rs
@@ -1,9 +1,16 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.1 OR LicenseRef-Slint-commercial
 
+mod interpreter;
+use interpreter::{ComponentCompiler, PyDiagnostic, PyDiagnosticLevel, PyValueType};
+
 use pyo3::prelude::*;
 
 #[pymodule]
 fn slint(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_class::<ComponentCompiler>()?;
+    m.add_class::<PyValueType>()?;
+    m.add_class::<PyDiagnosticLevel>()?;
+    m.add_class::<PyDiagnostic>()?;
     Ok(())
 }
---
+++ api/python/tests/test_compiler.py
@@ -0,0 +1,66 @@
+# Copyright © SixtyFPS GmbH <info@slint.dev>
+# SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.1 OR LicenseRef-Slint-commercial
+
+import pytest
+import slint
+from slint import ValueType;
+
+def test_basic_compiler():
+    compiler = slint.ComponentCompiler()
+
+    assert compiler.include_paths == []
+    compiler.include_paths = ["testing"]
+    assert compiler.include_paths == ["testing"]
+
+    assert compiler.build_from_source("Garbage", "") == None
+
+    compdef = compiler.build_from_source("""
+        export global TestGlobal {
+            in property <string> theglobalprop;
+            callback globallogic();
+        }
+
+        export component Test {
+            in property <string> strprop;
+            in property <int> intprop;
+            in property <float> floatprop;
+            in property <bool> boolprop;
+            in property <image> imgprop;
+            in property <brush> brushprop;
+            in property <color> colprop;
+            in property <[string]> modelprop;
+
+            callback test-callback();
+        }
+    """, "")
+    assert compdef != None
+
+    assert compdef.name == "Test"
+
+    props = [(name, type) for name, type in compdef.properties.items()]
+    assert props == [('boolprop', ValueType.Bool), ('brushprop', ValueType.Brush), ('colprop', ValueType.Brush), ('floatprop', ValueType.Number), ('imgprop', ValueType.Image), ('intprop', ValueType.Number), ('modelprop', ValueType.Model), ('strprop', ValueType.String)]
+
+    assert compdef.callbacks == ["test-callback"]
+
+    assert compdef.globals == ["TestGlobal"]
+
+    assert compdef.global_properties("Garbage") == None
+    assert [(name, type) for name, type in compdef.global_properties("TestGlobal").items()] == [('theglobalprop', ValueType.String)]
+
+    assert compdef.global_callbacks("Garbage") == None
+    assert compdef.global_callbacks("TestGlobal") == ["globallogic"]
+
+    instance = compdef.create()
+    assert instance != None
+
+def test_compiler_build_from_path():
+    compiler = slint.ComponentCompiler()
+
+    assert len(compiler.diagnostics) == 0
+
+    assert compiler.build_from_path("Nonexistent.slint") == None
+    diags = compiler.diagnostics
+    assert len(diags) == 1
+
+    assert diags[0].level == slint.DiagnosticLevel.Error
+    assert diags[0].message.startswith("Could not load Nonexistent.slint:")
