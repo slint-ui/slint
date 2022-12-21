dace899cc5ea226b084ac343e4050a2d1bfd063a
Olivier Goffart <olivier.goffart@slint-ui.com>
2022-12-21 09:42:19 +0100

    Pure function: make it a warning in legacy context instead of an error

--- internal/compiler/passes/purity_check.rs
+++ internal/compiler/passes/purity_check.rs
@@ -11,17 +11,21 @@ pub fn purity_check(doc: &crate::object_tree::Document, diag: &mut BuildDiagnost
             component,
             &(),
             &mut |elem, &()| {
+                let level = match elem.borrow().is_legacy_syntax {
+                    true => crate::diagnostics::DiagnosticLevel::Warning,
+                    false => crate::diagnostics::DiagnosticLevel::Error,
+                };
                 crate::object_tree::visit_element_expressions(elem, |expr, name, _| {
                     if let Some(name) = name {
                         let lookup = elem.borrow().lookup_property(name);
                         if lookup.declared_pure.unwrap_or(false)
                             || lookup.property_type.is_property_type()
                         {
-                            ensure_pure(expr, Some(diag));
+                            ensure_pure(expr, Some(diag), level);
                         }
                     } else {
                         // model expression must be pure
-                        ensure_pure(expr, Some(diag));
+                        ensure_pure(expr, Some(diag), level);
                     };
                 })
             },
@@ -29,13 +33,21 @@ pub fn purity_check(doc: &crate::object_tree::Document, diag: &mut BuildDiagnost
     }
 }
 
-fn ensure_pure(expr: &Expression, mut diag: Option<&mut BuildDiagnostics>) -> bool {
+fn ensure_pure(
+    expr: &Expression,
+    mut diag: Option<&mut BuildDiagnostics>,
+    level: crate::diagnostics::DiagnosticLevel,
+) -> bool {
     let mut r = true;
     expr.visit_recursive(&mut |e| match e {
         Expression::CallbackReference(nr, node) => {
             if !nr.element().borrow().lookup_property(nr.name()).declared_pure.unwrap_or(false) {
                 if let Some(diag) = diag.as_deref_mut() {
-                    diag.push_error(format!("Cannot call impure callback '{}'", nr.name()), node);
+                    diag.push_diagnostic(
+                        format!("Call of impure callback '{}'", nr.name()),
+                        node,
+                        level,
+                    );
                 }
                 r = false;
             }
@@ -45,9 +57,10 @@ fn ensure_pure(expr: &Expression, mut diag: Option<&mut BuildDiagnostics>) -> bo
                 Some(true) => return,
                 Some(false) => {
                     if let Some(diag) = diag.as_deref_mut() {
-                        diag.push_error(
-                            format!("Cannot call impure function '{}'", nr.name(),),
+                        diag.push_diagnostic(
+                            format!("Call of impure function '{}'", nr.name(),),
                             node,
+                            level,
                         );
                     }
                     r = false;
@@ -62,11 +75,13 @@ fn ensure_pure(expr: &Expression, mut diag: Option<&mut BuildDiagnostics>) -> bo
                             .borrow()
                             .expression,
                         None,
+                        level,
                     ) {
                         if let Some(diag) = diag.as_deref_mut() {
-                            diag.push_error(
-                                format!("Cannot call impure function '{}'", nr.name(),),
+                            diag.push_diagnostic(
+                                format!("Call of impure function '{}'", nr.name()),
                                 node,
+                                level,
                             );
                         }
                         r = false;
@@ -77,14 +92,14 @@ fn ensure_pure(expr: &Expression, mut diag: Option<&mut BuildDiagnostics>) -> bo
         Expression::BuiltinFunctionReference(func, node) => {
             if !func.is_pure() {
                 if let Some(diag) = diag.as_deref_mut() {
-                    diag.push_error("Cannot call impure function".into(), node);
+                    diag.push_diagnostic("Call of impure function".into(), node, level);
                 }
                 r = false;
             }
         }
         Expression::SelfAssignment { node, .. } => {
             if let Some(diag) = diag.as_deref_mut() {
-                diag.push_error("Cannot assign in a pure context".into(), node);
+                diag.push_diagnostic("Assignment in a pure context".into(), node, level);
             }
             r = false;
         }
