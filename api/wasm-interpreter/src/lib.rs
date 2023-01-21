587c9f694279c5ddf2acd587e03ca464446753bd
Simon Hausmann <hausmann@gmail.com>
2023-01-21 09:20:22 +0100

    Prospective build fix for vs code extension build
    The wasm interpreter isn't built with the highlight feature there.

--- api/wasm-interpreter/src/lib.rs
+++ api/wasm-interpreter/src/lib.rs
@@ -198,8 +198,9 @@ impl WrappedInstance {
     /// THIS FUNCTION IS NOT PART THE PUBLIC API!
     /// Highlights instances of the requested component
     #[wasm_bindgen]
-    pub fn highlight(&self, path: &str, offset: u32) {
-        self.0.highlight(path.into(), offset);
+    pub fn highlight(&self, _path: &str, _offset: u32) {
+        #[cfg(feature = "highlight")]
+        self.0.highlight(_path.into(), _offset);
         let _ = slint_interpreter::invoke_from_event_loop(|| {}); // wake event loop
     }
 }
