0f7c931ae894a96375b6c09c3e97a309addcc56c
Simon Hausmann <simon.hausmann@slint-ui.com>
2023-01-30 16:09:10 +0100

    Simplify wasm key input event conversion

--- internal/backends/winit/wasm_input_helper.rs
+++ internal/backends/winit/wasm_input_helper.rs
@@ -105,16 +105,11 @@ impl WasmInputHelper {
             if let (Some(window_adapter), Some(data)) = (win.upgrade(), e.data()) {
                 if !e.is_composing() && e.input_type() != "insertCompositionText" {
                     if !shared_state2.borrow_mut().has_key_down {
-                        for ch in data.chars() {
-                            window_adapter
-                                .window()
-                                .dispatch_event(WindowEvent::KeyPressed { text: ch.into() })
-                        }
-                        for ch in data.chars() {
-                            window_adapter
-                                .window()
-                                .dispatch_event(WindowEvent::KeyReleased { text: ch.into() })
-                        }
+                        let text: SharedString = data.into();
+                        window_adapter
+                            .window()
+                            .dispatch_event(WindowEvent::KeyPressed { text: text.clone() });
+                        window_adapter.window().dispatch_event(WindowEvent::KeyReleased { text });
                         shared_state2.borrow_mut().has_key_down = false;
                     }
                     input.set_value("");
@@ -224,9 +219,5 @@ fn event_text(e: &web_sys::KeyboardEvent) -> Option<SharedString> {
     }
     i_slint_common::for_each_special_keys!(check_non_printable_code);
 
-    let mut chars = key.chars();
-    match chars.next() {
-        Some(first_char) if chars.next().is_none() => Some(first_char.into()),
-        _ => None,
-    }
+    return Some(key.into());
 }
