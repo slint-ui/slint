3633bf0676fbe8fd143f8bfab41ee51d0457ee9f
Florian Blasius <florian.blasius@slint-ui.com>
2023-01-31 14:18:33 +0100

    energy_nmg: initial setup

---
+++ examples/energy_mng/build.rs
@@ -0,0 +1,15 @@
+// Copyright © SixtyFPS GmbH <info@slint-ui.com>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
+
+#[cfg(not(feature = "mcu-board-support"))]
+fn main() {
+    slint_build::compile("ui/main_window.slint").unwrap();
+}
+
+#[cfg(feature = "mcu-board-support")]
+fn main() {
+    let config = slint_build::CompilerConfiguration::new()
+        .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer);
+    slint_build::compile_with_config("ui/main_window.slint", config).unwrap();
+    slint_build::print_rustc_flags().unwrap();
+}
