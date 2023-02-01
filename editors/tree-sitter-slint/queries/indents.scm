ffaf1a7dc668c3ac0098d776395ed37a3e3f0b7a
Tobias Hunger <tobias.hunger@slint-ui.com>
2023-02-01 23:06:26 +0100

    tree-sitter: Make queries parse again

--- editors/tree-sitter-slint/queries/indents.scm
+++ editors/tree-sitter-slint/queries/indents.scm
@@ -1,15 +1,8 @@
 ; Copyright © SixtyFPS GmbH <info@slint-ui.com>
 ; SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
 
-[
-(field_declaration_list_body) 
-(list_definition_body) 
-(struct_field_declaration_list_body)
-] @indent
+"{" @indent
 
 "}" @indent_end
 
 (comment) @auto
-
-(string) @auto
-
