d5cb786d2c3806b354b3e75a82612441d555b3c3
Olivier Goffart <olivier.goffart@slint-ui.com>
2023-02-20 11:07:48 +0100

    Fix compilation after cherry picks

--- internal/backends/winit/renderer/femtovg/glcontext.rs
+++ internal/backends/winit/renderer/femtovg/glcontext.rs
@@ -61,7 +61,7 @@ impl OpenGLContext {
                 if #[cfg(target_os = "macos")] {
                     let prefs = [glutin::display::DisplayApiPreference::Cgl];
                 } else if #[cfg(all(feature = "x11", not(target_family = "windows")))] {
-                    let prefs = [glutin::display::DisplayApiPreference::Egl, glutin::display::DisplayApiPreference::Glx(Box::new(winit::platform::x11::register_xlib_error_hook))];
+                    let prefs = [glutin::display::DisplayApiPreference::Egl, glutin::display::DisplayApiPreference::Glx(Box::new(winit::platform::unix::register_xlib_error_hook))];
                 } else if #[cfg(not(target_family = "windows"))] {
                     let prefs = [glutin::display::DisplayApiPreference::Egl];
                 } else {
