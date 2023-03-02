1473637ce3fd5442dc786bea8a785abfac84e06e
Simon Hausmann <simon.hausmann@slint-ui.com>
2023-03-02 14:14:45 +0100

    Remove dark-light dependency on the web
    winit 0.28.2 was released today, which supports `Window::theme()` on the
    web.

--- internal/backends/winit/build.rs
+++ internal/backends/winit/build.rs
@@ -7,8 +7,7 @@ fn main() {
     // Setup cfg aliases
     cfg_aliases! {
        enable_skia_renderer: { any(feature = "renderer-winit-skia", feature = "renderer-winit-skia-opengl")},
-       // Upgrade to wasm once https://github.com/rust-windowing/winit/pull/2687 is merged & released.
-       use_winit_theme: { any(target_family = "windows", target_os = "macos", target_os = "ios") },
+       use_winit_theme: { any(target_family = "windows", target_os = "macos", target_os = "ios", target_arch = "wasm32") },
     }
 
     println!("cargo:rerun-if-env-changed=RUST_FONTCONFIG_DLOPEN");
