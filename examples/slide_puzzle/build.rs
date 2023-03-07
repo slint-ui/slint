b22ef6575c3c2d489146800a01f31edbc22b1534
Simon Hausmann <simon.hausmann@slint-ui.com>
2023-03-07 19:04:00 +0100

    MCU WIP: quick and dirty port of the slide_puzzle demo

--- examples/slide_puzzle/build.rs
+++ examples/slide_puzzle/build.rs
@@ -2,5 +2,9 @@
 // SPDX-License-Identifier: MIT
 
 fn main() {
-    slint_build::compile("slide_puzzle.slint").unwrap();
+    let config = slint_build::CompilerConfiguration::new();
+    #[cfg(feature = "mcu-board-support")]
+    let config = config.embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer);
+    slint_build::compile_with_config("slide_puzzle.slint", config).unwrap();
+    slint_build::print_rustc_flags().unwrap();
 }
