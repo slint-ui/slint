f8b1280fe95d4d800811c4e13e5cb6ae852c67ee
Simon Hausmann <simon.hausmann@slint-ui.com>
2023-03-09 18:41:16 +0100

    Add the energy monitor to the launcher

--- demos/energy-monitor/src/main.rs
+++ demos/energy-monitor/src/main.rs
@@ -8,10 +8,23 @@ pub fn main() {
     energy_monitor::main();
 }
 
-#[cfg(feature = "mcu-board-support")]
+#[cfg(all(
+    any(feature = "mcu-board-support", feature = "simulator"),
+    not(feature = "from_launcher")
+))]
 #[mcu_board_support::entry]
 fn main() -> ! {
     mcu_board_support::init();
     energy_monitor::main();
+    panic!("The MCU demo should not quit")
+}
+
+#[cfg(feature = "from_launcher")]
+pub fn main() -> ! {
+    let window = MainWindow::new().unwrap();
+
+    let _kiosk_mode_timer = kiosk_timer(&window);
+
+    window.run().unwrap();
     panic!("The MCU demo should not quit")
 }
