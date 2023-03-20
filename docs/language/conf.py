9da80d213d6efae4cb40a9212c8170ec633e78d3
Simon Hausmann <simon.hausmann@slint-ui.com>
2023-03-20 15:28:21 +0100

    Fix missing navigation sidebar in the slint language docs
    The navigation sidebar requires jquery, which from Sphinx >= 6.0 needs
    to be bundled via a separate extension. In the C++ docs Sphinx remains
    at version 4.5.0 (due to exhale), but the slint language site doesn't
    need exhale (no C++) and thus Pip ends up picking a newer Sphinx
    version.

--- docs/language/conf.py
+++ docs/language/conf.py
@@ -33,7 +33,7 @@ version = "1.0.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser", "sphinx_markdown_tables", "sphinx.ext.autosectionlabel"]
+extensions = ["myst_parser", "sphinx_markdown_tables", "sphinx.ext.autosectionlabel", "sphinxcontrib.jquery"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
