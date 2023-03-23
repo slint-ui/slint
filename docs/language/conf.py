88f9d0aed5302c8d075d31e18f0be91f1f2ee54a
Olivier Goffart <olivier.goffart@slint-ui.com>
2023-03-23 12:10:29 +0100

    Slint docs style cleanups
     - refresh copyright
     - link to slint-ui.com
     - add footer icons

--- docs/language/conf.py
+++ docs/language/conf.py
@@ -1,4 +1,4 @@
-# Copyright © 2021 SixtyFPS GmbH <info@slint-ui.com>
+# Copyright © SixtyFPS GmbH <info@slint-ui.com>
 # SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
 
 # Configuration file for the Sphinx documentation builder.
@@ -21,8 +21,8 @@ import textwrap
 
 # -- Project information -----------------------------------------------------
 
-project = "Slint Language Documentation"
-copyright = "2023, info@slint-ui.com"
+project = "Slint Language"
+copyright = "SixtyFPS GmbH"
 author = "Slint Developers <info@slint-ui.com>"
 
 # The full version, including alpha/beta/rc tags
@@ -57,7 +57,29 @@ exclude_patterns = [
 #
 html_theme = "furo"
 
-html_theme_options = {"collapse_navigation": False}
+html_theme_options = {
+    "collapse_navigation": False,
+    "footer_icons": [
+        {
+            "name": "Slint",
+            "url": "https://slint-ui.com",
+            "html": """
+                <img src="https://slint-ui.com/logo/slint-logo-small-light.svg">
+            """,
+            "class": "",
+        },
+        {
+            "name": "GitHub",
+            "url": "https://github.com/slint-ui/slint",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
