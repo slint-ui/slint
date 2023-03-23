b2b09359df582fabf01e83315bd3592314e772a7
Simon Hausmann <simon.hausmann@slint-ui.com>
2023-03-23 09:18:48 +0100

    Switch to the Furo theme for the Slint Language and C++ documentation

--- docs/language/conf.py
+++ docs/language/conf.py
@@ -55,7 +55,7 @@ exclude_patterns = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 html_theme_options = {"collapse_navigation": False}
 
