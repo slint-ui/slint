caa0684a8315541f73272f4dcd19a328f35a4964
Tobias Hunger <tobias.hunger@slint-ui.com>
2023-03-28 11:18:39 +0200

    online editor: Add dialog for gist export
    Allow to set a description and to make the gist secret

--- tools/online_editor/src/dialogs.ts
+++ tools/online_editor/src/dialogs.ts
@@ -1,11 +1,21 @@
 // Copyright © SixtyFPS GmbH <info@slint-ui.com>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
 
-function modal_dialog(extra_class: string, ...content: HTMLElement[]) {
+function modal_dialog(
+    extra_class: string,
+    content: HTMLElement[],
+    trigger_text = "OK",
+    trigger_action = () => {
+        /**/
+    },
+) {
     const dialog = document.createElement("dialog");
     dialog.classList.add("dialog");
     dialog.classList.add("modal");
     dialog.classList.add(extra_class);
+
+    const titlebar = document.createElement("div");
+    titlebar.classList.add("titlebar");
 
     const close_i = document.createElement("i");
     close_i.classList.add("close_button");
@@ -13,8 +23,10 @@ function modal_dialog(extra_class: string, ...content: HTMLElement[]) {
     close_i.classList.add("fa-times");
     close_i.onclick = () => dialog.close();
 
+    titlebar.appendChild(close_i);
+
     const content_div = document.createElement("div");
-    content_div.classList.add("contents");
+    content_div.classList.add("dialog_content");
 
     for (const c of content) {
         content_div.appendChild(c);
@@ -24,12 +36,15 @@ function modal_dialog(extra_class: string, ...content: HTMLElement[]) {
     button_div.classList.add("button_row");
 
     const ok_button = document.createElement("button");
-    ok_button.innerText = "OK";
-    ok_button.onclick = () => dialog.close();
+    ok_button.innerText = trigger_text;
+    ok_button.onclick = () => {
+        trigger_action();
+        dialog.close();
+    };
 
     button_div.appendChild(ok_button);
 
-    dialog.appendChild(close_i);
+    dialog.appendChild(titlebar);
     dialog.appendChild(content_div);
     dialog.appendChild(button_div);
 
@@ -66,5 +81,36 @@ export function report_export_url_dialog(url: string) {
     url_line_div.appendChild(p_url);
     url_line_div.appendChild(copy_button);
 
-    modal_dialog("report_export_url", p_message, url_line_div);
+    modal_dialog("report_export_url", [p_message, url_line_div]);
+}
+
+export async function export_gist_dialog(
+    exporter: (_description: string, _is_public: boolean) => void,
+) {
+    const description = document.createElement("textarea");
+    description.cols = 80;
+    description.rows = 5;
+    description.autofocus = true;
+    description.placeholder = "Description";
+
+    const is_public_div = document.createElement("div");
+
+    const is_public = document.createElement("input");
+    is_public.type = "checkbox";
+    is_public.id = "is_public";
+    is_public.checked = true;
+
+    const is_public_label = document.createElement("label");
+    is_public_label.innerText = "Create public Gist";
+    is_public_label.htmlFor = "is_public";
+
+    is_public_div.appendChild(is_public);
+    is_public_div.appendChild(is_public_label);
+
+    modal_dialog(
+        "gist_export_dialog",
+        [description, is_public_div],
+        "Export",
+        () => exporter(description.value, is_public.checked),
+    );
 }
