slint-ui/slint

commit cbc0c790a30b428dbb6dafd1313194e9b6d3f831

Make Disagnostice::line_colunm 1 based
Commit 893983e8d320bd248c47f3d57292d432ec4ded69 "fixed" it to be 0 based as it was
documented. But that's a change of behavior so restore the previous behavior and
document it properly

internal/compiler/diagnostics.rs
@@ -91,18 +91,18 @@ impl SourceFileInner {
         Rc::new(Self { path, ..Default::default() })
     }
 
-    /// Returns a tuple with the line (starting at 1) and column number (starting at 0)
+    /// Returns a tuple with the line (starting at 1) and column number (starting at 1)
     pub fn line_column(&self, offset: usize) -> (usize, usize) {
         let line_offsets = self.line_offsets();
         line_offsets.binary_search(&offset).map_or_else(
             |line| {
                 if line == 0 {
-                    (1, offset)
+                    (1, offset + 1)
                 } else {
-                    (line + 1, line_offsets.get(line - 1).map_or(0, |x| offset - x))
+                    (line + 1, line_offsets.get(line - 1).map_or(0, |x| offset - x + 1))
                 }
             },
-            |line| (line + 1, 0),
+            |line| (line + 1, 1),
         )
     }
 
@@ -219,7 +219,7 @@ impl Diagnostic {
         &self.message
     }
 
-    /// Returns a tuple with the line (starting at 1) and column number (starting at 0)
+    /// Returns a tuple with the line (starting at 1) and column number (starting at 1)
     pub fn line_column(&self) -> (usize, usize) {
         let offset = self.span.span.offset;
 

internal/compiler/tests/syntax_tests.rs
@@ -97,7 +97,7 @@ fn process_diagnostics(
     let lines = source
         .bytes()
         .enumerate()
-        .filter_map(|(i, c)| if c == b'\n' { Some(i + 1) } else { None })
+        .filter_map(|(i, c)| if c == b'\n' { Some(i) } else { None })
         .collect::<Vec<usize>>();
 
     // Find expected errors in the file. The first caret (^) points to the expected column. The number of

tools/slintpad/src/index.ts
@@ -478,9 +478,9 @@ function setup(lsp: Lsp) {
             url,
             LspRange.create(
                 start.line - 1,
-                start.column,
+                start.column - 1,
                 end.line - 1,
-                end.column,
+                end.column - 1,
             ),
         );
     };

commit 4076df928d94e63319a6fa49f9a96812847e134c

Fix panic when getting the line, column of diagnostic with invalid span
Introduced in cbc0c790a30b428dbb6dafd1313194e9b6d3f831

internal/compiler/diagnostics.rs
@@ -208,6 +208,7 @@ pub struct Diagnostic {
     level: DiagnosticLevel,
 }
 
+//NOTE! Diagnostic is re-exported in the public API of the interpreter
 impl Diagnostic {
     /// Return the level for this diagnostic
     pub fn level(&self) -> DiagnosticLevel {
@@ -220,7 +221,12 @@ impl Diagnostic {
     }
 
     /// Returns a tuple with the line (starting at 1) and column number (starting at 1)
+    ///
+    /// Can also return (0, 0) if the span is invalid
     pub fn line_column(&self) -> (usize, usize) {
+        if !self.span.span.is_valid() {
+            return (0, 0);
+        }
         let offset = self.span.span.offset;
 
         match &self.span.source_file {

api/cpp/tests/interpreter.cpp
@@ -304,6 +304,8 @@ SCENARIO("Component Compiler")
 
         REQUIRE(diags.size() == 1);
         REQUIRE(diags[0].message.starts_with("Could not load"));
+        REQUIRE(diags[0].line == 0);
+        REQUIRE(diags[0].column == 0);
     }
 
     SECTION("Compile from path")