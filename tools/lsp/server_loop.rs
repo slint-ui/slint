f1bfe21072c7744ae9f45503ba58ec2af5bbcf80
Tobias Hunger <tobias.hunger@slint-ui.com>
2023-04-20 16:49:49 +0200

    LSP: Properly use an URL in showPreview command
    This needs to be a proper URL for the WASM version, so treat it as an
    URL in the stand-alone LS as well.
    
    This way we can treat the WASM and the binary LS more similar to each
    other.

--- tools/lsp/server_loop.rs
+++ tools/lsp/server_loop.rs
@@ -224,7 +224,7 @@ impl DocumentCache {
 
     pub fn offset_to_position_mapper(
         &mut self,
-        uri: &lsp_types::Url,
+        uri: &Url,
     ) -> Result<OffsetToPositionMapper, Error> {
         self.newline_offsets_of_url(uri)
             .map(OffsetToPositionMapper)
@@ -579,12 +579,14 @@ pub fn show_preview_command(params: &[serde_json::Value], ctx: &Rc<Context>) -> 
 
     use crate::preview;
     let e = || "InvalidParameter";
-    let path = if let serde_json::Value::String(s) = params.get(0).ok_or_else(e)? {
-        std::path::PathBuf::from(s)
+    let url = if let serde_json::Value::String(s) = params.get(0).ok_or_else(e)? {
+        Url::parse(&s)?
     } else {
         return Err(e().into());
     };
-    let path_canon = dunce::canonicalize(&path).unwrap_or_else(|_| path.to_owned());
+
+    let path = std::path::PathBuf::from(url.path().to_owned());
+    let path_canon = dunce::canonicalize(&path).unwrap_or(path);
     let component = params.get(1).and_then(|v| v.as_str()).map(|v| v.to_string());
     preview::load_preview(
         connection.clone(),
