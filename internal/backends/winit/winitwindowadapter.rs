slint-ui/slint

commit 7fe4cb39aa249466933a8dd13b72af574ffad2c0

Cleanup: Remove unnecessary WinitWindow trait
WindowAdapter is not generic anymore, we can use it directly.

internal/backends/winit/winitwindowadapter.rs
@@ -13,7 +13,6 @@ use std::rc::Rc;
 #[cfg(target_arch = "wasm32")]
 use std::rc::Weak;
 
-use crate::event_loop::WinitWindow;
 use crate::renderer::WinitCompatibleRenderer;
 use const_field_offset::FieldOffsets;
 
@@ -101,7 +100,7 @@ fn window_is_resizable(min_size: Option<LogicalSize>, max_size: Option<LogicalSi
 
 /// GraphicsWindow is an implementation of the [WindowAdapter][`crate::eventloop::WindowAdapter`] trait. This is
 /// typically instantiated by entry factory functions of the different graphics back ends.
-pub(crate) struct WinitWindowAdapter {
+pub struct WinitWindowAdapter {
     window: OnceCell<corelib::api::Window>,
     #[cfg(target_arch = "wasm32")]
     self_weak: Weak<Self>,
@@ -254,19 +253,17 @@ impl WinitWindowAdapter {
 
         Ok(window_builder)
     }
-}
 
-impl WinitWindow for WinitWindowAdapter {
-    fn take_pending_redraw(&self) -> bool {
+    pub fn take_pending_redraw(&self) -> bool {
         self.pending_redraw.take()
     }
 
-    fn currently_pressed_key_code(&self) -> &Cell<Option<winit::event::VirtualKeyCode>> {
+    pub fn currently_pressed_key_code(&self) -> &Cell<Option<winit::event::VirtualKeyCode>> {
         &self.currently_pressed_key_code
     }
 
     /// Draw the items of the specified `component` in the given window.
-    fn draw(&self) -> Result<bool, PlatformError> {
+    pub fn draw(&self) -> Result<bool, PlatformError> {
         if !self.shown.get() {
             return Ok(false); // caller bug, doesn't make sense to call draw() when not shown
         }
@@ -283,16 +280,16 @@ impl WinitWindow for WinitWindowAdapter {
         callback(&self.winit_window());
     }
 
-    fn winit_window(&self) -> Rc<winit::window::Window> {
+    pub fn winit_window(&self) -> Rc<winit::window::Window> {
         self.winit_window.as_ref().unwrap().clone()
     }
 
-    fn is_shown(&self) -> bool {
+    pub fn is_shown(&self) -> bool {
         self.shown.get()
     }
 
     #[cfg(target_arch = "wasm32")]
-    fn input_method_focused(&self) -> bool {
+    pub fn input_method_focused(&self) -> bool {
         match self.virtual_keyboard_helper.try_borrow() {
             Ok(vkh) => vkh.as_ref().map_or(false, |h| h.has_focus()),
             // the only location in which the virtual_keyboard_helper is mutably borrowed is from
@@ -301,7 +298,12 @@ impl WinitWindow for WinitWindowAdapter {
         }
     }
 
-    fn resize_event(&self, size: winit::dpi::PhysicalSize<u32>) -> Result<(), PlatformError> {
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn input_method_focused(&self) -> bool {
+        false
+    }
+
+    pub fn resize_event(&self, size: winit::dpi::PhysicalSize<u32>) -> Result<(), PlatformError> {
         // slint::Window::set_size will call set_size() on this type, which would call
         // set_inner_size on the winit Window. On Windows that triggers an new resize event
         // in the next event loop iteration for mysterious reasons, with slightly different sizes.
@@ -324,7 +326,7 @@ impl WinitWindow for WinitWindowAdapter {
         }
     }
 
-    fn set_dark_color_scheme(&self, dark_mode: bool) {
+    pub fn set_dark_color_scheme(&self, dark_mode: bool) {
         self.dark_color_scheme
             .get_or_init(|| Box::pin(Property::new(false)))
             .as_ref()

internal/backends/winit/event_loop.rs
@@ -18,8 +18,10 @@ use corelib::api::EventLoopError;
 use corelib::graphics::euclid;
 use corelib::input::{KeyEventType, KeyInputEvent, MouseEvent};
 use corelib::window::*;
-use std::cell::{Cell, RefCell, RefMut};
+use std::cell::{RefCell, RefMut};
 use std::rc::{Rc, Weak};
+
+use crate::winitwindowadapter::WinitWindowAdapter;
 
 use winit::event::WindowEvent;
 #[cfg(not(target_arch = "wasm32"))]
@@ -29,34 +31,6 @@ use crate::SlintUserEvent;
 
 pub(crate) static QUIT_ON_LAST_WINDOW_CLOSED: std::sync::atomic::AtomicBool =
     std::sync::atomic::AtomicBool::new(true);
-
-pub trait WinitWindow: WindowAdapter {
-    fn currently_pressed_key_code(&self) -> &Cell<Option<winit::event::VirtualKeyCode>>;
-    /// Returns true if during the drawing request_redraw() was called.
-    fn draw(&self) -> Result<bool, i_slint_core::platform::PlatformError>;
-    fn with_window_handle(&self, callback: &mut dyn FnMut(&winit::window::Window));
-    fn winit_window(&self) -> Rc<winit::window::Window>;
-    fn is_shown(&self) -> bool;
-
-    /// Called by the event loop when a WindowEvent::Resized is received.
-    fn resize_event(
-        &self,
-        _size: winit::dpi::PhysicalSize<u32>,
-    ) -> Result<(), i_slint_core::platform::PlatformError> {
-        Ok(())
-    }
-
-    /// Return true if the proxy element used for input method has the focus
-    fn input_method_focused(&self) -> bool {
-        false
-    }
-    /// Returns true if request_redraw() was called since the last event loop iteration
-    /// and resets the state back to false.
-    fn take_pending_redraw(&self) -> bool;
-
-    /// Notify the window if the system theme has changed from light to dark mode
-    fn set_dark_color_scheme(&self, dark_mode: bool);
-}
 
 /// The Default, and the selection clippoard
 type ClipboardPair = (Box<dyn ClipboardProvider>, Box<dyn ClipboardProvider>);
@@ -163,7 +137,7 @@ impl<'a> EventLoopInterface for RunningEventLoop<'a> {
 }
 
 thread_local! {
-    static ALL_WINDOWS: RefCell<std::collections::HashMap<winit::window::WindowId, Weak<dyn WinitWindow>>> = RefCell::new(std::collections::HashMap::new());
+    static ALL_WINDOWS: RefCell<std::collections::HashMap<winit::window::WindowId, Weak<WinitWindowAdapter>>> = RefCell::new(std::collections::HashMap::new());
     static MAYBE_LOOP_INSTANCE: RefCell<Option<NotRunningEventLoop>> = RefCell::new(Some(NotRunningEventLoop::new()));
 }
 
@@ -229,7 +203,7 @@ pub(crate) fn with_window_target<T>(callback: impl FnOnce(&dyn EventLoopInterfac
     }
 }
 
-pub fn register_window(id: winit::window::WindowId, window: Rc<dyn WinitWindow>) {
+pub fn register_window(id: winit::window::WindowId, window: Rc<WinitWindowAdapter>) {
     ALL_WINDOWS.with(|windows| {
         windows.borrow_mut().insert(id, Rc::downgrade(&window));
     })
@@ -241,7 +215,7 @@ pub fn unregister_window(id: winit::window::WindowId) {
     });
 }
 
-fn window_by_id(id: winit::window::WindowId) -> Option<Rc<dyn WinitWindow>> {
+fn window_by_id(id: winit::window::WindowId) -> Option<Rc<WinitWindowAdapter>> {
     ALL_WINDOWS.with(|windows| windows.borrow().get(&id).and_then(|weakref| weakref.upgrade()))
 }
 
@@ -290,7 +264,7 @@ mod key_codes {
 }
 
 fn process_window_event(
-    window: Rc<dyn WinitWindow>,
+    window: Rc<WinitWindowAdapter>,
     event: WindowEvent,
     cursor_pos: &mut LogicalPoint,
     pressed: &mut bool,

internal/backends/winit/lib.rs
@@ -6,7 +6,7 @@
 
 extern crate alloc;
 
-use event_loop::{CustomEvent, WinitWindow};
+use event_loop::CustomEvent;
 use i_slint_core::platform::EventLoopProxy;
 use i_slint_core::window::WindowAdapter;
 use renderer::WinitCompatibleRenderer;

commit b1073b2ee37f6c807388373526ee486f2d44a351

Fix live-preview windows not closing when the user tries to close them
This is a regression from https://github.com/slint-ui/slint/pull/2708,
where previously we would have destroyed the winit window and that's how
it would have disappeared. Now we just need to call set_visible(false);

internal/backends/winit/winitwindowadapter.rs
@@ -595,6 +595,8 @@ impl WindowAdapterSealed for WinitWindowAdapter {
 
             self_.renderer().hide()?;
 
+            self_.winit_window().set_visible(false);
+
             /* FIXME:
             if let Some(existing_blinker) = self.cursor_blinker.borrow().upgrade() {
                 existing_blinker.stop();