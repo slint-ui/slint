439b0c09d89acee153e1391b2d66373ca9d9a749
Simon Hausmann <simon.hausmann@slint-ui.com>
2023-05-31 17:16:06 +0200

    Try to improve the documentation for ModelRc (#2791)
    - Mention in the type mapping table as well as in ModelRc that ModelRc is also used for array struct fields.
    - Use an itemized list for the different ways of constructing, with the from_slice variant being listed second.
    
    cc #2787

--- api/rs/slint/lib.rs
+++ api/rs/slint/lib.rs
@@ -185,14 +185,15 @@ The follow table summarizes the entire mapping:
 | `relative-font-size` | `f32` | Relative font size factor that is multiplied with the `Window.default-font-size` and can be converted to a `length`. |
 | structure | `struct` of the same name | |
 | anonymous object | anonymous tuple | The fields are in alphabetical order. |
-| array | [`ModelRc`] |  |
+| array | [`ModelRc`] | Arrays are represented as models, so that their contents can change dynamically. |
 
 For user defined structures in the .slint, an extra struct is generated.
 For example, if the `.slint` contains
-```slint,ignore
+```slint,no-preview
 export struct MyStruct {
     foo: int,
     bar: string,
+    names: [string],
 }
 ```
 
@@ -203,6 +204,7 @@ The following struct would be generated:
 struct MyStruct {
     foo : i32,
     bar: slint::SharedString,
+    names: slint::ModelRc<slint::SharedString>,
 }
 ```
 
