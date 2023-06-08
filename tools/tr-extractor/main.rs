15541bceeb9d411ab63a3173404c58aa6ca253e8
Olivier Goffart <olivier.goffart@slint-ui.com>
2023-06-08 13:13:13 +0200

    slint-tr-extractor: extract the comment

--- tools/tr-extractor/main.rs
+++ tools/tr-extractor/main.rs
@@ -121,18 +121,133 @@ fn visit_node(node: SyntaxNode, results: &mut Messages) {
                     }
                 }
 
-                /* TODO
-                let mut comments = get_comment_before_line(&self.source_lines, line);
-                if comments.is_none() {
-                    let ident_line = ident_span.start().line;
-                    if ident_line != line {
-                        comments = get_comment_before_line(&self.source_lines, ident_line);
-                    }
+                if message.comments.is_none() {
+                    message.comments = tr
+                        .child_token(SyntaxKind::StringLiteral)
+                        .and_then(get_comments_before_line)
+                        .or_else(|| tr.first_token().and_then(get_comments_before_line));
                 }
-                message.comments = comments;
-                */
             }
         }
         visit_node(n, results)
     }
+}
+
+fn get_comments_before_line(token: i_slint_compiler::parser::SyntaxToken) -> Option<String> {
+    let mut token = token.prev_token()?;
+    loop {
+        dbg!(&token);
+        if token.kind() == SyntaxKind::Whitespace {
+            let mut lines = token.text().lines();
+            lines.next();
+            if lines.next().is_some() {
+                // One \n
+                if lines.next().is_some() {
+                    return None; // two \n or more
+                }
+                token = token.prev_token()?;
+                if token.kind() == SyntaxKind::Comment && token.text().starts_with("//") {
+                    return Some(token.text().trim_start_matches('/').trim().into());
+                }
+                return None;
+            }
+        }
+        token = token.prev_token()?;
+    }
+}
+
+#[test]
+fn extract_messages() {
+    fn make(msg: &str, p: &str, ctx: &str, co: &str, loc: &[usize]) -> Message {
+        let opt = |x: &str| {
+            if x.is_empty() {
+                None
+            } else {
+                Some(x.to_owned())
+            }
+        };
+        let locations = loc
+            .iter()
+            .map(|l| messages::Location { file: "test.slint".to_owned().into(), line: *l })
+            .collect();
+        Message {
+            msgctxt: opt(ctx),
+            msgid: msg.into(),
+            plural: opt(p),
+            locations,
+            comments: opt(co),
+            index: 0,
+        }
+    }
+
+    let source = r##"export component Foo {
+        // comment 1
+        x: @tr("Message 1");
+        // comment does not count
+
+        // comment 2
+        y: @tr("ctx" => "Message 2");
+        // comment  does not count
+
+        z: @tr("Message 3" | "Messages 3" % x);
+
+        // comment 4
+        a: @tr("ctx4" => "Message 4" | "Messages 4" % x);
+
+        //recursive
+        rec: @tr("rec1 {}", @tr("rec2"));
+
+        nl: @tr("rw\nctx" => "r\nw");
+
+        // comment does not count : xgettext takes the comment next to the string
+        xx: @tr(
+            //multi line
+            "multi-line\nsecond line"
+        );
+
+        d: @tr("dup1");
+        d: @tr("ctx" => "dup1");
+        d: @tr("dup1");
+        d: @tr("ctx" => "dup1");
+
+        // two-line-comment
+        // macro and string on different line
+        x: @tr(
+            "x"
+        );
+
+    }"##;
+
+    let r = [
+        make("Message 1", "", "", "comment 1", &[3]),
+        make("Message 2", "", "ctx", "comment 2", &[7]),
+        make("Message 3", "Messages 3", "", "", &[10]),
+        make("Message 4", "Messages 4", "ctx4", "comment 4", &[13]),
+        make("rec1 {}", "", "", "recursive", &[16]),
+        make("rec2", "", "", "recursive", &[16]),
+        make("r\nw", "", "rw\nctx", "", &[18]),
+        make("multi-line\nsecond line", "", "", "multi line", &[21]),
+        make("dup1", "", "", "", &[26, 28]),
+        make("dup1", "", "ctx", "", &[27, 29]),
+        make("x", "", "", "macro and string on different line", &[33]),
+    ];
+
+    let mut diag = BuildDiagnostics::default();
+    let syntax_node = i_slint_compiler::parser::parse(
+        source.into(),
+        Some(std::path::Path::new("test.slint")),
+        &mut diag,
+    );
+
+    let mut messages = Messages::new();
+    visit_node(syntax_node, &mut messages);
+
+    let mut messages = messages.into_values().collect::<Vec<_>>();
+    messages.sort_by_key(|m| m.index);
+    let mlen = messages.len();
+    for (a, mut b) in r.iter().zip(messages) {
+        b.index = 0;
+        assert_eq!(*a, b);
+    }
+    assert_eq!(r.len(), mlen);
 }
