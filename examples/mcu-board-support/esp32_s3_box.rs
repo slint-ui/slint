5ea539002757d8ad0e5056da3fee3defe79ade65
Simon Hausmann <simon.hausmann@slint-ui.com>
2023-06-21 22:12:57 +0200

    Fix ESP32 S3 Box build
    With the latest esp toolchain the functions in esp32_s3_box.rs are
    duplicated.

--- examples/mcu-board-support/esp32_s3_box.rs
+++ examples/mcu-board-support/esp32_s3_box.rs
@@ -226,29 +226,3 @@ impl<
             .unwrap();
     }
 }
-
-// FIXME: implement properly upstream
-#[no_mangle]
-extern "C" fn fmaxf(a: f32, b: f32) -> f32 {
-    if a > b {
-        a
-    } else {
-        b
-    }
-}
-#[no_mangle]
-extern "C" fn fminf(a: f32, b: f32) -> f32 {
-    if a < b {
-        a
-    } else {
-        b
-    }
-}
-#[no_mangle]
-extern "C" fn fmodf(a: f32, b: f32) -> f32 {
-    ((a as u32) % (b as u32)) as f32
-}
-#[no_mangle]
-extern "C" fn fmod(a: f64, b: f64) -> f64 {
-    ((a as u32) % (b as u32)) as f64
-}
