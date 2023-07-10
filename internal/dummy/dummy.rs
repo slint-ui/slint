5c94a9e32214e9b4163708b902f851a4ce0a6529
Simon Hausmann <simon.hausmann@slint-ui.com>
2023-07-10 09:45:02 +0200

    Remove unused Royalty Free 1.0 license

--- internal/dummy/dummy.rs
+++
@@ -1,4 +0,0 @@
-// Copyright © SixtyFPS GmbH <info@slint.dev>
-// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.0 OR LicenseRef-Slint-commercial
-
-//! This is a dummy file
