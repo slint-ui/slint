36eaad96ad1d2c44a0172a2088f2e8a3b5d40923
Simon Hausmann <simon.hausmann@slint-ui.com>
2023-07-21 14:55:47 +0200

    Add support for terminating the app via ctrl+alt+backspace and ctrl+alt+delete

--- internal/backends/linuxkms/calloop_backend/input.rs
+++ internal/backends/linuxkms/calloop_backend/input.rs
@@ -12,8 +12,10 @@ use i_slint_core::api::LogicalPosition;
 use i_slint_core::platform::{PlatformError, PointerEventButton, WindowEvent};
 use input::LibinputInterface;
 
+use input::event::keyboard::KeyboardEventTrait;
 use input::event::touch::TouchEventPosition;
 use libc::{O_RDONLY, O_RDWR, O_WRONLY};
+use xkbcommon::*;
 
 struct Interface;
 
@@ -38,6 +40,7 @@ pub struct LibInputHandler<'a> {
     mouse_pos: LogicalPosition,
     last_touch_pos: LogicalPosition,
     window: &'a i_slint_core::api::Window,
+    keystate: xkb::State,
 }
 
 impl<'a> LibInputHandler<'a> {
@@ -48,12 +51,18 @@ impl<'a> LibInputHandler<'a> {
         let mut libinput = input::Libinput::new_with_udev(Interface);
         libinput.udev_assign_seat("seat0").unwrap();
 
+        let xkb_context = xkb::Context::new(xkb::CONTEXT_NO_FLAGS);
+        let keymap = xkb::Keymap::new_from_names(&xkb_context, "", "", "", "", None, 0)
+            .ok_or_else(|| format!("Error compiling keymap"))?;
+        let keystate = xkb::State::new(&keymap);
+
         let handler = Self {
             libinput,
             token: Default::default(),
             mouse_pos: Default::default(),
             last_touch_pos: Default::default(),
             window,
+            keystate,
         };
 
         event_loop_handle
@@ -150,6 +159,48 @@ impl<'a> calloop::EventSource for LibInputHandler<'a> {
                         _ => None,
                     } {
                         self.window.dispatch_event(event);
+                    }
+                }
+                input::Event::Keyboard(input::event::KeyboardEvent::Key(key_event)) => {
+                    // On Linux key codes have a fixed offset of 8: https://docs.rs/xkbcommon/0.5.0/xkbcommon/xkb/type.Keycode.html
+                    let key_code = key_event.key() + 8;
+                    let state = key_event.key_state();
+
+                    let sym = self.keystate.key_get_one_sym(key_code);
+
+                    self.keystate.update_key(
+                        key_code,
+                        match state {
+                            input::event::tablet_pad::KeyState::Pressed => xkb::KeyDirection::Down,
+                            input::event::tablet_pad::KeyState::Released => xkb::KeyDirection::Up,
+                        },
+                    );
+
+                    let control = self
+                        .keystate
+                        .mod_name_is_active(xkb::MOD_NAME_CTRL, xkb::STATE_MODS_EFFECTIVE);
+                    let alt = self
+                        .keystate
+                        .mod_name_is_active(xkb::MOD_NAME_ALT, xkb::STATE_MODS_EFFECTIVE);
+
+                    if state == input::event::keyboard::KeyState::Pressed {
+                        //eprintln!(
+                        //"key {} state {:#?} sym {:x} control {control} alt {alt}",
+                        //key_code, state, sym
+                        //);
+
+                        if control && alt && sym == xkb::KEY_BackSpace
+                            || control && alt && sym == xkb::KEY_Delete
+                        {
+                            i_slint_core::api::quit_event_loop()
+                                .expect("Unable to quit event loop multiple times");
+                        } else {
+                            if (xkb::KEY_XF86Switch_VT_1..=xkb::KEY_XF86Switch_VT_12).contains(&sym)
+                            {
+                                // let target_vt = (sym - xkb::KEY_XF86Switch_VT_1 + 1) as i32;
+                                // TODO: eprintln!("switch vt {target_vt}");
+                            }
+                        }
                     }
                 }
                 _ => {}
