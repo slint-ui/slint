930be7e771894c57234d4643c2a28160c16f78f8
Guilhem Vallat <guilhem.vallat@gmail.com>
2023-07-25 11:52:11 +0200

    WIP Take new geometry into account in useless_rectangle

--- internal/compiler/passes/optimize_useless_rectangles.rs
+++ internal/compiler/passes/optimize_useless_rectangles.rs
@@ -6,8 +6,11 @@
 //! Rectangles which do not draw anything and have no x or y don't need to be in
 //! the item tree, we can just remove them.
 
-use crate::langtype::ElementType;
+use crate::expression_tree::{BindingExpression, Expression};
+use crate::langtype::{ElementType, Type};
+use crate::namedreference::NamedReference;
 use crate::object_tree::*;
+use std::cell::RefCell;
 use std::rc::Rc;
 
 pub fn optimize_useless_rectangles(root_component: &Rc<Component>) {
@@ -16,17 +19,23 @@ pub fn optimize_useless_rectangles(root_component: &Rc<Component>) {
         let children = std::mem::take(&mut parent.children);
 
         for elem in children {
-            if !can_optimize(&elem) {
+            let res = can_optimize(&elem);
+            if !res.can_optimize {
                 parent.children.push(elem);
                 continue;
             }
 
-            parent.children.extend(std::mem::take(&mut elem.borrow_mut().children));
+            let children = std::mem::take(&mut elem.borrow_mut().children);
+            for child in &children {
+                patch_position(child, res.x.as_ref(), res.y.as_ref());
+            }
+            parent.children.extend(children);
 
             let enclosing = parent.enclosing_component.upgrade().unwrap();
 
             for popup in enclosing.popup_windows.borrow_mut().iter_mut() {
                 if Rc::ptr_eq(&popup.parent_element, &elem) {
+                    // TODO patch x/y
                     // parent element is use for x/y, and the position of the removed element is 0,0
                     popup.parent_element = parent_.clone();
                 }
@@ -37,16 +46,90 @@ pub fn optimize_useless_rectangles(root_component: &Rc<Component>) {
     });
 }
 
+/// Used for uniquely name some variables
+static COUNTER: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(1);
+
+fn patch_position(
+    elem: &ElementRc,
+    parent_x: Option<&NamedReference>,
+    parent_y: Option<&NamedReference>,
+) {
+    if elem.borrow().repeated.is_some() {
+        return;
+    }
+    let geo = elem.borrow_mut().geometry_props.take();
+    if let Some(mut geo) = geo {
+        if let Some(x) = parent_x {
+            let id = COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+            let new_x_name = format!("x-optimized-{id}");
+            elem.borrow_mut().property_declarations.insert(
+                new_x_name.clone(),
+                PropertyDeclaration {
+                    property_type: Type::LogicalLength,
+                    ..PropertyDeclaration::default()
+                },
+            );
+
+            elem.borrow_mut().bindings.insert(
+                new_x_name.clone(),
+                RefCell::new(BindingExpression::from(Expression::BinaryExpression {
+                    lhs: Box::new(Expression::PropertyReference(x.clone())),
+                    rhs: Box::new(Expression::PropertyReference(geo.x.clone())),
+                    op: '+',
+                })),
+            );
+
+            geo.x = NamedReference::new(elem, &new_x_name);
+        }
+
+        if let Some(y) = parent_y {
+            let id = COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+            let new_y_name = format!("y-optimized-{id}");
+            elem.borrow_mut().property_declarations.insert(
+                new_y_name.clone(),
+                PropertyDeclaration {
+                    property_type: Type::LogicalLength,
+                    ..PropertyDeclaration::default()
+                },
+            );
+
+            elem.borrow_mut().bindings.insert(
+                new_y_name.clone(),
+                RefCell::new(BindingExpression::from(Expression::BinaryExpression {
+                    lhs: Box::new(Expression::PropertyReference(y.clone())),
+                    rhs: Box::new(Expression::PropertyReference(geo.y.clone())),
+                    op: '+',
+                })),
+            );
+
+            geo.y = NamedReference::new(elem, &new_y_name);
+        }
+        elem.borrow_mut().geometry_props = Some(geo);
+    }
+}
+
+struct OptimizeResult {
+    can_optimize: bool,
+    x: Option<NamedReference>,
+    y: Option<NamedReference>,
+}
+
+impl OptimizeResult {
+    fn keep() -> Self {
+        Self { can_optimize: false, x: None, y: None }
+    }
+}
+
 /// Check that this is a element we can optimize
-fn can_optimize(elem: &ElementRc) -> bool {
+fn can_optimize(elem: &ElementRc) -> OptimizeResult {
     let e = elem.borrow();
     if e.is_flickable_viewport || e.has_popup_child {
-        return false;
+        return OptimizeResult::keep();
     };
 
     if e.child_of_layout {
         // The `LayoutItem` still has reference to this component, so we cannot remove it
-        return false;
+        return OptimizeResult::keep();
     }
 
     let base_type = match &e.base_type {
@@ -54,26 +137,33 @@ fn can_optimize(elem: &ElementRc) -> bool {
         ElementType::Builtin(base_type) if base_type.native_class.class_name == "Empty" => {
             base_type
         }
-        _ => return false,
+        _ => return OptimizeResult::keep(),
     };
 
-    if let Some(g) = &e.geometry_props {
-        for p in [&g.x, &g.y] {
-            let e = p.element();
-            let e = e.borrow();
-            if e.property_analysis.borrow().get(p.name()).map(|v| v.is_set) == Some(true)
-                || e.bindings.get(p.name()).is_some()
-            {
-                return false;
-            }
-        }
-    }
+    let (x, y) = if let Some(g) = &e.geometry_props {
+        let ex = g.x.element();
+        let ex = ex.borrow();
+        let x = if ex.bindings.get(g.x.name()).is_some() { Some(g.x.clone()) } else { None };
+
+        let ey = g.y.element();
+        let ey = ey.borrow();
+        let y = if ey.bindings.get(g.y.name()).is_some() { Some(g.y.clone()) } else { None };
+
+        (x, y)
+    } else {
+        (None, None)
+    };
 
     // Check that no Rectangle property other than height and width are set
     let analysis = e.property_analysis.borrow();
-    !e.bindings.keys().chain(analysis.iter().filter(|(_, v)| v.is_set).map(|(k, _)| k)).any(|k| {
-        !matches!(k.as_str(), "height" | "width")
-            && !e.property_declarations.contains_key(k.as_str())
-            && base_type.properties.contains_key(k.as_str())
-    })
+    let can_optimize =
+        !e.bindings.keys().chain(analysis.iter().filter(|(_, v)| v.is_set).map(|(k, _)| k)).any(
+            |k| {
+                !matches!(k.as_str(), "height" | "width")
+                    && !e.property_declarations.contains_key(k.as_str())
+                    && base_type.properties.contains_key(k.as_str())
+            },
+        );
+
+    OptimizeResult { can_optimize, x, y }
 }
