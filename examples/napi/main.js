a2f57e5fe47a856ad40404dfe36782d5038d54dd
Florian Blasius <florian.blasius@slint-ui.com>
2023-07-27 08:34:12 +0200

    Fix licenses for napi stuff.

--- examples/napi/main.js
+++
@@ -1,9 +0,0 @@
-// Copyright © SixtyFPS GmbH <info@slint-ui.com>
-// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-commercial
-
-import { ComponentCompiler } from 'slint-ui';
-
-let compiler = new ComponentCompiler();
-let definition = compiler.buildFromPath("window.slint");
-let instance = definition.create();
-instance.run();
