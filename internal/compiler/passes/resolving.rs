a310fdc912a330c996735becde76625d6eff153f
Olivier Goffart <olivier.goffart@slint.dev>
2023-08-09 19:18:20 +0200

    Fix panic when parsing invalid gradiant
    Fixes #3241

--- internal/compiler/passes/resolving.rs
+++ internal/compiler/passes/resolving.rs
@@ -486,7 +486,10 @@ impl Expression {
                 rest.iter().position(|s| !matches!(s.1, Expression::Invalid)).unwrap_or(rest.len());
             if pos > 0 && pos < rest.len() {
                 let (middle, after) = rest.split_at_mut(pos);
-                let begin = &before.last().expect("The first should never be invalid").1;
+                let begin = before
+                    .last()
+                    .map(|s| &s.1)
+                    .unwrap_or(&Expression::NumberLiteral(1., Unit::None));
                 let end = &after.first().expect("The last should never be invalid").1;
                 for (i, (_, e)) in middle.iter_mut().enumerate() {
                     debug_assert!(matches!(e, Expression::Invalid));
