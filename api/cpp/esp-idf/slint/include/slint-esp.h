354208c1234bd24d451c9b9e7fcc83ff5d5dbf47
Olivier Goffart <olivier.goffart@slint.dev>
2023-08-21 12:19:57 +0200

    esp-idf: Don't expose the platform: just a single function instead

---
+++ api/cpp/esp-idf/slint/include/slint-esp.h
@@ -0,0 +1,13 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.1 OR LicenseRef-Slint-commercial
+
+#pragma once
+
+#include "slint-platform.h"
+#include "esp_lcd_touch.h"
+#include "esp_lcd_types.h"
+
+void slint_esp_init(slint::PhysicalSize size, esp_lcd_panel_handle_t panel,
+                    std::optional<esp_lcd_touch_handle_t> touch,
+                    std::span<slint::platform::Rgb565Pixel> buffer1,
+                    std::optional<std::span<slint::platform::Rgb565Pixel>> buffer2 = {});
