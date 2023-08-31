9ebce3410716df93bf3f97b0dc21e6db32adfc58
Tobias Hunger <tobias.hunger@slint.dev>
2023-08-31 18:43:49 +0200

    LSP: Remove design mode custom commands

--- tools/lsp/common.rs
+++ tools/lsp/common.rs
@@ -11,8 +11,6 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// API used by the LSP to talk to the Preview. The other direction uses the
 /// ServerNotifier
 pub trait PreviewApi {
-    fn set_design_mode(&self, enable: bool);
-    fn design_mode(&self) -> bool;
     fn set_contents(&self, path: &Path, contents: &str);
     fn load_preview(&self, component: PreviewComponent, behavior: PostLoadBehavior);
     fn config_changed(&self, style: &str, include_paths: &[PathBuf]);
