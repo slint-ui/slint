30136c93a4e917249fcbb87bfde3fd679a1d4838
tronical <tronical@users.noreply.github.com>
2023-09-05 11:58:51 +0000

    Bump version number to 1.3.0

--- api/cpp/docs/conf.py
+++ api/cpp/docs/conf.py
@@ -22,7 +22,7 @@ import textwrap
 # -- Project information -----------------------------------------------------
 
 # The full version, including alpha/beta/rc tags
-version = "1.2.0"
+version = "1.3.0"
 
 project = f'Slint {version} C++ API'
 copyright = "SixtyFPS GmbH"
