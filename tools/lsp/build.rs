e50c577f4a532a1a268e9637b5e20746968da280
Tobias Hunger <tobias.hunger@slint-ui.com>
2023-09-14 17:42:30 +0200

    lsp: Use build.rs to enable experimental features in slint

---
+++ tools/lsp/build.rs
@@ -0,0 +1,6 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.1 OR LicenseRef-Slint-commercial
+
+fn main() {
+    println!("cargo:rustc-env=SLINT_ENABLE_EXPERIMENTAL_FEATURES=1");
+}
