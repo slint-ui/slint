slint-ui/slint

commit f1f5a86d0d75e139baff413b2079d6545369c0d9

Fix calling the `open_import_fallback` for non existing files
(Note: This code path is not run for wasm because dounce::canonicalize
is not used there, but it was used for the test)

internal/compiler/typeloader.rs
@@ -313,13 +313,30 @@ impl TypeLoader {
                     (path, None)
                 }
                 Err(err) if err.kind() == std::io::ErrorKind::NotFound => {
-                    borrowed_state.diag.push_error(
-                            format!(
-                                "Cannot find requested import \"{file_to_import}\" in the include search path",
-                            ),
-                            &import_token,
-                        );
-                    return None;
+                    // We will load using the `open_import_fallback`
+                    // Simplify the path to remove the ".."
+                    let mut path = import_token
+                        .as_ref()
+                        .and_then(|tok| tok.source_file().map(|s| s.path()))
+                        .and_then(|p| p.parent())
+                        .map_or(PathBuf::new(), |p| p.into());
+                    for c in Path::new(file_to_import).components() {
+                        use std::path::Component::*;
+                        match c {
+                            RootDir => path = PathBuf::new(),
+                            CurDir => {}
+                            ParentDir
+                                if matches!(
+                                    path.components().last(),
+                                    Some(Normal(_) | RootDir)
+                                ) =>
+                            {
+                                path.pop();
+                            }
+                            Prefix(_) | ParentDir | Normal(_) => path.push(c),
+                        }
+                    }
+                    (path, None)
                 }
                 Err(err) => {
                     borrowed_state.diag.push_error(
@@ -393,6 +410,15 @@ impl TypeLoader {
                 )
                 .await;
                 true
+            }
+            Err(err) if err.kind() == std::io::ErrorKind::NotFound => {
+                state.borrow_mut().diag.push_error(
+                        format!(
+                            "Cannot find requested import \"{file_to_import}\" in the include search path",
+                        ),
+                        &import_token,
+                    );
+                return None;
             }
             Err(err) => {
                 state.borrow_mut().diag.push_error(
@@ -738,7 +764,7 @@ fn test_load_from_callback_ok() {
     compiler_config.open_import_fallback = Some(Rc::new(move |path| {
         let ok_ = ok_.clone();
         Box::pin(async move {
-            assert_eq!(path, "../FooBar.slint");
+            assert_eq!(path.replace('\\', "/"), "../FooBar.slint");
             assert!(!ok_.get());
             ok_.set(true);
             Some(Ok("export XX := Rectangle {} ".to_owned()))
@@ -749,7 +775,7 @@ fn test_load_from_callback_ok() {
     let doc_node = crate::parser::parse(
         r#"
 /* ... */
-import { XX } from "../FooBar.slint";
+import { XX } from "../Ab/.././FooBar.slint";
 X := XX {}
 "#
         .into(),

commit df026a56cccc588dd834fe0dc926e1f5e14cb067

compiler: Fix loading the same file twice would result in infinite pending state
The problem is that the file stays in the `currently_loading` hashmap
and therefore we thinkg we need to await for the result, while it is not
actually loading

Regression since f1f5a86d0d75e139baff413b2079d6545369c0d9

Fixes #3468

internal/compiler/typeloader.rs
@@ -420,7 +420,7 @@ impl TypeLoader {
                         ),
                         &import_token,
                     );
-                return None;
+                false
             }
             Err(err) => {
                 state.borrow_mut().diag.push_error(
@@ -798,6 +798,56 @@ X := XX {}
     assert!(ok.get());
     assert!(!test_diags.has_error());
     assert!(!build_diagnostics.has_error());
+}
+
+#[test]
+fn test_load_error_twice() {
+    let mut compiler_config =
+        CompilerConfiguration::new(crate::generator::OutputFormat::Interpreter);
+    compiler_config.style = Some("fluent".into());
+    let mut test_diags = crate::diagnostics::BuildDiagnostics::default();
+
+    let doc_node = crate::parser::parse(
+        r#"
+/* ... */
+import { XX } from "error.slint";
+component Foo { XX {} }
+"#
+        .into(),
+        Some(std::path::Path::new("HELLO")),
+        &mut test_diags,
+    );
+
+    let doc_node: syntax_nodes::Document = doc_node.into();
+    let global_registry = TypeRegister::builtin();
+    let registry = Rc::new(RefCell::new(TypeRegister::new(&global_registry)));
+    let mut build_diagnostics = BuildDiagnostics::default();
+    let mut loader = TypeLoader::new(global_registry, compiler_config, &mut build_diagnostics);
+    spin_on::spin_on(loader.load_dependencies_recursively(
+        &doc_node,
+        &mut build_diagnostics,
+        &registry,
+    ));
+    assert!(!test_diags.has_error());
+    assert!(build_diagnostics.has_error());
+    let diags = build_diagnostics.to_string_vec();
+    assert_eq!(
+        diags,
+        &["HELLO:3: Cannot find requested import \"error.slint\" in the include search path"]
+    );
+    // Try loading another time with the same registery
+    let mut build_diagnostics = BuildDiagnostics::default();
+    spin_on::spin_on(loader.load_dependencies_recursively(
+        &doc_node,
+        &mut build_diagnostics,
+        &registry,
+    ));
+    assert!(build_diagnostics.has_error());
+    let diags = build_diagnostics.to_string_vec();
+    assert_eq!(
+        diags,
+        &["HELLO:3: Cannot find requested import \"error.slint\" in the include search path"]
+    );
 }
 
 #[test]