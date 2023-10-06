e34cd741a8678e87c8fb1e713808803426df12e1
Florian Blasius <florian.blasius@slint.dev>
2023-10-06 12:39:17 +0200

    Create components from a slint file for napi (#3566)
    * Get todo example running with napi.
    
    * Code review fixes.
    
    * Code review feedback.
    
    * Code review fixes.
    
    * docs fix
    
    * Update api/napi/__test__/resources/error.slint
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update api/napi/__test__/resources/test.slint
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * fix test
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ api/napi/__test__/api.spec.ts
@@ -0,0 +1,46 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.1 OR LicenseRef-Slint-commercial
+
+import test from 'ava'
+const path = require('node:path');
+
+import { loadFile, CompilerError, Diagnostic } from '../index'
+
+test('loadFile', (t) => {
+    let demo = loadFile(path.join(__dirname, "resources/test.slint"));
+    let test = new demo.Test();
+    t.is(test.check, "Test");
+
+    let errorPath = path.join(__dirname, "resources/error.slint");
+
+    const error = t.throws(() => {
+        loadFile(errorPath)
+        },
+        {instanceOf: CompilerError}
+    );
+
+    t.is(error?.message, "Could not compile " + errorPath);
+    t.deepEqual(error?.diagnostics, [
+        {
+            column: 18,
+            level: 0,
+            lineNumber: 7,
+            message: 'Missing type. The syntax to declare a property is `property <type> name;`. Only two way bindings can omit the type',
+            sourceFile: errorPath
+        },
+        {
+            column: 22,
+            level: 0,
+            lineNumber: 7,
+            message: 'Syntax error: expected \';\'',
+            sourceFile: errorPath
+        },
+        {
+            column: 22,
+            level: 0,
+            lineNumber: 7,
+            message: 'Parse error',
+            sourceFile: errorPath
+        },
+    ]);
+})
--- api/napi/index.ts
+++ api/napi/index.ts
@@ -1,6 +1,7 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.1 OR LicenseRef-Slint-commercial
 
+import { ComponentCompiler, ComponentInstance, Window, DiagnosticLevel, Diagnostic } from "./napi";
 export * from "./napi";
 
 /**
@@ -145,3 +146,109 @@ export class ArrayModel<T> implements Model<T> {
         return this.a.entries()
     }
 }
+
+export interface ComponentHandle {
+    run();
+    show();
+    hide();
+    get window(): Window;
+}
+
+class Component implements ComponentHandle {
+    private instance: ComponentInstance;
+
+    /**
+    * @hidden
+    */
+    constructor(instance: ComponentInstance) {
+        this.instance = instance;
+    }
+
+    run() {
+        this.instance.run();
+    }
+
+    show() {
+        this.instance.window().show();
+    }
+
+    hide() {
+        this.instance.window().hide();
+    }
+
+    get window(): Window {
+        return this.instance.window();
+    }
+}
+
+/**
+ * @hidden
+ */
+interface Callback {
+    (): any;
+    setHandler(cb: any): void;
+}
+
+export class CompilerError extends Error {
+    public diagnostics: Diagnostic[];
+
+    constructor(message: string, diagnostics: Diagnostic[]) {
+        super(message);
+        this.diagnostics = diagnostics;
+    }
+}
+
+export function loadFile(path: string) : Object {
+    let compiler = new ComponentCompiler;
+    let definition = compiler.buildFromPath(path);
+
+    let diagnostics = compiler.diagnostics;
+
+    if (diagnostics.length > 0) {
+        let warnings = diagnostics.filter((d) => d.level == DiagnosticLevel.Warning);
+        warnings.forEach((w) => console.log("Warning: " + w));
+
+        let errors = diagnostics.filter((d) => d.level == DiagnosticLevel.Error);
+
+        if (errors.length > 0) {
+            throw new CompilerError("Could not compile " + path, errors);
+        }
+    }
+
+    let slint_module = Object.create({});
+
+    Object.defineProperty(slint_module, definition!.name, {
+        value: function() {
+            let instance = definition!.create();
+
+            if (instance == null) {
+                throw Error("Could not create a component handle for" + path);
+            }
+
+            let componentHandle = new Component(instance!);
+            instance!.definition().properties.forEach((prop) => {
+                Object.defineProperty(componentHandle, prop.name.replace(/-/g, '_') , {
+                    get() { return instance!.getProperty(prop.name); },
+                    set(value) { instance!.setProperty(prop.name, value); },
+                    enumerable: true
+                })
+            });
+
+            instance!.definition().callbacks.forEach((cb) => {
+                Object.defineProperty(componentHandle, cb.replace(/-/g, '_') , {
+                    get() {
+                        let callback = function () { return instance!.invoke(cb, [...arguments]); } as Callback;
+                        callback.setHandler = function (callback) { instance!.setCallback(cb, callback) };
+                        return callback;
+                    },
+                    enumerable: true,
+                })
+            });
+
+            return componentHandle;
+        },
+    });
+
+    return slint_module;
+}
+
