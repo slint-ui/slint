0d35e01724d83a373c260777ac8741865977be17
Simon Hausmann <simon.hausmann@slint.dev>
2023-10-13 15:19:27 +0200

    Revert "linuxkms: Update drm and gbm dependencies"
    This reverts commit 17afe1aa8ac4c730a2025b30f7d5c32b3799632b.
    
    This causes
    
     thread '<unnamed>' panicked at 'assertion failed: (has_modifier && modifier.is_some()) || (!has_modifier && modifier.is_none())', drm-0.10.0/src/control/mod.rs:324:9
    
    when running on qemu-virgl.

--- internal/backends/linuxkms/display/egldisplay.rs
+++ internal/backends/linuxkms/display/egldisplay.rs
@@ -56,20 +56,9 @@ impl super::Presenter for EglDisplay {
 
         // TODO: support modifiers
         // TODO: consider falling back to the old non-planar API
-
-        let flags = if front_buffer
-            .modifier()
-            .map_err(|e| format!("Error retrieving modifiers from font buffer: {e}"))?
-            != gbm::Modifier::Invalid
-        {
-            drm::control::FbCmd2Flags::MODIFIERS
-        } else {
-            drm::control::FbCmd2Flags::empty()
-        };
-
         let fb = self
             .gbm_device
-            .add_planar_framebuffer(&front_buffer, flags)
+            .add_planar_framebuffer(&front_buffer, &[None, None, None, None], 0)
             .map_err(|e| format!("Error adding gbm buffer as framebuffer: {e}"))?;
 
         front_buffer
