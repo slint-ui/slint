f12b3d2df4ba3af5332e1ed92bcacc1dbb6bd883
Tobias Hunger <tobias.hunger@slint.dev>
2023-10-18 12:32:09 +0200

    vscode: Create highlight function again and pass it to the LSP
    This stops a panic then triggering a highlight to happen.

--- editors/vscode/src/browser-lsp-worker.ts
+++ editors/vscode/src/browser-lsp-worker.ts
@@ -23,6 +23,13 @@ slint_init(slint_wasm_data).then((_) => {
         return true;
     }
 
+    function highlight(path: string, offset: number) {
+        connection.sendRequest("slint/preview_message", {
+            command: "highlight",
+            data: { path: path, offset: offset },
+        });
+    }
+
     async function send_request(method: string, params: any): Promise<unknown> {
         return await connection.sendRequest(method, params);
     }
@@ -37,6 +44,7 @@ slint_init(slint_wasm_data).then((_) => {
             send_notification,
             send_request,
             load_file,
+            highlight,
         );
         return the_lsp.server_initialize_result(params.capabilities);
     });
