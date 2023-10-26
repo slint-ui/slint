44475d68920b131cdf5a537a94cdd981f11f3137
Tobias Hunger <tobias.hunger@slint.dev>
2023-10-26 13:46:41 +0200

    lsp: Build preview UI with Design Mode text and default style

--- tools/lsp/build.rs
+++ tools/lsp/build.rs
@@ -6,10 +6,6 @@ fn main() {
     // TODO: Remove if ComponentContainer anc component-factory no longer need this!
     std::env::set_var("SLINT_ENABLE_EXPERIMENTAL_FEATURES", "1");
     #[cfg(feature = "preview-engine")]
-    slint_build::compile_with_config(
-        "ui/main.slint",
-        slint_build::CompilerConfiguration::new().with_style("fluent-light".into()),
-    )
-    .unwrap();
+    slint_build::compile("ui/main.slint").unwrap();
     println!("cargo:rustc-env=SLINT_ENABLE_EXPERIMENTAL_FEATURES=1");
 }
