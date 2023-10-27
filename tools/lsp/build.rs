9093f74bef364a738fe3552ec87d7ee83982f510
Tobias Hunger <tobias.hunger@slint.dev>
2023-10-27 10:15:32 +0200

    LSP: Remove compiler env var line from build.rs

--- tools/lsp/build.rs
+++ tools/lsp/build.rs
@@ -3,9 +3,7 @@
 
 fn main() {
     // Make the compiler handle ComponentContainer:
-    // TODO: Remove if ComponentContainer anc component-factory no longer need this!
     std::env::set_var("SLINT_ENABLE_EXPERIMENTAL_FEATURES", "1");
     #[cfg(feature = "preview-engine")]
     slint_build::compile("ui/main.slint").unwrap();
-    println!("cargo:rustc-env=SLINT_ENABLE_EXPERIMENTAL_FEATURES=1");
 }
