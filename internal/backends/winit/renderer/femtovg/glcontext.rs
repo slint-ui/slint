f941357337b3f41c9d1eccfbb2201350102625a7
Simon Hausmann <simon.hausmann@slint.dev>
2023-10-30 08:58:25 +0100

    Upgrade to glutin 0.31.1 to fix window transparency on macOS
    Fixes #3760

--- internal/backends/winit/renderer/femtovg/glcontext.rs
+++ internal/backends/winit/renderer/femtovg/glcontext.rs
@@ -64,8 +64,8 @@ impl OpenGLContext {
         // the system doesn't support transparency, we want to fall back to a config that doesn't - better than not
         // rendering anything at all. So we don't want to limit the configurations we get to see early on.
         // Commented out due to https://github.com/rust-windowing/glutin/issues/1640
-        // #[cfg(target_os = "macos")]
-        // let config_template_builder = config_template_builder.with_transparency(true);
+        #[cfg(target_os = "macos")]
+        let config_template_builder = config_template_builder.with_transparency(true);
 
         let (window, gl_config) = glutin_winit::DisplayBuilder::new()
             .with_preference(glutin_winit::ApiPreference::FallbackEgl)
