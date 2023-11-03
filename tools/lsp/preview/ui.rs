259ec5d7a46ed601fed96384f9b17ee11133c82b
Tobias Hunger <tobias.hunger@slint.dev>
2023-11-03 13:30:49 +0100

    lsp: Add native style into preview and default to it

--- tools/lsp/preview/ui.rs
+++ tools/lsp/preview/ui.rs
@@ -1,6 +1,8 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.1 OR LicenseRef-Slint-commercial
 
+use std::iter::once;
+
 use i_slint_core::{
     model::{Model, VecModel},
     SharedString,
@@ -10,24 +12,19 @@ use slint_interpreter::PlatformError;
 slint::include_modules!();
 
 pub fn create_ui(style: String) -> Result<PreviewUi, PlatformError> {
-    let default_style =
-        i_slint_common::get_native_style(false, &std::env::var("TARGET").unwrap_or_default());
-
     let ui = PreviewUi::new()?;
 
     // design mode:
     ui.on_design_mode_changed(super::set_design_mode);
 
     // styles:
-    let known_styles = i_slint_compiler::fileaccess::styles()
-        .iter()
+    let known_styles = once(&"native")
+        .chain(i_slint_compiler::fileaccess::styles().iter())
         .filter(|s| s != &&"qt" || i_slint_backend_selector::HAS_NATIVE_STYLE)
         .cloned()
         .collect::<Vec<_>>();
     let style = if known_styles.contains(&style.as_str()) {
         style
-    } else if known_styles.contains(&default_style) {
-        default_style.to_string()
     } else {
         known_styles.first().map(|s| s.to_string()).unwrap_or_default()
     };
@@ -35,7 +32,7 @@ pub fn create_ui(style: String) -> Result<PreviewUi, PlatformError> {
     let style_model = std::rc::Rc::new({
         let model = VecModel::default();
         model.extend(known_styles.iter().map(|s| SharedString::from(*s)));
-        assert!(model.row_count() > 0);
+        assert!(model.row_count() > 1);
         model
     });
 
