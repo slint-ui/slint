770751a6cee8956d460af400e7edab9ee6cc0179
Olivier Goffart <olivier.goffart@slint.dev>
2023-11-10 11:04:07 +0100

    Updates to the publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,24 +4,21 @@
 
 cargo publish --manifest-path internal/common/Cargo.toml
 cargo publish --manifest-path internal/core-macros/Cargo.toml
-sleep 15
 cargo publish --manifest-path internal/compiler/Cargo.toml
 cargo publish --manifest-path internal/core/Cargo.toml
-sleep 15
 cargo publish --manifest-path api/rs/macros/Cargo.toml
-cargo publish --manifest-path internal/renderers/skia/Cargo.toml
+cargo publish --manifest-path internal/renderers/skia/Cargo.toml --features x11
 cargo publish --manifest-path internal/renderers/femtovg/Cargo.toml
 cargo publish --manifest-path internal/backends/winit/Cargo.toml --features x11,renderer-femtovg
 cargo publish --manifest-path api/rs/build/Cargo.toml
 cargo publish --manifest-path internal/backends/qt/Cargo.toml
 cargo publish --manifest-path internal/backends/linuxkms/Cargo.toml
-sleep 30
 cargo publish --manifest-path internal/backends/selector/Cargo.toml --features backend-winit-x11,renderer-femtovg
-sleep 30
 cargo publish --manifest-path internal/interpreter/Cargo.toml
 cargo publish --manifest-path api/rs/slint/Cargo.toml
 cargo publish --manifest-path tools/lsp/Cargo.toml
 cargo publish --manifest-path tools/viewer/Cargo.toml
 cargo publish --manifest-path tools/updater/Cargo.toml
 cargo publish --manifest-path tools/tr-extractor/Cargo.toml
+cargo publish --manifest-path internal/backends/android-activity/Cargo.toml --features native-activity
 
