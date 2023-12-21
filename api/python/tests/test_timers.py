d1529af3cc079c724fcbe1a4101a14013eda8b76
Simon Hausmann <simon.hausmann@slint.dev>
2023-12-21 11:37:53 +0100

    python: hide native module behind python front-end API
    Encapsulate the rust code behind a public API (in __init__.py for now)

--- api/python/tests/test_timers.py
+++ api/python/tests/test_timers.py
@@ -2,8 +2,8 @@
 # SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.1 OR LicenseRef-Slint-commercial
 
 import pytest
-import slint
-from slint import ValueType
+from slint import slint as native
+from slint.slint import ValueType;
 from datetime import timedelta
 
 def test_timer():
@@ -13,14 +13,14 @@ def test_timer():
         global counter
         counter = counter + 1
         if counter >= 2:
-            slint.quit_event_loop()
+            native.quit_event_loop()
 
-    test_timer = slint.Timer()        
-    test_timer.start(slint.TimerMode.Repeated, timedelta(milliseconds=100), quit_after_two_invocations)
-    slint.run_event_loop()
+    test_timer = native.Timer()        
+    test_timer.start(native.TimerMode.Repeated, timedelta(milliseconds=100), quit_after_two_invocations)
+    native.run_event_loop()
     test_timer.stop()
     assert(counter == 2)
 
 def test_single_shot():
-    slint.Timer.single_shot(timedelta(milliseconds=100), slint.quit_event_loop)
-    slint.run_event_loop()
+    native.Timer.single_shot(timedelta(milliseconds=100), native.quit_event_loop)
+    native.run_event_loop()
