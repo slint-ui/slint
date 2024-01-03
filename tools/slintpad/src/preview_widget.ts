b9059d6dc2ffb141f4339574c0bd314d87a92f45
Olivier Goffart <olivier.goffart@slint.dev>
2024-01-03 14:27:34 +0100

    Update tools/slintpad/src/preview_widget.ts
    Co-authored-by: Simon Hausmann <simon.hausmann@slint.dev>

--- tools/slintpad/src/preview_widget.ts
+++ tools/slintpad/src/preview_widget.ts
@@ -51,7 +51,7 @@ export class PreviewWidget extends Widget {
             // when searching the document.
             this.#previewer.show_ui().then(() => {
                 console.info("UI should be up!");
-                const canvas = document.getElementById(canvas_id) as HTMLCanvasElement;
+                const canvas = document.getElementById(canvas_id) as HTMLElement;
                 canvas.style.width = "100%";
                 canvas.style.height = "100%";
             });
