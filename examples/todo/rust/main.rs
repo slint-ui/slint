a3e3cccf0c6950c00ae73566dd50ee0881325421
Olivier Goffart <olivier.goffart@slint.dev>
2024-01-03 13:10:50 +0100

    Todo Android example: state saving/restore

--- examples/todo/rust/main.rs
+++ examples/todo/rust/main.rs
@@ -28,6 +28,13 @@ pub fn main() {
     ]));
 
     let main_window = MainWindow::new().unwrap();
+
+    #[cfg(target_os = "android")]
+    STATE.with(|ui| {
+        *ui.borrow_mut() =
+            Some(State { main_window: main_window.clone_strong(), todo_model: todo_model.clone() })
+    });
+
     main_window.on_todo_added({
         let todo_model = todo_model.clone();
         move |text| todo_model.push(TodoItem { checked: false, title: text })
@@ -100,11 +107,66 @@ pub fn main() {
 #[cfg(target_os = "android")]
 #[no_mangle]
 fn android_main(app: i_slint_backend_android_activity::AndroidApp) {
+    use i_slint_backend_android_activity::android_activity::{MainEvent, PollEvent};
     slint::platform::set_platform(Box::new(
         i_slint_backend_android_activity::AndroidPlatform::new_with_event_listener(app, |event| {
-            eprintln!("Got event: {event:?}")
+            eprintln!("Got event: {event:?}");
+            match event {
+                PollEvent::Main(MainEvent::SaveState { saver, .. }) => {
+                    STATE.with(|state| -> Option<()> {
+                        let todo_state = SerializedState::save(state.borrow().as_ref()?);
+                        saver.store(&serde_json::to_vec(&todo_state).ok()?);
+                        Some(())
+                    });
+                }
+                PollEvent::Main(MainEvent::Resume { loader, .. }) => {
+                    STATE.with(|state| -> Option<()> {
+                        let bytes: Vec<u8> = loader.load()?;
+                        let todo_state: SerializedState = serde_json::from_slice(&bytes).ok()?;
+                        todo_state.restore(state.borrow().as_ref()?);
+                        Some(())
+                    });
+                }
+                _ => {}
+            };
         }),
     ))
     .unwrap();
     main();
+}
+
+#[cfg(target_os = "android")]
+struct State {
+    main_window: MainWindow,
+    todo_model: Rc<slint::VecModel<TodoItem>>,
+}
+
+#[cfg(target_os = "android")]
+thread_local! {
+    static STATE : core::cell::RefCell<Option<State>> = Default::default();
+}
+
+#[cfg(target_os = "android")]
+#[derive(serde::Serialize, serde::Deserialize)]
+struct SerializedState {
+    items: Vec<TodoItem>,
+    sort: bool,
+    hide_done: bool,
+}
+
+#[cfg(target_os = "android")]
+impl SerializedState {
+    fn restore(mut self, state: &State) {
+        state.todo_model.set_vec(self.items);
+        state.main_window.set_hide_done_items(self.hide_done);
+        state.main_window.set_is_sort_by_name(self.sort);
+        state.main_window.invoke_apply_sorting_and_filtering();
+    }
+    fn save(state: &State) -> Self {
+        Self {
+            items: state.todo_model.iter().collect(),
+            sort: state.main_window.get_is_sort_by_name(),
+            hide_done: state.main_window.get_hide_done_items(),
+        }
+    }
 }
