a0eed4e58e4e4fc78b78b59175ff5705089cedd3
ogoffart <ogoffart@users.noreply.github.com>
2024-02-20 17:33:11 +0000

    Bump version number to 1.5.0

--- api/cpp/docs/conf.py
+++ api/cpp/docs/conf.py
@@ -22,7 +22,7 @@ import textwrap
 # -- Project information -----------------------------------------------------
 
 # The full version, including alpha/beta/rc tags
-version = "1.4.1"
+version = "1.5.0"
 
 project = f'Slint {version} C++ API'
 copyright = "SixtyFPS GmbH"
--- api/rs/slint/lib.rs
+++ api/rs/slint/lib.rs
@@ -324,7 +324,7 @@ pub mod android;
 /// Helper type that helps checking that the generated code is generated for the right version
 #[doc(hidden)]
 #[allow(non_camel_case_types)]
-pub struct VersionCheck_1_4_1;
+pub struct VersionCheck_1_5_0;
 
 #[cfg(doctest)]
 mod compile_fail_tests;
