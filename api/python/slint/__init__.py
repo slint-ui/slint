769a3f3461750610ce70b4cc1664e5f6998c2445
Simon Hausmann <simon.hausmann@slint.dev>
2024-03-06 11:24:54 +0100

    Python: rework the load_file API, part 2
    Take arguments for configuring style, etc., throw an exception on errors, and log warning diagnostics.

--- api/python/slint/__init__.py
+++ api/python/slint/__init__.py
@@ -3,11 +3,40 @@
 
 from . import slint as native
 import types
+import logging
 
 
-def load_file(path):
+class CompileError(Exception):
+    def __init__(self, message, diagnostics):
+        self.message = message
+        self.diagnostics = diagnostics
+
+
+def load_file(path, quiet=False, style=None, include_paths=None, library_paths=None, translation_domain=None):
     compiler = native.ComponentCompiler()
+
+    if style is not None:
+        compiler.style = style
+    if include_paths is not None:
+        compiler.include_paths = include_paths
+    if library_paths is not None:
+        compiler.library_paths = library_paths
+    if translation_domain is not None:
+        compiler.translation_domain = translation_domain
+
     compdef = compiler.build_from_path(path)
+
+    diagnostics = compiler.diagnostics
+    if diagnostics:
+        if not quiet:
+            for diag in diagnostics:
+                if diag.level == native.DiagnosticLevel.Warning:
+                    logging.warning(diag)
+
+            errors = [diag for diag in diagnostics if diag.level ==
+                      native.DiagnosticLevel.Error]
+            if errors:
+                raise CompileError(f"Could not compile {path}", diagnostics)
 
     module = types.SimpleNamespace()
     setattr(module, compdef.name, type("SlintMetaClass", (), {
--- api/python/tests/test_load_file.py
+++ api/python/tests/test_load_file.py
@@ -1,13 +1,22 @@
 # Copyright © SixtyFPS GmbH <info@slint.dev>
 # SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.1 OR LicenseRef-Slint-commercial
 
-from slint import load_file
+import pytest
+from slint import load_file, CompileError
 import os
 
 
-def test_load_file():
+def test_load_file(caplog):
     module = load_file(os.path.join(os.path.dirname(
-        __spec__.origin), "test_load_file.slint"))
+        __spec__.origin), "test_load_file.slint"), quiet=False)
+
+    assert "The property 'color' has been deprecated. Please use 'background' instead" in caplog.text
+
     assert list(module.__dict__.keys()) == ["App"]
     instance = module.App()
     del instance
+
+
+def test_load_file_fail():
+    with pytest.raises(CompileError, match="Could not compile non-existent.slint"):
+        load_file("non-existent.slint")
