a93e2be393f1f0b90044a4e05dcece62a112bedd
Simon Hausmann <simon.hausmann@slint.dev>
2024-03-07 11:49:48 +0100

    Python: Add support for a @slint.callback decorator to conveniently associated callbacks with python methods
    cc #4134

---
+++ api/python/tests/test_callback_decorators.py
@@ -0,0 +1,30 @@
+# Copyright © SixtyFPS GmbH <info@slint.dev>
+# SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.1 OR LicenseRef-Slint-commercial
+
+from slint import load_file, CompileError
+import slint
+import os
+
+
+def test_callback_decorators(caplog):
+    module = load_file(os.path.join(os.path.dirname(
+        __spec__.origin), "test_load_file.slint"), quiet=False)
+
+    class SubClass(module.App):
+        @slint.callback()
+        def say_hello_again(self, arg):
+            return "say_hello_again:" + arg
+
+        @slint.callback(name="say-hello")
+        def renamed(self, arg):
+            return "renamed:" + arg
+
+        @slint.callback(global_name="MyGlobal", name="global-callback")
+        def global_callback(self, arg):
+            return "global:" + arg
+
+    instance = SubClass()
+    assert instance.invoke_say_hello("ok") == "renamed:ok"
+    assert instance.invoke_say_hello_again("ok") == "say_hello_again:ok"
+    assert instance.invoke_global_callback("ok") == "global:ok"
+    del instance
