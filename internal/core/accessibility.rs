a2ec7798bdee1f0e397bf6400b646e32baac693a
Florian Blasius <florian.blasius@slint-ui.com>
2024-03-12 13:38:40 +0100

    Acessibility: added AccessibilityAction and SupportedAccessibilityAction

--- internal/core/accessibility.rs
+++ internal/core/accessibility.rs
@@ -6,7 +6,13 @@
 #[cfg(not(feature = "std"))]
 use alloc::vec::Vec;
 
-use crate::items::ItemRc;
+use crate::{
+    items::ItemRc,
+    lengths::{LogicalPoint, LogicalVector},
+    SharedString,
+};
+
+use bitflags::bitflags;
 
 // The property names of the accessible-properties
 #[repr(u32)]
@@ -22,6 +28,68 @@ pub enum AccessibleStringProperty {
     ValueMaximum,
     ValueMinimum,
     ValueStep,
+}
+
+// Defines an accessibility action.
+pub enum AccessibilityAction {
+    Default,
+    Focus,
+    Blur,
+    Collapse,
+    Expand,
+    CustomAction,
+    Decrement,
+    Increment,
+    // FIXME: Can it be currently supported?
+    HideToolTip,
+    // FIXME: Can it be currently supported?
+    ShowToolTip,
+    ReplaceSelectedText(SharedString),
+    ScrollBackward,
+    ScrollDown,
+    ScrollForward,
+    ScrollLeft,
+    ScrollRight,
+    ScrollUp,
+    ScrollIntoView,
+    ScrollToPoint(LogicalPoint),
+    SetScrollOffset(LogicalVector),
+    SetTextSelection(Option<core::ops::Range<i32>>),
+    // FIXME: Do we need this?
+    SetSequentialFocusNavigationStartingPoint,
+    SetValue(f64),
+    // FIXME: Can it be currently supported?
+    ShowContextMenu,
+}
+
+bitflags! {
+    // Define a accessibility actions that currently supported by Slint.
+    pub struct SupportedAccessibilityAction: u32 {
+        const Default = 0b00000001;
+        const Focus = 0b00000010;
+        const Blur = 0b00000011;
+        const Collapse = 0b00000100;
+        const Expand = 0b00000110;
+        const CustomAction = 0b00000111;
+        const Decrement = 0b00001000;
+        const Increment = 0b00001001;
+        const HideToolTip = 0b00001010;
+        const ShowToolTip = 0b00001011;
+        const ReplaceSelectedText = 0b00001100;
+        const ScrollBackward = 0b00001101;
+        const ScrollDown = 0b00001110;
+        const ScrollForward = 0b00001110;
+        const ScrollLeft = 0b00001111;
+        const ScrollRight = 0b00010000;
+        const ScrollUp = 0b00010001;
+        const ScrollIntoView = 0b00010010;
+        const ScrollToPoint = 0b00010011;
+        const SetScrollOffset = 0b00010100;
+        const SetTextSelection = 0b00010101;
+        const SetSequentialFocusNavigationStartingPoint = 0b00010110;
+        const SetValue = 0b00010111;
+        const ShowContextMenu = 0b00011000;
+    }
 }
 
 /// Find accessible descendents of `root_item`.
