5de03f774e5d1800db948c5f76b790b79693a6a6
Tobias Hunger <tobias.hunger@slint.dev>
2024-03-22 10:04:23 +0100

    interpreter: Fix `cargo test -p slint-interpreter`

--- internal/interpreter/tests.rs
+++ internal/interpreter/tests.rs
@@ -1,6 +1,7 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.1 OR LicenseRef-Slint-commercial
 
+#[cfg(feature = "internal")]
 #[test]
 fn reuse_window() {
     i_slint_backend_testing::init();
