3f707eed22de8ec8ea07cc1b4c150a2d8f672520
Florian Blasius <florian.blasius@slint-ui.com>
2024-03-25 11:53:17 +0100

    EW Espressif demo: setup skeleton.

---
+++ examples/usecases/cpp/main.cpp
@@ -0,0 +1,13 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: MIT
+
+#include "uscases.h"
+
+int main()
+{
+    auto app = usecases_ui::App::create();
+
+    });
+
+    app->run();
+}
