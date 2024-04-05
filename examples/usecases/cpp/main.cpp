a5bffbe8b9ef2a591499c4ad30e14f58f06e38d5
Florian Blasius <florian.blasius@slint-ui.com>
2024-04-05 11:25:52 +0200

    cpp setup

--- examples/usecases/cpp/main.cpp
+++ examples/usecases/cpp/main.cpp
@@ -1,13 +1,21 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: MIT
 
-#include "uscases.h"
+#include "usecases.h"
+
+void init_virtual_keyboard(slint::ComponentHandle<usecases_ui::App> app)
+{
+    app->global<VirtualKeyboardHandler>().on_key_pressed([=](auto key) {
+        app->window().dispatch_key_press_event(key);
+        app->window().dispatch_key_release_event(key);
+    });
+}
 
 int main()
 {
     auto app = usecases_ui::App::create();
 
-    });
+    init_virtual_keyboard(main_window);
 
     app->run();
 }
