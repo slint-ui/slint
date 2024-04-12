a857943603d2001e428f51fbac858d942f3c181a
Tobias Hunger <tobias.hunger@slint.dev>
2024-04-12 15:03:13 +0200

    Try an approach where we pass in Yes, No and Maybe for each element
    ... in the stack of elements at a position and to use that to find which
    element to actually drop into.
    
    Make the DropMark work with that apporach

--- tools/lsp/preview/ext.rs
+++ tools/lsp/preview/ext.rs
@@ -6,6 +6,7 @@ use i_slint_core::lengths::{LogicalPoint, LogicalRect};
 
 use crate::common;
 use crate::preview::ui;
+use crate::util;
 
 use slint_interpreter::ComponentInstance;
 
@@ -30,7 +31,14 @@ pub trait ElementRcNodeExt {
         &self,
         component_instance: &ComponentInstance,
         rect: &LogicalRect,
-    ) -> Vec<i_slint_core::lengths::LogicalRect>;
+    ) -> Option<i_slint_core::lengths::LogicalRect>;
+
+    /// Checks wether the given type is acceptable as a child
+    fn accepts_child_type(
+        &self,
+        component_instance: &ComponentInstance,
+        component_type: &str,
+    ) -> bool;
 }
 
 impl ElementRcNodeExt for common::ElementRcNode {
@@ -68,11 +76,31 @@ impl ElementRcNodeExt for common::ElementRcNode {
         &self,
         component_instance: &ComponentInstance,
         rect: &LogicalRect,
-    ) -> Vec<i_slint_core::lengths::LogicalRect> {
+    ) -> Option<i_slint_core::lengths::LogicalRect> {
         self.geometries(component_instance)
             .iter()
-            .filter(|g| rect.contains_rect(g))
+            .find(|g| rect.contains_rect(g))
             .cloned()
-            .collect()
+    }
+
+    fn accepts_child_type(
+        &self,
+        component_instance: &ComponentInstance,
+        component_type: &str,
+    ) -> bool {
+        let tl = component_instance.definition().type_loader();
+        let (path, _) = self.path_and_offset();
+        let Some(doc) = tl.get_document(&path) else {
+            return false;
+        };
+        let Some(element_type) = self.with_element_node(|node| {
+            util::lookup_current_element_type((node.clone()).into(), &doc.local_registry)
+        }) else {
+            return false;
+        };
+
+        self.layout_kind() != ui::LayoutKind::None
+            || element_type.accepts_child_element(component_type, &doc.local_registry).is_ok()
     }
 }
+
