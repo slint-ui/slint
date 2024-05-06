slint-ui/slint

commit 12d904a71ca31f947bfe983e7dbfe745b2439b74

Fix compiler panic with init in a component inlined into a repeater
The problem was that the code from #4322 inlined the init code in the
parent Component as at that point, the per-repeater component don't
exist yet.
Fix it by removing the workaround from #4322, but changing the order of
the passes so that the init code are already proccessed before any
inlining. This required to change the order of a bunch of passes.

Fixes #5146

As a drive-by, also add the missing C++ implementation of set_animated_value
for Brush that was discovered by the test. (Code wouldn't compile)

internal/compiler/object_tree.rs
@@ -686,6 +686,7 @@ pub fn pretty_print(
         expression_tree::pretty_print(f, &repeated.model)?;
         write!(f, ":")?;
         if let ElementType::Component(base) = &e.base_type {
+            write!(f, "(base) ")?;
             if base.parent_element.upgrade().is_some() {
                 pretty_print(f, &base.root_element.borrow(), indentation)?;
                 return Ok(());

internal/compiler/passes.rs
@@ -92,6 +92,13 @@ pub async fn run_passes(
             diag,
         );
         lower_tabwidget::lower_tabwidget(component, type_loader, diag).await;
+    }
+
+    collect_subcomponents::collect_subcomponents(root_component);
+
+    for component in (root_component.used_types.borrow().sub_components.iter())
+        .chain(std::iter::once(root_component))
+    {
         apply_default_properties_from_style::apply_default_properties_from_style(
             component,
             &style_metrics,
@@ -99,6 +106,9 @@ pub async fn run_passes(
         );
         lower_states::lower_states(component, &doc.local_registry, diag);
         lower_text_input_interface::lower_text_input_interface(component);
+        repeater_component::process_repeater_components(component);
+        lower_popups::lower_popups(component, &doc.local_registry, diag);
+        collect_init_code::collect_init_code(component);
     }
 
     inlining::inline(doc, inlining::InlineSelection::InlineOnlyRequiredComponents);
@@ -113,8 +123,6 @@ pub async fn run_passes(
     {
         border_radius::handle_border_radius(component, diag);
         flickable::handle_flickable(component, &global_type_registry.borrow());
-        repeater_component::process_repeater_components(component);
-        lower_popups::lower_popups(component, &doc.local_registry, diag);
         lower_component_container::lower_component_container(component, &doc.local_registry, diag);
 
         lower_layout::lower_layouts(component, type_loader, diag).await;
@@ -169,7 +177,6 @@ pub async fn run_passes(
         if type_loader.compiler_config.accessibility {
             lower_accessibility::lower_accessibility_properties(component, diag);
         }
-        collect_init_code::collect_init_code(component);
         materialize_fake_properties::materialize_fake_properties(component);
     }
     lower_layout::check_window_layout(root_component);

internal/compiler/passes/inlining.rs
@@ -54,11 +54,9 @@ pub fn inline(doc: &Document, inline_selection: InlineSelection) {
     }
     inline_components_recursively(&doc.root_component, inline_selection);
 
-    if matches!(inline_selection, InlineSelection::InlineAllComponents) {
-        let mut init_code = doc.root_component.init_code.borrow_mut();
-        let inlined_init_code = core::mem::take(&mut init_code.inlined_init_code);
-        init_code.constructor_code.splice(0..0, inlined_init_code.into_values());
-    }
+    let mut init_code = doc.root_component.init_code.borrow_mut();
+    let inlined_init_code = core::mem::take(&mut init_code.inlined_init_code);
+    init_code.constructor_code.splice(0..0, inlined_init_code.into_values());
 }
 
 fn clone_tuple<U: Clone, V: Clone>((u, v): (&U, &V)) -> (U, V) {
@@ -155,19 +153,7 @@ fn inline_element(
             .map(|p| duplicate_popup(p, &mut mapping, priority_delta)),
     );
 
-    // When inlining a component before the collect_init_code phase, do the collect_init_code phase for
-    // the init callback in the inlined component manually, by cloning the expression into the init_code
-    // and skipping "init" in the binding merge phase.
-    let maybe_init_callback_to_inline = inlined_component
-        .root_element
-        .borrow()
-        .bindings
-        .get("init")
-        .map(|binding| binding.borrow().expression.clone());
-
-    for (k, val) in
-        inlined_component.root_element.borrow().bindings.iter().filter(|(k, _)| *k != "init")
-    {
+    for (k, val) in inlined_component.root_element.borrow().bindings.iter() {
         match elem_mut.bindings.entry(k.clone()) {
             std::collections::btree_map::Entry::Vacant(entry) => {
                 let priority = &mut entry.insert(val.clone()).get_mut().priority;
@@ -200,13 +186,6 @@ fn inline_element(
         fixup_element_references(&mut init_code, &mapping);
         init_code
     };
-
-    root_component
-        .init_code
-        .borrow_mut()
-        .constructor_code
-        .extend(maybe_init_callback_to_inline.map(fixup_init_expression));
-
     let inlined_init_code = inlined_component
         .init_code
         .borrow()

api/cpp/include/slint_brush.h
@@ -157,6 +157,7 @@ private:
     using Tag = cbindgen_private::types::Brush::Tag;
     using Inner = cbindgen_private::types::Brush;
     Inner data;
+    friend struct private_api::Property<Brush>;
 };
 
 Color Brush::color() const
@@ -279,4 +280,18 @@ inline Brush Brush::with_alpha(float alpha) const
     }
     return result;
 }
+
+namespace private_api {
+
+template<>
+inline void Property<slint::Brush>::set_animated_value(
+        const slint::Brush &new_value,
+        const cbindgen_private::PropertyAnimation &animation_data) const
+{
+    cbindgen_private::slint_property_set_animated_value_brush(&inner, &value, &new_value,
+                                                              &animation_data);
 }
+
+} // namespace private_api
+
+} // namespace slint

internal/compiler/passes/collect_subcomponents.rs
@@ -37,6 +37,13 @@ fn collect_subcomponents_recursive(
             _ => return,
         };
         collect_subcomponents_recursive(&base_comp, result, hash);
+        if base_comp.parent_element.upgrade().is_some() {
+            // This is not a sub-component, but is a repeated component
+            return;
+        }
         result.push(base_comp);
     });
+    for popup in component.popup_windows.borrow().iter() {
+        collect_subcomponents_recursive(&popup.component, result, hash);
+    }
 }

internal/core/properties/ffi.rs
@@ -219,6 +219,17 @@ pub unsafe extern "C" fn slint_property_set_animated_value_color(
     c_set_animated_value(handle, from, to, animation_data);
 }
 
+/// Internal function to set up a property animation to the specified target value for a brush property.
+#[no_mangle]
+pub unsafe extern "C" fn slint_property_set_animated_value_brush(
+    handle: &PropertyHandleOpaque,
+    from: &Brush,
+    to: &Brush,
+    animation_data: &PropertyAnimation,
+) {
+    c_set_animated_value(handle, from.clone(), to.clone(), animation_data);
+}
+
 unsafe fn c_set_animated_binding<T: InterpolatedPropertyValue + Clone>(
     handle: &PropertyHandleOpaque,
     binding: extern "C" fn(*mut c_void, *mut T),

tests/cases/callbacks/init.slint
@@ -43,6 +43,14 @@ Container := Rectangle {
     @children
 }
 
+InsideRepeater := Rectangle {
+    property <int> index;
+    init => {
+        InitOrder.observed-order += "(i" + self.index + ")";
+    }
+    @children
+}
+
 TestCase := Rectangle {
     width: 300phx;
     height: 300phx;
@@ -74,11 +82,13 @@ TestCase := Rectangle {
         }
     }
 
-    for i in 2: Rectangle {
+    for i in 2: InsideRepeater {
+        index: i;
         init => {
             InitOrder.observed-order += "|repeater";
             InitOrder.observed-order += i;
         }
+        Rectangle {}
     }
 
     property <string> test_global_prop_value: InitOrder.observed-order;
@@ -94,7 +104,7 @@ let instance = TestCase::new().unwrap();
 
 assert!(instance.get_test());
 slint_testing::send_mouse_click(&instance, 5., 5.);
-assert_eq!(instance.global::<InitOrder<'_>>().get_observed_order(), instance.get_expected_static_order() + "|repeater0|repeater1");
+assert_eq!(instance.global::<InitOrder<'_>>().get_observed_order(), instance.get_expected_static_order() + "|repeater0(i0)|repeater1(i1)");
 ```
 
 ```cpp
@@ -102,7 +112,7 @@ auto handle = TestCase::create();
 const TestCase &instance = *handle;
 assert(instance.get_test());
 slint_testing::send_mouse_click(&instance, 5., 5.);
-assert_eq(instance.global<InitOrder>().get_observed_order(),  instance.get_expected_static_order() + "|repeater0|repeater1");
+assert_eq(instance.global<InitOrder>().get_observed_order(),  instance.get_expected_static_order() + "|repeater0(i0)|repeater1(i1)");
 ```
 
 
@@ -110,7 +120,7 @@ assert_eq(instance.global<InitOrder>().get_observed_order(),  instance.get_expec
 var instance = new slint.TestCase({});
 assert(instance.test);
 slintlib.private_api.send_mouse_click(instance, 5., 5.);
-assert.equal(instance.test_global_prop_value,  instance.expected_static_order + "|repeater0|repeater1");
+assert.equal(instance.test_global_prop_value,  instance.expected_static_order + "|repeater0(i0)|repeater1(i1)");
 ```
 
 

tests/cases/issues/issue_5146_init_animated.slint
@@ -1,0 +1,13 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.2 OR LicenseRef-Slint-commercial
+
+component MyComponent inherits Rectangle {
+    animate background { }
+    init => {
+        self.background = #000;
+    }
+}
+
+export component MyWindow inherits Window {
+    for _ in [1, 2, 3]: MyComponent { }
+}

commit 3bdc95a2ea1ec3f00b3d273196245abc65040243

Fix component that inherits from PopupWindow
This regressed in commit 12d904a71ca31f947bfe983e7dbfe745b2439b74
which changed the order of the pass to get the lower_popup pass before
the first inlining pass.
If by chance (which is likely if one have children), the component that
inherits from PopupWindow was inlined in that pass, it would be as
if it was not a component, and it would be removed from its parent.
But since it is no longer inlined, we need to support that case and
delay the removal when processing the component that holds that popup.

internal/compiler/object_tree.rs
@@ -337,6 +337,9 @@ pub struct Component {
     pub used_types: RefCell<UsedSubTypes>,
     pub popup_windows: RefCell<Vec<PopupWindow>>,
 
+    /// This component actually inherits PopupWindow (although that has been changed to a Waindow by the lower_popups pass)
+    pub inherits_popup_window: Cell<bool>,
+
     /// The names under which this component should be accessible
     /// if it is a global singleton and exported.
     pub exported_global_names: RefCell<Vec<ExportedName>>,

internal/compiler/passes.rs
@@ -78,6 +78,7 @@ pub async fn run_passes(
 
     let global_type_registry = type_loader.global_type_registry.clone();
     let root_component = &doc.root_component;
+    root_component.is_root_component.set(true);
     run_import_passes(doc, type_loader, diag);
     check_public_api::check_public_api(doc, diag);
 
@@ -271,8 +272,6 @@ pub async fn run_passes(
             );
         }
     }
-
-    root_component.is_root_component.set(true);
 }
 
 /// Run the passes on imported documents

internal/compiler/passes/inlining.rs
@@ -331,6 +331,7 @@ fn duplicate_sub_component(
         exported_global_names: component_to_duplicate.exported_global_names.clone(),
         is_root_component: Default::default(),
         private_properties: Default::default(),
+        inherits_popup_window: core::cell::Cell::new(false),
     };
 
     let new_component = Rc::new(new_component);

internal/compiler/passes/lower_popups.rs
@@ -22,7 +22,12 @@ pub fn lower_popups(
         component,
         &None,
         &mut |elem, parent_element: &Option<ElementRc>| {
-            let is_popup = matches!(&elem.borrow().base_type, ElementType::Builtin(base_type) if base_type.name == "PopupWindow");
+            let is_popup = match &elem.borrow().base_type {
+                ElementType::Builtin(base_type) => base_type.name == "PopupWindow",
+                ElementType::Component(base_type) => base_type.inherits_popup_window.get(),
+                _ => false,
+            };
+
             if is_popup {
                 lower_popup_window(elem, parent_element.as_ref(), &window_type, diag);
             }
@@ -37,18 +42,25 @@ fn lower_popup_window(
     window_type: &ElementType,
     diag: &mut BuildDiagnostics,
 ) {
+    let parent_component = popup_window_element.borrow().enclosing_component.upgrade().unwrap();
     let parent_element = match parent_element {
         None => {
-            diag.push_error(
-                "PopupWindow cannot be the top level".into(),
-                &*popup_window_element.borrow(),
-            );
+            if parent_component.is_root_component.get() {
+                diag.push_error(
+                    "PopupWindow cannot be the top level".into(),
+                    &*popup_window_element.borrow(),
+                );
+                return;
+            }
+            if matches!(popup_window_element.borrow().base_type, ElementType::Builtin(_)) {
+                popup_window_element.borrow_mut().base_type = window_type.clone();
+            }
+            parent_component.inherits_popup_window.set(true);
             return;
         }
         Some(parent_element) => parent_element,
     };
 
-    let parent_component = popup_window_element.borrow().enclosing_component.upgrade().unwrap();
     if Rc::ptr_eq(&parent_component.root_element, popup_window_element) {
         diag.push_error(
             "PopupWindow cannot be directly repeated or conditional".into(),
@@ -67,25 +79,42 @@ fn lower_popup_window(
     );
     parent_element.borrow_mut().has_popup_child = true;
 
-    popup_window_element.borrow_mut().base_type = window_type.clone();
+    if matches!(popup_window_element.borrow().base_type, ElementType::Builtin(_)) {
+        popup_window_element.borrow_mut().base_type = window_type.clone();
+    }
 
-    let close_on_click =
-        match popup_window_element.borrow_mut().bindings.remove("close-on-click").map_or_else(
-            || Ok(true),
-            |binding| match binding.borrow().expression {
-                Expression::BoolLiteral(value) => Ok(value),
-                _ => Err(binding.borrow().span.clone()),
-            },
-        ) {
-            Ok(coc) => coc,
-            Err(location) => {
+    const CLOSE_ON_CLICK: &str = "close-on-click";
+    let close_on_click = popup_window_element.borrow_mut().bindings.remove(CLOSE_ON_CLICK);
+    let close_on_click = close_on_click
+        .map(|b| {
+            let b = b.into_inner();
+            (b.expression, b.span)
+        })
+        .or_else(|| {
+            let mut base = popup_window_element.borrow().base_type.clone();
+            while let ElementType::Component(b) = base {
+                base = b.root_element.borrow().base_type.clone();
+                if let Some(binding) = b.root_element.borrow().bindings.get(CLOSE_ON_CLICK) {
+                    let b = binding.borrow();
+                    return Some((b.expression.clone(), b.span.clone()));
+                }
+            }
+            None
+        });
+
+    let close_on_click = match close_on_click {
+        Some((expr, location)) => match expr {
+            Expression::BoolLiteral(value) => value,
+            _ => {
                 diag.push_error(
                     "The close-on-click property only supports constants at the moment".into(),
                     &location,
                 );
                 return;
             }
-        };
+        },
+        None => true,
+    };
 
     let popup_comp = Rc::new(Component {
         root_element: popup_window_element.clone(),

internal/compiler/passes/materialize_fake_properties.rs
@@ -103,6 +103,9 @@ fn should_materialize(
         let ty = crate::typeregister::reserved_property(prop).property_type;
         if ty != Type::Invalid {
             return Some(ty);
+        } else if prop == "close-on-click" {
+            // PopupWindow::close-on-click
+            return Some(Type::Bool);
         }
     }
     None

internal/compiler/tests/syntax/elements/popup2.slint
@@ -1,0 +1,12 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.2 OR LicenseRef-Slint-commercial
+
+component MyPopup inherits PopupWindow {}
+
+component MyPopup2 inherits MyPopup {}
+
+export component TopLevel inherits MyPopup2 {
+//                                 ^error{PopupWindow cannot be the top level}
+    if true : MyPopup2 {}
+//            ^error{PopupWindow cannot be directly repeated or conditional}
+}

tests/cases/elements/popupwindow.slint
@@ -4,6 +4,8 @@
 // The presence of this component shouldn't break anything
 component Window {}
 
+component InheritsPopup inherits PopupWindow {}
+
 Combo := Rectangle {
     property <color> inner_color;
     my_popup := PopupWindow {
@@ -46,4 +48,5 @@ TypeConversionTestCase := Rectangle {
     TouchArea {
         clicked => { popup.show() }
     }
+    InheritsPopup {}
 }

tests/cases/elements/popupwindow_inherits.slint
@@ -1,0 +1,109 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.2 OR LicenseRef-Slint-commercial
+
+
+component MyPopup inherits PopupWindow {
+    callback clicked;
+    in property <bool> do-close: false;
+    width: 20px;
+    height: 20px;
+    TouchArea {
+        clicked => {
+            clicked();
+            if do-close {
+                root.close();
+            }
+        }
+    }
+}
+
+component MyPopup2 inherits MyPopup {
+    close-on-click: false;
+}
+
+export component TestCase {
+    width: 300px;
+    height: 300px;
+
+    property<int>p1_clicked;
+    property<int>p2_clicked;
+    property<int>p3_clicked;
+
+    p1 := MyPopup {
+        x: 10px;
+        y: 10px;
+        clicked => {p1_clicked+=1;}
+    }
+
+    p2 := MyPopup2 {
+        x: 30px;
+        y: 10px;
+        clicked => {p2_clicked+=1;}
+    }
+
+    p3 := MyPopup2 {
+        x: 60px;
+        y: 10px;
+        do-close: true;
+        clicked => {p3_clicked+=1;}
+    }
+
+    public function show_popup(p: int) {
+        if p == 1 {
+            p1.show();
+        } else if p == 2 {
+            p2.show();
+        } else if p == 3 {
+            p3.show();
+        }
+    }
+    public function hide_2() {
+        p2.close();
+    }
+
+    out property <string> value: @tr("p1={} p2={} p3={}", p1_clicked, p2_clicked, p3_clicked);
+
+}
+
+/*
+
+```rust
+let instance = TestCase::new().unwrap();
+slint_testing::send_mouse_click(&instance, 15., 15.);
+slint_testing::send_mouse_click(&instance, 35., 15.);
+slint_testing::send_mouse_click(&instance, 65., 15.);
+assert_eq!(instance.get_value(), "p1=0 p2=0 p3=0");
+
+instance.invoke_show_popup(1);
+slint_testing::send_mouse_click(&instance, 15., 15.);
+
+slint_testing::send_mouse_click(&instance, 15., 15.);
+slint_testing::send_mouse_click(&instance, 35., 15.);
+slint_testing::send_mouse_click(&instance, 65., 15.);
+assert_eq!(instance.get_value(), "p1=1 p2=0 p3=0");
+
+
+instance.invoke_show_popup(2);
+slint_testing::send_mouse_click(&instance, 35., 15.);
+slint_testing::send_mouse_click(&instance, 35., 15.);
+slint_testing::send_mouse_click(&instance, 15., 15.);
+slint_testing::send_mouse_click(&instance, 35., 15.);
+slint_testing::send_mouse_click(&instance, 65., 15.);
+assert_eq!(instance.get_value(), "p1=1 p2=3 p3=0");
+
+instance.invoke_hide_2();
+slint_testing::send_mouse_click(&instance, 35., 15.);
+slint_testing::send_mouse_click(&instance, 35., 15.);
+assert_eq!(instance.get_value(), "p1=1 p2=3 p3=0");
+
+instance.invoke_show_popup(3);
+slint_testing::send_mouse_click(&instance, 65., 15.);
+slint_testing::send_mouse_click(&instance, 65., 15.);
+slint_testing::send_mouse_click(&instance, 15., 15.);
+slint_testing::send_mouse_click(&instance, 35., 15.);
+slint_testing::send_mouse_click(&instance, 65., 15.);
+assert_eq!(instance.get_value(), "p1=1 p2=3 p3=1");
+```
+
+
+*/