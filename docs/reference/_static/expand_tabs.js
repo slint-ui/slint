b9c306a4eac08ef2facdd90bb6350d01fb215c3a
Chris Chinchilla <chris@chrischinchilla.com>
2024-05-14 16:25:17 +0200

    Clarify copyright header

--- docs/reference/_static/expand_tabs.js
+++ docs/reference/_static/expand_tabs.js
@@ -2,14 +2,8 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: MIT
 
- * Expands a specific tab of sphinx-tabs.
- * Usage:
- * - docs.readthedocs.io/?tab=Name
- * - docs.readthedocs.io/?tab=Name#section
- * Where 'Name' is the title of the tab (case sensitive).
- */
-
-// Thanks to https://github.com/readthedocs/readthedocs.org/commit/738b6b2836a7e0cadad48e7f407fdeaf7ba7a1d7
+// Based on code from stsewd her https://github.com/readthedocs/readthedocs.org/commit/738b6b2836a7e0cadad48e7f407fdeaf7ba7a1d7
+*/
 
 $(document).ready(function () {
     const tabName = location.hash.substring(1);
