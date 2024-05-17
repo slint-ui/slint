89ffb5d41695e10f04935d190fdf4c37edfcbf85
Tobias Hunger <tobias.hunger@slint.dev>
2024-05-17 11:23:46 +0200

    compiler: small docs fixes

--- internal/compiler/typeloader.rs
+++ internal/compiler/typeloader.rs
@@ -426,7 +426,7 @@ impl TypeLoader {
         ok.then_some(path_canon)
     }
 
-    /// Load a file, and its dependency not run the passes.
+    /// Load a file, and its dependency, running only the import passes.
     ///
     /// the path must be the canonical path
     pub async fn load_file(
@@ -451,7 +451,7 @@ impl TypeLoader {
         .await;
     }
 
-    /// Load a file, and its dependency not run the passes.
+    /// Load a file, and its dependency, running the full set of passes.
     ///
     /// the path must be the canonical path
     pub async fn load_root_file(
