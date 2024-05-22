4726fe9c617252c9017dd3617a41fcfb4f3da51e
Wilston Oreo <mirrorpoolmusic@gmx.net>
2024-05-22 17:32:00 +0200

    Update api/rs/slint/android.rs
    Co-authored-by: Simon Hausmann <hausmann@gmail.com>

--- api/rs/slint/android.rs
+++ api/rs/slint/android.rs
@@ -57,7 +57,7 @@
 //! The following environment variables need to be set:
 //! * `ANDROID_HOME`: The directory in which your Android SDK is located. Usually `$HOME/Android/Sdk`.
 //! * `ANDROID_NDK_ROOT`: The directory in which your Android NDK is located. Usually `$HOME/Android/Sdk/ndk/${NDK_VERSION}`. ${NDK_VERSION} is the version of the NDK you have installed.
-//! * `JAVA_HOME`: The directory in which your Java compiler (`javac`) is located. This variable is optional if a proper `javac` is found in your `$PATH`. 
+//! * `JAVA_HOME`: The directory in which your Java compiler (`javac`) is located. This variable is optional if a `javac` is found in your `$PATH`. 
 //!   Otherwise you can set `JAVA_HOME` to the `javac` installation shipped with Android Studio in `android-studio/jbr`.
 //!
 //! To build and deploy your application, we suggest the usage of [cargo-apk](https://github.com/rust-mobile/cargo-apk),
