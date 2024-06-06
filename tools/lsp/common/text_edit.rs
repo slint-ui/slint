26499fb45987fa44f1f11727a7de3d7a8c468bf9
Tobias Hunger <tobias.hunger@slint.dev>
2024-06-06 16:21:39 +0200

    live-preview: Handle known components using our local `DocumentCache`

--- tools/lsp/common/text_edit.rs
+++ tools/lsp/common/text_edit.rs
@@ -1,9 +1,6 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
 
-#[cfg(target_arch = "wasm32")]
-use crate::wasm_prelude::*;
-
 use std::collections::HashMap;
 
 use crate::common;
