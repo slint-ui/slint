44a537d90238efdf859b9fca933c941ffa8f1f97
Luc Fauvel <lfauvel@devolutions.net>
2024-06-06 05:17:05 -0400

    Implemented resizing for non-decorated winit windows (#5026)
    Fixes #5023
    
    * Implemented resizing for non-decorated winit windows
    
    * Fixed formatting
    
    * Changed direction match to use a tuple, reduced resize border size, returned after handling resize
    
    * Added is resizable check for before resizing winit window
    
    * Added resize-border property to window
    
    * Formatted code
    
    * Change resize-border from float to length

--- docs/reference/src/language/builtins/elements.md
+++ docs/reference/src/language/builtins/elements.md
@@ -878,4 +878,5 @@ or smaller. The initial width can be controlled with the `preferred-width` prope
 -   **`default-font-weight`** (_in_ _int_): The font weight to use as default in text elements inside this window, that don't have their `font-weight` property set. The values range from 100 (lightest) to 900 (thickest). 400 is the normal weight.
 -   **`icon`** (_in_ _image_): The window icon shown in the title bar or the task bar on window managers supporting it.
 -   **`no-frame`** (_in_ _bool_): Whether the window should be borderless/frameless or not.
+-   **`resize-border`** (_in_ _length_): Size of the resize border in borderless/frameless windows (winit only for now).
 -   **`title`** (_in_ _string_): The window title that is shown in the title bar.
---
+++ internal/backends/winit/drag_resize_window.rs
@@ -0,0 +1,91 @@
+use winit::window::{CursorIcon, ResizeDirection};
+
+pub fn handle_cursor_move_for_resize(
+    window: &winit::window::Window,
+    position: winit::dpi::PhysicalPosition<f64>,
+    current_direction: Option<ResizeDirection>,
+    border_size: f64,
+) -> Option<ResizeDirection> {
+    if !window.is_decorated() && window.is_resizable() {
+        let location = get_resize_direction(window.inner_size(), position, border_size);
+
+        if current_direction != location {
+            window.set_cursor_icon(resize_direction_cursor_icon(location));
+        }
+
+        return location;
+    }
+
+    None
+}
+
+pub fn handle_resize(window: &winit::window::Window, direction: Option<ResizeDirection>) {
+    if let Some(dir) = direction {
+        let _ = window.drag_resize_window(dir);
+    }
+}
+
+/// Get the cursor icon that corresponds to the resize direction.
+fn resize_direction_cursor_icon(resize_direction: Option<ResizeDirection>) -> CursorIcon {
+    match resize_direction {
+        Some(resize_direction) => match resize_direction {
+            ResizeDirection::East => CursorIcon::EResize,
+            ResizeDirection::North => CursorIcon::NResize,
+            ResizeDirection::NorthEast => CursorIcon::NeResize,
+            ResizeDirection::NorthWest => CursorIcon::NwResize,
+            ResizeDirection::South => CursorIcon::SResize,
+            ResizeDirection::SouthEast => CursorIcon::SeResize,
+            ResizeDirection::SouthWest => CursorIcon::SwResize,
+            ResizeDirection::West => CursorIcon::WResize,
+        },
+        None => CursorIcon::Default,
+    }
+}
+
+fn get_resize_direction(
+    win_size: winit::dpi::PhysicalSize<u32>,
+    position: winit::dpi::PhysicalPosition<f64>,
+    border_size: f64,
+) -> Option<ResizeDirection> {
+    enum X {
+        West,
+        East,
+        Default,
+    }
+
+    enum Y {
+        North,
+        South,
+        Default,
+    }
+
+    let xdir = if position.x < border_size {
+        X::West
+    } else if position.x > (win_size.width as f64 - border_size) {
+        X::East
+    } else {
+        X::Default
+    };
+
+    let ydir = if position.y < border_size {
+        Y::North
+    } else if position.y > (win_size.height as f64 - border_size) {
+        Y::South
+    } else {
+        Y::Default
+    };
+
+    Some(match (xdir, ydir) {
+        (X::West, Y::North) => ResizeDirection::NorthWest,
+        (X::West, Y::South) => ResizeDirection::SouthWest,
+        (X::West, Y::Default) => ResizeDirection::West,
+
+        (X::East, Y::North) => ResizeDirection::NorthEast,
+        (X::East, Y::South) => ResizeDirection::SouthEast,
+        (X::East, Y::Default) => ResizeDirection::East,
+
+        (X::Default, Y::North) => ResizeDirection::North,
+        (X::Default, Y::South) => ResizeDirection::South,
+        (X::Default, Y::Default) => return None,
+    })
+}
