ec0ddbc5b9b250ec98bd4857a8ddab9f20672a89
Olivier Goffart <olivier.goffart@slint.dev>
2024-06-06 11:18:15 +0200

    Add missing license header from previous commit

--- internal/backends/winit/drag_resize_window.rs
+++ internal/backends/winit/drag_resize_window.rs
@@ -1,3 +1,6 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
+
 use winit::window::{CursorIcon, ResizeDirection};
 
 pub fn handle_cursor_move_for_resize(
