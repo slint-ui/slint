355a8ab62beee01363096700830582de287f46d9
Simon Hausmann <simon.hausmann@slint.dev>
2024-06-07 08:19:51 +0200

    Attempt to upgrade the docs build to latest Ubuntu 24.04 in GH Actions
    This might bring in a newer version of clang-format. We don't need to install `clang-format` as GH sets up /usr/bin/clang-format according to their toolset.json file.

--- xtask/src/reuse_compliance_check.rs
+++ xtask/src/reuse_compliance_check.rs
@@ -21,12 +21,6 @@ pub fn find_reuse() -> Result<PathBuf> {
     which::which("reuse").context("Failed to find reuse")
 }
 
-pub fn install_reuse(sh: &Shell) -> Result<PathBuf> {
-    cmd(sh, "pip", &["install", "reuse"])?.run().context("Failed to pip install reuse.")?;
-
-    find_reuse().context("Could not find reuse after pip installing it")
-}
-
 pub fn reuse_download(sh: &Shell, reuse: &Path) -> Result<String> {
     Ok(cmd(sh, reuse, &["download", "--all"])?
         .read()
@@ -371,7 +365,7 @@ impl ReuseComplianceCheck {
 
         let sh = Shell::new()?;
 
-        let reuse = find_reuse().or_else(|_| install_reuse(&sh))?;
+        let reuse = find_reuse().context("Can not find reuse. Please make sure it is installed")?;
 
         println!("Reuse binary \"{}\".", reuse.to_string_lossy());
 
