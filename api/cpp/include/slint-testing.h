674b82c2facc5601c5dae294fc2e681d270ff482
Simon Hausmann <simon.hausmann@slint.dev>
2024-06-14 13:19:37 +0200

    Document that availability of slint::testing is conditional to enabling the feature and experimental features.

--- api/cpp/include/slint-testing.h
+++ api/cpp/include/slint-testing.h
@@ -10,6 +10,11 @@
 #if defined(SLINT_FEATURE_TESTING) || defined(DOXYGEN)
 #    if defined(SLINT_FEATURE_EXPERIMENTAL) || defined(DOXYGEN)
 
+/// Use the functions and classes in this namespace for in-process UI testing.
+///
+/// This module is still experimental - it's API is subject to changes and not stabilized yet. To
+/// use the module, you must enable the `SLINT_FEATURE_EXPERIMENTAL=ON` and `SLINT_FEATURE_TESTING`
+/// CMake options.
 namespace slint::testing {
 
 using slint::cbindgen_private::AccessibleRole;
