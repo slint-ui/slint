slint-ui/slint

commit 2fd8744cfcac76a633c085a6069fc91848221028

compiler: Move embedded_file_resources from the root Component to the Document

internal/compiler/passes/embed_images.rs
@@ -15,21 +15,25 @@ use std::pin::Pin;
 use std::rc::Rc;
 
 pub async fn embed_images(
-    component: &Rc<Component>,
+    doc: &Document,
     embed_files: EmbedResourcesKind,
     scale_factor: f64,
     resource_url_mapper: &Option<Rc<dyn Fn(&str) -> Pin<Box<dyn Future<Output = Option<String>>>>>>,
     diag: &mut BuildDiagnostics,
 ) {
-    let global_embedded_resources = &component.embedded_file_resources;
+    if embed_files == EmbedResourcesKind::Nothing {
+        return;
+    }
 
-    let all_components = component
-        .used_types
-        .borrow()
+    let global_embedded_resources = &doc.embedded_file_resources;
+
+    let used_types = doc.root_component.used_types.borrow();
+
+    let all_components = used_types
         .sub_components
         .iter()
-        .chain(component.used_types.borrow().globals.iter())
-        .chain(std::iter::once(component))
+        .chain(used_types.globals.iter())
+        .chain(std::iter::once(&doc.root_component))
         .cloned()
         .collect::<Vec<_>>();
 

api/rs/build/lib.rs
@@ -411,7 +411,7 @@ pub fn compile_with_config(
     write!(code_formatter, "{}", generated).map_err(CompileError::SaveError)?;
     println!("cargo:rerun-if-changed={}", path.display());
 
-    for resource in doc.root_component.embedded_file_resources.borrow().keys() {
+    for resource in doc.embedded_file_resources.borrow().keys() {
         if !resource.starts_with("builtin:") {
             println!("cargo:rerun-if-changed={}", resource);
         }

internal/compiler/generator/cpp.rs
@@ -546,7 +546,7 @@ pub fn generate(
     file.includes.push("<limits>".into());
     file.includes.push("<slint.h>".into());
 
-    for (path, er) in doc.root_component.embedded_file_resources.borrow().iter() {
+    for (path, er) in doc.embedded_file_resources.borrow().iter() {
         match &er.kind {
             crate::embedded_resources::EmbeddedResourcesKind::RawData => {
                 let resource_file =

internal/compiler/generator/rust.rs
@@ -2794,8 +2794,7 @@ fn generate_resources(doc: &Document) -> Vec<TokenStream> {
     let link_section =
         std::env::var("SLINT_ASSET_SECTION").ok().map(|section| quote!(#[link_section = #section]));
 
-    doc.root_component
-        .embedded_file_resources
+    doc.embedded_file_resources
         .borrow()
         .iter()
         .map(|(path, er)| {

internal/compiler/lib.rs
@@ -42,6 +42,8 @@ use std::path::Path;
 /// Specify how the resources are embedded by the compiler
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum EmbedResourcesKind {
+    /// Embeds nothing (only useful for interpreter)
+    Nothing,
     /// Only embed builtin resources
     OnlyBuiltinResources,
     /// Embed all images resources (the content of their files)
@@ -123,6 +125,7 @@ impl CompilerConfiguration {
             match output_format {
                 #[cfg(feature = "rust")]
                 crate::generator::OutputFormat::Rust => EmbedResourcesKind::EmbedAllResources,
+                crate::generator::OutputFormat::Interpreter => EmbedResourcesKind::Nothing,
                 _ => EmbedResourcesKind::OnlyBuiltinResources,
             }
         };

internal/compiler/object_tree.rs
@@ -50,6 +50,11 @@ pub struct Document {
     /// startup for custom font use.
     pub custom_fonts: Vec<(String, crate::parser::SyntaxToken)>,
     pub exports: Exports,
+
+    /// Map of resources that should be embedded in the generated code, indexed by their absolute path on
+    /// disk on the build system
+    pub embedded_file_resources:
+        RefCell<HashMap<String, crate::embedded_resources::EmbeddedResources>>,
 }
 
 impl Document {
@@ -245,6 +250,7 @@ impl Document {
             local_registry,
             custom_fonts,
             exports,
+            embedded_file_resources: RefCell::default(),
         }
     }
 }
@@ -317,11 +323,6 @@ pub struct Component {
     /// List of elements that are not attached to the root anymore because they have been
     /// optimized away, but their properties may still be in use
     pub optimized_elements: RefCell<Vec<ElementRc>>,
-
-    /// Map of resources that should be embedded in the generated code, indexed by their absolute path on
-    /// disk on the build system
-    pub embedded_file_resources:
-        RefCell<HashMap<String, crate::embedded_resources::EmbeddedResources>>,
 
     /// The layout constraints of the root item
     pub root_constraints: RefCell<LayoutConstraints>,

internal/compiler/passes.rs
@@ -229,7 +229,7 @@ pub async fn run_passes(
     collect_globals::collect_globals(doc, diag);
 
     embed_images::embed_images(
-        root_component,
+        doc,
         type_loader.compiler_config.embed_resources,
         type_loader.compiler_config.scale_factor,
         &type_loader.compiler_config.resource_url_mapper,
@@ -257,7 +257,7 @@ pub async fn run_passes(
             }
 
             embed_glyphs::embed_glyphs(
-                root_component,
+                doc,
                 type_loader.compiler_config.scale_factor,
                 font_pixel_sizes,
                 characters_seen,
@@ -268,7 +268,7 @@ pub async fn run_passes(
         _ => {
             // Create font registration calls for custom fonts, unless we're embedding pre-rendered glyphs
             collect_custom_fonts::collect_custom_fonts(
-                root_component,
+                doc,
                 std::iter::once(doc).chain(type_loader.all_documents()),
                 type_loader.compiler_config.embed_resources
                     == crate::EmbedResourcesKind::EmbedAllResources,

internal/compiler/passes/collect_custom_fonts.rs
@@ -8,12 +8,11 @@ use crate::{
     object_tree::*,
 };
 use std::collections::BTreeSet;
-use std::rc::Rc;
 
 /// Fill the root_component's used_globals
 pub fn collect_custom_fonts<'a>(
-    root_component: &Rc<Component>,
-    all_docs: impl Iterator<Item = &'a crate::object_tree::Document> + 'a,
+    doc: &Document,
+    all_docs: impl Iterator<Item = &'a Document> + 'a,
     embed_fonts: bool,
 ) {
     let mut all_fonts = BTreeSet::new();
@@ -32,7 +31,7 @@ pub fn collect_custom_fonts<'a>(
         Box::new(|font_path| {
             Expression::NumberLiteral(
                 {
-                    let mut resources = root_component.embedded_file_resources.borrow_mut();
+                    let mut resources = doc.embedded_file_resources.borrow_mut();
                     let resource_id = match resources.get(font_path) {
                         Some(r) => r.id,
                         None => {
@@ -56,11 +55,11 @@ pub fn collect_custom_fonts<'a>(
         Box::new(|font_path| Expression::StringLiteral(font_path.clone()))
     };
 
-    root_component.init_code.borrow_mut().font_registration_code.extend(all_fonts.into_iter().map(
-        |font_path| Expression::FunctionCall {
+    doc.root_component.init_code.borrow_mut().font_registration_code.extend(
+        all_fonts.into_iter().map(|font_path| Expression::FunctionCall {
             function: Box::new(registration_function.clone()),
             arguments: vec![prepare_font_registration_argument(font_path)],
             source_location: None,
-        },
-    ));
+        }),
+    );
 }

commit 9755af739a57348a7a5619bbedcedfb1c99a6c07

Fix wasm slintpad not loading images
Regressions since 2fd8744cfcac76a633c085a6069fc91848221028 that disabled
the pass that maps the URL with the resource_url_mapper.
Even if we don't embed the ressource, we must still go through them to
map them

internal/compiler/passes/embed_images.rs
@@ -21,7 +21,7 @@ pub async fn embed_images(
     resource_url_mapper: &Option<Rc<dyn Fn(&str) -> Pin<Box<dyn Future<Output = Option<String>>>>>>,
     diag: &mut BuildDiagnostics,
 ) {
-    if embed_files == EmbedResourcesKind::Nothing {
+    if embed_files == EmbedResourcesKind::Nothing && resource_url_mapper.is_none() {
         return;
     }
 
@@ -90,8 +90,9 @@ fn embed_images_from_expression(
             let mapped_path =
                 urls.get(path).unwrap_or(&Some(path.clone())).clone().unwrap_or(path.clone());
             *path = mapped_path;
-            if embed_files != EmbedResourcesKind::OnlyBuiltinResources
-                || path.starts_with("builtin:/")
+            if embed_files != EmbedResourcesKind::Nothing
+                && (embed_files != EmbedResourcesKind::OnlyBuiltinResources
+                    || path.starts_with("builtin:/"))
             {
                 *resource_ref = embed_image(
                     global_embedded_resources,