slint-ui/slint

commit 977b827df2607262a29f47eab07cc68008d218bf

Add Rust API to grab the contents of a slint::Window into a slint::SharedImageBuffer (#5445)

internal/renderers/skia/lib.rs
@@ -13,8 +13,7 @@ use i_slint_core::api::{
 };
 use i_slint_core::graphics::euclid::{self, Vector2D};
 use i_slint_core::graphics::rendering_metrics_collector::RenderingMetricsCollector;
-use i_slint_core::graphics::BorderRadius;
-use i_slint_core::graphics::FontRequest;
+use i_slint_core::graphics::{BorderRadius, FontRequest, SharedImageBuffer, SharedPixelBuffer};
 use i_slint_core::item_rendering::{ItemCache, ItemRenderer};
 use i_slint_core::lengths::{
     LogicalLength, LogicalPoint, LogicalRect, LogicalSize, PhysicalPx, ScaleFactor,
@@ -220,7 +219,7 @@ impl SkiaRenderer {
         &self,
         rotation_angle_degrees: f32,
         translation: (f32, f32),
-        surace_size: PhysicalWindowSize,
+        surface_size: PhysicalWindowSize,
         post_render_cb: Option<&dyn Fn(&mut dyn ItemRenderer)>,
     ) -> Result<(), i_slint_core::platform::PlatformError> {
         let surface = self.surface.borrow();
@@ -242,96 +241,121 @@ impl SkiaRenderer {
 
         let window_adapter = self.window_adapter()?;
         let window = window_adapter.window();
-        let window_inner = WindowInner::from_pub(window);
 
         surface.set_scale_factor(window.scale_factor());
 
         surface.render(
-            surace_size,
-            &|skia_canvas, mut gr_context| {
-                skia_canvas.rotate(rotation_angle_degrees, None);
-                skia_canvas.translate(translation);
-
-                window_inner.draw_contents(|components| {
-                    let window_background_brush =
-                        window_inner.window_item().map(|w| w.as_pin_ref().background());
-
-                    // Clear with window background if it is a solid color otherwise it will drawn as gradient
-                    if let Some(Brush::SolidColor(clear_color)) = window_background_brush {
-                        skia_canvas.clear(itemrenderer::to_skia_color(&clear_color));
-                    }
-
-                    if let Some(callback) = self.rendering_notifier.borrow_mut().as_mut() {
-                        // For the BeforeRendering rendering notifier callback it's important that this happens *after* clearing
-                        // the back buffer, in order to allow the callback to provide its own rendering of the background.
-                        // Skia's clear() will merely schedule a clear call, so flush right away to make it immediate.
-                        if let Some(ctx) = gr_context.as_mut() {
-                            ctx.flush(None);
-                        }
-
-                        surface.with_graphics_api(&mut |api| {
-                            callback.notify(RenderingState::BeforeRendering, &api)
-                        })
-                    }
-
-                    let mut box_shadow_cache = Default::default();
-
-                    self.image_cache.clear_cache_if_scale_factor_changed(window);
-                    self.path_cache.clear_cache_if_scale_factor_changed(window);
-
-                    let mut item_renderer = itemrenderer::SkiaItemRenderer::new(
-                        skia_canvas,
-                        window,
-                        &self.image_cache,
-                        &self.path_cache,
-                        &mut box_shadow_cache,
-                    );
-
-                    // Draws the window background as gradient
-                    match window_background_brush {
-                        Some(Brush::SolidColor(..)) | None => {}
-                        Some(brush @ _) => {
-                            item_renderer.draw_rect(
-                                i_slint_core::lengths::logical_size_from_api(
-                                    window.size().to_logical(window_inner.scale_factor()),
-                                ),
-                                brush,
-                            );
-                        }
-                    }
-
-                    for (component, origin) in components {
-                        i_slint_core::item_rendering::render_component_items(
-                            component,
-                            &mut item_renderer,
-                            *origin,
-                        );
-                    }
-
-                    if let Some(collector) = &self.rendering_metrics_collector.borrow_mut().as_ref()
-                    {
-                        collector.measure_frame_rendered(&mut item_renderer);
-                    }
-
-                    if let Some(cb) = post_render_cb.as_ref() {
-                        cb(&mut item_renderer)
-                    }
-
-                    drop(item_renderer);
-
-                    if let Some(ctx) = gr_context.as_mut() {
-                        ctx.flush(None);
-                    }
-                });
-
-                if let Some(callback) = self.rendering_notifier.borrow_mut().as_mut() {
-                    surface.with_graphics_api(&mut |api| {
-                        callback.notify(RenderingState::AfterRendering, &api)
-                    })
-                }
+            surface_size,
+            &|skia_canvas, gr_context| {
+                self.render_to_canvas(
+                    skia_canvas,
+                    rotation_angle_degrees,
+                    translation,
+                    gr_context,
+                    None,
+                    window,
+                    post_render_cb,
+                );
             },
             &self.pre_present_callback,
         )
+    }
+
+    fn render_to_canvas(
+        &self,
+        skia_canvas: &skia_safe::Canvas,
+        rotation_angle_degrees: f32,
+        translation: (f32, f32),
+        mut gr_context: Option<&mut skia_safe::gpu::DirectContext>,
+        surface: Option<&dyn Surface>,
+        window: &i_slint_core::api::Window,
+        post_render_cb: Option<&dyn Fn(&mut dyn ItemRenderer)>,
+    ) {
+        skia_canvas.rotate(rotation_angle_degrees, None);
+        skia_canvas.translate(translation);
+
+        let window_inner = WindowInner::from_pub(window);
+
+        window_inner.draw_contents(|components| {
+            let window_background_brush =
+                window_inner.window_item().map(|w| w.as_pin_ref().background());
+
+            // Clear with window background if it is a solid color otherwise it will drawn as gradient
+            if let Some(Brush::SolidColor(clear_color)) = window_background_brush {
+                skia_canvas.clear(itemrenderer::to_skia_color(&clear_color));
+            }
+
+            if let Some(callback) = self.rendering_notifier.borrow_mut().as_mut() {
+                // For the BeforeRendering rendering notifier callback it's important that this happens *after* clearing
+                // the back buffer, in order to allow the callback to provide its own rendering of the background.
+                // Skia's clear() will merely schedule a clear call, so flush right away to make it immediate.
+                if let Some(ctx) = gr_context.as_mut() {
+                    ctx.flush(None);
+                }
+
+                if let Some(surface) = surface {
+                    surface.with_graphics_api(&mut |api| {
+                        callback.notify(RenderingState::BeforeRendering, &api)
+                    })
+                }
+            }
+
+            let mut box_shadow_cache = Default::default();
+
+            self.image_cache.clear_cache_if_scale_factor_changed(window);
+            self.path_cache.clear_cache_if_scale_factor_changed(window);
+
+            let mut item_renderer = itemrenderer::SkiaItemRenderer::new(
+                skia_canvas,
+                window,
+                &self.image_cache,
+                &self.path_cache,
+                &mut box_shadow_cache,
+            );
+
+            // Draws the window background as gradient
+            match window_background_brush {
+                Some(Brush::SolidColor(..)) | None => {}
+                Some(brush @ _) => {
+                    item_renderer.draw_rect(
+                        i_slint_core::lengths::logical_size_from_api(
+                            window.size().to_logical(window_inner.scale_factor()),
+                        ),
+                        brush,
+                    );
+                }
+            }
+
+            for (component, origin) in components {
+                i_slint_core::item_rendering::render_component_items(
+                    component,
+                    &mut item_renderer,
+                    *origin,
+                );
+            }
+
+            if let Some(collector) = &self.rendering_metrics_collector.borrow_mut().as_ref() {
+                collector.measure_frame_rendered(&mut item_renderer);
+            }
+
+            if let Some(cb) = post_render_cb.as_ref() {
+                cb(&mut item_renderer)
+            }
+
+            drop(item_renderer);
+
+            if let Some(ctx) = gr_context.as_mut() {
+                ctx.flush(None);
+            }
+        });
+
+        if let Some(callback) = self.rendering_notifier.borrow_mut().as_mut() {
+            if let Some(surface) = surface {
+                surface.with_graphics_api(&mut |api| {
+                    callback.notify(RenderingState::AfterRendering, &api)
+                })
+            }
+        }
     }
 
     fn window_adapter(&self) -> Result<Rc<dyn WindowAdapter>, PlatformError> {
@@ -523,6 +547,33 @@ impl i_slint_core::renderer::RendererSealed for SkiaRenderer {
         } else {
             Ok(())
         }
+    }
+
+    /// Returns an image buffer of what was rendered last by reading the previous front buffer (using glReadPixels).
+    fn screenshot(&self) -> Result<SharedImageBuffer, PlatformError> {
+        let window_adapter = self.window_adapter()?;
+        let window = window_adapter.window();
+        let size = window_adapter.window().size();
+        let (width, height) = (size.width, size.height);
+        let mut target_buffer =
+            SharedPixelBuffer::<i_slint_core::graphics::Rgba8Pixel>::new(width, height);
+
+        let mut surface_borrow = skia_safe::surfaces::wrap_pixels(
+            &skia_safe::ImageInfo::new(
+                (width as i32, height as i32),
+                skia_safe::ColorType::RGBA8888,
+                skia_safe::AlphaType::Opaque,
+                None,
+            ),
+            target_buffer.make_mut_bytes(),
+            None,
+            None,
+        )
+        .ok_or_else(|| format!("Error wrapping target buffer for rendering into with Skia"))?;
+
+        self.render_to_canvas(surface_borrow.canvas(), 0., (0.0, 0.0), None, None, window, None);
+
+        Ok(SharedImageBuffer::RGBA8(target_buffer))
     }
 }
 

internal/renderers/femtovg/lib.rs
@@ -11,9 +11,9 @@ use std::rc::{Rc, Weak};
 
 use i_slint_common::sharedfontdb;
 use i_slint_core::api::{RenderingNotifier, RenderingState, SetRenderingNotifierError};
-use i_slint_core::graphics::BorderRadius;
-use i_slint_core::graphics::FontRequest;
 use i_slint_core::graphics::{euclid, rendering_metrics_collector::RenderingMetricsCollector};
+use i_slint_core::graphics::{BorderRadius, SharedImageBuffer};
+use i_slint_core::graphics::{FontRequest, SharedPixelBuffer};
 use i_slint_core::item_rendering::ItemRenderer;
 use i_slint_core::items::TextWrap;
 use i_slint_core::lengths::{
@@ -538,6 +538,23 @@ impl RendererSealed for FemtoVGRenderer {
             self.opengl_context.resize(width, height)?;
         };
         return Ok(());
+    }
+
+    /// Returns an image buffer of what was rendered last by reading the previous front buffer (using glReadPixels).
+    fn screenshot(&self) -> Result<SharedImageBuffer, PlatformError> {
+        self.opengl_context.ensure_current()?;
+        let screenshot = self
+            .canvas
+            .borrow_mut()
+            .screenshot()
+            .map_err(|e| format!("FemtoVG error reading current back buffer: {e}"))?;
+
+        use rgb::ComponentBytes;
+        Ok(SharedImageBuffer::RGBA8(SharedPixelBuffer::clone_from_slice(
+            screenshot.buf().as_bytes(),
+            screenshot.width() as u32,
+            screenshot.height() as u32,
+        )))
     }
 }
 

internal/backends/qt/qt_window.rs
@@ -2313,6 +2313,28 @@ impl i_slint_core::renderer::RendererSealed for QtWindow {
     fn set_window_adapter(&self, _window_adapter: &Rc<dyn WindowAdapter>) {
         // No-op because QtWindow is also the WindowAdapter
     }
+
+    fn screenshot(&self) -> Result<SharedImageBuffer, PlatformError> {
+        let widget_ptr = self.widget_ptr();
+
+        let size = cpp! {unsafe [widget_ptr as "QWidget*"] -> qttypes::QSize as "QSize" {
+            return widget_ptr->size();
+        }};
+
+        let rgb8_data = cpp! {unsafe [widget_ptr as "QWidget*"] -> qttypes::QByteArray as "QByteArray" {
+            QPixmap pixmap = widget_ptr->grab();
+            QImage image = pixmap.toImage();
+            image.convertTo(QImage::Format_RGB888);
+            return QByteArray(reinterpret_cast<const char *>(image.constBits()), image.sizeInBytes());
+        }};
+
+        let buffer = i_slint_core::graphics::SharedPixelBuffer::<i_slint_core::graphics::Rgb8Pixel>::clone_from_slice(
+            rgb8_data.to_slice(),
+            size.width,
+            size.height,
+        );
+        Ok(i_slint_core::graphics::SharedImageBuffer::RGB8(buffer))
+    }
 }
 
 fn accessible_item(item: Option<ItemRc>) -> Option<ItemRc> {

internal/core/api.rs
@@ -9,6 +9,7 @@ This module contains types that are public and re-exported in the slint-rs as we
 
 #[cfg(target_has_atomic = "ptr")]
 pub use crate::future::*;
+use crate::graphics::SharedImageBuffer;
 use crate::input::{KeyEventType, MouseEvent};
 use crate::item_tree::ItemTreeVTable;
 use crate::window::{WindowAdapter, WindowInner};
@@ -621,6 +622,14 @@ impl Window {
     #[cfg(feature = "raw-window-handle-06")]
     pub fn window_handle(&self) -> WindowHandle {
         WindowHandle { adapter: self.0.window_adapter() }
+    }
+
+    /// Returns an image buffer with the rendered contents.
+    ///
+    /// Note that this function may be slow to call. Reading from the framebuffer previously
+    /// rendered, too, may take a long time.
+    pub fn grab_window(&self) -> Result<SharedImageBuffer, PlatformError> {
+        self.0.window_adapter().renderer().screenshot()
     }
 }
 

internal/core/renderer.rs
@@ -7,6 +7,7 @@ use alloc::rc::Rc;
 use core::pin::Pin;
 
 use crate::api::PlatformError;
+use crate::graphics::SharedImageBuffer;
 use crate::item_tree::ItemTreeRef;
 use crate::items::TextWrap;
 use crate::lengths::{LogicalLength, LogicalPoint, LogicalRect, LogicalSize, ScaleFactor};
@@ -115,5 +116,11 @@ pub trait RendererSealed {
 
     fn resize(&self, _size: crate::api::PhysicalSize) -> Result<(), PlatformError> {
         Ok(())
+    }
+
+    /// Re-implement this function to support Window::grab_window(), i.e. return
+    /// the contents of the window in an image buffer.
+    fn screenshot(&self) -> Result<SharedImageBuffer, PlatformError> {
+        Err("WindowAdapter::grab_window is not implemented by the platform".into())
     }
 }

internal/core/software_renderer.rs
@@ -12,7 +12,7 @@ mod fixed;
 mod fonts;
 
 use self::fonts::GlyphRenderer;
-use crate::api::Window;
+use crate::api::{PlatformError, Window};
 use crate::graphics::rendering_metrics_collector::{RefreshMode, RenderingMetricsCollector};
 use crate::graphics::{BorderRadius, PixelFormat, SharedImageBuffer, SharedPixelBuffer};
 use crate::item_rendering::{CachedRenderingData, DirtyRegion, RenderBorderRectangle, RenderImage};
@@ -790,6 +790,34 @@ impl RendererSealed for SoftwareRenderer {
     fn set_window_adapter(&self, window_adapter: &Rc<dyn WindowAdapter>) {
         *self.maybe_window_adapter.borrow_mut() = Some(Rc::downgrade(window_adapter));
         self.partial_cache.borrow_mut().clear();
+    }
+
+    fn screenshot(&self) -> Result<SharedImageBuffer, PlatformError> {
+        let Some(window_adapter) =
+            self.maybe_window_adapter.borrow().as_ref().and_then(|w| w.upgrade())
+        else {
+            return Err(
+                "SoftwareRenderer's screenshot called without a window adapter present".into()
+            );
+        };
+
+        let window = window_adapter.window();
+        let size = window.size();
+
+        let Some((width, height)) = size.width.try_into().ok().zip(size.height.try_into().ok())
+        else {
+            // Nothing to render
+            return Err("grab_window() called on window with invalid size".into());
+        };
+
+        let mut target_buffer = SharedPixelBuffer::<crate::graphics::Rgb8Pixel>::new(width, height);
+
+        self.set_repaint_buffer_type(RepaintBufferType::NewBuffer);
+        self.render(target_buffer.make_mut_slice(), width as usize);
+        // ensure that caches are clear for the next call
+        self.set_repaint_buffer_type(RepaintBufferType::NewBuffer);
+
+        Ok(SharedImageBuffer::RGB8(target_buffer))
     }
 }
 

commit 98a4325f3f7c78d4e7804002e4e2f3139633afa2

Skia: Fix missing BeforeRendering rendering notifier state invocations
Commit 977b827df2607262a29f47eab07cc68008d218bf regressed this by passing `None` as surface, so we'd never try to retrieve the graphics api enum to pass to the notifier.

internal/renderers/skia/lib.rs
@@ -273,7 +273,7 @@ impl SkiaRenderer {
                     rotation_angle_degrees,
                     translation,
                     gr_context,
-                    None,
+                    Some(surface.as_ref()),
                     window,
                     post_render_cb,
                 );