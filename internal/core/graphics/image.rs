882cbd15413c271e6fc80fdfb314a706dad38747
Simon Hausmann <simon.hausmann@slint.dev>
2024-07-08 16:57:48 +0200

    Add comment to make SharedImageBuffer non_exhaustive when it ever becomes public

--- internal/core/graphics/image.rs
+++ internal/core/graphics/image.rs
@@ -157,6 +157,7 @@ pub type Rgba8Pixel = rgb::RGBA8;
 /// images in pixels.
 #[derive(Clone, Debug)]
 #[repr(C)]
+/// TODO: Make this non_exhaustive before making the type public!
 pub enum SharedImageBuffer {
     /// This variant holds the data for an image where each pixel has three color channels (red, green,
     /// and blue) and each channel is encoded as unsigned byte.
