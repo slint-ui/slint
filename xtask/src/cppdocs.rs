f47d8b0675cc58f9b427cb36836a80fa29df0ffe
Simon Hausmann <simon.hausmann@slint.dev>
2024-07-10 15:29:10 +0200

    fixup! API review: enable all features in the C++ docs

--- xtask/src/cppdocs.rs
+++ xtask/src/cppdocs.rs
@@ -101,7 +101,7 @@ pub fn generate(show_warnings: bool) -> Result<(), Box<dyn std::error::Error>> {
         accessibility: true,
         system_testing: true,
         freestanding: true,
-        experimental: true,
+        experimental: false,
     };
     cbindgen::gen_all(&root, &generated_headers_dir, enabled_features)?;
 
