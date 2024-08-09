1639274486e5540b394c7bdb1c76ab3a99045ce6
Tobias Hunger <tobias.hunger@gmail.com>
2024-08-09 11:09:26 +0000

    slintpad/vscode: Get rid of the shared files
    Theyre are no more shared files, so move the files
    into vscode and set up a syntax check over there.
    
    That was not possible before as the typescript
    compiler would error out on the shared files being
    outside its root directory.

--- editors/vscode/src/lsp_commands.ts
+++ editors/vscode/src/lsp_commands.ts
@@ -1,16 +1,8 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
 
-import {
-    OptionalVersionedTextDocumentIdentifier,
-    Range as LspRange,
-    URI as LspURI,
-    WorkspaceEdit,
-} from "vscode-languageserver-types";
-
+import { URI as LspURI } from "vscode-languageserver-types";
 import * as vscode from "vscode";
-
-export type WorkspaceEditor = (_we: WorkspaceEdit) => boolean;
 
 // Use the auto-registered VSCode command for the custom executables offered
 // by our language server.
@@ -31,17 +23,4 @@ export async function showPreview(
     component: string,
 ): Promise<unknown> {
     return vscode.commands.executeCommand("slint/showPreview", url, component);
-}
-
-export async function removeBinding(
-    doc: OptionalVersionedTextDocumentIdentifier,
-    element_range: LspRange,
-    property_name: string,
-): Promise<boolean> {
-    return vscode.commands.executeCommand(
-        "slint/removeBinding",
-        doc,
-        element_range,
-        property_name,
-    );
 }
