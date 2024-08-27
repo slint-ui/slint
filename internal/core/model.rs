d271f57925593ecbd7da001d66dc4f0997153ad8
Florian Blasius <florian.blasius@slint.dev>
2024-08-27 12:17:54 +0200

    Added swap to VecModel

--- internal/core/model.rs
+++ internal/core/model.rs
@@ -376,6 +376,15 @@ impl<T: 'static> VecModel<T> {
         self.array.borrow_mut().clear();
         self.notify.reset();
     }
+
+    /// Swaps two elements in the model.
+    ///
+    /// Similar to [`Vec::swap`]
+    pub fn swap(&self, a: usize, b: usize) {
+        self.array.borrow_mut().swap(a, b);
+        self.notify.row_changed(a);
+        self.notify.row_changed(b);
+    }
 }
 
 impl<T: Clone + 'static> VecModel<T> {
