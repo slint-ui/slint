9eac08d859b592ba174f14a2b02eff9334d66e30
Florian Blasius <florian.blasius@slint.dev>
2024-09-30 09:36:35 +0200

    Code review feedback

--- internal/compiler/passes/apply_default_properties_from_style.rs
+++ internal/compiler/passes/apply_default_properties_from_style.rs
@@ -60,7 +60,7 @@ pub fn apply_default_properties_from_style(
                     });
                 }
                 "Text" => {
-                    elem.set_binding_if_not_set("clor".into(), || Expression::Cast {
+                    elem.set_binding_if_not_set("color".into(), || Expression::Cast {
                         from: Expression::PropertyReference(NamedReference::new(
                             &palette.root_element,
                             "foreground",
