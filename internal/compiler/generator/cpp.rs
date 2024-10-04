eb0576d364f3e627f4c6bdbfc729289fbce95df9
Jakub Zeifert <kuba1102@gmail.com>
2024-10-04 11:28:13 +0200

    Refactor cpp.rs formatting and change lazy_static to OnceLock for Cpp_keyword check

--- internal/compiler/generator/cpp.rs
+++ internal/compiler/generator/cpp.rs
@@ -8,11 +8,10 @@
 
 use std::fmt::Write;
 use std::io::BufWriter;
-
+use std::collections::HashSet;
+use std::sync::OnceLock;
 use lyon_path::geom::euclid::approxeq::ApproxEq;
 
-use lazy_static::lazy_static;
-use std::collections::HashSet;
 
 /// The configuration for the C++ code generator
 #[derive(Clone, Debug, Default, PartialEq)]
@@ -22,115 +21,29 @@ pub struct Config {
     pub header_include: String,
 }
 
-// static initialization of C++ keywords Hashset
-lazy_static! {
-    static ref CPP_KEYWORDS: HashSet<&'static str> = {
-        let keywords: HashSet<&str> = HashSet::from([
-            "alignas",
-            "alignof",
-            "and",
-            "and_eq",
-            "asm",
-            "atomic_cancel",
-            "atomic_commit",
-            "atomic_noexcept",
-            "auto",
-            "bitand",
-            "bitor",
-            "bool",
-            "break",
-            "case",
-            "catch",
-            "char",
-            "char8_t",
-            "char16_t",
-            "char32_t",
-            "class",
-            "compl",
-            "concept",
-            "const",
-            "consteval",
-            "constexpr",
-            "constinit",
-            "const_cast",
-            "continue",
-            "co_await",
-            "co_return",
-            "co_yield",
-            "decltype",
-            "default",
-            "delete",
-            "do",
-            "double",
-            "dynamic_cast",
-            "else",
-            "enum",
-            "explicit",
-            "export",
-            "extern",
-            "false",
-            "float",
-            "for",
-            "friend",
-            "goto",
-            "if",
-            "inline",
-            "int",
-            "long",
-            "mutable",
-            "namespace",
-            "new",
-            "noexcept",
-            "not",
-            "not_eq",
-            "nullptr",
-            "operator",
-            "or",
-            "or_eq",
-            "private",
-            "protected",
-            "public",
-            "reflexpr",
-            "register",
-            "reinterpret_cast",
-            "requires",
-            "return",
-            "short",
-            "signed",
-            "sizeof",
-            "static",
-            "static_assert",
-            "static_cast",
-            "struct",
-            "switch",
-            "synchronized",
-            "template",
-            "this",
-            "thread_local",
-            "throw",
-            "true",
-            "try",
-            "typedef",
-            "typeid",
-            "typename",
-            "union",
-            "unsigned",
-            "using",
-            "virtual",
-            "void",
-            "volatile",
-            "wchar_t",
-            "while",
-            "xor",
-            "xor_eq",
-        ]);
-        keywords
-    };
-}
 
 // Check if word is one of C++ keywords
 fn is_cpp_keyword(word: &str) -> bool {
-    CPP_KEYWORDS.contains(word)
+    static CPP_KEYWORDS: OnceLock<HashSet<&'static str>> = OnceLock::new();
+    let keywords = CPP_KEYWORDS.get_or_init(|| {
+        let keywords: HashSet<&str> = HashSet::from([
+            "alignas", "alignof", "and", "and_eq", "asm", "atomic_cancel", "atomic_commit",
+            "atomic_noexcept", "auto", "bitand", "bitor", "bool", "break", "case", "catch",
+            "char", "char8_t", "char16_t", "char32_t", "class", "compl", "concept", "const",
+            "consteval", "constexpr", "constinit", "const_cast", "continue", "co_await",
+            "co_return", "co_yield", "decltype", "default", "delete", "do", "double",
+            "dynamic_cast", "else", "enum", "explicit", "export", "extern", "false", "float",
+            "for", "friend", "goto", "if", "inline", "int", "long", "mutable", "namespace",
+            "new", "noexcept", "not", "not_eq", "nullptr", "operator", "or", "or_eq", "private",
+            "protected", "public", "reflexpr", "register", "reinterpret_cast", "requires",
+            "return", "short", "signed", "sizeof", "static", "static_assert", "static_cast",
+            "struct", "switch", "synchronized", "template", "this", "thread_local", "throw",
+            "true", "try", "typedef", "typeid", "typename", "union", "unsigned", "using",
+            "virtual", "void", "volatile", "wchar_t", "while", "xor", "xor_eq",
+        ]);
+        keywords
+    });
+    keywords.contains(word)
 }
 
 fn ident(ident: &str) -> String {
