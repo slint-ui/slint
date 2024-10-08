a41ef3a7b78461d2c839a8a61bace4be088d4b85
Aurindam Jana <aurindam.jana@slint.dev>
2024-10-08 15:08:13 +0200

    Add Live Preview

--- docs/reference/_ext/codemirror.py
+++ docs/reference/_ext/codemirror.py
@@ -11,13 +11,14 @@ class CodeMirrorNode(nodes.General, nodes.Element):
 
 class CodeMirrorDirective(SphinxDirective):
     has_content = True
-    required_arguments = 0  # We require one argument: the programming language
+    required_arguments = 0  # 0 means argument is optional
+    optional_arguments = 1  # Allows one optional positional argument
     option_spec = {
         'language': directives.unchanged,
     }
 
     def run(self):
-        language = self.arguments[0]
+        language = self.arguments[0] if self.arguments else self.options.get('language', 'javascript')
         content = '\n'.join(self.content)
         
         # Generate a unique ID for the editor (e.g., using a hash of the content)
@@ -28,15 +29,20 @@ class CodeMirrorDirective(SphinxDirective):
         node = CodeMirrorNode()
         node['content'] = content
         node['editor_id'] = editor_id
+        node['language'] = language
         return [node]
 
 def visit_codemirror_node(self, node):
     language = node['language']
     content = node['content']
     editor_id = node['editor_id']
+    readonly = 'true'
 
     # Create a container for the editor with a unique ID
-    self.body.append(f'<div id="{editor_id}" class="codemirror-editor" data-lang="{language}" style="height: 300px;"><div class="codemirror-content" style="display:none">{content}"</div></div>')
+    if language == 'slint':
+        readonly = 'false'
+    
+    self.body.append(f'<div id="{editor_id}" class="codemirror-editor" data-lang="{language}" data-readonly="{readonly}"><div class="codemirror-content" style="display:none">{content}</div></div>')
 
 def depart_codemirror_node(self, node):
     pass
