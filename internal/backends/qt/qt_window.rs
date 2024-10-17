slint-ui/slint

commit 5718b1589904ef8b2666e4cfc0bbb661a6eaa5c4

Qt platform: use QWindow::requestUpdate instead of QWidget::update
Otherwise we may repaint too fast

Fixes #3193

internal/backends/qt/qt_window.rs
@@ -1451,20 +1451,9 @@ impl QtWindow {
                 collector.measure_frame_rendered(&mut renderer);
             }
 
-            i_slint_core::animations::CURRENT_ANIMATION_DRIVER.with(|driver| {
-                if !driver.has_active_animations() {
-                    return;
-                }
-                let widget_ptr = self.widget_ptr();
-                cpp! {unsafe [widget_ptr as "QWidget*"] {
-                    // FIXME: using QTimer -::singleShot is not optimal. We should use Qt animation timer
-                    QTimer::singleShot(16, [widget_ptr = QPointer<QWidget>(widget_ptr)] {
-                        if (widget_ptr)
-                            widget_ptr->update();
-                    });
-                    //return widget_ptr->update();
-                }}
-            });
+            if self.window.has_active_animations() {
+                self.request_redraw();
+            }
         });
 
         // Update the accessibility tree (if the component tree has changed)
@@ -1616,7 +1605,9 @@ impl WindowAdapter for QtWindow {
     fn request_redraw(&self) {
         let widget_ptr = self.widget_ptr();
         cpp! {unsafe [widget_ptr as "QWidget*"] {
-            return widget_ptr->update();
+            if (auto w = widget_ptr->window()->windowHandle()) {
+                w->requestUpdate();
+            }
         }}
     }
 

commit d645a8f4eb4b64198b54106e2289c6dbb876d23b

Fix missing redraws when embedding Slint into QWidget hierarchy
Commit 5718b1589904ef8b2666e4cfc0bbb661a6eaa5c4 regressed this by
issuing the update on the window, which breaks when the widget is
embedded in a hierarchy (like a QGraphicsView). We really need to mark
the widget as dirty, even if that means we don't get frame throttling
then.

internal/backends/qt/qt_window.rs
@@ -1873,7 +1873,11 @@ impl WindowAdapter for QtWindow {
     fn request_redraw(&self) {
         let widget_ptr = self.widget_ptr();
         cpp! {unsafe [widget_ptr as "QWidget*"] {
-            if (auto w = widget_ptr->window()->windowHandle()) {
+            // If embedded as a QWidget, just use regular QWidget::update(), but if we're a top-level,
+            // then use requestUpdate() to achieve frame-throttling.
+            if (widget_ptr->parentWidget()) {
+                widget_ptr->update();
+            } else if (auto w = widget_ptr->window()->windowHandle()) {
                 w->requestUpdate();
             }
         }}