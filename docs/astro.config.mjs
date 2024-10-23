0743e0aefada1678f071201527cf3c38dd19e557
Nigel Breslaw <nigel.breslaw@slint.dev>
2024-10-23 20:44:27 +0300

    Add biome and fix formatting
    Lint fixes

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import fs from 'node:fs';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import fs from "node:fs";
 
 // https://astro.build/config
 export default defineConfig({
