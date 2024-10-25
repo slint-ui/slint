e0557536a66e409668db65fa117482104dac368d
Nigel Breslaw <nigel.breslaw@slint.dev>
2024-10-25 15:55:03 +0200

    Add new home-automation demo (#6654)

---
+++ demos/home-automation/node/main.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: MIT
+
+import * as slint from "slint-ui";
+
+const ui = slint.loadFile("../ui/demo.slint");
+const window = new ui.AppWindow();
+
+await window.run();
