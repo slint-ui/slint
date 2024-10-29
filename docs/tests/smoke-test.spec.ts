d4f15edf522422c1e1024f8ff7fd0bd80e6dba17
Nigel Breslaw <nigel.breslaw@slint.dev>
2024-10-29 13:33:29 +0200

    tidy
    Ignore reports

--- docs/tests/smoke-test.spec.ts
+++ docs/tests/smoke-test.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('smoke test', async ({ page }) => {
   await page.goto('http://localhost:4321/tng');
   await expect(page.getByRole('banner')).toContainText('Slint Language Docs');
   await page.getByRole('link', { name: 'Language Docs', exact: true }).click();
