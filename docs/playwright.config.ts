844e66669e1bb63249b0e58e5153499667554e67
Nigel Breslaw <nigel.breslaw@slint.dev>
2024-10-29 12:17:19 +0200

    Seems slow way is the best way
    Fix test report upload
    
    Test report publishing

--- docs/playwright.config.ts
+++ docs/playwright.config.ts
@@ -22,7 +22,16 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: "html",
+    reporter: [
+        ["html"], // You can combine multiple reporters
+        [
+            "playwright-ctrf-json-reporter",
+            {
+                outputFile: "ctrf-report.json",
+                outputDir: "playwright-report",
+            },
+        ],
+    ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
