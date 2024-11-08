0362df45f8c6417c0b239818d1537acb97bd89d7
Nigel Breslaw <nigel.breslaw@slint.dev>
2024-11-08 14:23:29 +0200

    Remove extra landing page

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -32,7 +32,7 @@ export default defineConfig({
             },
             sidebar: [
                 {
-                    slug: "overview",
+                    slug: "index",
                 },
                 {
                     label: "Getting started",
