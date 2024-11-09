43ff8880fc374f921246ad7124d540aaa1bb9a8c
Nigel Breslaw <nigel.breslaw@slint.dev>
2024-11-09 16:07:31 +0200

    Fix smoke test

--- docs/tests/smoke-test.spec.ts
+++ docs/tests/smoke-test.spec.ts
@@ -1,8 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('smoke test', async ({ page }) => {
-  await page.goto('http://localhost:4321/tng');
-  await expect(page.getByRole('banner')).toContainText('Slint Language Docs');
-  await page.getByRole('link', { name: 'Language Docs', exact: true }).click();
-  await expect(page.locator('sl-sidebar-state-persist')).toContainText('Getting started');
+test("smoke test", async ({ page }) => {
+    await page.goto("http://localhost:4321/tng");
+    await expect(page.locator('[id="_top"]')).toContainText("Welcome");
+    await expect(page.getByRole("main")).toContainText(
+        "Get started building your product with Slint.",
+    );
+    await page
+        .locator("summary")
+        .filter({ hasText: "Reference" })
+        .first()
+        .click();
+    await page.getByRole("link", { name: "Rectangle" }).click();
+    await page.getByRole("link", { name: "border-color" }).click();
+    await expect(
+        page.getByLabel("On this page", { exact: true }),
+    ).toContainText("border-color");
 });
