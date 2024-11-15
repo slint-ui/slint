b62f3603f6b35848fac37ccf4d5e3893cb25519c
Nigel Breslaw <nigel.breslaw@slint.dev>
2024-11-15 23:28:20 +0200

    Put back missing _templates to help nightly snapshots

--- xtask/src/license_headers_check.rs
+++ xtask/src/license_headers_check.rs
@@ -435,7 +435,7 @@ lazy_static! {
         ("^editors/tree-sitter-slint/corpus/", LicenseLocation::NoLicense), // liberal license
         ("^api/cpp/docs/_static/", LicenseLocation::NoLicense),
         ("^api/cpp/docs/_templates/", LicenseLocation::NoLicense),
-        ("^docs/_templates/", LicenseLocation::NoLicense),
+        ("^docs/src/utils/_templates/", LicenseLocation::NoLicense),
         ("^docs/quickstart/theme/", LicenseLocation::NoLicense),
         ("^editors/tree-sitter-slint/queries/", LicenseLocation::NoLicense), // liberal license
 
