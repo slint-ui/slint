9ce4bd4fd82a33b853769b8be5b256e3b1965734
Nigel Breslaw <nigel.breslaw@slint.dev>
2024-11-21 17:48:45 +0200

    Remove cypress

--- tools/slintpad/cypress/support/commands.ts
+++
@@ -1,6 +0,0 @@
-// Copyright © SixtyFPS GmbH <info@slint.dev>
-// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
-
-// cSpell: ignore frsource
-
-import "@frsource/cypress-plugin-visual-regression-diff/dist/support";
