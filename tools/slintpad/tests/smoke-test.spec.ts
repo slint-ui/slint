598f5acbf5e34351cee219695a34b919b6cbcea5
Nigel Breslaw <nigel.breslaw@slint.dev>
2024-11-22 11:06:34 +0200

    Make smoke test more robust

--- tools/slintpad/tests/smoke-test.spec.ts
+++ tools/slintpad/tests/smoke-test.spec.ts
@@ -2,8 +2,12 @@
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
 import { test, expect } from "@playwright/test";
 
-test("test", async ({ page }) => {
+test("smoke test", async ({ page }) => {
+    const logs: string[] = [];
+    page.on("console", (msg) => logs.push(msg.text()));
     await page.goto("http://localhost:3000/");
     await expect(page.getByRole("menubar")).toContainText("Project");
     await expect(page.locator("#tab-key-1-0")).toContainText("main.slint");
+    console.log(logs);
+    expect(logs).toContain("UI should be up!");
 });
