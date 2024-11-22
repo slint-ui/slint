114d97c9b2c1719ca71a6fcc106beb6c1e64b7ef
Nigel Breslaw <nigel.breslaw@slint.dev>
2024-11-22 12:51:24 +0200

    give up on using console.info

--- tools/slintpad/tests/smoke-test.spec.ts
+++ tools/slintpad/tests/smoke-test.spec.ts
@@ -3,11 +3,7 @@
 import { test, expect } from "@playwright/test";
 
 test("smoke test", async ({ page }) => {
-    const logs: string[] = [];
-    page.on("console", (msg) => logs.push(msg.text()));
     await page.goto("http://localhost:3000/");
     await expect(page.getByRole("menubar")).toContainText("Project");
     await expect(page.locator("#tab-key-1-0")).toContainText("main.slint");
-    console.log(logs);
-    expect(logs).toContain("UI should be up!");
 });
