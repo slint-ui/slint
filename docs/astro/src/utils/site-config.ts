004b790e4e59e07d6573dc9244af6c0b48514cdf
Simon Hausmann <simon.hausmann@slint.dev>
2024-12-19 10:08:16 +0100

    Fix base path for snapshot doc builds

--- docs/astro/src/utils/site-config.ts
+++ docs/astro/src/utils/site-config.ts
@@ -2,4 +2,4 @@
 // SPDX-License-Identifier: MIT
 
 export const BASE_URL = "https://snapshots.slint.dev";
-export const BASE_PATH = "/1.9.0/docs/slint/";
+export const BASE_PATH = "/master/docs/slint/";
