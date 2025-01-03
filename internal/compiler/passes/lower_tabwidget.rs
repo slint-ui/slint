14466d1be6cbe3b61bc291344db1227cb4385064
Arnold Loubriat <datatriny@gmail.com>
2025-01-03 21:08:14 +0100

    Set `accessible-label` on tab panels

--- internal/compiler/passes/lower_tabwidget.rs
+++ internal/compiler/passes/lower_tabwidget.rs
@@ -125,6 +125,17 @@ fn process_tabwidget(
                 &old.into_inner(),
             );
         }
+        let title_ref = RefCell::new(
+            Expression::PropertyReference(NamedReference::new(child, "title".into())).into(),
+        );
+        let old = child.borrow_mut().bindings.insert("accessible-label".into(), title_ref);
+        if let Some(old) = old {
+            diag.push_error(
+                "The property 'accessible-label' cannot be set for Tabs inside a TabWidget"
+                    .to_owned(),
+                &old.into_inner(),
+            );
+        }
 
         let mut tab = Element {
             id: format_smolstr!("{}-tab{}", elem.borrow().id, index),
