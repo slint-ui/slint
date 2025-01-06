540e5931cf0d45614167809bbf6fd27cc408a755
Andrew Wagner <andrew@otiv.ai>
2025-01-06 16:12:35 +0100

    Add a trivial widget library

--- examples/bazel_build/src/build.rs
+++ examples/bazel_build/src/build.rs
@@ -1,7 +1,11 @@
+use std::path::PathBuf;
 use std::collections::HashMap;
+use widget_library_utils::WIDGET_LIBRARY_NAME;
 
 fn main() {
-    let library_paths = HashMap::new();
+    let mut library_paths = HashMap::new();
+    let ui_lib_path = PathBuf::from(std::env::var_os("SLINT_WIDGET_LIBRARY_INDEX_PATH").unwrap());
+    library_paths.insert(WIDGET_LIBRARY_NAME.to_string(), ui_lib_path);
 
     slint_build::compile_with_config(
         std::env::var_os("SLINT_INDEX_FILE_PATH").unwrap(),
---
+++ examples/bazel_build/widget_library/lib.rs
@@ -0,0 +1,1 @@
+pub const WIDGET_LIBRARY_NAME: &str = "initech_widgets";
