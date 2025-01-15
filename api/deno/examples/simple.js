7db20aa4f6952a768117b7f04b86de26b95a3555
FloVanGH <florian.blasius@slint.dev>
2025-01-15 15:02:59 +0100

    deno runtime: small async test (comment out call)

--- api/deno/examples/simple.js
+++ api/deno/examples/simple.js
@@ -1,1 +1,12 @@
 console.log("hello from js");
+
+var test = 0;
+
+async function asyncCall() {
+    while (true) {
+        test++;
+        console.log("update " + test);
+    }
+}
+
+// asyncCall();
