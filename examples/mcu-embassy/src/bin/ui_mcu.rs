206b7c83635824966d3a68f72a39114cd56f27fe
David Haig <david@ninjametal.com>
2025-02-02 01:41:15 +0000

    Change touchscreen i2c to blocking and add comments

--- examples/mcu-embassy/src/bin/ui_mcu.rs
+++ examples/mcu-embassy/src/bin/ui_mcu.rs
@@ -10,19 +10,20 @@
 #![no_std]
 #![no_main]
 #![macro_use]
-#![allow(static_mut_refs)]
 
 extern crate alloc;
 
+use core::{mem::MaybeUninit, ptr::addr_of_mut};
+
 use alloc::{boxed::Box, rc::Rc};
-use defmt::{info, unwrap};
+use defmt::{error, info, unwrap};
 use embassy_executor::Spawner;
 use embassy_futures::select::{select, Either};
 use embassy_stm32::{
     bind_interrupts,
     exti::ExtiInput,
     gpio::{Level, Output, Pull, Speed},
-    i2c::{self, I2c},
+    i2c::I2c,
     ltdc::{
         self, Ltdc, LtdcConfiguration, LtdcLayer, LtdcLayerConfig, PolarityActive, PolarityEdge,
     },
@@ -31,7 +32,7 @@ use embassy_stm32::{
     time::Hertz,
 };
 use embassy_time::{Duration, Timer};
-use gt911::Gt911;
+use gt911::Gt911Blocking;
 use mcu_embassy::{
     controller::{self, Action, Controller},
     mcu::{double_buffer::DoubleBuffer, hardware::HardwareMcu, rcc_setup, ALLOCATOR},
@@ -45,30 +46,33 @@ use slint::{
     ComponentHandle,
 };
 use slint_generated::MainWindow;
+use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
 const MY_TASK_POOL_SIZE: usize = 2;
 const HEAP_SIZE: usize = 200 * 1024;
-static mut HEAP: [u8; HEAP_SIZE] = [0; HEAP_SIZE];
-
-// the following two display buffers consume 1536000 bytes that just about fits into the ram found on the mcu
-static mut FB1: [TargetPixelType; DISPLAY_WIDTH * DISPLAY_HEIGHT] =
-    [Rgb565Pixel(0); DISPLAY_WIDTH * DISPLAY_HEIGHT];
-static mut FB2: [TargetPixelType; DISPLAY_WIDTH * DISPLAY_HEIGHT] =
-    [Rgb565Pixel(0); DISPLAY_WIDTH * DISPLAY_HEIGHT];
 
 bind_interrupts!(struct Irqs {
     LTDC => ltdc::InterruptHandler<peripherals::LTDC>;
-    I2C2_EV => i2c::EventInterruptHandler<peripherals::I2C2>;
-    I2C2_ER => i2c::ErrorInterruptHandler<peripherals::I2C2>;
 });
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = rcc_setup::stm32u5g9zj_init();
 
+    static HEAP: StaticCell<[u8; HEAP_SIZE]> = StaticCell::new();
+    static FB1: StaticCell<[TargetPixelType; DISPLAY_WIDTH * DISPLAY_HEIGHT]> = StaticCell::new();
+    static FB2: StaticCell<[TargetPixelType; DISPLAY_WIDTH * DISPLAY_HEIGHT]> = StaticCell::new();
+
+    // Safety: HEAP.uninit() will panic if HEAP has already been set. The rest of the function writes zeros to the memory region without allocating memory on the stack
+    // Reasoning: HEAP is a very large buffer so we cannot have a copy of it on the stack and this is a way around that
+    let heap = unsafe { init_heap_in_place(HEAP.uninit()) };
+    let heap_size = core::mem::size_of_val(heap);
+    assert!(heap_size > 0);
+
     // setup an allocator
-    unsafe { ALLOCATOR.init(&mut HEAP as *const u8 as usize, core::mem::size_of_val(&HEAP)) }
+    // Safety: this is only called once and heapsize has already been checked
+    unsafe { ALLOCATOR.init(heap as *const u8 as usize, heap_size) }
 
     // enable instruction cache
     embassy_stm32::pac::ICACHE.cr().write(|w| {
@@ -76,26 +80,28 @@ async fn main(spawner: Spawner) {
     });
 
     // enable data cache 1
+    // NOTE: be careful of using dcache as some stm32 mcus (especially stm32h7 lines) do not work well with DMA and data cache
+    // you may need to disable dcache for specific memory regions (for example FB1 and FB2) or disable dcache altogether
     embassy_stm32::pac::DCACHE1.cr().write(|w| {
         w.set_en(true);
     });
 
-    // enable data cache 2
+    // disable data cache 2
     embassy_stm32::pac::DCACHE2.cr().write(|w| {
         w.set_en(true);
     });
 
+    // Safety: FB1.uninit() will panic if FB1 has already been set. The rest of the function writes zeros to the memory region without allocating memory on the stack
+    // Reasoning: FB1 is a very large buffer so we cannot have a copy of it on the stack and this is a way around that
+    let fb1 = unsafe { init_fb_in_place(FB1.uninit()) };
+
+    // Safety: same as above
+    let fb2 = unsafe { init_fb_in_place(FB2.uninit()) };
+
     // used for the touch events
-    let i2c = I2c::new(
-        p.I2C2,
-        p.PF1,
-        p.PF0,
-        Irqs,
-        p.GPDMA1_CH0,
-        p.GPDMA1_CH1,
-        Hertz(100_000),
-        Default::default(),
-    );
+    // NOTE: Async i2c communication returns a Timeout error so we will use blocking i2c until this is fixed
+    let i2c: I2c<'_, mode::Blocking> =
+        I2c::new_blocking(p.I2C2, p.PF1, p.PF0, Hertz(100_000), Default::default());
 
     // TASK: blink the red led on another task
     let red_led = Output::new(p.PD2, Level::High, Speed::Low);
@@ -184,10 +190,9 @@ async fn main(spawner: Spawner) {
     // enable the bottom layer
     ltdc.init_layer(&layer_config, None);
 
-    // Safety: the DoubleBuffer controls access to the statically allocated frame buffers
+    // the DoubleBuffer controls access to the statically allocated frame buffers
     // and it is the only thing that mutates their content
-    let double_buffer =
-        DoubleBuffer::new(unsafe { FB1.as_mut() }, unsafe { FB2.as_mut() }, layer_config);
+    let double_buffer = DoubleBuffer::new(fb1, fb2, layer_config);
 
     // create a slint window and register it with slint
     let window = MinimalSoftwareWindow::new(RepaintBufferType::SwappedBuffers);
@@ -208,6 +213,24 @@ async fn main(spawner: Spawner) {
     // run the controller event loop
     let mut controller = Controller::new(&main_window, hardware);
     controller.run().await;
+}
+
+unsafe fn init_heap_in_place(buf: &mut MaybeUninit<[u8; HEAP_SIZE]>) -> &mut [u8; HEAP_SIZE] {
+    let ptr = buf.as_mut_ptr();
+    addr_of_mut!((*ptr)).write([0u8; HEAP_SIZE]);
+
+    // Safety: we have written valid bytes to the data structure
+    buf.assume_init_mut()
+}
+
+unsafe fn init_fb_in_place(
+    buf: &mut MaybeUninit<[TargetPixelType; DISPLAY_WIDTH * DISPLAY_HEIGHT]>,
+) -> &mut [TargetPixelType; DISPLAY_WIDTH * DISPLAY_HEIGHT] {
+    let ptr = buf.as_mut_ptr();
+    addr_of_mut!((*ptr)).write([Rgb565Pixel(0); DISPLAY_WIDTH * DISPLAY_HEIGHT]);
+
+    // Safety: we have written valid bytes to the data structure
+    buf.assume_init_mut()
 }
 
 #[embassy_executor::task(pool_size = MY_TASK_POOL_SIZE)]
@@ -264,17 +287,17 @@ pub async fn render_loop(
     window: Rc<MinimalSoftwareWindow>,
     mut double_buffer: DoubleBuffer,
     mut ltdc: Ltdc<'static, peripherals::LTDC>,
-    mut i2c: I2c<'static, mode::Async>,
+    mut i2c: I2c<'static, mode::Blocking>,
 ) {
     let mut last_touch: Option<slint::LogicalPosition> = None;
-    let touch = Gt911::default();
-    touch.init(&mut i2c).await.unwrap();
+    let touch = Gt911Blocking::default();
+    touch.init(&mut i2c).unwrap();
 
     loop {
         slint::platform::update_timers_and_animations();
 
         // process touchscreen events
-        process_touch(&touch, &mut i2c, &mut last_touch, window.clone()).await;
+        process_touch(&touch, &mut i2c, &mut last_touch, window.clone());
 
         // blocking render
         let is_dirty = window.draw_if_needed(|renderer| {
@@ -286,42 +309,50 @@ pub async fn render_loop(
             // async transfer of frame buffer to lcd
             double_buffer.swap(&mut ltdc).await.unwrap();
         } else {
-            Timer::after(Duration::from_millis(10)).await;
+            Timer::after_millis(10).await
         }
     }
 }
 
-async fn process_touch(
-    touch: &Gt911<I2c<'static, mode::Async>>,
-    i2c: &mut I2c<'static, mode::Async>,
+fn process_touch(
+    touch: &Gt911Blocking<I2c<'static, mode::Blocking>>,
+    i2c: &mut I2c<'static, mode::Blocking>,
     last_touch: &mut Option<slint::LogicalPosition>,
     window: Rc<MinimalSoftwareWindow>,
 ) {
     // process touchscreen touch events
-    if let Ok(point) = touch.get_touch(i2c).await {
-        let button = PointerEventButton::Left;
-        let event = match point {
-            Some(point) => {
-                let position = slint::PhysicalPosition::new(point.x as i32, point.y as i32)
-                    .to_logical(window.scale_factor());
-                Some(match last_touch.replace(position) {
-                    Some(_) => WindowEvent::PointerMoved { position },
-                    None => WindowEvent::PointerPressed { position, button },
-                })
-            }
-            None => {
-                last_touch.take().map(|position| WindowEvent::PointerReleased { position, button })
-            }
-        };
+    match touch.get_touch(i2c) {
+        Ok(point) => {
+            let button = PointerEventButton::Left;
+            let event = match point {
+                Some(point) => {
+                    let position = slint::PhysicalPosition::new(point.x as i32, point.y as i32)
+                        .to_logical(window.scale_factor());
+                    Some(match last_touch.replace(position) {
+                        Some(_) => WindowEvent::PointerMoved { position },
+                        None => WindowEvent::PointerPressed { position, button },
+                    })
+                }
+                None => last_touch
+                    .take()
+                    .map(|position| WindowEvent::PointerReleased { position, button }),
+            };
 
-        if let Some(event) = event {
-            let is_pointer_release_event = matches!(event, WindowEvent::PointerReleased { .. });
-            window.dispatch_event(event);
+            if let Some(event) = event {
+                let is_pointer_release_event = matches!(event, WindowEvent::PointerReleased { .. });
+                window.dispatch_event(event);
 
-            // removes hover state on widgets
-            if is_pointer_release_event {
-                window.dispatch_event(WindowEvent::PointerExited);
+                // removes hover state on widgets
+                if is_pointer_release_event {
+                    window.dispatch_event(WindowEvent::PointerExited);
+                }
             }
+        }
+        Err(gt911::Error::I2C(e)) => {
+            error!("failed to get touch point: {:?}", e);
+        }
+        Err(_) => {
+            // ignore as these are expected NotReady messages from the touchscreen
         }
     }
 }
