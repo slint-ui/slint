89cefad96d5bc780f91ad1e72dad84d5aa13d347
FloVanGH <florian.blasius@slint.dev>
2025-02-11 11:48:30 +0100

    usecases demo added filter to rust example

--- demos/usecases/rust/src/lib.rs
+++ demos/usecases/rust/src/lib.rs
@@ -41,57 +41,78 @@ mod virtual_keyboard {
 }
 
 mod data {
+    use std::rc::Rc;
+
     use super::*;
     use slint::*;
 
     pub fn init(app: &App) {
         let mail_box_adapter = MailBoxViewAdapter::get(app);
 
-        let mails = VecModel::from_slice(&[
+        let message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.".to_string();
 
-         CardListViewItem{
-            title: "Simon Hausmann".into(),
-            note: "1 hour ago".into(),
-            sub_title: "Meeting tomorrow".into(),
-            caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.".into()
-        },
-//       CardListViewItem  { title: "Tobias Hunger".into(), note: "1 day ago".into(), sub_title: "Meeting tomorrow".into(),  caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.".into() },
-//       CardListViewItem  {
-//             title: "Olivier Goffart".into(),
-//             note: "2 hour ago".into(),
-//             sub_title: "Meeting tomorrow".into(),
-//             caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.".into()
-//         },
-//      CardListViewItem   {
-//             title: "Aurindam Jana".into(),
-//             note: "5 hour ago".into(),
-//             sub_title: "Meeting tomorrow".into(),
-//             caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-// .into()
-//         },
-//      CardListViewItem   {
-//             title: "Simon Hausmann".into(),
-//             note: "7 hour ago".into(),
-//             sub_title: "Meeting tomorrow".into(),
-//             caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-// .into()
-//         },
-//       CardListViewItem  { title: "Tobias Hunger".into(), note: "1 day ago".into(), sub_title: "Meeting tomorrow".into(),  caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."into() },
-//       CardListViewItem  {
-//             title: "Olivier Goffart".into(),
-//             note: "8 hour ago".into(),
-//             sub_title: "Meeting tomorrow".into(),
-//             caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-// .into()
-//         },
-//        CardListViewItem {
-//             title: "Aurindam Jana".into(),
-//             note: "9 hour ago".into(),
-//             sub_title: "Meeting tomorrow".into(),
-//             caption: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-// .into()
-//         }
+        let mails = VecModel::from_slice(&[
+            CardListViewItem {
+                title: "Simon Hausmann".into(),
+                note: "1 hour ago".into(),
+                sub_title: "Meeting tomorrow".into(),
+                caption: message.clone().into(),
+            },
+            CardListViewItem {
+                title: "Tobias Hunger".into(),
+                note: "1 day ago".into(),
+                sub_title: "Meeting tomorrow".into(),
+                caption: message.clone().into(),
+            },
+            CardListViewItem {
+                title: "Olivier Goffart".into(),
+                note: "1 day".into(),
+                sub_title: "Meeting tomorrow".into(),
+                caption: message.clone().into(),
+            },
+            CardListViewItem {
+                title: "Aurindam Jana".into(),
+                note: "2 hour ago".into(),
+                sub_title: "Meeting tomorrow".into(),
+                caption: message.clone().into(),
+            },
+            CardListViewItem {
+                title: "Simon Hausmann".into(),
+                note: "5 hour ago".into(),
+                sub_title: "Meeting tomorrow".into(),
+                caption: message.clone().into(),
+            },
+            CardListViewItem {
+                title: "Tobias Hunger".into(),
+                note: "7 hours ago".into(),
+                sub_title: "Meeting tomorrow".into(),
+                caption: message.clone().into(),
+            },
+            CardListViewItem {
+                title: "Olivier Goffart".into(),
+                note: "8 hour ago".into(),
+                sub_title: "Meeting tomorrow".into(),
+                caption: message.clone().into(),
+            },
+            CardListViewItem {
+                title: "Aurindam Jana".into(),
+                note: "9 hour ago".into(),
+                sub_title: "Meeting tomorrow".into(),
+                caption: message.into(),
+            },
         ]);
+
+        mail_box_adapter.on_search_text_changed({
+            let app_weak = app.as_weak();
+            let mails = mails.clone();
+
+            move |text| {
+                let mails = mails
+                    .clone()
+                    .filter(move |e| e.title.to_lowercase().contains(text.to_lowercase().as_str()));
+                MailBoxViewAdapter::get(&app_weak.unwrap()).set_mails(Rc::new(mails).into());
+            }
+        });
 
         mail_box_adapter.set_mails(mails.into());
     }
