c415fcc8adc722ecd80b0e1d4c9af59594c23696
Nigel Breslaw <nigel.breslaw@slint.dev>
2025-02-17 13:40:43 +0200

    Move tests into test folder

--- tools/figma-inspector/tests/property-parsing.test.ts
+++ tools/figma-inspector/tests/property-parsing.test.ts
@@ -1,10 +1,10 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
 
-import { getBorderRadius, rgbToHex, indentation } from "./property-parsing";
+import { getBorderRadius, rgbToHex, indentation } from "../backend/utils/property-parsing";
 import { expect, test } from "vitest";
 
-const testJson = require("../../figma_output.json");
+const testJson = require("./figma_output.json");
 
 // Json node ID for various tests.
 const testBorderRadius55px = "163:266";
