4ef27e3ab29bf33c284dd6f415e18f9b11ecffed
Alex Badics <admin@stickman.hu>
2025-02-19 13:38:08 +0100

    compiler: don't inline repeater components
    They don't need to be inlined, and a lot of the times inlining them incur
    a significant overhead (e.g. in case of conditional complex components)

--- internal/compiler/passes/inlining.rs
+++ internal/compiler/passes/inlining.rs
@@ -43,8 +43,9 @@ pub fn inline(doc: &Document, inline_selection: InlineSelection, diag: &mut Buil
                     InlineSelection::InlineOnlyRequiredComponents => {
                         component_requires_inlining(&c)
                             || element_require_inlining(elem)
-                            // We always inline the root in case the element that instantiate this component needs full inlining
-                            || Rc::ptr_eq(elem, &component.root_element)
+                            // We always inline the root in case the element that instantiate this component needs full inlining,
+                            // except when the root is a repeater component, which are never inlined.
+                            || component.parent_element.upgrade().is_none() && Rc::ptr_eq(elem, &component.root_element)
                             // We always inline other roots as a component can't be both a sub component and a root
                             || roots.contains(&ByAddress(c.clone()))
                     }
@@ -629,6 +630,12 @@ fn element_require_inlining(elem: &ElementRc) -> bool {
         // the generators assume that the children list is complete, which sub-components may break
         return true;
     }
+
+    // Popup windows need to be inlined for root.close() to work properly.
+    if super::lower_popups::is_popup_window(elem) {
+        return true;
+    }
+
     for (prop, binding) in &elem.borrow().bindings {
         if prop == "clip" {
             // otherwise the children of the clipped items won't get moved as child of the Clip element
