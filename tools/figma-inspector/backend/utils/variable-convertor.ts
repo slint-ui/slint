9e1d2a160341e0e55174905e3591104e4f1c8e3a
szecket <daniel.szecket@slint.dev>
2025-02-20 14:29:26 -0500

    added a new set of modes

--- tools/figma-inspector/backend/utils/variable-convertor.ts
+++ tools/figma-inspector/backend/utils/variable-convertor.ts
@@ -21,4 +21,19 @@ export async function startVariableConversion() {
     // While this returns everything and I am not sure there is any structure to it. 
     const allLocalVariables = await figma.variables.getLocalVariablesAsync();
     console.log("All local variables IDs:", allLocalVariables);
+
+    // Build the set of modes dynamically.
+    const modeSet = new Set<string>();
+    for (const collection of variableCollections) {
+        const collVars = allLocalVariables.filter(v => v.variableCollectionId === collection.id);
+        console.log("mode name: ", collection.name," for id ", collection.id)
+        for (const variable of collVars) {
+            Object.keys(variable.valuesByMode).forEach(mode => modeSet.add(mode));
+        }
+    }
+
+    const modes = Array.from(modeSet);
+    const filesContent: { [mode: string]: string } = {};
+    console.log("Modes: ", modes);
+
 }
