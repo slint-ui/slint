958fb8ccd92fe904f6143dff3f88571054b124f6
Nigel Breslaw <nigel.breslaw@slint.dev>
2025-02-24 14:48:51 +0200

    Dark theme for code snippet

--- tools/figma-inspector/src/misc/CodeSnippet.tsx
+++ tools/figma-inspector/src/misc/CodeSnippet.tsx
@@ -3,13 +3,13 @@
 
 import { useEffect, useRef, useState, type ReactNode } from "react";
 import parse from "html-react-parser";
-import nord from "@shikijs/themes/nord";
+import darkSlint from "./dark-theme.json";
 
 // The default Shiki bundle is >9MB due to all the default themes and languages.
 // The following setup if for a minimal bundle size of ~1MB.
 import { createHighlighterCore } from "shiki/core";
 import { createOnigurumaEngine } from "shiki/engine/oniguruma";
-import type { LanguageRegistration } from "@shikijs/types";
+import type { LanguageRegistration, ThemeRegistration } from "@shikijs/types";
 import OnigurumaEngine from "shiki/wasm";
 
 import slintLang from "./Slint-tmLanguage.json";
@@ -17,7 +17,7 @@ import slintLang from "./Slint-tmLanguage.json";
 let highlighter: any;
 const initHighlighter = async () => {
     highlighter = await createHighlighterCore({
-        themes: [nord],
+        themes: [darkSlint as ThemeRegistration],
         langs: [slintLang as LanguageRegistration],
         engine: createOnigurumaEngine(OnigurumaEngine),
     });
@@ -35,7 +35,7 @@ export default function CodeSnippet({ code }: { code: string }) {
             await initHighlighter();
             const html = await highlighter.codeToHtml(code, {
                 lang: "slint",
-                theme: "nord",
+                theme: "dark-slint",
             });
 
             if (isMounted) {
