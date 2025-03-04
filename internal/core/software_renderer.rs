6746cf6ccbdba87f24cd00119e05017ac2da7fc3
Simon Hausmann <simon.hausmann@slint.dev>
2025-03-04 14:31:24 +0100

    Fix TargetPixelBuffer functions being called with regions not clipped against the dirty region
    This got lost in the rebase/squash session of https://github.com/slint-ui/slint/pull/7685
    
    (cherry picked from commit 2c0b55646380104ea9d3cc76999669cd28b7dbc6)

--- internal/core/software_renderer.rs
+++ internal/core/software_renderer.rs
@@ -1178,28 +1178,28 @@ impl<B: target_pixel_buffer::TargetPixelBuffer> RenderToBuffer<'_, B> {
     }
 
     fn process_texture_impl(&mut self, geometry: PhysicalRect, texture: SceneTexture<'_>) {
-        if !self.buffer.draw_texture(
-            geometry.origin.x,
-            geometry.origin.y,
-            geometry.size.width,
-            geometry.size.height,
-            target_pixel_buffer::Texture {
-                bytes: texture.data,
-                pixel_format: texture.format,
-                pixel_stride: texture.pixel_stride,
-                width: texture.source_size().width as u16,
-                height: texture.source_size().height as u16,
-                delta_x: texture.extra.dx.0,
-                delta_y: texture.extra.dy.0,
-                source_offset_x: texture.extra.off_x.0,
-                source_offset_y: texture.extra.off_y.0,
-            },
-            texture.extra.colorize.as_argb_encoded(),
-            texture.extra.alpha,
-            texture.extra.rotation,
-            CompositionMode::default(),
-        ) {
-            self.foreach_region(&geometry, |buffer, rect, extra_left_clip, extra_right_clip| {
+        self.foreach_region(&geometry, |buffer, rect, extra_left_clip, extra_right_clip| {
+            if !buffer.draw_texture(
+                geometry.origin.x,
+                geometry.origin.y,
+                geometry.size.width,
+                geometry.size.height,
+                target_pixel_buffer::Texture {
+                    bytes: texture.data,
+                    pixel_format: texture.format,
+                    pixel_stride: texture.pixel_stride,
+                    width: texture.source_size().width as u16,
+                    height: texture.source_size().height as u16,
+                    delta_x: texture.extra.dx.0,
+                    delta_y: texture.extra.dy.0,
+                    source_offset_x: texture.extra.off_x.0,
+                    source_offset_y: texture.extra.off_y.0,
+                },
+                texture.extra.colorize.as_argb_encoded(),
+                texture.extra.alpha,
+                texture.extra.rotation,
+                CompositionMode::default(),
+            ) {
                 let begin = rect.min_x();
                 let end = rect.max_x();
                 for l in rect.y_range() {
@@ -1212,8 +1212,8 @@ impl<B: target_pixel_buffer::TargetPixelBuffer> RenderToBuffer<'_, B> {
                         extra_right_clip,
                     );
                 }
-            });
-        }
+            }
+        });
     }
 
     fn process_rectangle_impl(
@@ -1222,15 +1222,15 @@ impl<B: target_pixel_buffer::TargetPixelBuffer> RenderToBuffer<'_, B> {
         color: PremultipliedRgbaColor,
         composition_mode: CompositionMode,
     ) {
-        if !self.buffer.fill_rectangle(
-            geometry.origin.x,
-            geometry.origin.y,
-            geometry.size.width,
-            geometry.size.height,
-            color,
-            composition_mode,
-        ) {
-            self.foreach_region(&geometry, |buffer, rect, _extra_left_clip, _extra_right_clip| {
+        self.foreach_region(&geometry, |buffer, rect, _extra_left_clip, _extra_right_clip| {
+            if !buffer.fill_rectangle(
+                geometry.origin.x,
+                geometry.origin.y,
+                geometry.size.width,
+                geometry.size.height,
+                color,
+                composition_mode,
+            ) {
                 let begin = rect.min_x();
                 let end = rect.max_x();
 
@@ -1252,8 +1252,8 @@ impl<B: target_pixel_buffer::TargetPixelBuffer> RenderToBuffer<'_, B> {
                         }
                     }
                 }
-            })
-        };
+            }
+        })
     }
 }
 
