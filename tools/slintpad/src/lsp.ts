31241c6bd8552db5bdd28099eeb2eb0f0ea456ae
Simon Hausmann <simon.hausmann@slint.dev>
2025-04-08 10:06:06 +0200

    Remove unnecessary try / catch blocks when using the winit-based wasm-preview
    Commit b2645dff6c60770b9ba53ac4496ade0dbd8c8e66 and 4b37d9a1f110fa9bc7dbd2bfcae83a899074ab4e introduced the use of spawn, which doesn't thrown anymore.

--- tools/slintpad/src/lsp.ts
+++ tools/slintpad/src/lsp.ts
@@ -222,11 +222,7 @@ export class Lsp {
         style: string,
     ): Promise<Previewer> {
         if (this.#preview_connector === null) {
-            try {
-                slint_preview.run_event_loop();
-            } catch (e) {
-                // this is not an error!
-            }
+            slint_preview.run_event_loop();
 
             const params = new URLSearchParams(window.location.search);
             const experimental = params.get("SLINT_EXPERIMENTAL_FEATURES");
