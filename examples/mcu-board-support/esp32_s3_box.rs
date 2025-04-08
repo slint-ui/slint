919c59902d3e4faa0bbd49c2d159b02dfb474fc2
Juraj Michálek <juraj.michalek@espressif.com>
2025-04-08 16:32:41 +0200

    initial migration to esp-hal-1.0.0

--- examples/mcu-board-support/esp32_s3_box.rs
+++ examples/mcu-board-support/esp32_s3_box.rs
@@ -1,42 +1,64 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: MIT
 
+#![no_std]
+#![no_main]
+
+use esp_hal::peripherals::Peripherals;
+use esp_hal::gpio::DriveMode;
+
 use alloc::boxed::Box;
 use alloc::rc::Rc;
 use core::cell::RefCell;
 use core::mem::MaybeUninit;
+use core::time::Duration;
 use embedded_graphics_core::geometry::OriginDimensions;
 use embedded_hal::digital::OutputPin;
 use esp_alloc as _;
 use esp_backtrace as _;
-use esp_hal::delay::Delay;
-pub use esp_hal::entry;
-use esp_hal::gpio::{Input, Level, Output, Pull};
-use esp_hal::i2c::master as i2c;
-use esp_hal::prelude::*;
-use esp_hal::rtc_cntl::Rtc;
-use esp_hal::spi::master as spi;
-use esp_hal::timer::{systimer::SystemTimer, timg::TimerGroup};
-use mipidsi::{options::Orientation, Display};
+use esp_hal::{
+    delay::Delay,
+    gpio::{Input, Level, Output, OutputConfig, Pull},
+    i2c::master::I2c,
+    // init,
+    spi::master::{Spi, Config as SpiConfig},
+    spi::Mode as SpiMode,
+    timer::systimer::SystemTimer,
+    time::Rate,
+};
+use mipidsi::{options::{Orientation, Rotation, ColorOrder}, Display};
 use slint::platform::WindowEvent;
+use embedded_hal_bus::spi::ExclusiveDevice;
+use esp_hal::gpio::InputConfig;
+use esp_println::println;
 
+/// Initializes the heap and sets the Slint platform.
 pub fn init() {
-    const HEAP_SIZE: usize = 250 * 1024;
-    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
-    unsafe {
-        esp_alloc::HEAP.add_region(esp_alloc::HeapRegion::new(
-            HEAP.as_mut_ptr() as *mut u8,
-            HEAP_SIZE,
-            esp_alloc::MemoryCapability::Internal.into(),
-        ));
-    }
-    slint::platform::set_platform(Box::new(EspBackend::default()))
+    println!("Initializing esp32");
+
+    // Initialize peripherals first.
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    println!("Peripherals initialized");
+
+    // Initialize the PSRAM allocator.
+    esp_alloc::psram_allocator!(peripherals.PSRAM, esp_hal::psram);
+
+    // Create an EspBackend that now owns the peripherals.
+    slint::platform::set_platform(Box::new(EspBackend {
+        peripherals: RefCell::new(Some(peripherals)),
+        window: RefCell::new(None),
+        start_ticks: 0,
+    }))
         .expect("backend already initialized");
 }
+
+
 
 #[derive(Default)]
 struct EspBackend {
     window: RefCell<Option<Rc<slint::platform::software_renderer::MinimalSoftwareWindow>>>,
+    peripherals: RefCell<Option<Peripherals>>,
+    start_ticks: u32,
 }
 
 impl slint::platform::Platform for EspBackend {
@@ -50,119 +72,144 @@ impl slint::platform::Platform for EspBackend {
         Ok(window)
     }
 
-    fn duration_since_start(&self) -> core::time::Duration {
-        core::time::Duration::from_millis(
-            SystemTimer::now() / (SystemTimer::ticks_per_second() / 1000),
-        )
+
+
+    fn duration_since_start(&self) -> Duration {
+        // let systimer = self.systimer.as_ref().expect("SystemTimer not initialized");
+        let elapsed_ns = 10000;
+        Duration::from_nanos(elapsed_ns)
     }
 
     fn run_event_loop(&self) -> Result<(), slint::PlatformError> {
-        let peripherals = esp_hal::init(esp_hal::Config::default());
-
-        let mut rtc = Rtc::new(peripherals.LPWR);
-        rtc.rwdt.disable();
-        let mut timer_group0 = TimerGroup::new(peripherals.TIMG0);
-        timer_group0.wdt.disable();
-        let mut timer_group1 = TimerGroup::new(peripherals.TIMG1);
-        timer_group1.wdt.disable();
-
+        // Initialize the peripherals using the new esp-hal 1.0.0 API.
+        let mut peripherals = self
+            .peripherals
+            .borrow_mut()
+            .take()
+            .expect("Peripherals already taken");
         let mut delay = Delay::new();
-
-        let i2c = i2c::I2c::new(
+        // I2C initialization.
+        let i2c = I2c::new(
             peripherals.I2C0,
-            i2c::Config { frequency: 400u32.kHz(), ..i2c::Config::default() },
+            esp_hal::i2c::master::Config::default().with_frequency(Rate::from_khz(400)),
         )
-        .with_sda(peripherals.GPIO8)
-        .with_scl(peripherals.GPIO18);
+            .unwrap()
+            .with_sda(peripherals.GPIO8)
+            .with_scl(peripherals.GPIO18);
 
-        let mut touch = tt21100::TT21100::new(i2c, Input::new(peripherals.GPIO3, Pull::Up))
-            .expect("Initialize the touch device");
+        // Initialize the touch driver
+        // let mut touch = tt21100::TT21100::new(i2c, Input::new(peripherals.GPIO3, InputConfig::default().with_pull(Pull::Up)))
+        //     .expect("Initialize the touch device");
 
-        let spi = spi::Spi::new_with_config(
+        // SPI initialization: using Blocking mode. Update the configuration to use Rate.
+        let spi = Spi::<esp_hal::Blocking>::new(
             peripherals.SPI2,
-            spi::Config { frequency: 60u32.MHz(), ..spi::Config::default() },
+            SpiConfig::default()
+                .with_frequency(Rate::from_mhz(60))
+                .with_mode(SpiMode::_0),
         )
-        .with_sck(peripherals.GPIO7)
-        .with_mosi(peripherals.GPIO6);
+            .unwrap()
+            .with_sck(peripherals.GPIO7)
+            .with_mosi(peripherals.GPIO6);
 
-        let dc = Output::new(peripherals.GPIO4, Level::Low);
-        let cs = Output::new(peripherals.GPIO5, Level::Low);
-        let rst = Output::new(peripherals.GPIO48, Level::Low);
+        // Configure display control and chip select pins with an output config.
+        let dc = Output::new(peripherals.GPIO4, Level::Low, OutputConfig::default());
+        let cs = Output::new(peripherals.GPIO5, Level::Low, OutputConfig::default());
+        // Reset pin for display: GPIO48 (OpenDrain required).
+        let rst = Output::new(
+            peripherals.GPIO48,
+            Level::High,
+            OutputConfig::default().with_drive_mode(DriveMode::OpenDrain),
+        );
 
-        let spi = embedded_hal_bus::spi::ExclusiveDevice::new_no_delay(spi, cs).unwrap();
+        // Wrap the SPI bus in an ExclusiveDevice.
+        let spi_delay = Delay::new();
+        let spi_device = ExclusiveDevice::new(spi, cs, spi_delay).unwrap();
+
+        // Create a temporary buffer for the SPI interface.
         let mut buffer = [0u8; 512];
-        let di = mipidsi::interface::SpiInterface::new(spi, dc, &mut buffer);
-        let display = mipidsi::Builder::new(mipidsi::models::ILI9342CRgb565, di)
+        let di = mipidsi::interface::SpiInterface::new(spi_device, dc, &mut buffer);
+
+        // Initialize the display with updated builder settings.
+        let display = mipidsi::Builder::new(mipidsi::models::ILI9486Rgb565, di)
             .reset_pin(rst)
-            .orientation(Orientation::new().rotate(mipidsi::options::Rotation::Deg180))
-            .color_order(mipidsi::options::ColorOrder::Bgr)
+            .orientation(Orientation::new().rotate(Rotation::Deg180))
+            .color_order(ColorOrder::Bgr)
             .init(&mut delay)
             .unwrap();
 
-        let mut backlight = Output::new(peripherals.GPIO45, Level::High);
+        // Set up the backlight pin. Using an output config as required by the new API.
+        let mut backlight = Output::new(peripherals.GPIO47, Level::Low, OutputConfig::default());
         backlight.set_high();
 
+        // Use the display size to size the Slint window.
         let size = display.size();
         let size = slint::PhysicalSize::new(size.width, size.height);
 
-        self.window.borrow().as_ref().unwrap().set_size(size);
+        self.window
+            .borrow()
+            .as_ref()
+            .unwrap()
+            .set_size(size);
 
+        // Prepare a draw buffer that is used by the Slint software renderer.
         let mut buffer_provider = DrawBuffer {
             display,
             buffer: &mut [slint::platform::software_renderer::Rgb565Pixel(0); 320],
         };
 
-        let mut last_touch = None;
+        // Variable to track touch state between iterations.
+        // let mut last_touch = None;
 
+        // Main event loop.
         loop {
             slint::platform::update_timers_and_animations();
 
             if let Some(window) = self.window.borrow().clone() {
                 let mut event_count = 0;
-                // The hardware keeps a queue of events. We should ideally process all event from the queue before rendering
-                // or we will get outdated event in the next frames. But move events are constantly added to the queue
-                // so we would block the whole interface, so add an arbitrary threshold
-                while event_count < 15 && touch.data_available().unwrap() {
-                    event_count += 1;
-                    match touch.event() {
-                        // Ignore error because we sometimes get an error at the beginning
-                        Err(_) => (),
-                        Ok(tt21100::Event::Button(..)) => (),
-                        Ok(tt21100::Event::Touch { report: _, touches }) => {
-                            let button = slint::platform::PointerEventButton::Left;
-                            if let Some(event) = touches
-                                .0
-                                .map(|record| {
-                                    let position = slint::PhysicalPosition::new(
-                                        ((319. - record.x as f32) * size.width as f32 / 319.) as _,
-                                        (record.y as f32 * size.height as f32 / 239.) as _,
-                                    )
-                                    .to_logical(window.scale_factor());
-                                    match last_touch.replace(position) {
-                                        Some(_) => WindowEvent::PointerMoved { position },
-                                        None => WindowEvent::PointerPressed { position, button },
-                                    }
-                                })
-                                .or_else(|| {
-                                    last_touch.take().map(|position| WindowEvent::PointerReleased {
-                                        position,
-                                        button,
-                                    })
-                                })
-                            {
-                                let is_pointer_release_event =
-                                    matches!(event, WindowEvent::PointerReleased { .. });
-
-                                window.try_dispatch_event(event)?;
-
-                                // removes hover state on widgets
-                                if is_pointer_release_event {
-                                    window.try_dispatch_event(WindowEvent::PointerExited)?;
-                                }
-                            }
-                        }
-                    }
-                }
+                // Process up to 15 events per frame so that we do not block rendering.
+                // while event_count < 15 && touch.data_available().unwrap() {
+                //     event_count += 1;
+                //     match touch.event() {
+                //         // Discard errors (e.g. transient initialization issues).
+                //         Err(_) => (),
+                //         Ok(tt21100::Event::Button(..)) => (),
+                //         Ok(tt21100::Event::Touch { report: _, touches }) => {
+                //             let button = slint::platform::PointerEventButton::Left;
+                //             if let Some(event) = touches
+                //                 .0
+                //                 .map(|record| {
+                //                     let position = slint::PhysicalPosition::new(
+                //                         // Map coordinates from display to logical coordinates.
+                //                         ((319. - record.x as f32) * size.width as f32 / 319.) as _,
+                //                         (record.y as f32 * size.height as f32 / 239.) as _,
+                //                     )
+                //                         .to_logical(window.scale_factor());
+                //                     match last_touch.replace(position) {
+                //                         Some(_) => WindowEvent::PointerMoved { position },
+                //                         None => WindowEvent::PointerPressed { position, button },
+                //                     }
+                //                 })
+                //                 .or_else(|| {
+                //                     last_touch.take().map(|position| WindowEvent::PointerReleased {
+                //                         position,
+                //                         button,
+                //                     })
+                //                 })
+                //             {
+                //                 let is_pointer_release_event =
+                //                     matches!(event, WindowEvent::PointerReleased { .. });
+                //
+                //                 window.try_dispatch_event(event)?;
+                //
+                //                 // Remove hover state on widgets after a pointer release.
+                //                 if is_pointer_release_event {
+                //                     window.try_dispatch_event(WindowEvent::PointerExited)?;
+                //                 }
+                //             }
+                //         }
+                //     }
+                // }
 
                 window.draw_if_needed(|renderer| {
                     renderer.render_by_line(&mut buffer_provider);
@@ -171,7 +218,7 @@ impl slint::platform::Platform for EspBackend {
                     continue;
                 }
             }
-            // TODO
+            // TODO: Add sleep or power saving functionality if necessary.
         }
     }
 
@@ -180,16 +227,17 @@ impl slint::platform::Platform for EspBackend {
     }
 }
 
+/// Provides a draw buffer for the MinimalSoftwareWindow renderer.
 struct DrawBuffer<'a, Display> {
     display: Display,
     buffer: &'a mut [slint::platform::software_renderer::Rgb565Pixel],
 }
 
 impl<
-        DI: mipidsi::interface::Interface<Word = u8>,
-        RST: OutputPin<Error = core::convert::Infallible>,
-    > slint::platform::software_renderer::LineBufferProvider
-    for &mut DrawBuffer<'_, Display<DI, mipidsi::models::ILI9342CRgb565, RST>>
+    DI: mipidsi::interface::Interface<Word = u8>,
+    RST: OutputPin<Error = core::convert::Infallible>,
+> slint::platform::software_renderer::LineBufferProvider
+for &mut DrawBuffer<'_, mipidsi::Display<DI, mipidsi::models::ILI9486Rgb565, RST>>
 {
     type TargetPixel = slint::platform::software_renderer::Rgb565Pixel;
 
@@ -200,19 +248,16 @@ impl<
         render_fn: impl FnOnce(&mut [slint::platform::software_renderer::Rgb565Pixel]),
     ) {
         let buffer = &mut self.buffer[range.clone()];
-
         render_fn(buffer);
 
-        // We send empty data just to get the device in the right window
+        // Update the display with the rendered line.
         self.display
             .set_pixels(
                 range.start as u16,
-                line as _,
+                line as u16,
                 range.end as u16,
                 line as u16,
-                buffer
-                    .iter()
-                    .map(|x| embedded_graphics_core::pixelcolor::raw::RawU16::new(x.0).into()),
+                buffer.iter().map(|x| embedded_graphics_core::pixelcolor::raw::RawU16::new(x.0).into()),
             )
             .unwrap();
     }
