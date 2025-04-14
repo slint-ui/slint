c91ef1d7e6ae4cce58f9cd6b0b4fc5735c9aeadd
Simon Hausmann <simon.hausmann@slint.dev>
2025-04-14 17:28:11 +0200

    Prospective build fix for non-esp mcu-board-support crate builds

--- demos/energy-monitor/src/main.rs
+++ demos/energy-monitor/src/main.rs
@@ -4,6 +4,7 @@
 #![cfg_attr(all(feature = "mcu-board-support", not(feature = "simulator")), no_main)]
 
 #[cfg(feature = "mcu-board-support")]
+#[allow(unused_imports)]
 use mcu_board_support::prelude::*;
 
 #[cfg(not(feature = "mcu-board-support"))]
