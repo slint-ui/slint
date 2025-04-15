d1f2fef430294e937c883b974821e21bae1218f3
Joshua Goins <josh@redstrate.com>
2025-04-15 02:08:11 -0400

    Menu: Add enabled property (#8145)
    By default it's enabled, of course. This property is not only added to
    MenuItem, but Menu as well - since they can be nested. It's also
    possible to select a disabled item, but it's hard to modify that since
    it's logic is written in Slint. You should be prevented from activating
    it with a tap or key press at least.
    
    See #7791

--- internal/common/builtin_structs.rs
+++ internal/common/builtin_structs.rs
@@ -194,8 +194,8 @@ macro_rules! for_each_builtin_structs {
                     /// an opaque id that can be used to identify the menu entry
                     id: SharedString,
                     // keyboard_shortcut: KeySequence,
-                    // /// whether the menu entry is enabled
-                    // enabled: bool,
+                    /// whether the menu entry is enabled
+                    enabled: bool,
                     /// Sub menu
                     has_sub_menu: bool,
                     /// The menu entry is a separator
