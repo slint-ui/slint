2a3f45f8eccd738985ce5c550f6ee855d595f222
Olivier Goffart <olivier.goffart@slint.dev>
2025-04-29 10:15:56 +0200

    Adapt nema-gfx to the new API

--- examples/mcu-board-support/stm32u5g9j_dk2/nema_gfx_renderer.rs
+++ examples/mcu-board-support/stm32u5g9j_dk2/nema_gfx_renderer.rs
@@ -6,6 +6,7 @@ use core::pin::Pin;
 use nema_gfx_rs::*;
 
 use slint::platform::software_renderer;
+use software_renderer::PhysicalRegion;
 
 pub struct NemaGFXEnhancedBuffer<'a> {
     data: &'a mut [software_renderer::Rgb565Pixel],
@@ -102,17 +103,17 @@ impl<'a> i_slint_core::software_renderer::TargetPixelBuffer
 
     fn draw_texture(
         &mut self,
-        x: i16,
-        y: i16,
-        width: i16,
-        height: i16,
-        src_texture: software_renderer::Texture<'_>,
-        colorize: u32,
-        alpha: u8,
-        rotation: software_renderer::RenderingRotation,
-        composition_mode: software_renderer::CompositionMode,
+        texture: &software_renderer::DrawTextureArgs,
+        clip: &PhysicalRegion,
     ) -> bool {
-        let (mut source_blend_factor, texture_format) = match src_texture.pixel_format {
+        if texture.rotation != software_renderer::RenderingRotation::NoRotation
+            || texture.tiling.is_some()
+        {
+            return false;
+        }
+
+        let source = texture.source();
+        let (mut source_blend_factor, texture_format) = match source.pixel_format {
             software_renderer::TexturePixelFormat::Rgb => (NEMA_BF_SRCALPHA, NEMA_RGB24),
             software_renderer::TexturePixelFormat::Rgba => (NEMA_BF_SRCALPHA, NEMA_RGBA8888),
             software_renderer::TexturePixelFormat::RgbaPremultiplied => {
@@ -124,23 +125,15 @@ impl<'a> i_slint_core::software_renderer::TargetPixelBuffer
             }
         };
 
-        let dst_blend_factor = match composition_mode {
-            software_renderer::CompositionMode::Source => NEMA_BF_ZERO,
-            software_renderer::CompositionMode::SourceOver => NEMA_BF_INVSRCALPHA,
-            _ => return false,
-        };
-
-        //defmt::info!("BLIT");
-
         self.ensure_command_list_bound();
 
         unsafe {
             nema_bind_src_tex(
-                src_texture.bytes.as_ptr() as _,
-                src_texture.width as _,
-                src_texture.height as _,
+                source.data.as_ptr() as _,
+                source.width as _,
+                source.height as _,
                 texture_format,
-                (src_texture.pixel_stride * src_texture.pixel_format.bpp() as u16) as i32,
+                source.byte_stride as i32,
                 NEMA_FILTER_PS as _,
             );
 
@@ -155,8 +148,7 @@ impl<'a> i_slint_core::software_renderer::TargetPixelBuffer
             nema_set_clip(0, 0, self.width, self.height);
 
             let mut blop = NEMA_BLOP_NONE;
-            if colorize != 0 {
-                let colorize = slint::Color::from_argb_encoded(colorize);
+            if let Some(colorize) = texture.colorize {
                 if texture_format == NEMA_A8 {
                     // const color modulation doesn't seem to work with A8 textures, so instead, set the
                     // texture color for the missing channels.
@@ -179,32 +171,27 @@ impl<'a> i_slint_core::software_renderer::TargetPixelBuffer
             }
 
             nema_set_blend(
-                nema_blending_mode(source_blend_factor, dst_blend_factor, blop),
+                nema_blending_mode(source_blend_factor, NEMA_BF_INVSRCALPHA, blop),
                 nema_tex_t_NEMA_TEX0,
                 nema_tex_t_NEMA_TEX1,
                 nema_tex_t_NEMA_NOTEX,
             );
 
-            // Try to re-compute the original clip from the source_offset
-            let dst_off_x =
-                ((src_texture.source_offset_x as i32) << 4) / src_texture.delta_x as i32;
-            let dst_off_y =
-                ((src_texture.source_offset_y as i32) << 4) / src_texture.delta_y as i32;
-            nema_set_clip(x as _, y as _, width as _, height as _);
-
-            nema_blit_subrect_fit(
-                x as i32 - dst_off_x,
-                y as i32 - dst_off_y,
-                ((src_texture.width as i32) << 8) / src_texture.delta_x as i32,
-                ((src_texture.height as i32) << 8) / src_texture.delta_y as i32,
-                0,
-                0,
-                src_texture.width as _,
-                src_texture.height as _,
-            );
+            for (origin, size) in clip.iter() {
+                nema_set_clip(origin.x as _, origin.y as _, size.width as _, size.height as _);
+                nema_blit_subrect_fit(
+                    texture.dst_x as i32,
+                    texture.dst_y as i32,
+                    texture.dst_width as i32,
+                    texture.dst_height as i32,
+                    0,
+                    0,
+                    source.width as _,
+                    source.height as _,
+                );
+                self.ops_count += 1;
+            }
         }
-
-        self.ops_count += 1;
 
         true
     }
