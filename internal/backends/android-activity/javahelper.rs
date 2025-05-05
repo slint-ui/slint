7cf5fc2bdeb8e0d84f831842fed9d29510c4f8ab
wuwbobo2021 <wuwbobo@outlook.com>
2025-05-05 17:47:54 +0800

    Android: use `android-build` dev dependency; small fixes (#8334)

--- internal/backends/android-activity/javahelper.rs
+++ internal/backends/android-activity/javahelper.rs
@@ -49,69 +49,33 @@ fn load_java_helper(app: &AndroidApp) -> Result<jni::objects::GlobalRef, jni::er
             &[JValue::Object(&dex_buffer), JValue::Object(&parent_class_loader)],
         )?
     } else {
-        // writes the dex data into the application internal storage
-        let dex_data_len = dex_data.len() as i32;
-        let dex_byte_array = env.byte_array_from_slice(dex_data).unwrap();
+        let code_cache_path = {
+            let dir = env
+                .call_method(&native_activity, "getCodeCacheDir", "()Ljava/io/File;", &[])?
+                .l()?;
+            let path =
+                env.call_method(&dir, "getAbsolutePath", "()Ljava/lang/String;", &[])?.l()?;
+            jni_get_string(&path, &mut env)
+                .map(|s| s.to_string_lossy().into_owned())
+                .map(std::path::PathBuf::from)?
+        };
+        let dex_name = env!("CARGO_CRATE_NAME").to_string() + ".dex";
+        let dex_file_path = code_cache_path.join(dex_name);
+        std::fs::write(&dex_file_path, dex_data).unwrap(); // Note: this panics on failure
+        let dex_file_path = env.new_string(&dex_file_path.as_os_str().to_string_lossy())?;
 
-        let dex_dir = env.new_string("dex")?;
-        let dex_dir_path = env
-            .call_method(
-                &native_activity,
-                "getDir",
-                "(Ljava/lang/String;I)Ljava/io/File;",
-                &[JValue::Object(&dex_dir), JValue::from(0 as jint)],
-            )?
-            .l()?;
-        let dex_name = env.new_string(env!("CARGO_CRATE_NAME").to_string() + ".dex")?;
-        let dex_path = env.new_object(
-            "java/io/File",
-            "(Ljava/io/File;Ljava/lang/String;)V",
-            &[JValue::Object(&dex_dir_path), JValue::Object(&dex_name)],
-        )?;
-        let dex_path =
-            env.call_method(dex_path, "getAbsolutePath", "()Ljava/lang/String;", &[])?.l()?;
+        let oats_dir_path = code_cache_path.join("oats");
+        let _ = std::fs::create_dir(&oats_dir_path);
+        let oats_dir_path = env.new_string(&oats_dir_path.as_os_str().to_string_lossy())?;
 
-        // prepares the folder for optimized dex generated while creating `DexClassLoader`
-        let out_dex_dir = env.new_string("outdex")?;
-        let out_dex_dir_path = env
-            .call_method(
-                &native_activity,
-                "getDir",
-                "(Ljava/lang/String;I)Ljava/io/File;",
-                &[JValue::Object(&out_dex_dir), JValue::from(0 as jint)],
-            )?
-            .l()?;
-        let out_dex_dir_path = env
-            .call_method(&out_dex_dir_path, "getAbsolutePath", "()Ljava/lang/String;", &[])?
-            .l()?;
-
-        // writes the dex data
-        let write_stream = env.new_object(
-            "java/io/FileOutputStream",
-            "(Ljava/lang/String;)V",
-            &[(&dex_path).into()],
-        )?;
-        env.call_method(
-            &write_stream,
-            "write",
-            "([BII)V",
-            &[
-                JValue::Object(&dex_byte_array),
-                JValue::from(0 as jint),
-                JValue::from(dex_data_len as jint),
-            ],
-        )?;
-        env.call_method(&write_stream, "close", "()V", &[])?;
-
-        // loads the dex file
         env.new_object(
             "dalvik/system/DexClassLoader",
             "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/ClassLoader;)V",
             &[
-                JValue::Object(&dex_path),
-                JValue::Object(&out_dex_dir_path),
-                JValue::Object(&JObject::null()),
-                JValue::Object(&parent_class_loader),
+                (&dex_file_path).into(),
+                (&oats_dir_path).into(),
+                (&JObject::null()).into(),
+                (&parent_class_loader).into(),
             ],
         )?
     };
@@ -366,10 +330,22 @@ extern "system" fn Java_SlintAndroidJavaHelper_updateText(
             adaptor.show_cursor_handles.set(false);
             let runtime_window = i_slint_core::window::WindowInner::from_pub(&adaptor.window);
             let event = if preedit_start != preedit_end {
-                let adjust = |pos| if pos <= preedit_start { pos } else if pos >= preedit_end { pos - preedit_end + preedit_start } else { preedit_start } as i32;
+                let adjust = |pos| {
+                    if pos <= preedit_start {
+                        pos
+                    } else if pos >= preedit_end {
+                        preedit_start + (pos - preedit_end)
+                    } else {
+                        preedit_start
+                    }
+                } as i32;
                 i_slint_core::input::KeyEvent {
                     event_type: i_slint_core::input::KeyEventType::UpdateComposition,
-                    text: i_slint_core::format!( "{}{}", &text[..preedit_start], &text[preedit_end..]),
+                    text: i_slint_core::format!(
+                        "{}{}",
+                        &text[..preedit_start],
+                        &text[preedit_end..]
+                    ),
                     preedit_text: text[preedit_start..preedit_end].into(),
                     preedit_selection: Some(0..(preedit_end - preedit_start) as i32),
                     replacement_range: Some(i32::MIN..i32::MAX),
@@ -537,6 +513,7 @@ fn jni_get_string<'e, 'a>(
     use jni::errors::{Error::*, JniError};
 
     let string_class = env.find_class("java/lang/String")?;
+    let string_class = env.auto_local(string_class);
     let obj_class = env.get_object_class(obj)?;
     let obj_class = env.auto_local(obj_class);
     if !env.is_assignable_from(string_class, obj_class)? {
