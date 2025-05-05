slint-ui/slint

commit e1a6cbf8b4b4dc5623182a1de7a01dcd71626f26

Add links to the slint-build crate

api/rs/slint/lib.rs
@@ -65,7 +65,9 @@ When your design becomes bigger in terms of markup code, you may want move it to
 /*!Use a [build script](https://doc.rust-lang.org/cargo/reference/build-scripts.html) to compile
 your main `.slint` file:
 
-In your Cargo.toml add a `build` assignment and use the `slint-build` crate in `build-dependencies`:
+*/
+#![doc = i_slint_core_macros::slint_doc_str!("In your Cargo.toml add a `build` assignment and use the [`slint-build`](slint:rust:slint_build/) crate in `build-dependencies`:")]
+/*
 
 ```toml
 [package]
@@ -352,6 +354,7 @@ pub fn spawn_local<F: core::future::Future + 'static>(
 /// available for you to instantiate.
 ///
 /// Check the documentation of the `slint-build` crate for more information.
+#[doc = i_slint_core_macros::slint_doc_str!("Check the documentation of the [`slint-build`](slint:rust:slint_build/) crate for more information.")]
 #[macro_export]
 macro_rules! include_modules {
     () => {

commit 6e316a99293cd8576d883a455ddda3edd77f7609

docs: Fix missing paragraphs in the slint crate docs
Regression in e1a6cbf8b4b4dc5623182a1de7a01dcd71626f26

api/rs/slint/lib.rs
@@ -66,7 +66,7 @@ your main `.slint` file:
 
 */
 #![doc = i_slint_core_macros::slint_doc_str!("In your Cargo.toml add a `build` assignment and use the [`slint-build`](slint:rust:slint_build/) crate in `build-dependencies`:")]
-/*
+/*!
 
 ```toml
 [package]
@@ -348,12 +348,12 @@ pub fn spawn_local<F: core::future::Future + 'static>(
         .map_err(|_| EventLoopError::NoEventLoopProvider)?
 }
 
+#[i_slint_core_macros::slint_doc]
 /// Include the code generated with the slint-build crate from the build script. After calling `slint_build::compile`
 /// in your `build.rs` build script, the use of this macro includes the generated Rust code and makes the exported types
 /// available for you to instantiate.
 ///
-/// Check the documentation of the `slint-build` crate for more information.
-#[doc = i_slint_core_macros::slint_doc_str!("Check the documentation of the [`slint-build`](slint:rust:slint_build/) crate for more information.")]
+/// Check the documentation of the [`slint-build`](slint:rust:slint_build) crate for more information.
 #[macro_export]
 macro_rules! include_modules {
     () => {