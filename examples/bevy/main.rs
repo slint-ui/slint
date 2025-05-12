c7682e94fa10f5f9dfbaf3d72cf9eaa70fbf584f
Simon Hausmann <simon.hausmann@slint.dev>
2025-05-12 12:44:40 +0200

    bevy example: Permit selecting different models via a combobox

--- examples/bevy/main.rs
+++ examples/bevy/main.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 use bevy::prelude::*;
-use slint::SharedString;
+use slint::{Model, SharedString};
 
 mod slint_bevy_adapter;
 mod web_asset;
@@ -14,9 +14,12 @@ export component AppWindow inherits Window {
     out property <length> requested-texture-width: i.width;
     out property <length> requested-texture-height: i.height;
 
-    in property <bool> show-loading-screen: true;
+    in property <bool> show-loading-screen: false;
     in property <string> download-url;
     in property <percent> download-progress;
+
+    in property <[string]> available-models;
+    callback load-model(index: int);
 
     title: @tr("Slint & Bevy");
     preferred-width: 500px;
@@ -27,10 +30,21 @@ export component AppWindow inherits Window {
         Rectangle {
             background: Palette.alternate-background;
 
-            HorizontalBox {
+            VerticalBox {
                 Text {
                     text: "This text is rendered using Slint. The animation below is rendered using Bevy code.";
                     wrap: word-wrap;
+                }
+
+                HorizontalBox {
+                    Text {
+                        text: "Select Model:";
+                        vertical-alignment: center;
+                    }
+                    ComboBox {
+                        model: root.available-models;
+                        selected(current-value) => { root.load-model(self.current-index) }
+                    }
                 }
             }
         }
@@ -66,6 +80,8 @@ export component AppWindow inherits Window {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (model_selector_sender, model_selector_receiver) = smol::channel::bounded::<GLTFModel>(1);
+
     let (download_progress_sender, download_progress_receiver) =
         smol::channel::bounded::<(SharedString, f32)>(5);
 
@@ -75,7 +91,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 app.add_plugins(web_asset::WebAssetReaderPlugin(download_progress_sender));
             },
             |mut app| {
-                app.add_systems(Startup, setup).add_systems(Update, animate_camera).run();
+                app.insert_resource(CameraPos(Vec3::new(3., 4.0, 4.0)))
+                    //                    .insert_resource(ModelBasePath("".into()))
+                    .insert_resource(ModelBasePath(
+                        "https://github.com/KhronosGroup/glTF-Sample-Assets/raw/refs/heads/main/"
+                            .into(),
+                    ))
+                    .add_systems(Startup, setup)
+                    .add_systems(Update, reload_model_from_channel(model_selector_receiver))
+                    .add_systems(Update, animate_camera)
+                    .run();
             },
         ))?;
 
@@ -131,39 +156,116 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let Some(app) = app_weak.upgrade() else { return };
             app.set_download_url(url);
             app.set_download_progress(progress * 100.);
-            if progress >= 1.0 {
-                app.set_show_loading_screen(false);
-            }
+            app.set_show_loading_screen(progress < 1.0);
         }
     })
     .unwrap();
+
+    let models = slint::VecModel::from_slice(&[
+        GLTFModel {
+            name: "Damaged Helmet".into(),
+            path: "Models/DamagedHelmet/glTF-Binary/DamagedHelmet.glb".into(),
+            center: Vec3::new(3.0, 4.0, 4.0),
+        },
+        GLTFModel {
+            name: "Fish".into(),
+            path: "Models/BarramundiFish/glTF-Binary/BarramundiFish.glb".into(),
+            center: Vec3::new(3.0, 2.0, 1.0),
+        },
+        GLTFModel {
+            name: "Box".into(),
+            path: "Models/Box/glTF-Binary/Box.glb".into(),
+            center: Vec3::new(3.0, 4.0, 4.0),
+        },
+    ]);
+
+    app_window
+        .set_available_models(slint::ModelRc::new(models.clone().map(|model| model.name.clone())));
+
+    model_selector_sender.send_blocking(models.row_data(0).unwrap()).unwrap();
+
+    app_window.on_load_model(move |index| {
+        let model = models.row_data(index as usize).unwrap();
+        let model_selector_sender = model_selector_sender.clone();
+        slint::spawn_local(async move {
+            model_selector_sender.send(model).await.ok();
+        })
+        .unwrap();
+    });
 
     app_window.run()?;
 
     Ok(())
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+#[derive(Clone)]
+struct GLTFModel {
+    name: SharedString,
+    path: SharedString,
+    center: Vec3,
+}
+
+#[derive(Resource)]
+struct CameraPos(Vec3);
+
+#[derive(Resource)]
+struct ModelBasePath(String);
+
+fn setup(mut commands: Commands, camera: Res<CameraPos>) {
     commands.spawn(DirectionalLight { illuminance: 100_000.0, ..default() });
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(0.7, 0.0, 4.0).looking_at(Vec3::new(0.0, -0.5, 0.0), Vec3::Y),
+        Transform::from_translation(camera.0).looking_at(Vec3::new(0.0, -0.5, 0.0), Vec3::Y),
         PointLight { color: Color::linear_rgb(0.5, 0., 0.), ..default() },
     ));
 
+    /*
     commands.spawn(SceneRoot(
         //asset_server.load(GltfAssetLabel::Scene(0).from_asset("DamagedHelmet.glb")),
         asset_server.load(
             GltfAssetLabel::Scene(0)
-                .from_asset("https://github.com/KhronosGroup/glTF-Sample-Assets/raw/refs/heads/main/Models/DamagedHelmet/glTF-Binary/DamagedHelmet.glb"),
+                .from_asset("Models/DamagedHelmet/glTF-Binary/DamagedHelmet.glb"), //  GltfAssetLabel::Scene(0)
+                                                                                   //      .from_asset("https://github.com/KhronosGroup/glTF-Sample-Assets/raw/refs/heads/main/Models/DamagedHelmet/glTF-Binary/DamagedHelmet.glb"),
         ),
     ));
+    */
 }
 
-fn animate_camera(mut cameras: Query<&mut Transform, With<Camera3d>>, time: Res<Time>) {
+fn reload_model_from_channel(
+    receiver: smol::channel::Receiver<GLTFModel>,
+) -> impl FnMut(
+    Commands,
+    Res<AssetServer>,
+    Query<Entity, With<SceneRoot>>,
+    ResMut<CameraPos>,
+    Res<ModelBasePath>,
+) {
+    move |mut commands, asset_server, loaded_bundles, mut camera, base_path| {
+        let Ok(new_model) = receiver.try_recv() else {
+            return;
+        };
+        for loaded_bundle in loaded_bundles.iter() {
+            commands.entity(loaded_bundle).despawn();
+        }
+        commands.spawn(SceneRoot(
+            //asset_server.load(GltfAssetLabel::Scene(0).from_asset("DamagedHelmet.glb")),
+            asset_server.load(
+                GltfAssetLabel::Scene(0).from_asset(format!("{}{}", base_path.0, new_model.path)),
+            ),
+        ));
+        camera.0 = new_model.center;
+    }
+}
+
+fn animate_camera(
+    mut cameras: Query<&mut Transform, With<Camera3d>>,
+    time: Res<Time>,
+    camera: Res<CameraPos>,
+) {
     let now = time.elapsed_secs();
     for mut transform in cameras.iter_mut() {
-        transform.translation = vec3(ops::cos(now), 0.0, ops::sin(now)) * vec3(3.0, 4.0, 4.0);
-        transform.look_at(Vec3::new(0.0, -0.5, 0.0), Vec3::Y);
+        // transform.translation = vec3(ops::cos(now), 0.0, ops::sin(now)) * vec3(3.0, 4.0, 4.0);
+        transform.translation = vec3(ops::cos(now), 0.0, ops::sin(now)) * camera.0;
+        transform.look_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y);
     }
 }
