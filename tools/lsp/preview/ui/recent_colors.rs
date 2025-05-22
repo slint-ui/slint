c25d15c3d2f454a22b35a11382e9fb3d8ac26af6
Tobias Hunger <tobias.hunger@slint.dev>
2025-05-22 11:19:12 +0000

    live-preview: Remember recent colors
    ... for the duration of the session. How can we save those
    colors?
    
    Recent colors change a lot, we probably do not want that stored
    inside the git repository hosting the slint files... and this is
    probably "personal data" and nor "project data" in the first place.

---
+++ tools/lsp/preview/ui/recent_colors.rs
@@ -0,0 +1,45 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
+
+use std::rc::Rc;
+
+use crate::preview::ui;
+
+use slint::{ComponentHandle, Model};
+
+const MAX_RECENT_COLORS: usize = 9;
+
+pub fn setup(ui: &ui::PreviewUi) {
+    let api = ui.global::<ui::Api>();
+
+    api.set_recent_colors(Rc::new(slint::VecModel::default()).into());
+
+    let weak_ui = ui.as_weak();
+
+    api.on_add_recent_color(move |color| {
+        if let Some(ui) = weak_ui.upgrade() {
+            add_recent_color(ui, color);
+        }
+    });
+}
+
+pub fn add_recent_color(ui: ui::PreviewUi, value: slint::Color) {
+    eprintln!("RUST SIDE: add_recent_color: {value:?}");
+
+    let api = ui.global::<ui::Api>();
+
+    let model = api.get_recent_colors();
+    let Some(model) = model.as_any().downcast_ref::<slint::VecModel<slint::Color>>() else {
+        return;
+    };
+
+    if let Some(position) = model.iter().position(|c| c == value) {
+        model.remove(position);
+    } else {
+        while model.row_count() >= MAX_RECENT_COLORS {
+            model.remove(model.row_count() - 1);
+        }
+    }
+
+    model.insert(0, value);
+}
