slint-ui/slint

commit f09fb68632332ca5fb3331e9cc891beef35162a8

Femtovg text renderer: avoid leaving space for the `\n` when considering alignment
Fixes #7970

internal/renderers/femtovg/fonts.rs
@@ -686,7 +686,7 @@ pub(crate) fn layout_text_lines(
                 break;
             }
             let index = start + index;
-            let line = &string[start..index];
+            let line = string[start..index].trim_end_matches('\n');
             let text_metrics = text_context.measure_text(0., 0., line, paint).unwrap();
             process_line(line, y, start, &text_metrics);
             y += font_height;
@@ -695,7 +695,7 @@ pub(crate) fn layout_text_lines(
             let index = if single_line {
                 string.len()
             } else {
-                string[start..].find('\n').map_or(string.len(), |i| start + i + 1)
+                string[start..].find('\n').map_or(string.len(), |i| start + i)
             };
             let line = &string[start..index];
             let text_metrics = text_context.measure_text(0., 0., line, paint).unwrap();
@@ -722,7 +722,7 @@ pub(crate) fn layout_text_lines(
                             process_line(txt, y, start, &text_metrics);
                         }
                         y += font_height;
-                        start = index;
+                        start = index + 1;
                         continue 'lines;
                     }
                 }
@@ -730,13 +730,13 @@ pub(crate) fn layout_text_lines(
                     let elided = format!("{}…", line.strip_suffix('\n').unwrap_or(line));
                     process_line(&elided, y, start, &text_metrics);
                     y += font_height;
-                    start = index;
+                    start = index + 1;
                     continue 'lines;
                 }
             }
             process_line(line, y, start, &text_metrics);
             y += font_height;
-            start = index;
+            start = index + 1;
         }
     }
 

commit 9add1adc8429102702ba14469e5b5d172c637473

Femtovg: Fix putting the cursor in an empty line
Regression from f09fb68632332ca5fb3331e9cc891beef35162a8 since now the
line no longer contain the `\n` char

Reported in https://github.com/slint-ui/slint/issues/8551#issuecomment-2910456439

internal/renderers/femtovg/fonts.rs
@@ -640,13 +640,8 @@ pub(crate) fn layout_text_lines(
             layout_line(text_span, line_pos, start, line_metrics);
 
             if let Some(cursor_byte_offset) = cursor_byte_offset {
-                let text_span_range = start..(start + text_span.len());
-
-                if text_span_range.contains(&cursor_byte_offset)
-                    || (cursor_byte_offset == text_span_range.end
-                        && cursor_byte_offset == string.len()
-                        && !string.ends_with('\n'))
-                {
+                let text_span_range = start..=(start + text_span.len());
+                if text_span_range.contains(&cursor_byte_offset) {
                     let cursor_x = PhysicalLength::new(
                         line_metrics
                             .glyphs