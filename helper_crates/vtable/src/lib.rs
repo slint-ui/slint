43ecb0ffb9a03488ee5d5ee3d8840e7c9da6bcd5
Simon Hausmann <simon.hausmann@slint.dev>
2025-06-06 11:59:43 +0200

    CI: Fix  lifetime annotation warnings in vtable/vrc crates
    Amends de81fa1dba512e58e43c061383a31daced6e635e

--- helper_crates/vtable/src/lib.rs
+++ helper_crates/vtable/src/lib.rs
@@ -526,7 +526,7 @@ impl<Base, T: ?Sized + VTableMeta> VOffset<Base, T, AllowPin> {
     /// Apply this offset to a reference to the base to obtain a `Pin<VRef<'a, T>>` with the same
     /// lifetime as the base lifetime
     #[inline]
-    pub fn apply_pin(self, base: Pin<&Base>) -> Pin<VRef<T>> {
+    pub fn apply_pin(self, base: Pin<&Base>) -> Pin<VRef<'_, T>> {
         let ptr = base.get_ref() as *const Base as *mut u8;
         unsafe {
             Pin::new_unchecked(VRef::from_raw(
