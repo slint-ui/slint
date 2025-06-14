0a10c99e20ca54561b6ab8f9429c97f9ce710e6a
Nigel Breslaw <nigel.breslaw@slint.dev>
2025-06-14 19:50:22 +0200

    test-data now creates ALL data upfront
    This is a new approach that first creates the main data. It then scans this data for variable references that point to deleted variables and/or variable collections. Figma then lets you get those collections and variables via another api and we then insert them back into the data.
    
    This means the converter can be simplified in a later refactor as there was complexity in the logic to handle this problems after it was given the data. Now the data is complete the convertor can be simpler.

---
+++ tools/figma-inspector/shared/custom-figma-types.d.ts
@@ -0,0 +1,26 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: MIT
+
+/// <reference types="@figma/plugin-typings" />
+
+// branded types
+export type CollectionId = string & { readonly brand: unique symbol };
+export type VariableId = string & { readonly brand: unique symbol };
+
+// Create our own types that extend the Figma ones
+export interface VariableCollectionSU extends Omit<VariableCollection, "id"> {
+    id: CollectionId;
+    variables: Map<VariableId, VariableSU>;
+}
+
+export interface VariableSU
+    extends Omit<Variable, "id" | "variableCollectionId"> {
+    id: VariableId;
+    variableCollectionId: CollectionId;
+}
+
+export interface VariableCollectionForJSON
+    extends Omit<VariableCollection, "id"> {
+    id: CollectionId;
+    variables: VariableSU[];
+}
