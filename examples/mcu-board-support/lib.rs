e1f799210725c1b91933560e2d4c24aca2101b5c
Tasuku Suzuki <tasuku.suzuki@slint.dev>
2025-06-27 03:09:18 +0900

    Reorganize MCU board support examples into modules
    Move MCU board support example files into their own directories

--- examples/mcu-board-support/lib.rs
+++ examples/mcu-board-support/lib.rs
@@ -10,26 +10,31 @@
 extern crate alloc;
 
 #[cfg(feature = "pico-st7789")]
+#[path = "pico_st7789/pico_st7789.rs"]
 mod pico_st7789;
 #[cfg(feature = "pico-st7789")]
 pub use pico_st7789::*;
 
 #[cfg(feature = "pico2-st7789")]
+#[path = "pico2_st7789/pico2_st7789.rs"]
 mod pico2_st7789;
 #[cfg(feature = "pico2-st7789")]
 pub use pico2_st7789::*;
 
 #[cfg(feature = "stm32h735g")]
+#[path = "stm32h735g/stm32h735g.rs"]
 mod stm32h735g;
 #[cfg(feature = "stm32h735g")]
 pub use stm32h735g::*;
 
 #[cfg(feature = "stm32u5g9j-dk2")]
+#[path = "stm32u5g9j_dk2/stm32u5g9j_dk2.rs"]
 mod stm32u5g9j_dk2;
 #[cfg(feature = "stm32u5g9j-dk2")]
 pub use stm32u5g9j_dk2::*;
 
 #[cfg(feature = "esp32-s3-box-3")]
+#[path = "esp32_s3_box_3/esp32_s3_box_3.rs"]
 mod esp32_s3_box_3;
 #[cfg(feature = "esp32-s3-box-3")]
 pub use esp32_s3_box_3::*;
