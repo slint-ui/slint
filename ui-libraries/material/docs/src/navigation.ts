351e2dd956a7e877dc3f4ca3c41c65869a033e82
Nigel Breslaw <nigel.breslaw@slint.dev>
2025-06-26 21:46:35 +0300

    material: Add announcement banner (https://github.com/slint-ui/material-components/issues/53)
    Imported from https://github.com/slint-ui/material-components/commit/3fdf54b7946e2c2f6a1222e1c5ae277fec346430

--- ui-libraries/material/docs/src/navigation.ts
+++ ui-libraries/material/docs/src/navigation.ts
@@ -17,54 +17,15 @@ export const headerData = {
 
 export const footerData = {
     links: [
-        // {
-        //   title: 'Product',
-        //   links: [
-        //     { text: 'Features', href: '#' },
-        //     { text: 'Security', href: '#' },
-        //     { text: 'Team', href: '#' },
-        //     { text: 'Enterprise', href: '#' },
-        //     { text: 'Customer stories', href: '#' },
-        //     { text: 'Pricing', href: '#' },
-        //     { text: 'Resources', href: '#' },
-        //   ],
-        // },
-        // {
-        //   title: 'Platform',
-        //   links: [
-        //     { text: 'Developer API', href: '#' },
-        //     { text: 'Partners', href: '#' },
-        //     { text: 'Atom', href: '#' },
-        //     { text: 'Electron', href: '#' },
-        //     { text: 'AstroWind Desktop', href: '#' },
-        //   ],
-        // },
-        // {
-        //   title: 'Support',
-        //   links: [
-        //     { text: 'Docs', href: '#' },
-        //     { text: 'Community Forum', href: '#' },
-        //     { text: 'Professional Services', href: '#' },
-        //     { text: 'Skills', href: '#' },
-        //     { text: 'Status', href: '#' },
-        //   ],
-        // },
-        // {
-        //   title: 'Company',
-        //   links: [
-        //     { text: 'About', href: '#' },
-        //     { text: 'Blog', href: '#' },
-        //     { text: 'Careers', href: '#' },
-        //     { text: 'Press', href: '#' },
-        //     { text: 'Inclusion', href: '#' },
-        //     { text: 'Social Impact', href: '#' },
-        //     { text: 'Shop', href: '#' },
-        //   ],
-        // },
+        {
+            title: "Company",
+            links: [
+                { text: "Privacy Policy", href: getPermalink("/privacy") },
+                { text: "Learn more about Slint", href: "https://slint.dev/" },
+            ],
+        },
     ],
-    secondaryLinks: [
-        { text: "Privacy Policy", href: getPermalink("/privacy") },
-    ],
+    secondaryLinks: [],
     socialLinks: [
         {
             ariaLabel: "Github",
