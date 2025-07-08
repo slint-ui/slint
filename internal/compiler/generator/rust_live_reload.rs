59a4cc24e42a2f6c60b41c75407631c24593bdef
Olivier Goffart <olivier.goffart@slint.dev>
2025-07-08 11:05:50 +0200

    Update internal/compiler/generator/rust_live_reload.rs

--- internal/compiler/generator/rust_live_reload.rs
+++ internal/compiler/generator/rust_live_reload.rs
@@ -79,7 +79,7 @@ fn generate_public_component(
     let public_component_id = ident(&llr.name);
     let component_name = llr.name.as_str();
 
-    let main_file = if main_file.ends_with("/Cargo.toml") {
+    let main_file = if main_file.ends_with("Cargo.toml") {
         // We couldn't get the actual .rs file from a slint! macro, so use file!() which will expand to the actual file name
         quote!(file!())
     } else {
