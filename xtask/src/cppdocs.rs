b9c82876c8ed722c19847ce0cce3e2159c55b16d
Simon Hausmann <simon.hausmann@slint.dev>
2025-07-10 23:25:17 +0200

    cppdocs: Switch from pipenv to uv
    uv is generally faster than pip, and we save ourselves the step of installing/upgrading that in the CI.

--- xtask/src/cppdocs.rs
+++ xtask/src/cppdocs.rs
@@ -106,7 +106,7 @@ pub fn generate(show_warnings: bool, experimental: bool) -> Result<(), Box<dyn s
     };
     cbindgen::gen_all(&root, &generated_headers_dir, enabled_features)?;
 
-    let pip_env = vec![(OsString::from("PIPENV_PIPFILE"), docs_source_dir.join("docs/Pipfile"))];
+    let uv_project = vec![(OsString::from("UV_PROJECT"), docs_source_dir.join("docs"))];
 
     println!("Generating third-party license list with cargo-about");
 
@@ -121,7 +121,7 @@ pub fn generate(show_warnings: bool, experimental: bool) -> Result<(), Box<dyn s
             "-o",
             docs_build_dir.join("thirdparty.md").to_str().unwrap(),
         ],
-        pip_env.clone(),
+        std::iter::empty::<(std::ffi::OsString, std::ffi::OsString)>(),
     )?;
 
     println!(
@@ -130,22 +130,17 @@ pub fn generate(show_warnings: bool, experimental: bool) -> Result<(), Box<dyn s
         String::from_utf8_lossy(&cargo_about_output.stderr)
     );
 
-    println!("Running pipenv install");
-
-    super::run_command("pipenv", &["install"], pip_env.clone())
-        .context("Error running pipenv install")?;
-
     println!("Running sphinx-build");
 
     let output = super::run_command(
-        "pipenv",
+        "uv",
         &[
             "run",
             "sphinx-build",
             docs_build_dir.to_str().unwrap(),
             docs_build_dir.join("html").to_str().unwrap(),
         ],
-        pip_env,
+        uv_project,
     )
     .context("Error running pipenv install")?;
 
