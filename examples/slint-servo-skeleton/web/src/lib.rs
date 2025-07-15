e631b54659e3a4b775677fe91dab3a19efdce625
Anujaya Vijayakumar <anujayav1997@gmaail.com>
2025-07-15 10:50:13 +0200

    Fixes layout bug in HiDPI rendering

--- examples/slint-servo-skeleton/web/src/lib.rs
+++ examples/slint-servo-skeleton/web/src/lib.rs
@@ -7,15 +7,12 @@ impl Browser {
     pub fn new() -> Self { 
         Browser { current_url: String::new() } 
     } 
- 
     pub fn load_page(&mut self, url: &str) { 
          
         self.current_url = url.to_string(); 
         println!("Servo browser is loading: {}", self.current_url); 
- 
     } 
-    pub fn render_view_as_image(&self) -> slint::Image { 
-         
+    pub fn render_view_as_image(&self) -> slint::Image {     
         slint::Image::load_from_path(Path::new("placeholder.png")).unwrap() 
     } 
     pub fn current_page(&self) -> &str { 
