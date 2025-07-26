d0a227a8628cbe2e78a1f5eef56b969747dc85d1
minicx <minicx@disroot.org>
2025-07-26 15:37:30 +0300

    LinuxKMS: expand pixel_format_params to support many more DRM pixel formats
    - Added support for a wide range of 32-bit, 30-bit, 24-bit, 16-bit, and 8-bit RGB formats.
    - Included detection for packed and planar YUV and NV formats.
    
    Signed-off-by: minicx <minicx@disroot.org>

--- internal/backends/linuxkms/display/swdisplay/dumbbuffer.rs
+++ internal/backends/linuxkms/display/swdisplay/dumbbuffer.rs
@@ -130,12 +130,75 @@ struct DumbBuffer {
 /// Returns the pixel depth and bits-per-pixel values for a given DRM pixel format.
 fn pixel_format_params(format: drm::buffer::DrmFourcc) -> Option<(u32, u32)> {
     match format {
+        // 32-bit RGB formats
         drm::buffer::DrmFourcc::Xrgb8888 => Some((24, 32)),
         drm::buffer::DrmFourcc::Argb8888 => Some((32, 32)),
-        drm::buffer::DrmFourcc::Rgb565 => Some((16, 16)),
         drm::buffer::DrmFourcc::Xbgr8888 => Some((24, 32)),
         drm::buffer::DrmFourcc::Abgr8888 => Some((32, 32)),
-        // TODO: Add more formats
+        drm::buffer::DrmFourcc::Rgbx8888 => Some((24, 32)),
+        drm::buffer::DrmFourcc::Rgba8888 => Some((32, 32)),
+        drm::buffer::DrmFourcc::Bgrx8888 => Some((24, 32)),
+        drm::buffer::DrmFourcc::Bgra8888 => Some((32, 32)),
+
+        // 30-bit RGB formats (10 bits per channel)
+        drm::buffer::DrmFourcc::Xrgb2101010 => Some((30, 32)),
+        drm::buffer::DrmFourcc::Argb2101010 => Some((32, 32)),
+        drm::buffer::DrmFourcc::Xbgr2101010 => Some((30, 32)),
+        drm::buffer::DrmFourcc::Abgr2101010 => Some((32, 32)),
+        drm::buffer::DrmFourcc::Rgbx1010102 => Some((30, 32)),
+        drm::buffer::DrmFourcc::Rgba1010102 => Some((32, 32)),
+        drm::buffer::DrmFourcc::Bgrx1010102 => Some((30, 32)),
+        drm::buffer::DrmFourcc::Bgra1010102 => Some((32, 32)),
+
+        // 24-bit RGB formats
+        drm::buffer::DrmFourcc::Rgb888 => Some((24, 24)),
+        drm::buffer::DrmFourcc::Bgr888 => Some((24, 24)),
+
+        // 16-bit RGB formats
+        drm::buffer::DrmFourcc::Rgb565 => Some((16, 16)),
+        drm::buffer::DrmFourcc::Bgr565 => Some((16, 16)),
+        drm::buffer::DrmFourcc::Xrgb1555 => Some((15, 16)),
+        drm::buffer::DrmFourcc::Argb1555 => Some((16, 16)),
+        drm::buffer::DrmFourcc::Xbgr1555 => Some((15, 16)),
+        drm::buffer::DrmFourcc::Abgr1555 => Some((16, 16)),
+        drm::buffer::DrmFourcc::Rgbx5551 => Some((15, 16)),
+        drm::buffer::DrmFourcc::Rgba5551 => Some((16, 16)),
+        drm::buffer::DrmFourcc::Bgrx5551 => Some((15, 16)),
+        drm::buffer::DrmFourcc::Bgra5551 => Some((16, 16)),
+        drm::buffer::DrmFourcc::Xrgb4444 => Some((12, 16)),
+        drm::buffer::DrmFourcc::Argb4444 => Some((16, 16)),
+        drm::buffer::DrmFourcc::Xbgr4444 => Some((12, 16)),
+        drm::buffer::DrmFourcc::Abgr4444 => Some((16, 16)),
+        drm::buffer::DrmFourcc::Rgbx4444 => Some((12, 16)),
+        drm::buffer::DrmFourcc::Rgba4444 => Some((16, 16)),
+        drm::buffer::DrmFourcc::Bgrx4444 => Some((12, 16)),
+        drm::buffer::DrmFourcc::Bgra4444 => Some((16, 16)),
+
+        // 8-bit indexed formats
+        drm::buffer::DrmFourcc::C8 => Some((8, 8)),
+
+        // YUV packed formats
+        drm::buffer::DrmFourcc::Yuyv => Some((16, 16)),
+        drm::buffer::DrmFourcc::Yvyu => Some((16, 16)),
+        drm::buffer::DrmFourcc::Uyvy => Some((16, 16)),
+        drm::buffer::DrmFourcc::Vyuy => Some((16, 16)),
+
+        // YUV planar formats
+        drm::buffer::DrmFourcc::Yuv420 => Some((12, 12)), // 4:2:0 = 8 + 2 + 2 = 12 bpp
+        drm::buffer::DrmFourcc::Yvu420 => Some((12, 12)),
+        drm::buffer::DrmFourcc::Yuv422 => Some((16, 16)), // 4:2:2 = 8 + 4 + 4 = 16 bpp
+        drm::buffer::DrmFourcc::Yvu422 => Some((16, 16)),
+        drm::buffer::DrmFourcc::Yuv444 => Some((24, 24)), // 4:4:4 = 8 + 8 + 8 = 24 bpp
+        drm::buffer::DrmFourcc::Yvu444 => Some((24, 24)),
+
+        // NV formats (semi-planar YUV)
+        drm::buffer::DrmFourcc::Nv12 => Some((12, 12)),
+        drm::buffer::DrmFourcc::Nv21 => Some((12, 12)),
+        drm::buffer::DrmFourcc::Nv16 => Some((16, 16)),
+        drm::buffer::DrmFourcc::Nv61 => Some((16, 16)),
+        drm::buffer::DrmFourcc::Nv24 => Some((24, 24)),
+        drm::buffer::DrmFourcc::Nv42 => Some((24, 24)),
+
         _ => None,
     }
 }
