95171f82f2a375c7e5545adbb4ab7b752302b76f
David Faure <faure@kde.org>
2025-08-12 01:30:54 +0200

    Fix grammar and typos in comments (in the compiler passes)

--- internal/compiler/passes/lower_layout.rs
+++ internal/compiler/passes/lower_layout.rs
@@ -1,7 +1,7 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
 
-//! Passe that compute the layout constraint
+//! This pass computes the layout constraint
 
 use lyon_path::geom::euclid::approxeq::ApproxEq;
 
--- internal/compiler/passes/lower_menus.rs
+++ internal/compiler/passes/lower_menus.rs
@@ -1,7 +1,7 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
 
-//! Passe lower the `MenuBar` and `ContextMenuArea` as well as all their contents
+//! This pass lowers the `MenuBar` and `ContextMenuArea` as well as all their contents
 //!
 //! We can't have properties of type Model because that is not binary compatible with C++,
 //! so all the code that handle model of MenuEntry need to be handle by code in the generated code
