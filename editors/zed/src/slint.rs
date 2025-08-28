efb19880e769a342e2e9d1979748d446c01e81c4
Olivier Goffart <olivier.goffart@slint.dev>
2025-08-28 13:25:16 +0200

    Zed-extension: Synchronize the version with Slint version (#9271)
     - Use always the same github release as the github version
     - opt-in the nightly release with the SLINT_DEV_MODE env variable
     - Remove the way it checks for `slint-lsp` in the `$PATH`
     - Update dependencies

--- editors/zed/src/slint.rs
+++ editors/zed/src/slint.rs
@@ -30,10 +30,6 @@ impl SlintExtension {
             return Ok(SlintBinary { path, args: binary_args });
         }
 
-        if let Some(path) = worktree.which("slint-lsp") {
-            return Ok(SlintBinary { path, args: binary_args });
-        }
-
         if let Some(path) = &self.cached_binary_path {
             if fs::metadata(path).is_ok_and(|stat| stat.is_file()) {
                 zed::set_language_server_installation_status(
@@ -49,10 +45,11 @@ impl SlintExtension {
             &zed::LanguageServerInstallationStatus::CheckingForUpdate,
         );
 
-        let release = zed::latest_github_release(
-            "slint-ui/slint",
-            zed::GithubReleaseOptions { require_assets: true, pre_release: false },
-        )?;
+        let dev_mode = worktree.shell_env().iter().any(|(k, v)| k == "SLINT_DEV_MODE" && v == "1");
+        let release_tag =
+            if dev_mode { "nightly" } else { concat!("v", env!("CARGO_PKG_VERSION")) };
+        let release = zed::github_release_by_tag_name("slint-ui/slint", release_tag)
+            .map_err(|e| format!("Failed to get Slint release {release_tag} from GitHub: {e}",))?;
 
         let target = zed::current_platform();
 
@@ -62,7 +59,7 @@ impl SlintExtension {
             (Os::Windows, Architecture::Aarch64) => "slint-lsp-windows-arm64",
             (Os::Linux, Architecture::X8664) => "slint-lsp-linux",
             (Os::Linux, Architecture::Aarch64) => "slint-lsp-aarch64-unknown-linux-gnu",
-            (_, _) => return Err("platform or architecture not supported".to_string()),
+            (_, _) => return Err("Platform or architecture not supported for automatic slint-lsp download. Please install it manually and use the 'lsp.slint.binary' setting to point to it.".to_string()),
         };
 
         let (asset_file_type, asset_name_ext, binary_ext) = match target.0 {
@@ -78,15 +75,28 @@ impl SlintExtension {
             .find(|asset| asset.name == asset_name)
             .ok_or_else(|| format!("no asset found matching {:?}", asset_name))?;
 
-        let extension_dir = "slint-lsp";
-        let binary_path = format!(
-            "{extension_dir}/{}/slint-lsp{binary_ext}",
-            if target_name == "slint-lsp-aarch64-unknown-linux-gnu" {
-                target_name
+        let extension_dir = format!(
+            "slint-lsp-{}{}",
+            release.version,
+            if dev_mode {
+                // Add a timestamp to the extension directory to invalidate the cache every day
+                (std::time::SystemTime::now()
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs()
+                    / (60 * 60 * 24))
+                    .to_string()
             } else {
-                "slint-lsp"
-            },
+                String::new()
+            }
         );
+        // The directory in the tarball is usually named "slint-lsp", but it is different for the slint-lsp-*-linux-*
+        let subdir = if target_name == "slint-lsp-aarch64-unknown-linux-gnu" {
+            target_name
+        } else {
+            "slint-lsp"
+        };
+        let binary_path = format!("{extension_dir}/{subdir}/slint-lsp{binary_ext}",);
 
         if !fs::metadata(&binary_path).is_ok_and(|stat| stat.is_file()) {
             zed::set_language_server_installation_status(
@@ -94,14 +104,14 @@ impl SlintExtension {
                 &zed::LanguageServerInstallationStatus::Downloading,
             );
 
-            zed::download_file(&asset.download_url, extension_dir, asset_file_type)
+            zed::download_file(&asset.download_url, &extension_dir, asset_file_type)
                 .map_err(|e| format!("failed to download file: {e}"))?;
 
             let entries =
                 fs::read_dir(".").map_err(|e| format!("failed to list working directory {e}"))?;
             for entry in entries {
                 let entry = entry.map_err(|e| format!("failed to load directory entry {e}"))?;
-                if entry.file_name().to_str() != Some(extension_dir) {
+                if entry.file_name().to_str() != Some(&extension_dir) {
                     fs::remove_dir_all(entry.path()).ok();
                 }
             }
