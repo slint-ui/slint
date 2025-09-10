c6ee0db3cc691655d2373e8afe92ae9ac053dd7a
Ashley <ashley.ruglys@gmail.com>
2025-09-10 13:50:41 +0200

    Add fontique as a font database alongside fontdb (#9379)
    * Add fontique as a font database alongside fontdb
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- internal/common/lib.rs
+++ internal/common/lib.rs
@@ -3,11 +3,14 @@
 
 #![doc = include_str!("README.md")]
 #![doc(html_logo_url = "https://slint.dev/logo/slint-logo-square-light.svg")]
-#![cfg_attr(not(feature = "shared-fontdb"), no_std)]
+#![cfg_attr(not(any(feature = "shared-fontdb", feature = "shared-fontique")), no_std)]
 
 pub mod builtin_structs;
 pub mod enums;
 pub mod key_codes;
+
+#[cfg(feature = "shared-fontique")]
+pub mod sharedfontique;
 
 #[cfg(feature = "shared-fontdb")]
 pub mod sharedfontdb;
---
+++ internal/common/sharedfontique.rs
@@ -0,0 +1,8 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
+
+pub use fontique;
+
+thread_local! {
+    pub static COLLECTION: std::cell::RefCell<fontique::Collection> = Default::default()
+}
