9f1a1129e593234a36f841cbfd4c73baf63333fa
Ashley Ruglys <ashley.ruglys@gmail.com>
2025-09-12 10:14:57 +0200

    Cargo fmt tools/lsp changes

--- tools/lsp/preview/properties.rs
+++ tools/lsp/preview/properties.rs
@@ -467,13 +467,11 @@ pub(super) fn get_properties(
                     group_priority: depth,
                 });
 
-                    result.extend(get_reserved_properties(
-                        &b.name,
-                        depth,
-                        i_slint_compiler::typeregister::RESERVED_TRANSFORM_PROPERTIES
-                            .iter()
-                            .cloned(),
-                    ));
+                result.extend(get_reserved_properties(
+                    &b.name,
+                    depth,
+                    i_slint_compiler::typeregister::RESERVED_TRANSFORM_PROPERTIES.iter().cloned(),
+                ));
 
                 if matches!(b.name.as_str(), "GridLayout" | "HorizontalLayout" | "VerticalLayout") {
                     // Add the padding that is otherwise filtered out
