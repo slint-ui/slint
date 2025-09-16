a3a3844cef37d5bac14f913a4d14e490ee4154e8
Ashley <ashley.ruglys@gmail.com>
2025-09-16 09:32:04 +0200

    feature/fontique - switch femtovg over and use a default fontdb for usvg (#9426)
    * Use default fontdb for usvg
    
    * Start adapting femtovg
    
    * Use DesignFontMetrics from sharedfontique
    
    * Restore fallback fonts
    
    * [autofix.ci] apply automated fixes
    
    * Add allow(dead_code) tag
    
    * [autofix.ci] apply automated fixes
    
    * Tiny comment change
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- internal/core/graphics.rs
+++ internal/core/graphics.rs
@@ -155,19 +155,6 @@ pub struct FontRequest {
     pub italic: bool,
 }
 
-#[cfg(feature = "shared-fontdb")]
-impl FontRequest {
-    /// Returns the relevant properties of this FontRequest propagated into a fontdb Query.
-    pub fn to_fontdb_query(&self) -> i_slint_common::sharedfontdb::fontdb::Query<'_> {
-        use i_slint_common::sharedfontdb::fontdb::{Query, Style, Weight};
-        Query {
-            style: if self.italic { Style::Italic } else { Style::Normal },
-            weight: Weight(self.weight.unwrap_or(/* CSS normal*/ 400) as _),
-            ..Default::default()
-        }
-    }
-}
-
 #[cfg(feature = "shared-fontique")]
 impl FontRequest {
     /// Attempts to query the fontique font collection for a matching font.
