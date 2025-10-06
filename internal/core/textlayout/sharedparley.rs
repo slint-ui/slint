slint-ui/slint

commit 7efa2886b8572fff007cc53e7b7d4c8fba1d56a9

parley: Fix rendering of characters from the dingbats block
Fix default font selection to include GenericFamily::SystemUi. This typically expands to a list of ui fonts for use by user interfaces, which support these.

Fixes #9608

internal/core/textlayout/sharedparley.rs
@@ -134,13 +134,17 @@ fn layout(text: &str, scale_factor: ScaleFactor, options: LayoutOptions) -> Layo
 
     let push_to_builder = |builder: &mut parley::RangedBuilder<_>| {
         if let Some(ref font_request) = options.font_request {
-            if let Some(family) = &font_request.family {
-                builder.push_default(parley::StyleProperty::FontStack(
-                    parley::style::FontStack::Single(parley::style::FontFamily::Named(
-                        family.as_str().into(),
-                    )),
-                ));
-            }
+            let font_stack = if let Some(family) = &font_request.family {
+                parley::style::FontStack::List(std::borrow::Cow::Borrowed(&[
+                    parley::style::FontFamily::Named(family.as_str().into()),
+                    parley::style::FontFamily::Generic(parley::fontique::GenericFamily::SystemUi),
+                ]))
+            } else {
+                parley::style::FontStack::Single(parley::style::FontFamily::Generic(
+                    parley::fontique::GenericFamily::SystemUi,
+                ))
+            };
+            builder.push_default(font_stack);
             if let Some(weight) = font_request.weight {
                 builder.push_default(parley::StyleProperty::FontWeight(
                     parley::style::FontWeight::new(weight as f32),

commit 5450ce8de712e84942590f23805c96238528aafd

parley: Fix missing glyphs on macOS with FemtoVG renderer
Regression introduced with 7efa2886b8572fff007cc53e7b7d4c8fba1d56a9, where the fonts reported by GenericFamily::SystemUi can't be rendered.

This is I think a bug with FemtoVG that deserves a separate investigation, but for now: Fix the regression.

internal/core/textlayout/sharedparley.rs
@@ -138,15 +138,27 @@ fn layout(text: &str, scale_factor: ScaleFactor, options: LayoutOptions) -> Layo
                 builder.push_default(parley::style::FontStack::List(std::borrow::Cow::Borrowed(
                     &[
                         parley::style::FontFamily::Named(family.as_str().into()),
+                        // FemtoVG renderer needs SansSerif first, as it has difficulties rendering from SystemUi on macOS
+                        parley::style::FontFamily::Generic(
+                            parley::fontique::GenericFamily::SansSerif,
+                        ),
                         parley::style::FontFamily::Generic(
                             parley::fontique::GenericFamily::SystemUi,
                         ),
                     ],
                 )));
             } else {
-                builder.push_default(parley::style::FontStack::Single(
-                    parley::style::FontFamily::Generic(parley::fontique::GenericFamily::SystemUi),
-                ));
+                builder.push_default(parley::style::FontStack::List(std::borrow::Cow::Borrowed(
+                    &[
+                        // FemtoVG renderer needs SansSerif first, as it has difficulties rendering from SystemUi on macOS
+                        parley::style::FontFamily::Generic(
+                            parley::fontique::GenericFamily::SansSerif,
+                        ),
+                        parley::style::FontFamily::Generic(
+                            parley::fontique::GenericFamily::SystemUi,
+                        ),
+                    ],
+                )));
             }
             if let Some(weight) = font_request.weight {
                 builder.push_default(parley::StyleProperty::FontWeight(