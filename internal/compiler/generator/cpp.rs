ea1ccffc9c666a4a2f2e3ad6f46130fddaa8751a
Olivier Goffart <olivier.goffart@slint.dev>
2025-10-07 16:11:52 +0200

    `Math.sign`: Change behavior to return 1 when 0
    This is consistant to the Rust/C++ behavior
    Even if this depart from the JS behavior
    
    Also replace the funciton with a Macro
    
    As discussed in https://github.com/slint-ui/slint/issues/9651#issuecomment-3376318439

--- internal/compiler/generator/cpp.rs
+++ internal/compiler/generator/cpp.rs
@@ -3622,9 +3622,6 @@ fn compile_builtin_function_call(
             ctx.generator_state.conditional_includes.cmath.set(true);
             format!("std::exp({})", a.next().unwrap())
         }
-        BuiltinFunction::Sign => {
-            format!("[](auto a){{ return a == 0 ? 0 : a < 0 ? -1 : 1; }}({})", a.next().unwrap())
-        }
         BuiltinFunction::Sin => {
             ctx.generator_state.conditional_includes.cmath.set(true);
             format!("std::sin(({}) * {})", a.next().unwrap(), pi_180)
