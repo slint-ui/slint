24a806e7cdb104eaf3cf4c51e4213ddecc40c958
Ashley Ruglys <ashley.ruglys@gmail.com>
2025-10-08 11:24:06 +0200

    Set alpha colors, physical clip

--- internal/core/software_renderer.rs
+++ internal/core/software_renderer.rs
@@ -2660,7 +2660,8 @@ impl<T: ProcessScene> sharedparley::GlyphRenderer for SceneBuilder<'_, T> {
                     + global_offset
                     + glyph_offset,
                 glyph.size(),
-            );
+            )
+            .transformed(self.rotation);
 
             let data = {
                 let source_rect = euclid::rect(0, 0, glyph.width.0, glyph.height.0);
@@ -2673,7 +2674,13 @@ impl<T: ProcessScene> sharedparley::GlyphRenderer for SceneBuilder<'_, T> {
                 }
             };
 
-            let target_rect = target_rect.transformed(self.rotation);
+            let color = self.alpha_color(color);
+            let physical_clip =
+                (self.current_state.clip.translate(self.current_state.offset.to_vector()).cast()
+                    * self.scale_factor)
+                    .round()
+                    .transformed(self.rotation);
+
             let t = target_pixel_buffer::DrawTextureArgs {
                 data,
                 colorize: Some(color),
@@ -2687,7 +2694,7 @@ impl<T: ProcessScene> sharedparley::GlyphRenderer for SceneBuilder<'_, T> {
                 tiling: None,
             };
 
-            self.processor.process_target_texture(&t, target_rect.cast());
+            self.processor.process_target_texture(&t, physical_clip.cast());
         }
     }
 }
