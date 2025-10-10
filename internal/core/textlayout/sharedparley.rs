03059bbf4dffe6f4ff3ae24d626b0fb668c6097e
Ashley <ashley.ruglys@gmail.com>
2025-10-10 12:34:29 +0200

    parley: only cull lines where text_wrap != TextWrap::NoWrap (#9693)
    * Only cull lines if wrapping

--- internal/core/textlayout/sharedparley.rs
+++ internal/core/textlayout/sharedparley.rs
@@ -274,7 +274,13 @@ fn layout(text: &str, scale_factor: ScaleFactor, mut options: LayoutOptions) -> 
         (None, _) | (Some(_), TextVerticalAlignment::Top) => PhysicalLength::new(0.0),
     };
 
-    Layout { inner: layout, y_offset, elision_info, max_physical_height }
+    Layout {
+        inner: layout,
+        y_offset,
+        elision_info,
+        max_physical_height,
+        is_wrapping: options.text_wrap != TextWrap::NoWrap,
+    }
 }
 
 struct ElisionInfo {
@@ -287,6 +293,7 @@ struct Layout {
     y_offset: PhysicalLength,
     max_physical_height: Option<PhysicalLength>,
     elision_info: Option<ElisionInfo>,
+    is_wrapping: bool,
 }
 
 impl Layout {
@@ -348,7 +355,9 @@ impl Layout {
             .take_while(|line| {
                 let metrics = line.metrics();
                 match self.max_physical_height {
-                    Some(max_physical_height) => max_physical_height.get() > metrics.max_coord,
+                    Some(max_physical_height) if self.is_wrapping => {
+                        max_physical_height.get() > metrics.max_coord
+                    }
                     _ => true,
                 }
             })
