f6dbe7749f9ee5d0da52334ace07682361e4f16a
Simon Hausmann <simon.hausmann@slint.dev>
2025-10-13 17:42:15 +0200

    Python: Add support for async callbacks with @slint.callback
    Permit the use of `@slint.callback` on async functions. They will be run as tasks.

--- api/python/slint/tests/test_callback_decorators.py
+++ api/python/slint/tests/test_callback_decorators.py
@@ -5,6 +5,7 @@ from slint import load_file
 import slint
 import pytest
 from pathlib import Path
+import asyncio
 
 
 def base_dir() -> Path:
@@ -36,3 +37,51 @@ def test_callback_decorators(caplog: pytest.LogCaptureFixture) -> None:
     assert instance.invoke_say_hello_again("ok") == "say_hello_again:ok"
     assert instance.invoke_global_callback("ok") == "global:ok"
     del instance
+
+
+def test_callback_decorators_async() -> None:
+    module = load_file(base_dir() / "test-load-file.slint", quiet=False)
+
+    class SubClass(module.App):  # type: ignore
+        def __init__(self, in_queue: asyncio.Queue[int], out_queue: asyncio.Queue[int]):
+            super().__init__()
+            self.in_queue = in_queue
+            self.out_queue = out_queue
+
+        @slint.callback()
+        async def call_void(self) -> None:
+            value = await self.in_queue.get()
+            await self.out_queue.put(value + 1)
+
+    async def main(
+        instance: SubClass, in_queue: asyncio.Queue[int], out_queue: asyncio.Queue[int]
+    ) -> None:
+        await in_queue.put(42)
+        instance.invoke_call_void()
+        assert await out_queue.get() == 43
+        slint.quit_event_loop()
+
+    in_queue: asyncio.Queue[int] = asyncio.Queue()
+    out_queue: asyncio.Queue[int] = asyncio.Queue()
+    instance = SubClass(in_queue, out_queue)
+    slint.run_event_loop(main(instance, in_queue, out_queue))
+
+
+def test_callback_decorators_async_err() -> None:
+    module = load_file(base_dir() / "test-load-file.slint", quiet=False)
+
+    class SubClass(module.App):  # type: ignore
+        def __init__(self) -> None:
+            super().__init__()
+
+        @slint.callback()
+        async def say_hello(self, msg: str) -> str:
+            return msg
+
+    with pytest.raises(RuntimeError) as excinfo:
+        SubClass()
+    err = excinfo.value
+    assert (
+        str(err)
+        == "Callback 'say_hello' cannot be used with a callback decorator for an async function, as it doesn't return void"
+    )
