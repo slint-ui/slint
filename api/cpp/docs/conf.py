db7e383e33986d0bb7bd6d08a62d9bb91ac2d01c
ogoffart <ogoffart@users.noreply.github.com>
2025-10-22 13:17:31 +0000

    Bump version number to 1.14.1

--- api/cpp/docs/conf.py
+++ api/cpp/docs/conf.py
@@ -23,7 +23,7 @@ import json
 # -- Project information -----------------------------------------------------
 
 # The full version, including alpha/beta/rc tags
-version = "1.14.0"
+version = "1.14.1"
 
 project = f"Slint {version} C++ API"
 copyright = "SixtyFPS GmbH"
