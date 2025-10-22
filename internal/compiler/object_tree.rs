f6a3d67eca79e906c5563da0d3c1f8baa8ff6a28
Nathan Collins <nathan.collins@kdab.com>
2025-10-22 14:09:33 +0100

    compiler: An interface can have public properties
    Warn if private properties are declared.

--- internal/compiler/object_tree.rs
+++ internal/compiler/object_tree.rs
@@ -1076,7 +1076,7 @@ impl Element {
             .to_string();
         let mut r = Element {
             id,
-            base_type,
+            base_type: base_type.clone(),
             debug: vec![ElementDebugInfo {
                 qualified_id,
                 element_hash: 0,
@@ -1159,6 +1159,13 @@ impl Element {
                     PropertyVisibility::Private
                 }
             });
+
+            if base_type == ElementType::Interface && visibility == PropertyVisibility::Private {
+                diag.push_warning(
+                    "'private' properties are inaccessible in an interface".into(),
+                    &prop_decl,
+                );
+            }
 
             r.property_declarations.insert(
                 prop_name.clone().into(),
