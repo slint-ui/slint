f8ece22260d7c44ad0b0420c7999df6aca8898de
GreyElaina <GreyElaina@outlook.com>
2025-10-24 04:16:33 +0800

    fix: cleanup slint.codegen.__init__

--- api/python/slint/slint/codegen/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright © SixtyFPS GmbH <info@slint.dev>
-# SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
-
-from __future__ import annotations
-
-from .cli import main
-
-__all__ = ["main"]
