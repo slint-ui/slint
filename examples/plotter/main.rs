f2713bd9b20fc8201b0136cea5fb4ac0463a7eb1
nanopink <nano_pink@outlook.com>
2025-10-24 08:57:35 -0400

    Simplification & Cleanup

--- examples/plotter/main.rs
+++ examples/plotter/main.rs
@@ -1,6 +1,8 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: MIT
 
+use plotters::prelude::*;
+
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
@@ -11,7 +13,16 @@ slint::slint! {
     export { MainWindow } from "plotter.slint";
 }
 
-fn render_plot(pitch: f32, yaw: f32, amplitude: f32, width: u32, height: u32) -> slint::Image {
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct PlotVariables {
+    pitch: f32,
+    yaw: f32,
+    amplitude: f32,
+    width: u32,
+    height: u32,
+}
+
+fn render_plot(vars: &PlotVariables) -> slint::Image {
     fn probability_density_function(x: f64, y: f64, a: f64) -> f64 {
         const SDX: f64 = 0.1;
         const SDY: f64 = 0.1;
@@ -20,40 +31,35 @@ fn render_plot(pitch: f32, yaw: f32, amplitude: f32, width: u32, height: u32) ->
         a * (-x * x / 2.0 / SDX / SDX - y * y / 2.0 / SDY / SDY).exp()
     }
 
-    use plotters::prelude::*;
-    let mut pixel_buffer = slint::SharedPixelBuffer::new(width, height);
+    let mut pixel_buffer = slint::SharedPixelBuffer::new(vars.width, vars.height);
     {
         let size = (pixel_buffer.width(), pixel_buffer.height());
-        let root =
-            BitMapBackend::with_buffer(pixel_buffer.make_mut_bytes(), size).into_drawing_area();
-        root.fill(&plotters::style::RGBColor(28, 28, 28)).expect("error filling drawing area");
+        let backend = BitMapBackend::with_buffer(pixel_buffer.make_mut_bytes(), size);
+
+        // Plotters requires TrueType fonts from the file system to draw axis text - we skip that for
+        // WASM for now.
+        #[cfg(target_arch = "wasm32")]
+        let backend = wasm_backend::BackendWithoutText { backend };
+
+        let root = backend.into_drawing_area();
+        root.fill(&WHITE).expect("error filling drawing area");
 
         let mut chart = ChartBuilder::on(&root)
             .build_cartesian_3d(-3.0..3.0, 0.0..6.0, -3.0..3.0)
-            .expect("error building coordinate system");
+            .expect("error building chart");
         chart.with_projection(|mut p| {
-            p.pitch = pitch as f64;
-            p.yaw = yaw as f64;
+            p.pitch = vars.pitch as f64;
+            p.yaw = vars.yaw as f64;
             p.scale = 0.62;
             p.into_matrix()
         });
-
-        let gray = &plotters::style::RGBColor(64, 64, 64);
-        chart
-            .configure_axes()
-            .label_style(("sans-serif", 19).into_font().color(&WHITE))
-            .light_grid_style(gray)
-            .bold_grid_style(gray)
-            .max_light_lines(4)
-            .draw()
-            .expect("error drawing");
-        let precision = 30;
+        chart.configure_axes().draw().expect("error drawing");
         chart
             .draw_series(
                 SurfaceSeries::xoz(
-                    (-precision..=precision).map(|x| x as f64 / (precision as f64 / 3.0)),
-                    (-precision..=precision).map(|x| x as f64 / (precision as f64 / 3.0)),
-                    |x, y| probability_density_function(x, y, (amplitude as f64 / 1.0) * 6.0),
+                    (-30..=30).map(|x| x as f64 / 10.0),
+                    (-30..=30).map(|x| x as f64 / 10.0),
+                    |x, y| probability_density_function(x, y, (vars.amplitude as f64 / 1.0) * 6.0),
                 )
                 .style_func(&|&v| {
                     (&HSLColor(240.0 / 360.0 - 240.0 / 360.0 * v / 5.0, 1.0, 0.7)).into()
@@ -76,42 +82,27 @@ pub fn main() {
     let main_window = MainWindow::new().expect("Cannot create main window");
     let main_window_weak = main_window.as_weak();
 
-    let mut current_amplitude = -1.0f32;
-    let mut current_pitch = -1.0f32;
-    let mut current_yaw = -1.0f32;
-    let mut current_width = 0u32;
-    let mut current_height = 0u32;
+    let mut plot_variables =
+        PlotVariables { pitch: -1.0, yaw: -1.0, amplitude: -1.0, width: 0, height: 0 };
 
     main_window
         .window()
-        .set_rendering_notifier(move |state, _graphics_api| {
+        .set_rendering_notifier(move |state, _| {
             if let slint::RenderingState::BeforeRendering = state {
-                if let Some(main_window_strong) = main_window_weak.upgrade() {
-                    let new_pitch = main_window_strong.get_pitch();
-                    let new_yaw = main_window_strong.get_yaw();
-                    let new_amplitude = main_window_strong.get_amplitude();
-                    let new_width = main_window_strong.get_texture_width() as u32;
-                    let new_height = main_window_strong.get_texture_height() as u32;
-                    if current_pitch != new_pitch
-                        || current_yaw != new_yaw
-                        || current_amplitude != new_amplitude
-                        || current_width != new_width
-                        || current_height != new_height
-                    {
-                        current_pitch = new_pitch;
-                        current_yaw = new_yaw;
-                        current_amplitude = new_amplitude;
-                        current_width = new_width;
-                        current_height = new_height;
-                        main_window_strong.set_texture(render_plot(
-                            new_pitch,
-                            new_yaw,
-                            new_amplitude,
-                            new_width,
-                            new_height,
-                        ));
+                if let Some(main_window) = main_window_weak.upgrade() {
+                    let current_plot_variables = PlotVariables {
+                        pitch: main_window.get_pitch(),
+                        yaw: main_window.get_yaw(),
+                        amplitude: main_window.get_amplitude(),
+                        width: main_window.get_texture_width() as u32,
+                        height: main_window.get_texture_height() as u32,
+                    };
+
+                    if current_plot_variables != plot_variables {
+                        plot_variables = current_plot_variables;
+                        main_window.set_texture(render_plot(&current_plot_variables));
                     }
-                    main_window_strong.window().request_redraw();
+                    main_window.window().request_redraw();
                 }
             }
         })
