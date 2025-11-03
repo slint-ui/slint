9eb1902ffa0ca1ba2723d2cb79f0c141745fdd50
Ashley Ruglys <ashley.ruglys@gmail.com>
2025-11-03 16:17:59 +0100

    Put open_url behind a flag

--- internal/core/lib.rs
+++ internal/core/lib.rs
@@ -149,7 +149,9 @@ pub fn is_apple_platform() -> bool {
     matches!(detect_operating_system(), OperatingSystemType::Macos | OperatingSystemType::Ios)
 }
 
+#[cfg_attr(not(feature = "std"), allow(unused))]
 pub fn open_url(url: &str) {
+    #[cfg(feature = "std")]
     if let Err(err) = webbrowser::open(url) {
         debug_log!("Error opening url {}: {}", url, err);
     }
