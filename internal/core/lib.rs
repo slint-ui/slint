e566500cb109897ae77590042dcc4ab06c62e09c
Ashley Ruglys <ashley.ruglys@gmail.com>
2025-11-13 14:43:19 +0100

    Markdown stuff

--- internal/core/lib.rs
+++ internal/core/lib.rs
@@ -172,3 +172,13 @@ pub fn open_url(url: &str) {
         debug_log!("Error opening url {}: {}", url, err);
     }
 }
+
+pub fn escape_markdown(text: &str) -> std::string::String  {
+    std::dbg!(text);
+    text.into()
+}
+
+pub fn parse_markdown(text: &str) -> std::string::String {
+    std::dbg!(text);
+    text.into()
+}
