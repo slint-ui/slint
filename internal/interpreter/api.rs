bce8665d38e1297a18a7c15cb180338a2ff93883
Ashley Ruglys <ashley.ruglys@gmail.com>
2025-11-21 15:23:20 +0100

    Remove whitespace change

--- internal/interpreter/api.rs
+++ internal/interpreter/api.rs
@@ -19,6 +19,7 @@ use std::rc::Rc;
 
 #[doc(inline)]
 pub use i_slint_compiler::diagnostics::{Diagnostic, DiagnosticLevel};
+
 // keep in sync with api/rs/slint/lib.rs
 pub use i_slint_backend_selector::api::*;
 #[cfg(feature = "std")]
