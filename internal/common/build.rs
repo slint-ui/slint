7cd2d3a2e7d9d3e09777832ba40bac5bf6d92660
Olivier Goffart <olivier.goffart@slint.dev>
2025-11-25 10:28:44 +0100

    Revert "Debug"
    This reverts commit 119cff99b6fcb664848fb39cc7f97378b5d7a0eb.

--- internal/common/build.rs
+++
@@ -1,8 +0,0 @@
-// Copyright © SixtyFPS GmbH <info@slint.dev>
-// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
-
-fn main() {
-    println!("cargo:warning=ENV={:?}", std::env::vars().collect::<Vec<_>>());
-
-    println!("cargo:warning=FONTCONFIG={:?}", pkg_config::find_library("fontconfig"))
-}
