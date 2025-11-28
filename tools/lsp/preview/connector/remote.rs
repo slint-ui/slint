126574b14bcd3ca6bfd6533655e305dd42d8d305
Andreas Monitzer <andreas@monitzer.com>
2025-11-28 19:10:45 +0100

    Started to integrate websocket support into the LSP for the preview.

--- tools/lsp/preview/connector/remote.rs
+++ tools/lsp/preview/connector/remote.rs
@@ -2,23 +2,33 @@ use std::{
     cell::RefCell,
     net::{Ipv6Addr, SocketAddr, SocketAddrV6},
     str::FromStr,
-    sync::{Arc, Mutex},
+    sync::Arc,
+    thread::JoinHandle,
 };
 
-use futures_util::stream::StreamExt;
+use futures_util::{stream::StreamExt, SinkExt};
 use mdns_sd::{ServiceDaemon, ServiceInfo};
+use tokio::sync;
 
 use crate::common;
 
-struct RemoteLspToPreviewInner {
-    communication_handle: std::thread::JoinHandle<std::result::Result<(), String>>,
-    to_child: std::process::ChildStdin,
-    child: Arc<Mutex<std::process::Child>>,
+#[derive(Default)]
+enum ServeTask {
+    #[default]
+    Stopped,
+    Waiting {
+        preview_to_lsp_channel: crossbeam_channel::Sender<common::PreviewToLspMessage>,
+        lsp_to_preview_channel: sync::broadcast::Receiver<common::LspToPreviewMessage>,
+    },
+    Running {
+        join_handle: JoinHandle<()>,
+        notify_stop: Arc<sync::Notify>,
+    },
 }
 
 pub struct RemoteLspToPreview {
-    inner: RefCell<Option<RemoteLspToPreviewInner>>,
-    preview_to_lsp_channel: crossbeam_channel::Sender<common::PreviewToLspMessage>,
+    sender: sync::broadcast::Sender<common::LspToPreviewMessage>,
+    serve_task: RefCell<ServeTask>,
 }
 
 impl RemoteLspToPreview {
@@ -26,106 +36,192 @@ impl RemoteLspToPreview {
         preview_to_lsp_channel: crossbeam_channel::Sender<common::PreviewToLspMessage>,
         _server_notifier: crate::ServerNotifier,
     ) -> Self {
-        Self { inner: RefCell::new(None), preview_to_lsp_channel }
-    }
-}
-
-impl common::LspToPreview for RemoteLspToPreview {
-    fn send(&self, message: &common::LspToPreviewMessage) {
-        todo!()
-    }
-
-    fn set_preview_target(&self, target: common::PreviewTarget) -> common::Result<()> {
-        todo!()
+        let (sender, lsp_to_preview_channel) = sync::broadcast::channel(128);
+        Self {
+            sender,
+            serve_task: RefCell::new(ServeTask::Waiting {
+                preview_to_lsp_channel,
+                lsp_to_preview_channel,
+            }),
+        }
     }
 
-    fn preview_target(&self) -> common::PreviewTarget {
-        todo!()
+    fn ensure_task(&self) {
+        match self.serve_task.take() {
+            ServeTask::Waiting { preview_to_lsp_channel, lsp_to_preview_channel } => {
+                let notify_stop = Arc::new(sync::Notify::new());
+                let inner_notify_stop = notify_stop.clone();
+
+                self.serve_task.replace(ServeTask::Running {
+                    join_handle: std::thread::spawn(move || {
+                        if let Err(err) = Self::serve(
+                            preview_to_lsp_channel,
+                            lsp_to_preview_channel,
+                            inner_notify_stop,
+                        ) {
+                            eprintln!("WebSocket thread failed: {err}");
+                        }
+                    }),
+                    notify_stop,
+                });
+            }
+            other => {
+                self.serve_task.replace(other);
+            }
+        }
     }
-}
 
-pub struct WebSocketControlledPreviewToLsp {}
+    fn serve(
+        preview_to_lsp_channel: crossbeam_channel::Sender<common::PreviewToLspMessage>,
+        lsp_to_preview_channel: sync::broadcast::Receiver<common::LspToPreviewMessage>,
+        notify_stop: Arc<sync::Notify>,
+    ) -> common::Result<()> {
+        let address: Option<String> = None;
+        let announce = true;
 
-impl Default for WebSocketControlledPreviewToLsp {
-    fn default() -> Self {
-        Self {}
+        tokio::runtime::Builder::new_current_thread().build().unwrap().block_on(async {
+            eprintln!("Hello world");
+            let listen_address = address
+                .as_deref()
+                .map(SocketAddr::from_str)
+                .unwrap_or(
+                    const { Ok(SocketAddr::V6(SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, 0, 0, 0))) },
+                )
+                .map_err(Box::new)?;
+            let listener = tokio::net::TcpListener::bind(listen_address).await.map_err(Box::new)?;
+            let local_addr = listener.local_addr().map_err(Box::new)?;
+            eprintln!("Listening on {}", local_addr);
+
+            let mdns = if announce {
+                let mdns = ServiceDaemon::new().map_err(Box::new)?;
+
+                let service = ServiceInfo::new(
+                    "_slint-preview._tcp.local.",
+                    "lsp",
+                    hostname::get().map_err(Box::new)?.to_str().unwrap_or_default(),
+                    local_addr.ip(),
+                    local_addr.port(),
+                    None,
+                )
+                .map_err(Box::new)?
+                .enable_addr_auto();
+
+                mdns.register(service).map_err(Box::new)?;
+
+                Some(mdns)
+            } else {
+                None
+            };
+
+            loop {
+                tokio::select! {
+                    conn = listener.accept() => {
+                        match conn {
+                            Ok((stream, addr)) => {
+                                eprintln!("New connection from {addr}");
+                                tokio::spawn(Self::handle_client(
+                                    addr,
+                                    stream,
+                                    preview_to_lsp_channel.clone(),
+                                    lsp_to_preview_channel.resubscribe(),
+                                ));
+                            }
+                            Err(err) => {
+                                return Err(Box::new(err) as Box<dyn std::error::Error>);
+                            }
+                        }
+                    }
+                    _ = notify_stop.notified() => {
+                        if let Some(mdns) = mdns {
+                            mdns.shutdown().map_err(Box::new)?;
+                        }
+                        return Ok(());
+                    }
+                }
+            }
+        })
     }
-}
 
-impl common::PreviewToLsp for WebSocketControlledPreviewToLsp {
-    fn send(&self, message: &common::PreviewToLspMessage) -> common::Result<()> {
-        todo!()
-    }
-}
-
-fn wrap_err(err: impl std::error::Error + Send + Sync + 'static) -> slint::PlatformError {
-    slint::PlatformError::OtherError(Box::new(err))
-}
-
-pub fn serve(args: &crate::Serve) -> Result<(), slint::PlatformError> {
-    tokio::runtime::Builder::new_current_thread().build().unwrap().block_on(async {
-        eprintln!("Hello world");
-        let listen_address = args
-            .address
-            .as_deref()
-            .map(SocketAddr::from_str)
-            .unwrap_or(
-                const { Ok(SocketAddr::V6(SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, 0, 0, 0))) },
-            )
-            .map_err(wrap_err)?;
-        let listener = tokio::net::TcpListener::bind(listen_address).await.map_err(wrap_err)?;
-        let local_addr = listener.local_addr().map_err(wrap_err)?;
-        eprintln!("Listening on {}", local_addr);
-
-        let mdns = if args.announce {
-            let mdns = ServiceDaemon::new().map_err(wrap_err)?;
-
-            let service = ServiceInfo::new(
-                "_slint-preview._tcp.local.",
-                "lsp",
-                hostname::get().map_err(wrap_err)?.to_str().unwrap_or_default(),
-                local_addr.ip(),
-                local_addr.port(),
-                None,
-            )
-            .map_err(wrap_err)?
-            .enable_addr_auto();
-
-            mdns.register(service).map_err(wrap_err)?;
-
-            Some(mdns)
-        } else {
-            None
-        };
-
+    async fn handle_client(
+        addr: SocketAddr,
+        stream: tokio::net::TcpStream,
+        preview_to_lsp_channel: crossbeam_channel::Sender<common::PreviewToLspMessage>,
+        mut lsp_to_preview_channel: sync::broadcast::Receiver<common::LspToPreviewMessage>,
+    ) {
+        let ws_stream = tokio_tungstenite::accept_async(stream)
+            .await
+            .expect("Error during the websocket handshake occurred");
+        eprintln!("WebSocket connection established: {addr}");
+        let standard_config = bincode::config::standard();
+        let (write, mut read) = ws_stream.split();
         loop {
-            let (stream, addr) = listener.accept().await.map_err(wrap_err)?;
-            eprintln!("New connection from {addr}");
-            tokio::spawn(async move {
-                let ws_stream = tokio_tungstenite::accept_async(stream)
-                    .await
-                    .expect("Error during the websocket handshake occurred");
-                eprintln!("WebSocket connection established: {addr}");
-                let (write, mut read) = ws_stream.split();
-                while let Some(message) = read.next().await {
+            tokio::select! {
+                message = read.next() => {
                     match message {
-                        Ok(msg) => eprintln!(
-                            "Received a message from {addr}: {}",
-                            msg.into_text().unwrap_or_default()
-                        ),
-                        Err(e) => {
+                        None => break,
+                        Some(Ok(msg)) => {
+                            eprintln!(
+                                "Received a message from {addr}: {}",
+                                msg.into_text().unwrap_or_default()
+                            );
+                        }
+                        Some(Err(e)) => {
                             eprintln!("Error receiving message from {addr}: {e}");
                             break;
                         }
                     }
                 }
-            });
+                message = lsp_to_preview_channel.recv() => {
+                    match message {
+                        Ok(msg) => {
+                            write.send(tokio_tungstenite::tungstenite::Message::binary(
+                                bincode::encode_to_vec(&msg, standard_config).unwrap(),
+                            )).await;
+                        }
+                        Err(_) => break,
+                    }
+                }
+            }
         }
-
-        if let Some(mdns) = mdns {
-            mdns.shutdown().map_err(wrap_err)?;
-        }
-
-        Ok(())
-    })
+    }
 }
+
+impl Drop for RemoteLspToPreview {
+    fn drop(&mut self) {
+        if let ServeTask::Running { join_handle, notify_stop } = self.serve_task.take() {
+            notify_stop.notify_waiters();
+            join_handle.join();
+        }
+    }
+}
+
+impl common::LspToPreview for RemoteLspToPreview {
+    fn send(&self, message: &common::LspToPreviewMessage) {
+        self.ensure_task();
+        if let Err(err) = self.sender.send(message.clone()) {
+            eprintln!("Failed sending message to WebSocket thread: {err}");
+        }
+    }
+
+    fn set_preview_target(&self, _target: common::PreviewTarget) -> common::Result<()> {
+        Err("Can not change the preview target".into())
+    }
+
+    fn preview_target(&self) -> common::PreviewTarget {
+        common::PreviewTarget::Remote
+    }
+}
+
+// pub struct WebSocketControlledPreviewToLsp {}
+
+// impl Default for WebSocketControlledPreviewToLsp {
+//     fn default() -> Self {
+//         Self {}
+//     }
+// }
+
+// impl common::PreviewToLsp for WebSocketControlledPreviewToLsp {
+//     fn send(&self, message: &common::PreviewToLspMessage) -> common::Result<()> {
+//         todo!()
+//     }
+// }
