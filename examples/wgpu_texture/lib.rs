a44d2ce2a87c63dd9a18af6138771c95718a015e
burhankhanzada <burhan.khanzada@slint.dev>
2025-12-04 13:51:21 +0100

    Revert "fix: fix wgpu_texture to run on android emulator on mac with disable push constants"
    This reverts commit 57ac9c7c41837312696af796d0c7c6a8c8f67958.

--- examples/wgpu_texture/lib.rs
+++ examples/wgpu_texture/lib.rs
@@ -13,6 +13,12 @@ struct DemoRenderer {
     start_time: std::time::Instant,
 }
 
+#[repr(C)]
+#[derive(Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
+struct PushConstants {
+    light_color_and_time: [f32; 4],
+}
+
 impl DemoRenderer {
     fn new(device: &wgpu::Device, queue: &wgpu::Queue) -> Self {
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
@@ -22,9 +28,18 @@ impl DemoRenderer {
             ))),
         });
 
+        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: None,
+            bind_group_layouts: &[],
+            push_constant_ranges: &[wgpu::PushConstantRange {
+                stages: wgpu::ShaderStages::FRAGMENT,
+                range: 0..16, // full size in bytes, aligned
+            }],
+        });
+
         let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: None,
-            layout: None, // Auto-layout
+            layout: Some(&pipeline_layout),
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: Some("vs_main"),
@@ -35,7 +50,7 @@ impl DemoRenderer {
                 module: &shader,
                 entry_point: Some("fs_main"),
                 compilation_options: Default::default(),
-                targets: &[Some(wgpu::TextureFormat::Rgba8Unorm.into())],
+                targets: &[Some(wgpu::TextureFormat::Rgba8UnormSrgb.into())],
             }),
             primitive: wgpu::PrimitiveState::default(),
             depth_stencil: None,
@@ -62,15 +77,27 @@ impl DemoRenderer {
             mip_level_count: 1,
             sample_count: 1,
             dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Rgba8Unorm,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT | wgpu::TextureUsages::TEXTURE_BINDING,
             view_formats: &[],
         })
     }
 
-    fn render(&self, _size: slint::PhysicalSize) -> wgpu::Texture {
-        let _time = self.start_time.elapsed().as_secs_f32();
-        let texture_view = self.texture.create_view(&wgpu::TextureViewDescriptor::default());
+    fn render(
+        &mut self,
+        light_red: f32,
+        light_green: f32,
+        light_blue: f32,
+        width: u32,
+        height: u32,
+    ) -> wgpu::Texture {
+        if self.texture.size().width != width || self.texture.size().height != height {
+            self.texture = Self::create_texture(&self.device, width, height);
+        }
+
+        let elapsed: f32 = self.start_time.elapsed().as_millis() as f32 / 500.;
+        let push_constants =
+            PushConstants { light_color_and_time: [light_red, light_green, light_blue, elapsed] };
 
         let mut encoder =
             self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
@@ -78,10 +105,10 @@ impl DemoRenderer {
             let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: None,
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &texture_view,
+                    view: &self.texture.create_view(&wgpu::TextureViewDescriptor::default()),
                     resolve_target: None,
                     ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color { r: 0.0, g: 0.0, b: 0.0, a: 1.0 }),
+                        load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
                         store: wgpu::StoreOp::Store,
                     },
                     depth_slice: None,
@@ -91,6 +118,11 @@ impl DemoRenderer {
                 occlusion_query_set: None,
             });
             rpass.set_pipeline(&self.pipeline);
+            rpass.set_push_constants(
+                wgpu::ShaderStages::FRAGMENT, // Stage (your constants are for fragment shader)
+                0,                            // Offset in bytes (start at 0)
+                bytemuck::bytes_of(&push_constants),
+            );
             rpass.draw(0..3, 0..1);
         }
 
@@ -100,8 +132,7 @@ impl DemoRenderer {
     }
 }
 
-#[cfg(not(target_os = "android"))]
-fn main() {
+pub fn main() {
     let mut wgpu_settings = WGPUSettings::default();
     wgpu_settings.device_required_features = wgpu::Features::PUSH_CONSTANTS;
     wgpu_settings.device_required_limits.max_push_constant_size = 16;
@@ -132,72 +163,13 @@ fn main() {
                 }
                 slint::RenderingState::BeforeRendering => {
                     if let (Some(renderer), Some(app)) = (renderer.as_mut(), app_weak.upgrade()) {
-                        let texture = renderer.render(slint::PhysicalSize::new(
+                        let texture = renderer.render(
+                            app.get_selected_red(),
+                            app.get_selected_green(),
+                            app.get_selected_blue(),
                             app.get_requested_texture_width() as u32,
                             app.get_requested_texture_height() as u32,
-                        ));
-                        app.set_texture(slint::Image::try_from(texture).unwrap());
-                        app.window().request_redraw();
-                    }
-                }
-                slint::RenderingState::AfterRendering => {}
-                slint::RenderingState::RenderingTeardown => {
-                    drop(renderer.take());
-                }
-                _ => {}
-            }
-        })
-        .expect("Unable to set rendering notifier");
-
-    app.run().unwrap();
-}
-
-#[cfg(target_os = "android")]
-fn main() {
-    let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
-        flags: wgpu::InstanceFlags::empty(),
-        ..Default::default()
-    });
-
-    let adapter = spin_on::spin_on(async {
-        instance
-            .request_adapter(&Default::default())
-            .await
-            .expect("Failed to find an appropriate WGPU adapter")
-    });
-
-    let (device, queue) = spin_on::spin_on(async {
-        adapter.request_device(&Default::default()).await.expect("Failed to create WGPU device")
-    });
-
-    slint::BackendSelector::new()
-        .require_wgpu_27(WGPUConfiguration::Manual { instance, adapter, device, queue })
-        .select()
-        .expect("Unable to create Slint backend with WGPU based renderer");
-
-    let app = App::new().unwrap();
-
-    let mut renderer = None;
-
-    let app_weak = app.as_weak();
-
-    app.window()
-        .set_rendering_notifier(move |state, graphics_api| {
-            match state {
-                slint::RenderingState::RenderingSetup => {
-                    match graphics_api {
-                        slint::GraphicsAPI::WGPU27 { device, queue, .. } => {
-                            renderer = Some(DemoRenderer::new(device, queue));
-                        }
-                        _ => return,
-                    };
-                }
-                slint::RenderingState::BeforeRendering => {
-                    if let (Some(renderer), Some(app)) = (renderer.as_mut(), app_weak.upgrade()) {
-                        let texture = renderer.render(slint::PhysicalSize::new(
-                            app.get_requested_texture_width() as u32,
-                            app.get_requested_texture_height() as u32,
-                        ));
+                        );
                         app.set_texture(slint::Image::try_from(texture).unwrap());
                         app.window().request_redraw();
                     }
