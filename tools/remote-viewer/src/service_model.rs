111e7f980878e082e4361da26540adea4b67c6a0
Andreas Monitzer <andreas@monitzer.com>
2025-12-07 17:54:12 +0100

    mdns_sd is conflicting with mDnsResponder on macOS, using the system-provided functionality (through the zeroconf crate) instead.

--- tools/remote-viewer/src/service_model.rs
+++ tools/remote-viewer/src/service_model.rs
@@ -1,6 +1,10 @@
 use std::{cell::RefCell, rc::Rc};
 
-use mdns_sd::ResolvedService;
+#[cfg(not(target_vendor = "apple"))]
+pub use mdns_sd::ResolvedService;
+
+#[cfg(target_vendor = "apple")]
+pub type ResolvedService = zeroconf_tokio::ServiceDiscovery;
 
 #[derive(Clone, Default)]
 pub struct ServiceModelController {
@@ -11,10 +15,29 @@ pub struct ServiceModelController {
 impl ServiceModelController {
     pub fn insert(&self, service: ResolvedService) {
         let mut services = self.services.borrow_mut();
-        services.push(service);
-        self.notify.row_added(services.len(), 1);
+        if !services.contains(&service) {
+            services.push(service);
+            self.notify.row_added(services.len(), 1);
+        }
     }
 
+    #[cfg(target_vendor = "apple")]
+    pub fn remove(&self, removal: &zeroconf_tokio::ServiceRemoval) {
+        let mut services = self.services.borrow_mut();
+        for (index, service) in services.iter().enumerate() {
+            let service_type = service.service_type();
+            if service_type.name() == removal.name()
+                && service_type.protocol() == removal.kind()
+                && service.domain() == removal.domain()
+            {
+                services.remove(index);
+                self.notify.row_removed(index, 1);
+                return;
+            }
+        }
+    }
+
+    #[cfg(not(target_vendor = "apple"))]
     pub fn remove(&self, fullname: &str) {
         let mut services = self.services.borrow_mut();
         for (index, service) in services.iter().enumerate() {
