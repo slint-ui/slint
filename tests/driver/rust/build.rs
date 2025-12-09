eb80f8a1a55722416a2703e91699c47b55d7b29c
David Faure <david.faure@kdab.com>
2025-12-09 15:49:35 +0100

    test-driver-rust: skip rust-analyzer on the generated code
    Idea by Simon.
    In my testing it reduces greatly the memory consumption from
    rust-anayzer.

--- tests/driver/rust/build.rs
+++ tests/driver/rust/build.rs
@@ -57,6 +57,7 @@ fn main() -> std::io::Result<()> {
             write!(
                 output,
                 r"
+#[rust_analyzer::skip]
 #[test] {} fn t_{}() -> ::std::result::Result<(), ::std::boxed::Box<dyn ::std::error::Error>> {{
     use i_slint_backend_testing as slint_testing;
     slint_testing::init_no_event_loop();
