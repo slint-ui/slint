268977cb8c591bf39289fc5bd523a00395765e5f
Burhan Khanzada <burhankhanzada199888@gmail.com>
2025-12-10 21:26:49 +0100

    Revert "feat: Update OpenGL ES and GL versions"
    This reverts commit a3baa4f6c0fe7b95345986232d9e200d8bc161b2.

--- examples/servo/src/webview/rendering_context/surfman_context.rs
+++ examples/servo/src/webview/rendering_context/surfman_context.rs
@@ -51,8 +51,8 @@ impl SurfmanRenderingContext {
             | ContextAttributeFlags::STENCIL;
         let gl_api = connection.gl_api();
         let version = match &gl_api {
-            GLApi::GLES => surfman::GLVersion { major: 3, minor: 2 },
-            GLApi::GL => surfman::GLVersion { major: 4, minor: 5 },
+            GLApi::GLES => surfman::GLVersion { major: 3, minor: 0 },
+            GLApi::GL => surfman::GLVersion { major: 3, minor: 2 },
         };
         let context_descriptor =
             device.create_context_descriptor(&ContextAttributes { flags, version })?;
