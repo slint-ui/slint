a463191e2a20779b8aabbe09ae48f2d7798b3144
Nigel Breslaw <nigel.breslaw@slint.dev>
2025-12-29 15:09:10 +0200

    Make more use of shared files

--- docs/common/src/utils/utils.ts
+++ docs/common/src/utils/utils.ts
@@ -211,3 +211,15 @@ export function removeLeadingSpaces(input: string, spaces = 4): string {
     });
     return modifiedLines.join("\n");
 }
+
+export const trim = (str = "", ch?: string) => {
+    let start = 0,
+        end = str.length || 0;
+    while (start < end && str[start] === ch) {
+        ++start;
+    }
+    while (end > start && str[end - 1] === ch) {
+        --end;
+    }
+    return start > 0 || end < str.length ? str.substring(start, end) : str;
+};
