2214b0e5833da44a24acd48b39e661de6b87e455
Marco Thaller <marco.thaller@kdab.com>
2026-01-09 13:08:23 +0100

    Move pixel format specific code into new module

---
+++ examples/safe-ui/core/src/pixels/mod.rs
@@ -0,0 +1,6 @@
+// Copyright © Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+// SPDX-License-Identifier: MIT
+
+mod bgra8888;
+
+pub use bgra8888::Bgra8888Pixel;
--- examples/safe-ui/core/src/platform.rs
+++ examples/safe-ui/core/src/platform.rs
@@ -7,6 +7,7 @@ use alloc::rc::Rc;
 //use alloc::vec::Vec;
 //use core::cell::RefCell;
 
+use crate::pixels::Bgra8888Pixel;
 use slint::platform::software_renderer::MinimalSoftwareWindow;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
@@ -103,49 +104,6 @@ fn render_wrapper<F: Fn(&mut [Bgra8888Pixel], usize)>(f: &F) {
     }
 
     unsafe { slint_safeui_platform_render(user_data, Some(c_render_wrap::<F>)) }
-}
-
-#[repr(transparent)]
-#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
-pub struct Bgra8888Pixel(pub u32);
-
-impl From<Bgra8888Pixel> for slint::platform::software_renderer::PremultipliedRgbaColor {
-    #[inline]
-    fn from(pixel: Bgra8888Pixel) -> Self {
-        let v = pixel.0;
-        slint::platform::software_renderer::PremultipliedRgbaColor {
-            blue: (v >> 0) as u8,
-            green: (v >> 8) as u8,
-            red: (v >> 16) as u8,
-            alpha: (v >> 24) as u8,
-        }
-    }
-}
-
-impl From<slint::platform::software_renderer::PremultipliedRgbaColor> for Bgra8888Pixel {
-    #[inline]
-    fn from(pixel: slint::platform::software_renderer::PremultipliedRgbaColor) -> Self {
-        Self(
-            (pixel.alpha as u32) << 24
-                | ((pixel.red as u32) << 16)
-                | ((pixel.green as u32) << 8)
-                | (pixel.blue as u32),
-        )
-    }
-}
-
-impl slint::platform::software_renderer::TargetPixel for Bgra8888Pixel {
-    fn blend(&mut self, color: slint::platform::software_renderer::PremultipliedRgbaColor) {
-        let mut x = slint::platform::software_renderer::PremultipliedRgbaColor::from(*self);
-        x.blend(color);
-        *self = x.into();
-    }
-    fn from_rgb(r: u8, g: u8, b: u8) -> Self {
-        Self(0xff000000 | ((r as u32) << 16) | ((g as u32) << 8) | (b as u32))
-    }
-    fn background() -> Self {
-        Self(0)
-    }
 }
 
 pub fn slint_init_safeui_platform() {
