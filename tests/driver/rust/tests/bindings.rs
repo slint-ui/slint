8e703d43b6c331b1f5e7c84316260ab50cc26677
Leon Matthes <leon.matthes@slint.dev>
2026-01-14 14:06:53 +0100

    Fix typo

--- tests/driver/rust/tests/bindings.rs
+++ tests/driver/rust/tests/bindings.rs
@@ -1,7 +1,7 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
 
-// NOTE: The files under tests/driver/rust/tests/ are genreated and validated against this template.
+// NOTE: The files under tests/driver/rust/tests/ are generated and validated against this template.
 // Do not edit them directly, instead edit the template.rs file!
 
 // needs to be crate-level
