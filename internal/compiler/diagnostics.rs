8825c6b2379bf236a426feda66f20903320b7eac
Leon Matthes <leon.matthes@slint.dev>
2026-01-19 17:31:34 +0100

    Re-enable returning 0-length diagnostics
    The LSP clients seem to handle this just fine, it was just our tests
    that were failing, so make our tests more robust instead.
    
    Note: It is still somewhat unclear why the compiler emits 0-length
    diagnostics in the first place, but for the moment it doesn't hurt
    anything.

--- internal/compiler/diagnostics.rs
+++ internal/compiler/diagnostics.rs
@@ -301,18 +301,7 @@ impl Diagnostic {
 
     /// Return the length of this diagnostic in UTF-8 encoded bytes.
     pub fn length(&self) -> usize {
-        let length = self.span.span.length;
-        // If the length is 0, try to return a length of at least one character.
-        // Make sure this does fall on a new character, as otherwise string indexing may break.
-        // See also: https://github.com/slint-ui/slint/issues/10273
-        if length == 0 {
-            if let Some(source) = self.span.source_file.as_ref().and_then(|file| file.source()) {
-                let offset = self.span.span.offset;
-                let next_char = source.ceil_char_boundary(offset + 1);
-                return next_char - offset;
-            }
-        }
-        length
+        self.span.span.length
     }
 
     // NOTE: The return-type differs from the Spanned trait.
