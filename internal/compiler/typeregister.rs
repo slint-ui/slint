f8b4dcee35d25174e4e581fcf7200b3cd3b39e7a
Olivier Goffart <olivier.goffart@slint.dev>
2026-01-23 11:06:26 +0100

    Make Window::hide as experimental (#10505)
    We are not happy with the current behavior:
     - Closes the preview
     - works on sub windows, instead of being a compiler error

--- internal/compiler/typeregister.rs
+++ internal/compiler/typeregister.rs
@@ -597,6 +597,18 @@ impl TypeRegister {
         register.types.remove("DropEvent").unwrap(); // Also removed in xtask/src/slintdocs.rs
 
         register.elements.remove("StyledText").unwrap();
+        register.types.remove("styled-text").unwrap();
+
+        match register.elements.get_mut("Window").unwrap() {
+            ElementType::Builtin(b) => {
+                Rc::get_mut(b)
+                    .expect("Should not be shared at this point")
+                    .properties
+                    .remove("hide")
+                    .unwrap();
+            }
+            _ => unreachable!(),
+        }
 
         Rc::new(RefCell::new(register))
     }
