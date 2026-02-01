7fdf13424b26285c8b989cbc6423914894bd95c5
Nigel Breslaw <nigel.breslaw@slint.dev>
2026-02-01 12:49:41 +0200

    Refactor Printer MCU demo (#10646)

--- demos/printerdemo_mcu/main.rs
+++ demos/printerdemo_mcu/main.rs
@@ -39,7 +39,7 @@ impl PrinterQueueData {
 fn main() -> ! {
     mcu_board_support::init();
     let main_window = MainWindow::new().unwrap();
-    main_window.set_ink_levels(
+    main_window.global::<PrinterState>().set_ink_levels(
         [
             InkLevel { color: slint::Color::from_rgb_u8(0, 255, 255), level: 0.40 },
             InkLevel { color: slint::Color::from_rgb_u8(255, 0, 255), level: 0.20 },
