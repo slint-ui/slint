68023ed26b8ae6e49633417b24b862c2482299b2
Olivier Goffart <olivier.goffart@slint.dev>
2026-02-04 18:09:11 +0100

    Bump version number to 1.16.0 (#10695)

--- api/rs/slint/lib.rs
+++ api/rs/slint/lib.rs
@@ -447,7 +447,7 @@ pub mod android;
 /// Helper type that helps checking that the generated code is generated for the right version
 #[doc(hidden)]
 #[allow(non_camel_case_types)]
-pub struct VersionCheck_1_15_0;
+pub struct VersionCheck_1_16_0;
 
 #[cfg(doctest)]
 mod compile_fail_tests;
@@ -479,7 +479,7 @@ pub mod wgpu_27 {
     //!
     //! `Cargo.toml`:
     //! ```toml
-    //! slint = { version = "~1.15", features = ["unstable-wgpu-27"] }
+    //! slint = { version = "~1.16", features = ["unstable-wgpu-27"] }
     //! ```
     //!
     //! `main.rs`:
@@ -575,7 +575,7 @@ pub mod wgpu_28 {
     //!
     //! `Cargo.toml`:
     //! ```toml
-    //! slint = { version = "~1.15", features = ["unstable-wgpu-28"] }
+    //! slint = { version = "~1.16", features = ["unstable-wgpu-28"] }
     //! ```
     //!
     //! `main.rs`:
@@ -662,7 +662,7 @@ pub mod winit_030 {
     //!
     //! `Cargo.toml`:
     //! ```toml
-    //! slint = { version = "~1.15", features = ["unstable-winit-030"] }
+    //! slint = { version = "~1.16", features = ["unstable-winit-030"] }
     //! ```
     //!
     //! `main.rs`:
@@ -743,7 +743,7 @@ pub mod fontique_07 {
     ///
     /// `Cargo.toml`:
     /// ```toml
-    /// slint = { version = "~1.15", features = ["unstable-fontique-07"] }
+    /// slint = { version = "~1.16", features = ["unstable-fontique-07"] }
     /// ```
     ///
     /// `main.rs`:
