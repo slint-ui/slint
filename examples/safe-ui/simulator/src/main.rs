7aeba357ab6f7e167b6645d80ec5bfee45266760
Marco Thaller <marco.thaller@kdab.com>
2026-02-09 13:30:48 +0100

    safe-ui-simulator: Capture mouse events and print to stdout
    Add logic to simulator to capture and log mouse press, release
    and movement events using minifb.

--- examples/safe-ui/simulator/src/main.rs
+++ examples/safe-ui/simulator/src/main.rs
@@ -30,7 +30,26 @@ fn main() {
 
     let mut buffer: Vec<u32> = vec![0; (SCALED_WIDTH * SCALED_HEIGHT) as usize];
 
+    let mut last_mouse_down = false;
+    let mut last_mouse_pos = (0.0f32, 0.0f32);
+
     while window.is_open() && !window.is_key_down(Key::Escape) {
+        // Handle mouse inputs
+        window.get_mouse_pos(minifb::MouseMode::Clamp).map(|(x, y)| {
+            let mouse_down = window.get_mouse_down(minifb::MouseButton::Left);
+
+            if mouse_down && !last_mouse_down {
+                println!("MOUSE DOWN: ({:.1}, {:.1})", x, y);
+            } else if !mouse_down && last_mouse_down {
+                println!("MOUSE UP:   ({:.1}, {:.1})", x, y);
+            } else if mouse_down && (x, y) != last_mouse_pos {
+                println!("MOUSE MOVE: ({:.1}, {:.1})", x, y);
+            }
+
+            last_mouse_down = mouse_down;
+            last_mouse_pos = (x, y);
+        });
+
         // Perform drain loop dropping all previous frames
         // only keeping latest frame.
         let mut new_frame = None;
