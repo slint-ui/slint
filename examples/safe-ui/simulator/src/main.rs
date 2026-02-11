b4601342a605501de25edb2ef5370fb83dc0f8b1
Olivier Goffart <olivier.goffart@slint.dev>
2026-02-11 09:34:51 +0100

    Revert "safe-ui-simulator: replace Slint UI with minifb and remove Slint dependency"
    This reverts commit 75f49e8cac71b98a091b440bbc558ab6e1a0b02b.
    
    We'll keep using it and use the private API to use a different context
    instead

--- examples/safe-ui/simulator/src/main.rs
+++ examples/safe-ui/simulator/src/main.rs
@@ -3,8 +3,13 @@
 
 mod desktop_platform;
 
-use desktop_platform::{SCALED_HEIGHT, SCALED_WIDTH};
-use minifb::{Key, Window, WindowOptions};
+slint::slint! {import { AboutSlint, VerticalBox } from "std-widgets.slint";
+
+export component MainWindow inherits Window {
+    in property <image> image <=> screen.source;
+    screen := Image { }
+}
+}
 
 fn main() {
     let (pixel_sender, pixel_receiver) = smol::channel::unbounded();
@@ -16,37 +21,27 @@ fn main() {
         slint_safeui_core::slint_app_main()
     });
 
-    let mut window = Window::new(
-        "Slint SafeUI Simulator",
-        SCALED_WIDTH as usize,
-        SCALED_HEIGHT as usize,
-        WindowOptions::default(),
-    )
-    .unwrap_or_else(|e| {
-        panic!("{}", e);
-    });
+    let window = MainWindow::new().unwrap();
 
-    window.set_target_fps(60);
+    let window_weak = window.as_weak();
 
-    let mut buffer: Vec<u32> = vec![0; (SCALED_WIDTH * SCALED_HEIGHT) as usize];
+    slint::spawn_local(async move {
+        use desktop_platform::{SCALED_HEIGHT, SCALED_WIDTH};
 
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        // Perform drain loop dropping all previous frames
-        // only keeping latest frame.
-        let mut new_frame = None;
-        while let Ok(frame) = pixel_receiver.try_recv() {
-            new_frame = Some(frame);
-        }
+        loop {
+            if let Ok(source_pixels) = pixel_receiver.recv().await
+                && let Some(window) = window_weak.upgrade()
+            {
+                let mut pixel_buf: slint::SharedPixelBuffer<slint::Rgb8Pixel> =
+                    slint::SharedPixelBuffer::new(SCALED_WIDTH, SCALED_HEIGHT);
+                let pixel_dest = pixel_buf.make_mut_slice();
+                pixel_dest.copy_from_slice(&source_pixels);
 
-        if let Some(pixels) = new_frame {
-            for (i, pixel) in pixels.iter().enumerate() {
-                if i < buffer.len() {
-                    buffer[i] =
-                        ((pixel.r as u32) << 16) | ((pixel.g as u32) << 8) | (pixel.b as u32);
-                }
+                window.set_image(slint::Image::from_rgb8(pixel_buf));
             }
         }
+    })
+    .unwrap();
 
-        window.update_with_buffer(&buffer, SCALED_WIDTH as usize, SCALED_HEIGHT as usize).unwrap();
-    }
+    window.run().unwrap();
 }
