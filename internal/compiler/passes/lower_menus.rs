cae93a7e7706d2d021a072fa32c2b23a8edad132
Leon Matthes <leon.matthes@slint.dev>
2026-03-05 16:45:42 +0100

    Add note diagnostics for first duplicate MenuBar

--- internal/compiler/passes/lower_menus.rs
+++ internal/compiler/passes/lower_menus.rs
@@ -355,11 +355,12 @@ fn process_window(
     no_native_menu: bool,
     diag: &mut BuildDiagnostics,
 ) -> bool {
-    let mut menu_bar = None;
+    let mut menu_bar: Option<Rc<RefCell<Element>>> = None;
     win.borrow_mut().children.retain(|x| {
         if matches!(&x.borrow().base_type, ElementType::Builtin(b) if b.name == "MenuBar") {
-            if menu_bar.is_some() {
+            if let Some(ref menu_bar) = menu_bar {
                 diag.push_error("Only one MenuBar is allowed in a Window".into(), &*x.borrow());
+                diag.push_note("First MenuBar defined here".into(), &*menu_bar.borrow());
             } else {
                 menu_bar = Some(x.clone());
             }
