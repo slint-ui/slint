5c5dca9241cc9aefcbc929ad3c31935cf4a32bd7
Burhan Khanzada <burhan.khanzada@slint.dev>
2026-03-08 09:05:42 +0100

    feat: Add KeyboardModifiers named tuple

--- api/python/slint/slint/language.pyi
+++ api/python/slint/slint/language.pyi
@@ -8,3 +8,19 @@ class StandardListViewItem(typing.NamedTuple):
 
     text: str = ""
     """The text content of the item"""
+
+class KeyboardModifiers(typing.NamedTuple):
+    """Indicates which modifier keys are pressed during an event.
+
+    On macOS, the Command key (⌘) is mapped to ``control`` and the Control key is mapped to ``meta``.
+    On Windows, the Windows key is mapped to ``meta``.
+    """
+
+    shift: bool = False
+    """Indicates the Shift key on a keyboard."""
+    control: bool = False
+    """Indicates the Control key on a keyboard, except on macOS, where it is the Command key (⌘)."""
+    alt: bool = False
+    """Indicates the Alt key on a keyboard."""
+    meta: bool = False
+    """Indicates the Control key on macOS, and the Windows key on Windows."""
