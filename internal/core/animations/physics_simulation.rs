a7dd0a35a3baa40521770b4e82033cd7397712f8
Martin Marmsoler <martin.marmsoler@gmail.com>
2026-03-17 16:57:28 +0100

    fix test

--- internal/core/animations/physics_simulation.rs
+++ internal/core/animations/physics_simulation.rs
@@ -1,7 +1,7 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
 use crate::animations::Instant;
-#[cfg(test)]
+#[cfg(all(not(feature = "std"), test))]
 use num_traits::Float;
 
 #[derive(Debug)]
