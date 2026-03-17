46e848601b3060296aa152101c9451fd531924b8
Leon Matthes <leon.matthes@slint.dev>
2026-03-17 16:43:18 +0100

    Winit: Ignore duplicate/stale Exit events
    Previously calling `quit_event_loop` twice caused the next call to
    `run_event_loop` to exit immediately.
    
    This was especially problematic because window hide calls
    `quit_event_loop` as well, which is called at the end of `run`.

--- internal/backends/tests/tests/cases/mod.rs
+++ internal/backends/tests/tests/cases/mod.rs
@@ -1,13 +1,9 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
 
-use std::{
-    sync::{
-        Arc,
-        atomic::{AtomicBool, Ordering},
-    },
-    thread,
-    time::Duration,
+use std::sync::{
+    Arc,
+    atomic::{AtomicBool, Ordering},
 };
 
 #[test]
@@ -25,22 +21,31 @@ fn multiple_quit_event_loop_calls() {
     // First run: quit immediately.
     slint::invoke_from_event_loop(|| {
         slint::quit_event_loop().unwrap();
+        // duplicate quit_event_loop
+        slint::quit_event_loop().unwrap();
     })
     .unwrap();
+    // Note: Exiting run will call `hide()`, which will trigger another `quit_event_loop()`
     app.run().unwrap();
 
+    // quit_event_loop() while no event loop is active
+    slint::quit_event_loop().unwrap();
+
     // Second run: the event loop must not exit immediately from the stale
-    // quit above. A background thread sends the real quit after a delay.
+    // quit above.
     let did_process_events = Arc::new(AtomicBool::new(false));
     let did_process_events_clone = did_process_events.clone();
-    thread::spawn(move || {
-        thread::sleep(Duration::from_millis(100));
+    slint::invoke_from_event_loop(move || {
+        // Note that we need to queue another invoke_from_event_loop here, as otherwise
+        // the winit backend might not trigger the bug we want to test, because it
+        // still processes the events in the same run() call before exiting.
         slint::invoke_from_event_loop(move || {
             did_process_events_clone.store(true, Ordering::Relaxed);
             slint::quit_event_loop().unwrap();
         })
         .unwrap();
-    });
+    })
+    .unwrap();
     app.run().unwrap();
 
     assert!(
