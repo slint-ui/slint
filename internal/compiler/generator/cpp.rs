0a20f59646b08f5c5378ab43914666d2460d833c
U. Bruhin <urbibruhin@bluewin.ch>
2026-03-24 17:03:22 +0100

    slint-compiler: Write output files only if content modified
    Before writing any output file, try to read the content of the file
    first and compare it with the content to be written. Only if the read
    operation failed (e.g. file didn't exist) or the content differs, the
    file will be written.
    
    This avoids build systems like Ninja to detect the output file as
    modified every time slint-compiler is run, even if its output didn't
    change. This in turn heavily reduces compile times during development.
    Especially for C++ it means that appwindow.h is now modified way less
    often, avoiding recompilation of lots of C++ files depending on
    appwindow.h.

--- internal/compiler/generator/cpp.rs
+++ internal/compiler/generator/cpp.rs
@@ -6,9 +6,9 @@
 
 // cSpell:ignore cmath constexpr cstdlib decltype intptr itertools nullptr prepended struc subcomponent uintptr vals
 
+use crate::fileaccess;
 use std::collections::HashSet;
 use std::fmt::Write;
-use std::io::BufWriter;
 use std::sync::OnceLock;
 
 use smol_str::{SmolStr, StrExt, format_smolstr};
@@ -900,10 +900,9 @@ pub fn generate(
     let cpp_files = file.split_off_cpp_files(config.header_include, config.cpp_files.len());
 
     for (cpp_file_name, cpp_file) in config.cpp_files.iter().zip(cpp_files) {
-        use std::io::Write;
-        let mut cpp_writer = BufWriter::new(std::fs::File::create(cpp_file_name)?);
-        write!(&mut cpp_writer, "{cpp_file}")?;
-        cpp_writer.flush()?;
+        // Important: Write without unnecessary mtime modification to avoid
+        // build systems to always detect the generated file as modified.
+        fileaccess::write_file_if_changed(cpp_file_name, cpp_file.to_string().as_bytes())?;
     }
 
     Ok(file)
