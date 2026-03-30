95a4906958e93ad322b6ddfd9171db7b8ee595f2
Andreas Monitzer <andreas.monitzer@slint.dev>
2026-03-30 10:05:58 +0200

    Accidentally reversed the logic of wasm/non-wasm preview

--- editors/vscode/src/wasm_preview.ts
+++ editors/vscode/src/wasm_preview.ts
@@ -25,7 +25,7 @@ export function update_configuration() {
     if (language_client) {
         send_to_lsp({
             PreviewTypeChanged: {
-                target: (previewPanel !== null || use_wasm_preview()) ? 'child-process' : 'embedded-wasm',
+                target: (previewPanel !== null || use_wasm_preview()) ? 'embedded-wasm' : 'child-process',
             },
         });
     }
