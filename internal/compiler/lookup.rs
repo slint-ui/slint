b37174481c5fec06d815884f0016f042b84adf93
Chisom Uma <chisomuma476@gmail.com>
2026-04-02 09:26:54 +0100

    feat: added font-size namespace for easier accessibility
    Closes #11207
    
    This PR adds a requested feature where fontClasses are assigned namespaces:
    100 - Thin
    200 - Extra Light
    300 - Light
    400 - Normal
    500 - Medium
    600 - Semi Bold
    700 - Bold
    800 - Extra Bold
    900 - Black

--- internal/compiler/lookup.rs
+++ internal/compiler/lookup.rs
@@ -119,6 +119,7 @@ pub enum BuiltinNamespace {
     Easing,
     Math,
     Key,
+    FontWeight,
     SlintInternal,
 }
 
@@ -200,6 +201,9 @@ impl LookupObject for LookupResult {
             }
             LookupResult::Namespace(BuiltinNamespace::Math) => MathFunctions.for_each_entry(ctx, f),
             LookupResult::Namespace(BuiltinNamespace::Key) => KeysLookup.for_each_entry(ctx, f),
+            LookupResult::Namespace(BuiltinNamespace::FontWeight) => {
+                FontWeightLookup.for_each_entry(ctx, f)
+            }
             LookupResult::Namespace(BuiltinNamespace::SlintInternal) => {
                 SlintInternal.for_each_entry(ctx, f)
             }
@@ -217,6 +221,9 @@ impl LookupObject for LookupResult {
             LookupResult::Namespace(BuiltinNamespace::Easing) => EasingSpecific.lookup(ctx, name),
             LookupResult::Namespace(BuiltinNamespace::Math) => MathFunctions.lookup(ctx, name),
             LookupResult::Namespace(BuiltinNamespace::Key) => KeysLookup.lookup(ctx, name),
+            LookupResult::Namespace(BuiltinNamespace::FontWeight) => {
+                FontWeightLookup.lookup(ctx, name)
+            }
             LookupResult::Namespace(BuiltinNamespace::SlintInternal) => {
                 SlintInternal.lookup(ctx, name)
             }
@@ -725,6 +732,27 @@ impl LookupObject for EasingSpecific {
     }
 }
 
+struct FontWeightLookup;
+impl LookupObject for FontWeightLookup {
+    fn for_each_entry<R>(
+        &self,
+        _ctx: &LookupCtx,
+        f: &mut impl FnMut(&SmolStr, LookupResult) -> Option<R>,
+    ) -> Option<R> {
+        let mut weight =
+            |n, v: f64| f(&SmolStr::new_static(n), Expression::NumberLiteral(v, Unit::None).into());
+        None.or_else(|| weight("thin", 100.0))
+            .or_else(|| weight("extra-light", 200.0))
+            .or_else(|| weight("light", 300.0))
+            .or_else(|| weight("normal", 400.0))
+            .or_else(|| weight("medium", 500.0))
+            .or_else(|| weight("semi-bold", 600.0))
+            .or_else(|| weight("bold", 700.0))
+            .or_else(|| weight("extra-bold", 800.0))
+            .or_else(|| weight("black", 900.0))
+    }
+}
+
 impl LookupObject for Rc<Enumeration> {
     fn for_each_entry<R>(
         &self,
@@ -870,6 +898,7 @@ impl LookupObject for BuiltinNamespaceLookup {
             .or_else(|| f("Easing", LookupResult::Namespace(BuiltinNamespace::Easing)))
             .or_else(|| f("Math", LookupResult::Namespace(BuiltinNamespace::Math)))
             .or_else(|| f("Key", LookupResult::Namespace(BuiltinNamespace::Key)))
+            .or_else(|| f("FontWeight", LookupResult::Namespace(BuiltinNamespace::FontWeight)))
             .or_else(|| {
                 if ctx.type_register.expose_internal_types {
                     f("SlintInternal", LookupResult::Namespace(BuiltinNamespace::SlintInternal))
