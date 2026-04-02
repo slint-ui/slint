18d816a366c81c07ab7f538a4b37fbcff1afc2e8
Ashley Ruglys <ashley.ruglys@gmail.com>
2026-04-02 16:55:39 +0200

    Change example to use SystemTray type

--- examples/system_tray/main.rs
+++ examples/system_tray/main.rs
@@ -6,7 +6,7 @@ slint::include_modules!();
 fn main() {
     let my_tray = App::new().unwrap();
     let _tray = slint::private_unstable_api::create_system_tray(slint::system_tray::Params {
-        icon: &my_tray.get_favicon(),
+        icon: &my_tray.get_icon(),
         tooltip: "my-tray",
     })
     .unwrap();
