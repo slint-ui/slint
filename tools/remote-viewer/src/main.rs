4e80aa4de3823f013d0fd70a1c2ea09600e50843
Andreas Monitzer <andreas.monitzer@slint.dev>
2026-04-15 17:18:54 +0200

    Implemented disconnection events in the remote viewer. This required switching the main UI to be interpreted.

--- tools/remote-viewer/src/main.rs
+++ tools/remote-viewer/src/main.rs
@@ -6,7 +6,7 @@ use std::rc::Rc;
 use i_slint_compiler::diagnostics::BuildDiagnostics;
 use i_slint_core::InternalToken;
 use mdns_sd::ServiceDaemon;
-use slint::ComponentHandle;
+use slint::{ComponentHandle, SharedString};
 use tokio::sync;
 use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt as _, util::SubscriberInitExt as _};
 #[cfg(target_vendor = "apple")]
@@ -15,7 +15,7 @@ use zeroconf_tokio::txt_record::TTxtRecord as _;
 mod compilation;
 mod connection;
 
-slint::include_modules!();
+const MAIN_SLINT: &str = include_str!("../ui/main.slint");
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -34,12 +34,32 @@ async fn main() -> anyhow::Result<()> {
     let service = connection.service()?;
     mdns.register(service)?;
 
-    let window = EmptyWindow::new()?;
     let mut compiler = compilation::init_compiler(Rc::downgrade(&connection));
+    let current_exe = std::env::current_exe().unwrap();
+    let compilation_result = compiler
+        .build_from_source(MAIN_SLINT.to_owned(), current_exe.parent().unwrap().to_owned())
+        .await;
+    if compilation_result.has_errors() {
+        let mut build_diagnostics = BuildDiagnostics::default();
+        for d in compilation_result.diagnostics() {
+            build_diagnostics.push_compiler_error(d);
+        }
+        let diagnostics = build_diagnostics.diagnostics_as_string();
+        tracing::error!("Failed compiling main.slint: {diagnostics}");
+        anyhow::bail!("Failed compiling main.slint: {diagnostics}");
+    }
+    let main_ui = compilation_result.component("EmptyWindow").unwrap();
+    let window = main_ui.create().unwrap();
 
-    let inner_window = window.clone_strong();
+    let local_port = connection.local_port();
+    let local_ip_str: Vec<String> =
+        connection.local_ips().into_iter().map(|ip| format!("{ip}:{local_port}")).collect();
+
+    let mut inner_window = window.clone_strong();
+    let inner_local_ip_str = local_ip_str.clone();
     slint::spawn_local(async move {
         let mut last_connection = None;
+        let mut instance = None;
         while let Some(msg) = message_receiver.recv().await {
             match msg {
                 connection::ConnectionMessage::SetConfiguration { config } => {
@@ -57,10 +77,19 @@ async fn main() -> anyhow::Result<()> {
                             build_diagnostics.push_compiler_error(d);
                         }
 
-                        inner_window.set_message(build_diagnostics.diagnostics_as_string().into());
+                        if let Err(err) = inner_window.set_property(
+                            "message",
+                            SharedString::from(build_diagnostics.diagnostics_as_string()).into(),
+                        ) {
+                            tracing::error!("Failed setting property: {err}");
+                        }
                         continue;
                     }
-                    inner_window.set_message("".into());
+                    if let Err(err) =
+                        inner_window.set_property("message", SharedString::new().into())
+                    {
+                        tracing::error!("Failed setting property: {err}");
+                    }
 
                     let Some(component) = preview_component
                         .component
@@ -68,44 +97,79 @@ async fn main() -> anyhow::Result<()> {
                         .or_else(|| compilation_result.component_names().next())
                         .and_then(|name| compilation_result.component(name))
                     else {
-                        inner_window.set_message("Component not found".into());
+                        if let Err(err) = inner_window.set_property(
+                            "message",
+                            SharedString::from("Component not found").into(),
+                        ) {
+                            tracing::error!("Failed setting property: {err}");
+                        }
                         tracing::error!("Component not found");
                         continue;
                     };
 
-                    let Ok(instance) = component
+                    let Ok(inner_instance) = component
                         .create_with_existing_window(inner_window.window())
                         .inspect_err(|err| {
-                            inner_window.set_message(format!("{err}").into());
+                            if let Err(err) = inner_window.set_property(
+                                "message",
+                                SharedString::from(format!("{err}")).into(),
+                            ) {
+                                tracing::error!("Failed setting property: {err}");
+                            }
                             tracing::warn!("Platform error: {err}");
                         })
                     else {
                         return;
                     };
 
-                    if let Err(err) = instance.show() {
-                        inner_window.set_message(format!("{err}").into());
+                    if let Err(err) = inner_instance.show() {
+                        if let Err(err) = inner_window
+                            .set_property("message", SharedString::from(format!("{err}")).into())
+                        {
+                            tracing::error!("Failed setting property: {err}");
+                        }
                         tracing::warn!("Platform error: {err}");
+                    } else {
+                        instance = Some(inner_instance);
                     }
                 }
                 connection::ConnectionMessage::HighlightFromEditor { url, offset } => {}
                 connection::ConnectionMessage::Connected { remote_addr } => {
-                    inner_window.set_message(format!("Connected to {remote_addr}").into());
+                    if let Err(err) = inner_window.set_property(
+                        "message",
+                        SharedString::from(format!("Connected to {remote_addr}")).into(),
+                    ) {
+                        tracing::error!("Failed setting property: {err}");
+                    }
                     last_connection = Some(remote_addr);
                 }
                 connection::ConnectionMessage::Disconnected { remote_addr } => {
                     if last_connection == Some(remote_addr) {
-                        // tracing::error!("Platform error: {err}");
+                        last_connection = None;
+                        inner_window = instance
+                            .as_ref()
+                            .map(|instance| {
+                                main_ui.create_with_existing_window(instance.window()).unwrap()
+                            })
+                            .unwrap_or_else(|| main_ui.create().unwrap());
+                        if let Err(err) = inner_window.set_property(
+                            "address",
+                            SharedString::from(inner_local_ip_str.join("\n")).into(),
+                        ) {
+                            tracing::error!("Failed setting property: {err}");
+                        }
+                        inner_window.show().unwrap();
                     }
                 }
             }
         }
     })?;
 
-    let local_port = connection.local_port();
-    let local_ip_str: Vec<String> =
-        connection.local_ips().into_iter().map(|ip| format!("{ip}:{local_port}")).collect();
-    window.set_address(local_ip_str.join("\n").into());
+    if let Err(err) =
+        window.set_property("address", SharedString::from(local_ip_str.join("\n")).into())
+    {
+        tracing::error!("Failed setting property: {err}");
+    }
 
     window.show().inspect_err(|err| tracing::error!("window show: {err}"))?;
 
