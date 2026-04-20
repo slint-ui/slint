47e4e28e46a65ebcde0c23a5ebbf354cc65e2b0f
Nigel Breslaw <nigel.breslaw@slint.dev>
2026-04-20 13:51:35 +0300

    Update knip config

--- tools/figma-inspector/src/vite-env.d.ts
+++
@@ -1,6 +0,0 @@
-// Copyright © SixtyFPS GmbH <info@slint.dev>
-// SPDX-License-Identifier: MIT
-
-/// <reference types="svelte" />
-/// <reference types="vite/client" />
-/// <reference types="figma" />
