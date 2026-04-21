1aa8e61f85332213702ac2a31769d36589fb20ff
Eira Fransham <eira.fransham@slint.dev>
2026-04-21 10:05:12 +0200

    Fix doc link

--- internal/core/any_data.rs
+++ internal/core/any_data.rs
@@ -1,7 +1,7 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
 
-//! Dynamically-typed data, for runtime-generic code such as [`ClipboardData`](crate::clipboard::Clipboard).
+//! Dynamically-typed data, for runtime-generic code such as [`ClipboardData`](crate::clipboard::ClipboardData).
 
 use crate::SharedString;
 
