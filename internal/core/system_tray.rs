55fe2fc43bc659c587fbb4c1d8c15ce30b3edc87
Simon Hausmann <simon.hausmann@slint.dev>
2026-04-22 17:37:41 +0200

    Remove unused imports in system_tray.rs

--- internal/core/system_tray.rs
+++ internal/core/system_tray.rs
@@ -1,12 +1,7 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
 
-use crate::{
-    graphics::Image,
-    items::SystemTrayListItem,
-    model::{Model, ModelRc},
-};
-use std::string::ToString;
+use crate::graphics::Image;
 
 #[cfg(not(any(target_os = "macos", target_os = "windows")))]
 use ksni::TrayMethods;
