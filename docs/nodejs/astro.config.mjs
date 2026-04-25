d1af42d8dd1c27a44aba627708e471b49fbd6bef
Nigel Breslaw <nigel.breslaw@slint.dev>
2026-04-25 12:04:19 +0300

    Have a header with version selection

--- docs/nodejs/astro.config.mjs
+++ docs/nodejs/astro.config.mjs
@@ -82,7 +82,7 @@ export default defineConfig({
             favicon: "favicon.svg",
             components: {
                 Footer: "@slint/common-files/src/components/Footer.astro",
-                Header: "@slint/common-files/src/components/Header.astro",
+                Header: "./src/components/Header.astro",
                 Banner: "@slint/common-files/src/components/Banner.astro",
             },
             plugins: [
