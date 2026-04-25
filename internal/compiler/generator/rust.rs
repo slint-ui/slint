99064484dc7683d5c3562a6e98824e687293f551
Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>
2026-04-26 02:28:12 +0330

    compiler/rust: drop styled text parse generics
    Signed-off-by: Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>

--- internal/compiler/generator/rust.rs
+++ internal/compiler/generator/rust.rs
@@ -3876,7 +3876,7 @@ fn compile_builtin_function_call(
         BuiltinFunction::ParseMarkdown => {
             let format_string = a.next().unwrap();
             let args = a.next().unwrap();
-            quote!(sp::parse_markdown::<sp::StyledText>(&#format_string, &#args))
+            quote!(sp::parse_markdown(&#format_string, &#args))
         }
         BuiltinFunction::StringToStyledText => {
             let string = a.next().unwrap();
