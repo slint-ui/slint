65ee0387b431a6045852a6d45a4b76c566abd98b
Martin Marmsoler <martin.marmsoler@gmail.com>
2026-04-29 15:51:29 +0200

    fix running on qt

--- internal/backends/qt/qt_window.rs
+++ internal/backends/qt/qt_window.rs
@@ -2229,7 +2229,7 @@ fn into_qsize(logical_size: i_slint_core::api::LogicalSize) -> qttypes::QSize {
 
 impl WindowAdapterInternal for QtWindow {
     fn get_parent(&self) -> Option<Rc<dyn WindowAdapter>> {
-        Some(self.parent.clone().upgrade().unwrap())
+        self.parent.clone().upgrade().map(|rc| rc as _)
     }
 
     fn register_item_tree(&self, _: ItemTreeRefPin) {
