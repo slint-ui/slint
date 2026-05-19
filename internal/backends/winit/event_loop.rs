0ba2392864a19df454e6a097908e583b9e7d9d18
U-ALDEBERAN\Nate <npwoods@mess.org>
2026-05-19 19:20:37 -0400

    `cargo clippy` fixes

--- internal/backends/winit/event_loop.rs
+++ internal/backends/winit/event_loop.rs
@@ -170,7 +170,7 @@ impl winit::application::ApplicationHandler<SlintEvent> for EventLoopState {
                     handler.window_event(
                         event_loop,
                         window_id,
-                        winit_window.as_ref().map(|w| &**w),
+                        winit_window.as_deref(),
                         window.as_ref().map(|w| w.window()),
                         &event,
                     )
