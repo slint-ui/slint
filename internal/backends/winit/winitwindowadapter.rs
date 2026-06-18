9b62cd2a1fe968a46a0e227331cd35013d88e4fc
Martin Marmsoler <martin.marmsoler@gmail.com>
2026-06-18 15:13:35 +0200

    update to newer winit version

--- internal/backends/winit/winitwindowadapter.rs
+++ internal/backends/winit/winitwindowadapter.rs
@@ -1573,7 +1573,7 @@ impl WindowAdapterInternal for WinitWindowAdapter {
                 .with_title("child window")
                 .with_decorations(false)
                 .with_visible(true)
-                .with_window_type(WindowType::Popup);
+                .with_window_type(WindowType::Popup { grab_keyboard: true });
 
             if let Ok(parent) = winit_window.window_handle() {
                 window_attributes =
