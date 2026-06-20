183e2529cf4d40f3d37d7b5849d72dd0a924a2ae
uAtomicBoolean <uatomicboolean@proton.me>
2026-06-20 14:48:11 +0200

    Update Node API to use splice following rename

--- api/node/typescript/models.ts
+++ api/node/typescript/models.ts
@@ -288,7 +288,7 @@ export class ArrayModel<T> extends Model<T> {
         // Validate index range to prevent out-of-bounds access as this method is used by
         // the `array.insert(index, value)` slint method.
         if (_index < 0 || _index > this.#array.length) return;
-        this.insert(_index, _data);
+        this.splice(_index, 0, _data);
     }
 
     /**
