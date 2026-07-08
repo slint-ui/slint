6edc48ecc9d7ce0cce6c8b4a4a4f118da848fa4b
Eira Fransham <eira.fransham@slint.dev>
2026-07-08 16:22:03 +0200

    Restore state

--- scripts/build_local_visual_editor_flatpak.bash
+++ scripts/build_local_visual_editor_flatpak.bash
@@ -11,16 +11,25 @@ if [ -z "${1}" ]; then
     exit 1
 fi
 
-export FLATPAK_CARGO_GENERATOR_PATH="$(mktemp)"
-export OUTPUT_FLATPAK_MANIFEST="${repo_root}/tools/lsp/org.sixtyfps.SlintVisualEditor.yml"
+if [ -z "${CARGO_PROFILE:-}" ]; then
+    echo -e "CARGO_PROFILE unset, defaulting to dev (--debug)"
+fi
 
+export FLATPAK_CARGO_GENERATOR_PATH="$(mktemp)"
+
+export CARGO_SOURCES_PATH="${repo_root}/tools/lsp/cargo-sources.json"
+trap "rm ${CARGO_SOURCES_PATH}" EXIT
+
+export OUTPUT_FLATPAK_MANIFEST="${repo_root}/tools/lsp/org.sixtyfps.SlintVisualEditor.yml"
 trap "rm ${OUTPUT_FLATPAK_MANIFEST}" EXIT
 
 cargo_generator_url=https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/737c0085912f9f7dabf9341d4608e2a77a51a73a/cargo/flatpak-cargo-generator.py
 
 flatpak_repo_dir="$(mktemp -d)"
 flatpak_build_dir="$(mktemp -d)"
-flatpak_state_dir="$(mktemp -d)"
+flatpak_state_dir="${TMPDIR:-/tmp}/flatpak-build-state"
+
+mkdir -p "${flatpak_state_dir}"
 
 curl -o "${FLATPAK_CARGO_GENERATOR_PATH}" \
         "${cargo_generator_url}"
@@ -37,8 +46,12 @@ flatpak-builder \
     "${flatpak_build_dir}" \
     "${OUTPUT_FLATPAK_MANIFEST}"
 
+flatpak_out_name="$(basename "${1}")"
+
 flatpak build-bundle \
     "${flatpak_repo_dir}" \
-    "${1}" \
+    "${flatpak_out_name}" \
     org.sixtyfps.SlintVisualEditor \
     --runtime-repo=https://dl.flathub.org/repo/flathub.flatpakrepo
+
+mv "${flatpak_out_name}" "${1}"
