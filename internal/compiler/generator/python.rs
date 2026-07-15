d96ab6ce44171909a7bb34977590f9b3098477b3
Olivier Goffart <olivier.goffart@slint.dev>
2026-07-15 16:51:17 +0000

    python: remove the non-functional MouseCursorInner stub
    The exported slint.MouseCursorInner could not be constructed, read, or
    converted in either direction. Drop it; the code generator maps the
    mouse-cursor type to None, like the other value types Python does not model.

--- internal/compiler/generator/python.rs
+++ internal/compiler/generator/python.rs
@@ -705,7 +705,7 @@ fn python_type_name(ty: &Type) -> SmolStr {
         }
         Type::Keys => SmolStr::new_static("slint.Keys"),
         Type::DataTransfer => SmolStr::new_static("slint.DataTransfer"),
-        Type::MouseCursor => SmolStr::new_static("slint.MouseCursorInner"),
+        Type::MouseCursor => SmolStr::new_static("None"),
         ty => unimplemented!("implemented type conversion {:#?}", ty),
     }
 }
