4784cde3182c178a264f3513c877cdaaef8aa48e
Simon Hausmann <simon.hausmann@slint.dev>
2026-07-16 18:44:19 +0000

    Upgrade cfg_aliases to 0.2.2 and drop the nightly lint workaround
    cfg_aliases 0.2.2 fixes the macro expansion that placed semicolons in
    expression position (https://github.com/katharostech/cfg_aliases/pull/15),
    which nightly rejects via the semicolon_in_expressions_from_macros lint
    since 2026-07-16.
    
    Bump the minimum version to 0.2.2 and revert the
    allow(semicolon_in_expressions_from_macros) workaround from commit
    b7a7676e7137 in the affected build scripts.

--- examples/gstreamer-player/build.rs
+++ examples/gstreamer-player/build.rs
@@ -1,11 +1,6 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: MIT
 
-// The expansion of cfg_aliases! places macro-emitted semicolons in expression
-// position, which nightly rejects by default since 2026-07. Allow it until a
-// fixed cfg-aliases release is available.
-#![allow(semicolon_in_expressions_from_macros)]
-
 use cfg_aliases::cfg_aliases;
 
 fn main() {
