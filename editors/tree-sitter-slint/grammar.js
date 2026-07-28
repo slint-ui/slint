39225712620d99693ca19faf6451522468632ac5
Nicholas Turner <nicholas.turner@kdab.com>
2026-07-28 11:47:39 -0400

    Updated parser tree grammar to accept empty wildcard cases
    Updated parser tree grammar to recognize an empty element for
    a wildcard case as valid syntax, which previously it did not.

--- editors/tree-sitter-slint/grammar.js
+++ editors/tree-sitter-slint/grammar.js
@@ -400,7 +400,7 @@ module.exports = grammar({
 
     match_statement: ($) =>
       seq("match",
-        field("value", $.simple_identifier),
+        field("value", $.expression),
         "{",
         repeat($.match_case),
         optional($.wildcard_match_case),
@@ -410,13 +410,14 @@ module.exports = grammar({
     match_case: ($) =>
       seq(
         field("case", choice($._basic_value,
+          $.user_type_identifier,
           seq($.user_type_identifier, ".", $.user_type_identifier))),
         ":",
         choice($.component, seq("{", "}"))
       ),
 
     wildcard_match_case: ($) =>
-      seq("*", ":", $.component),
+      seq("*", ":", choice($.component, seq("{", "}"))),
 
     type_list: ($) => seq("[", commaSep($.type), optional(","), "]"),
 
