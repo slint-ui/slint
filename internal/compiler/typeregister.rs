d56b88d190a8eb74efb3cbe4087bafac6927fe35
Arnold Loubriat <datatriny@gmail.com>
2026-07-31 17:19:34 +0200

    compiler+docs: name the set-selection action's arguments

--- internal/compiler/typeregister.rs
+++ internal/compiler/typeregister.rs
@@ -93,7 +93,7 @@ pub struct BuiltinTypes {
     pub enums: BuiltinEnums,
     pub noarg_callback_type: Type,
     pub strarg_callback_type: Type,
-    pub two_int_args_callback_type: Type,
+    pub set_selection_callback_type: Type,
     pub logical_point_type: Arc<Struct>,
     pub logical_size_type: Arc<Struct>,
     pub font_metrics_type: Type,
@@ -159,10 +159,10 @@ impl BuiltinTypes {
                 args: vec![Type::String],
                 arg_names: Vec::new(),
             })),
-            two_int_args_callback_type: Type::Callback(Arc::new(Function {
+            set_selection_callback_type: Type::Callback(Arc::new(Function {
                 return_type: Type::Void,
                 args: vec![Type::Int32, Type::Int32],
-                arg_names: Vec::new(),
+                arg_names: vec![SmolStr::new_static("anchor"), SmolStr::new_static("focus")],
             })),
             layout_info_type: layout_info_type.clone(),
             state_info_type: Arc::new(Struct::new(
@@ -256,8 +256,8 @@ fn strarg_callback_type() -> Type {
     BUILTIN.strarg_callback_type.clone()
 }
 
-fn two_int_args_callback_type() -> Type {
-    BUILTIN.two_int_args_callback_type.clone()
+fn set_selection_callback_type() -> Type {
+    BUILTIN.set_selection_callback_type.clone()
 }
 
 pub fn reserved_accessibility_properties() -> impl Iterator<Item = (&'static str, Type)> {
@@ -281,7 +281,7 @@ pub fn reserved_accessibility_properties() -> impl Iterator<Item = (&'static str
         ("accessible-action-increment", noarg_callback_type()),
         ("accessible-action-decrement", noarg_callback_type()),
         ("accessible-action-set-value", strarg_callback_type()),
-        ("accessible-action-set-selection", two_int_args_callback_type()),
+        ("accessible-action-set-selection", set_selection_callback_type()),
         ("accessible-action-expand", noarg_callback_type()),
         ("accessible-item-selectable", Type::Bool),
         ("accessible-item-selected", Type::Bool),
