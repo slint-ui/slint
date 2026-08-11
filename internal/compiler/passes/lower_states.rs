e9bffdb665f0d17a08b01e5dbea09ccf86ebd883
Robin Cramer <robin.cramer@kdab.com>
2026-08-11 07:49:43 -0400

    Throws the correct error instead of panicking

--- internal/compiler/passes/lower_states.rs
+++ internal/compiler/passes/lower_states.rs
@@ -74,8 +74,6 @@ fn lower_state_in_element(
             };
         }
         for (property_reference, expr, node) in state.property_changes {
-            affected_properties.insert(property_reference.clone());
-            state_properties.entry(idx as i32 + 1).or_default().insert(property_reference.clone());
             let element = property_reference.element();
             let property_expr = match expression_for_property(
                 &element,
@@ -92,6 +90,8 @@ fn lower_state_in_element(
                 }
                 ExpressionForProperty::Expression(e) => e,
             };
+            affected_properties.insert(property_reference.clone());
+            state_properties.entry(idx as i32 + 1).or_default().insert(property_reference.clone());
             let new_expr = Expression::Condition {
                 condition: Box::new(Expression::BinaryExpression {
                     source_location: None,
