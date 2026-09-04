204f92c9d4d5cddae55b2a24afe76c84ea966f07
Ian McFarlane <ian.mcfarlane@kdab.com>
2026-09-04 18:02:59 -0400

    Fix transparent windows being all white under DX12
    With DX12 we want to use a composition swapchain to get transparency,
    but the redirection bitmap that window uses it compositing a bunch of
    opaque white pixels underneath everything. To fix this, clear the
    redirection bitmaps with transparent pixels to erase the white ones.

--- internal/renderers/skia/lib.rs
+++ internal/renderers/skia/lib.rs
@@ -541,6 +541,15 @@ impl SkiaRenderer {
         renderer
     }
 
+    /// Returns true if the current surface will be presented with transparency enabled
+    /// See [`Surface::presentation_may_use_transparency`].
+    pub fn presentation_may_use_transparency(&self) -> bool {
+        self.surface
+            .borrow()
+            .as_ref()
+            .is_some_and(|surface| surface.presentation_may_use_transparency())
+    }
+
     /// Reset the surface to a new surface. (destroy the previously set surface if any)
     pub fn set_surface(&self, surface: Box<dyn Surface + 'static>) {
         self.image_cache.clear_all();
