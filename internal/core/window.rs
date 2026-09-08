396ce4e0d89e3b6b1eed63436e8b9b80a5748ef8
Joshua Goins <joshua.goins@kdab.com>
2026-09-08 12:00:46 -0400

    Scale the drag image based on scale factor
    This makes it nicer from a user's point-of-view when dragging from an
    Image that's also scaled.

--- internal/core/window.rs
+++ internal/core/window.rs
@@ -1856,6 +1856,7 @@ impl WindowInner {
 
         item_renderer.save_state();
         item_renderer.translate(top_left.to_vector());
+        item_renderer.scale(self.scale_factor(), self.scale_factor());
         item_renderer.draw_image_direct(image);
         item_renderer.restore_state();
 
