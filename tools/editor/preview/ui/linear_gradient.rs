052deb512de85ff3500619b790817634a9b36289
Nigel Breslaw <nigel.breslaw@slint.dev>
2026-09-11 14:42:42 +0300

    Register the linear gradient math API spelling

--- tools/editor/preview/ui/linear_gradient.rs
+++ tools/editor/preview/ui/linear_gradient.rs
@@ -1,6 +1,8 @@
 // Copyright © SixtyFPS GmbH <info@slint.dev>
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
 
+// cspell:ignore hypot
+
 use super::{FillData, LinearGradientAxis};
 use slint::{LogicalPosition as Point, LogicalSize, Model, VecModel};
 use std::rc::Rc;
