40e05db4b18cb9553a1a7acdd5f679fb503ab505
Martin Marmsoler <martin.marmsoler@slint.dev>
2026-09-18 10:01:24 +0200

    fix spelling

--- internal/core/input.rs
+++ internal/core/input.rs
@@ -207,7 +207,7 @@ impl MouseEvent {
 #[derive(Debug, Clone, PartialEq, Default)]
 pub struct TouchHistory {
     pub event_pos: Option<LogicalPoint>,
-    /// - Duration to the event ponint this history relates
+    /// - Duration to the event point this history relates
     /// - If event_pos is not None, the history points are absolute values
     /// otherwise they are deltas to the next event point
     pub history: Vec<(LogicalPoint, Duration)>,
