fcac260213dbd7b6aeb06a2d686bb978f7f51756
Olivier Goffart <olivier.goffart@slint.dev>
2026-09-24 13:56:47 +0000

    images: drop "instead of expanding them to RGB8" from the feature docs
    It read as if expanding had been the caller's job before.

--- api/cpp/cmake/SlintFeatures.cmake
+++ api/cpp/cmake/SlintFeatures.cmake
@@ -48,9 +48,9 @@ endfunction()
 
 define_cargo_feature(freestanding "Enable use of freestanding environment. This is only for bare-metal systems. Most other features are incompatible with this one" OFF)
 
-define_cargo_feature(image-pixel-format-rgb565 "Enable storing an image's pixels in the RGB565 format, instead of expanding them to RGB8" OFF)
+define_cargo_feature(image-pixel-format-rgb565 "Enable storing an image's pixels in the RGB565 format" OFF)
 
-define_cargo_feature(image-pixel-format-gray8 "Enable storing an image's pixels as a single 8 bit grayscale channel, instead of expanding them to RGB8. Also embeds an image whose pixels are all gray in that format" OFF)
+define_cargo_feature(image-pixel-format-gray8 "Enable storing an image's pixels as a single 8 bit grayscale channel. Also embeds an image whose pixels are all gray in that format" OFF)
 
 # Compat options (must be declared after the STD feature, but before the options they replace)
 function(define_compat_option deprecated replacement)
